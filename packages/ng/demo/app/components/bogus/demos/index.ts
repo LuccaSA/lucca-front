49f55d3dd3746b65c6cd559fe90fa6db24c7c2e6
lucienbertin <me@lucienbertin.com>
2017-04-18 17:45:22 +0200

    ng-demo [add] prismjs

--- packages/ng/demo/app/components/bogus/demos/index.ts
+++ packages/ng/demo/app/components/bogus/demos/index.ts
@@ -1,19 +1,12 @@
+declare var require: any;
 import {BogusBasic} from './basic/bogus-basic';
 
-export const DEMO_DIRECTIVES =
-    [BogusBasic];
-declare var require: any;
+export const DEMO_DIRECTIVES = [
+  BogusBasic,
+];
 export const DEMO_SNIPPETS = {
-  // basic: {
-  //   code: './basic/bogus-basic',
-  //   markup: './basic/bogus-basic.html',
-  // },
   basic: {
-    code: require('./basic/bogus-basic.ts'),
-    markup: require('./basic/bogus-basic.html'),
+    code: require('!!prismjs-loader?lang=typescript!./basic/bogus-basic'),
+    markup: require('!!prismjs-loader?lang=markup!./basic/bogus-basic.html')
   },
-  // basic: {
-  //   code: require('!!prismjs-loader?lang=typescript!./basic/bogus-basic'),
-  //   markup: require('!!prismjs-loader?lang=markup!./basic/bogus-basic.html')
-  // },
 };
