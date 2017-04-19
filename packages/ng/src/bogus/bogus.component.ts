8145fe17229b55965bb933a1bdf83ca6e98927c8
lucienbertin <me@lucienbertin.com>
2017-04-19 11:16:47 +0200

    ng [add] doc for bogus component

--- packages/ng/src/bogus/bogus.component.ts
+++ packages/ng/src/bogus/bogus.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+
+/**
+ * This is a bogus component with bogus documentation
+ */
 @Component({
   selector: 'lu-bogus',
   templateUrl: './bogus.component.html',
   styleUrls: ['./bogus.component.scss']
 })
 export class BogusComponent implements OnInit {
+
+  /**
+   * a bogus input
+   */
+  @Input() bogusInput: number = 0;
+  /**
+   * another bogus input
+   */
+  @Input() bogusLol: string = "rofl";
+  /**
+   * a bogus output
+   */
+  @Output() bougusOutput = new EventEmitter<any>();
 
   constructor() { }
 
