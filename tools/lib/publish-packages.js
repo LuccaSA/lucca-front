92179b96faa57c950dcac6028c272b14a52417c3
lucienbertin <me@lucienbertin.com>
2017-04-19 17:33:26 +0200

    publish - does the dry run for all package then publish, not dry-run, publish one package at a time

--- tools/lib/publish-packages.js
+++ tools/lib/publish-packages.js
@@ -11,6 +11,15 @@ module.exports = function publishPackages(packages) {
 
 		// does a dry run before doing the same but with publication
 		npmRun.execSync('npm pack', { cwd: `${__dirname}/../${path}`, stdio: [0, 1, 2] });
+		
+		console.log();
+	});
+	packages.forEach(_package => {
+		const path = _package.replace(/package.json$/i, '');
+		const config = getPackageConfig(_package);
+
+		console.log(`Publishing ${colors.green(config.name)}`);
+
 		// publish
 		npmRun.execSync('npm publish --access=public', { cwd: `${__dirname}/../${path}`, stdio: [0, 1, 2] });
 		
