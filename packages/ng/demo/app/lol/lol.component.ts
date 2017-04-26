d26bacd66ad7d4e5224908604f4c724053c4ca27
lucienbertin <me@lucienbertin.com>
2017-04-26 16:23:16 +0200

    ng-demo [add] snippets

--- packages/ng/demo/app/lol/lol.component.ts
+++ packages/ng/demo/app/lol/lol.component.ts
@@ -1,5 +1,5 @@
-// declare var require: any;
 import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
 	selector: 'demo-lol',
@@ -10,13 +10,12 @@ export class DemoLolComponent implements OnInit {
 
 	constructor() { }
 
-	// snippets = {
-	// 	basic: {
-	// 		code: require('!!prismjs-loader?lang=typescript!./basic/basic.component'),
-	// 		markup: require('!!prismjs-loader?lang=markup!./basic/basic.component.html')
-	// 	},
-	// }
-
+	snippets = {
+		basic: {
+			code: require('!!prismjs-loader?lang=typescript!./basic/basic.component'),
+			markup: require('!!prismjs-loader?lang=markup!./basic/basic.component.html')
+		},
+	}
 	ngOnInit() {
 	}
 }
