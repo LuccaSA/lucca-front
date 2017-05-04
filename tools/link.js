29874cd5ec861c643ecc2f3a2e5df14eb132ab6d
lucienbertin <me@lucienbertin.com>
2017-05-04 14:23:01 +0200

    reworked link script

--- tools/link.js
+++ tools/link.js
@@ -1,24 +1,17 @@
 const npmRun = require('npm-run');
 
+// respect dependency order here
 const packages = [
-	// { name: 'scss', dependencies: ['icons'] },
-	// { name: 'ng', dependencies: ['scss', 'icons'] },
+	{ name: 'scss', dependencies: [] },
 	{ name: 'ng', dependencies: ['scss'] },
 ];
 
-// extract dependencies to call `npm link` on each
-// equivalent to _.chain(packages).pluck('dependencies').flatten().uniq().value();
-let dependencies = [...new Set([].concat(...packages.map(d => d.dependencies)))];
-
-dependencies.forEach(d => {
-	// console.log(`${d}`);
-	npmRun.execSync('npm link', { cwd: `${__dirname}/../packages/${d}`, stdio: [0, 1, 2] });
-});
-
-// link the pck with their dependencies
 packages.forEach(pck => {
+	// link the pck with their dependencies
+	// need this first cuz `npm link` launches `prepublish`
 	pck.dependencies.forEach(d => {
-		// console.log(`${pck.name} -> ${d}`);
 		npmRun.execSync(`npm link @lucca-front/${d}`, { cwd: `${__dirname}/../packages/${pck.name}`, stdio: [0, 1, 2] });
 	});
+	// then register the pck with link
+	npmRun.execSync('npm link', { cwd: `${__dirname}/../packages/${pck.name}`, stdio: [0, 1, 2] });
 });
