60d2ffbe6e36a264a74c907af99456c3acc5d525
versinia <angelin.versini@gmail.com>
2017-08-25 14:01:22 +0200

    ADD select in date-range-picker

--- packages/ng/src/app/date-range-picker/date-range-picker.component.ts
+++ packages/ng/src/app/date-range-picker/date-range-picker.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import * as moment from 'moment';
 
+/**
+ * Pick date ranges from a customizable list displayed inside an Angular Material MdSelect.
+ */
 @Component({
 	selector: 'lu-date-range-picker',
 	templateUrl: './date-range-picker.component.html',
 	styleUrls: ['./date-range-picker.component.scss']
 })
-export class LuDateRangePickerComponent implements OnInit {
+export class LuDateRangePickerComponent {
+
+	/**
+	 * Placeholder string of the MDSelect
+	 */
+	@Input() placeholder: string;
+
+	/**
+	 * Emits last select value for min date of the range.
+	 * Used by dateMin two-way binding, typically you should not have to use it directly.
+	 * @type {EventEmitter<Moment>}
+	 */
+	@Output() dateMinChange = new EventEmitter<moment.Moment>();
+	_dateMin: moment.Moment;
+
+	/**
+	 * Emits last select value for max date of the range.
+	 * Used by dateMax two-way binding, typically you should not have to use it directly.
+	 * @type {EventEmitter<Moment>}
+	 */
+	@Output() dateMaxChange = new EventEmitter<moment.Moment>();
+	_dateMax: moment.Moment;
+
+	/**
+	 * Array of choices the user will select from.
+	 * Import class DateRangeSelectChoice in order to build choices.
+	 */
+	@Input() preConfiguredRanges: DateRangeSelectChoice[];
+
 
 	constructor() { }
 
-	ngOnInit() {
+	/**
+	 * Min date of selected ranged. Allows two-way binding.
+	 * @returns {Moment}
+	 */
+	@Input()
+	get dateMin(): moment.Moment {
+		return this._dateMin;
 	}
 
+	set dateMin(newDateMin: moment.Moment) {
+		this._dateMin = newDateMin;
+		this.dateMinChange.emit(this._dateMin);
+	}
+
+	/**
+	 * Max date of selected ranged. Allows two-way binding.
+	 * @returns {Moment}
+	 */
+	@Input()
+	get dateMax(): moment.Moment {
+		return this._dateMax;
+	}
+
+	set dateMax(newDateMax: moment.Moment) {
+		this._dateMax = newDateMax;
+		this.dateMaxChange.emit(this._dateMax);
+	}
+
+	onChoiceChange(choice: DateRangeSelectChoice) {
+		this.dateMin = choice.dateMin;
+		this.dateMax = choice.dateMax;
+	}
+
+}
+
+export class DateRangeSelectChoice {
+	dateMin: moment.Moment;
+	dateMax: moment.Moment;
+	label: string;
 }
--- packages/ng/src/app/date-range-picker/date-range-picker.module.ts
+++ packages/ng/src/app/date-range-picker/date-range-picker.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LuDateRangePickerComponent } from './date-range-picker.component';
+import { CommonModule } from '@angular/common'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdDatepickerModule, MdSelectModule} from '@angular/material';
+import {LuDateRangePickerComponent} from './date-range-picker.component';
 
 @NgModule({
 	imports: [
-		CommonModule
+		BrowserAnimationsModule,
+		CommonModule,
+		MdSelectModule,
+		MdDatepickerModule,
 	],
 	declarations: [LuDateRangePickerComponent],
 	exports: [LuDateRangePickerComponent]
