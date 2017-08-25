3b6355b9c997db0ad2b3c5d6a3758c86df11e054
versinia <angelin.versini@gmail.com>
2017-08-25 14:05:19 +0200

    ADD basic demo for date-range-picker

--- packages/ng/demo/app/date-range-picker/basic/basic.component.ts
+++ packages/ng/demo/app/date-range-picker/basic/basic.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as moment from 'moment';
+import {DateRangeSelectChoice} from '../../../../src/app/date-range-picker/date-range-picker.component';
 
 @Component({
 	selector: 'demo-basic-date-range-picker',
 	templateUrl: './basic.component.html',
-	styles: []
+	styleUrls: ['./basic.component.scss']
 })
-export class BasicComponent implements OnInit {
+export class BasicComponent {
 
-	constructor() { }
+	preConfiguredRanges: DateRangeSelectChoice[];
+	placeholder: string;
 
-	ngOnInit() {
+	dateMin: moment.Moment;
+	dateMax: moment.Moment;
+
+	constructor() {
+		const today = moment().startOf('day');
+
+		this.placeholder = 'Select date range';
+
+		this.preConfiguredRanges = [
+			{label: 'This day', dateMin: today.clone(), dateMax:today.clone().add(1, 'day')},
+			{label: 'This month', dateMin: today.clone().startOf('month'), dateMax: today.clone().add(1, 'month').startOf('month')}
+		];
 	}
 
 }
