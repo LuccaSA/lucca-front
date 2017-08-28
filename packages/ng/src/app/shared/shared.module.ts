6858346e2534258fcc074445b5174adce38b1cc5
versinia <angelin.versini@gmail.com>
2017-08-28 13:39:59 +0200

    ADD translate service wrapper
    sets setTranslation merge option to true by default

--- packages/ng/src/app/shared/shared.module.ts
+++ packages/ng/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 import {TranslateStore} from '@ngx-translate/core/src/translate.store';
+import {LuTranslateService} from './translation.service';
 
 @NgModule({
   imports: [
@@ -9,9 +10,7 @@ import {TranslateStore} from '@ngx-translate/core/src/translate.store';
 		TranslateModule.forRoot({loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}}),
   ],
   declarations: [],
-	providers: [TranslateService, TranslateStore],
-	exports: [
-		TranslateModule
-	]
+	providers: [LuTranslateService, TranslateService, TranslateStore],
+	exports: [TranslateModule]
 })
 export class LuSharedModule { }
