3092a8ad3df362ddf3c90255fb00b111d2840642
lucienbertin <me@lucienbertin.com>
2017-09-26 15:42:28 +0200

    ng-demo [add] basic router and navigation layout

--- packages/ng/demo/app/app.module.ts
+++ packages/ng/demo/app/app.module.ts
@@ -4,6 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+// router
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.router';
 
 import { DemoLolModule } from './lol/lol.module';
 import {DemoDateRangePickerModule} from './date-range-picker/date-range-picker.module';
@@ -12,8 +17,11 @@ import {DemoUserTileModule} from './user-tile/user-tile.module';
 @NgModule({
 	declarations: [
 		AppComponent,
+		NavigationComponent,
 	],
 	imports: [
+		RouterModule.forRoot(appRoutes),
+
 		DemoLolModule,
 		DemoDateRangePickerModule,
 		DemoUserTileModule,
