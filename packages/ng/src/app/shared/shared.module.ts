72cc9fa52e0c309e6cf6c207e557ef05f6666ce5
lucienbertin <me@lucienbertin.com>
2017-11-08 18:20:59 +0100

    ng [rem] the copy pasta we did for a moment date adapter (oops)

--- packages/ng/src/app/shared/shared.module.ts
+++ packages/ng/src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import {TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 import {TranslateStore} from '@ngx-translate/core/src/translate.store';
 import {LuTranslateService} from './translation.service';
-import {MomentDateAdapter} from './moment/moment-date-adapter';
 
 @NgModule({
 	imports: [
@@ -11,7 +10,7 @@ import {MomentDateAdapter} from './moment/moment-date-adapter';
 		TranslateModule.forRoot({loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}}),
 	],
 	declarations: [],
-	providers: [LuTranslateService, TranslateService, TranslateStore, MomentDateAdapter],
+	providers: [LuTranslateService, TranslateService, TranslateStore],
 	exports: [TranslateModule],
 })
 export class LuSharedModule { }
