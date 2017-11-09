6a961980b21b6b27dc16401ce4b953a3cc4669d0
lucienbertin <me@lucienbertin.com>
2017-11-09 10:56:42 +0100

    ng [rem] remaining lol, 0 lol remain :'(

--- packages/ng/src/app/lu-root.module.ts
+++ packages/ng/src/app/lu-root.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material';
-import { LuLolModule } from './lol/lol.module';
 import { LuDateRangePickerModule } from './date-range-picker/date-range-picker.module';
 import { LuUserTileModule } from './user';
 import { LuPopoverModule } from './popover/popover.module';
@@ -9,13 +8,11 @@ import { LuPopoverModule } from './popover/popover.module';
 @NgModule({
 	imports: [
 		CommonModule,
-		LuLolModule,
 		LuDateRangePickerModule,
 		LuUserTileModule,
 		LuPopoverModule,
 	],
 	exports: [
-		LuLolModule,
 		LuDateRangePickerModule,
 		LuUserTileModule,
 		LuPopoverModule,
--- packages/ng/src/index.ts
+++ packages/ng/src/index.ts
@@ -1,4 +1,3 @@
-export * from './app/lol/lol.module';
 export * from './app/date-range-picker/date-range-picker.module';
 export { IDateRangeSelectChoice, IDateRange } from './app/date-range-picker/date-range-picker.model';
 export * from './app/user';
