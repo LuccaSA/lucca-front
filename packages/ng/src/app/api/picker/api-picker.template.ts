34dde2c4f5b531a0f656ad1ca6406f16cd97b623
lucienbertin <me@lucienbertin.com>
2017-12-07 13:21:02 +0100

    ng-api [enh] add custom apipickertemplate to ease template edition

--- packages/ng/src/app/api/picker/api-picker.template.ts
+++ packages/ng/src/app/api/picker/api-picker.template.ts
@@ -1,12 +1,17 @@
 import { customPopoverTemplate } from '../../popover/popover.template';
-export const standardApiPickerTemplate = customPopoverTemplate(`
+const contentPlaceholder = '######## This is where the option goes #######';
+const pickerTemplate = `
 <ul class="api-picker-options">
 	<li
 	*ngFor="let option of (options$ | async); let i = index"
 	(click)="selectOption(option)"
 	[ngClass]="{'is-focus': (i === (highlightIndex$ | async))}"
 	class="api-picker-option">
-		{{ option.name }}
+		${contentPlaceholder}
 	</li>
 </ul>
-`);
+`;
+export function customApiPickerTemplate(content: string) {
+	return customPopoverTemplate(pickerTemplate.replace(contentPlaceholder, content));
+}
+export const standardApiPickerTemplate = customApiPickerTemplate(`<span>{{ option.name }}</span>`);
