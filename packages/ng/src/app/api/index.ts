04b1b572add00378878c0aea6b3cfa8b0a84b848
lucienbertin <me@lucienbertin.com>
2017-12-07 13:18:42 +0100

    ng [fix] some exports, also exports formly module in root index

--- packages/ng/src/app/api/index.ts
+++ packages/ng/src/app/api/index.ts
@@ -1,3 +1,3 @@
 export { LuApiModule } from './api.module';
-export { LuApiDirective } from './api.directive';
 export { ICoerce, IApiItem } from './api.model';
+export { LuApiPickerModule, LuApiPickerComponent, LuApiPickerDirective } from './picker/';
--- packages/ng/src/index.ts
+++ packages/ng/src/index.ts
@@ -1,7 +1,8 @@
 export * from './app/date-range-picker/date-range-picker.module';
 export { IDateRangeSelectChoice, IDateRange } from './app/date-range-picker/date-range-picker.model';
 export * from './app/user';
-export { LuPopoverModule, LuPopoverComponent } from './app/popover/popover.module';
+export { LuPopoverModule, LuPopoverComponent } from './app/popover';
 export { LuRootModule } from './app/lu-root.module';
 export { LuEmptyModule } from './app/empty';
 export * from './app/animations';
+export { LuFormlyModule } from './app/formly';
