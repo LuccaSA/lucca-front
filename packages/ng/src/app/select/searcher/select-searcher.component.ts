fa34ce21e730409defd38632e4ebb384bbd38672
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-02-15 16:41:05 +0100

    document the search component

--- packages/ng/src/app/select/searcher/select-searcher.component.ts
+++ packages/ng/src/app/select/searcher/select-searcher.component.ts
@@ -16,7 +16,7 @@ import 'rxjs/add/operator/distinctUntilChanged';
 	styleUrls: ['./select-searcher.component.scss'],
 })
 /**
- * Component that manage the possibility to clear a select. null value will be set when we click on it
+ * Component that manage the possibility to search in the options of a select.
  */
 export class LuSelectSearcherComponent<T> implements ISelectSearcher<T>  {
 
@@ -25,6 +25,9 @@ export class LuSelectSearcherComponent<T> implements ISelectSearcher<T>  {
 	private _focus = false;
 	private _clue$: Subject<string> = new Subject<string>();
 
+	/**
+	 * The options detected
+	 */
 	@ContentChildren(LuSelectOption, { descendants: true }) luOptions: QueryList<LuSelectOption<T>>;
 
 	constructor() {
@@ -38,18 +41,21 @@ export class LuSelectSearcherComponent<T> implements ISelectSearcher<T>  {
 		});
 	}
 
-	onMouseDown($e) {
+	private _onMouseDown($e) {
 		this._focus = true;
 		// We prevent propagation to avoid lost of focus in input field
 		$e.stopPropagation();
 	}
 
-	onBlur() {
+	private _onBlur() {
 		this._focus = false;
 		// When we quit the field, we reset the search item
 		this._clue = '';
 	}
 
+	/**
+	 * See ISelectSearcher
+	 */
 	filter(clue: string, options: LuSelectOption<T>[]): LuSelectOption<T>[] {
 		return options.filter((option) => {
 			const match = option.viewValue.indexOf(clue) !== -1;
@@ -61,6 +67,9 @@ export class LuSelectSearcherComponent<T> implements ISelectSearcher<T>  {
 		});
 	}
 
+	/**
+	 * See ISelectSearcher
+	 */
 	hasFocus(): boolean {
 		return this._focus;
 	}
--- packages/ng/src/app/select/searcher/select-searcher.model.ts
+++ packages/ng/src/app/select/searcher/select-searcher.model.ts
@@ -2,9 +2,14 @@ import { LuSelectOption } from '../option/select-option.component';
 
 export interface ISelectSearcher<T> {
 
-	// setOptions(options: LuSelectOption<T>[]): void;
-	// subscribe(filterCallback: (optionsFiltered: LuSelectOption<T>[]) => void);
-
+	/**
+	 * Filter the options passed in parameters with the clue as search element
+	 * @param clue : the key to search
+	 * @param options : the options to filtered
+	 */
 	filter(clue: string, options: LuSelectOption<T>[]): LuSelectOption<T>[];
+	/**
+	 * @returns true if the focus is on the field
+	 */
 	hasFocus(): boolean;
 }
