4177df0768abed12e2b4d5cb26280aeb88160309
Sandy <sgoldscheider@lucca.fr>
2018-02-21 14:25:18 +0100

    [add] Add support for displayFormat changes

--- packages/ng/src/app/user/tile/user-tile.component.ts
+++ packages/ng/src/app/user/tile/user-tile.component.ts
@@ -11,16 +11,25 @@ import { LuUserDisplayPipe } from '../display';
 	styleUrls: ['./user-tile.component.scss']
 })
 export class LuUserTileComponent {
-
 	/**
 	 * IUser to display.
 	 */
 	@Input() user: IUser;
 
 	/**
+	 * User Display format.
+	 * It is set to 'lf' by default
+	 */
+	@Input() displayFormat = 'lf';
+
+	/**
 	 * IUser role to display
 	 */
 	@Input() role: string;
+
+	get userName() {
+		return this.displayPipe.transform(this.user, this.displayFormat);
+	}
 
 	constructor(private displayPipe: LuUserDisplayPipe) {	}
 }
