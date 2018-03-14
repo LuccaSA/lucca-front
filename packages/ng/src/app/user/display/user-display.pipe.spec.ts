1200da961c1c9ce6f8c715f6f5d958afbcef8432
Sandy <sgoldscheider@lucca.fr>
2018-03-14 15:34:23 +0100

    [fix] fixes broken tests and adds new for new formats

--- packages/ng/src/app/user/display/user-display.pipe.spec.ts
+++ packages/ng/src/app/user/display/user-display.pipe.spec.ts
@@ -1,5 +1,6 @@
 import { LuUserDisplayPipe } from './user-display.pipe';
 import { IUser } from '../user.model';
+import { DisplayFullname, DisplayHybrid, DisplayInitials } from './display-format.model';
 
 describe('UserNamePipe', () => {
 	let user: IUser;
@@ -14,41 +15,61 @@ describe('UserNamePipe', () => {
 
 	it('should return the right value with \'lf\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'lf')).toBe('Doe John');
+		expect(pipe.transform(user, DisplayFullname.lastfirst)).toBe('Doe John');
 	});
 
 	it('should return the right value with \'Lf\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'Lf')).toBe('D. John');
+		expect(pipe.transform(user, DisplayHybrid.lastIfirstFull)).toBe('D. John');
 	});
 
 	it('should return the right value with \'LF\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'LF')).toBe('DJ');
+		expect(pipe.transform(user, DisplayInitials.lastfirst)).toBe('DJ');
 	});
 
 	it('should return the right value with \'lF\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'lF')).toBe('Doe J.');
+		expect(pipe.transform(user, DisplayHybrid.lastFullfirstI)).toBe('Doe J.');
+	});
+
+	it('should return the right value with \'l\' format', () => {
+		const pipe = new LuUserDisplayPipe();
+		expect(pipe.transform(user, DisplayFullname.last)).toBe('Doe');
+	});
+
+	it('should return the right value with \'L\' format', () => {
+		const pipe = new LuUserDisplayPipe();
+		expect(pipe.transform(user, DisplayInitials.last)).toBe('D');
 	});
 
 	it('should return the right value with \'fl\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'fl')).toBe('John Doe');
+		expect(pipe.transform(user, DisplayFullname.firstlast)).toBe('John Doe');
 	});
 
 	it('should return the right value with \'Fl\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'Fl')).toBe('J. Doe');
+		expect(pipe.transform(user, DisplayHybrid.firstIlastFull)).toBe('J. Doe');
 	});
 
 	it('should return the right value with \'FL\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'FL')).toBe('JD');
+		expect(pipe.transform(user, DisplayInitials.firstlast)).toBe('JD');
 	});
 
 	it('should return the right value with \'fL\' format', () => {
 		const pipe = new LuUserDisplayPipe();
-		expect(pipe.transform(user, 'fL')).toBe('John D.');
+		expect(pipe.transform(user, DisplayHybrid.firstFulllastI)).toBe('John D.');
+	});
+
+	it('should return the right value with \'f\' format', () => {
+		const pipe = new LuUserDisplayPipe();
+		expect(pipe.transform(user, DisplayFullname.first)).toBe('John');
+	});
+
+	it('should return the right value with \'F\' format', () => {
+		const pipe = new LuUserDisplayPipe();
+		expect(pipe.transform(user, DisplayInitials.first)).toBe('J');
 	});
 });
