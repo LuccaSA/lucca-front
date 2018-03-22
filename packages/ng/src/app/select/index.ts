20d4cafd492467f6dbf73423ccbd422b44eb7247
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-03-22 18:04:13 +0100

    [NG] sort imports

--- packages/ng/src/app/select/index.ts
+++ packages/ng/src/app/select/index.ts
@@ -1,11 +1,10 @@
 export { LuSelectModule } from './select.module';
 export { LuSelect } from './select.component';
-export * from './picker';
-export * from './option';
-export * from './option';
-export * from './directive';
 export * from './api';
 export * from './clearer';
+export * from './directive';
+export * from './option';
+export * from './picker';
 export * from './searcher';
 export * from './scroll';
 export * from './utils';
