b7413699ae9c9ede711cf89d7795b89b6570fa11
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-03-26 17:14:39 +0200

    [NG] fix travis

--- packages/ng/demo/app/select/api-select/basic/basic.ts
+++ packages/ng/demo/app/select/api-select/basic/basic.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {DemoApiSelectFeeder} from './api-select-feeder';
 
 @Component({
+	// tslint:disable-next-line:component-selector
 	selector: 'basic-api-select',
 	templateUrl: './basic.html',
 })
 export class BasicApiSelectComponent {
 
+	itemSelect: any;
 	constructor(
 		public apiSelectFeeder: DemoApiSelectFeeder) {
 	}
