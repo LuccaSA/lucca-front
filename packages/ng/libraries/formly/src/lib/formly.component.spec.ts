0ee68714d2e4100cc49e619ff107ddf7b31550d8
lucienbertin <me@lucienbertin.com>
2018-05-14 14:53:39 +0200

    aadded 2 libs generated with ng-cli, core + formly - will serve as recipient for current code

---
+++ packages/ng/libraries/formly/src/lib/formly.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormlyComponent } from './formly.component';
+
+describe('FormlyComponent', () => {
+  let component: FormlyComponent;
+  let fixture: ComponentFixture<FormlyComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FormlyComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormlyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
