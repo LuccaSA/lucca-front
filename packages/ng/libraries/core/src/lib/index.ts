b7ef50af4470ebeba03886897b2f7fde27d5c048
lucienbertin <me@lucienbertin.com>
2018-05-15 11:34:44 +0200

    fix imports

--- packages/ng/libraries/core/src/lib/index.ts
+++ packages/ng/libraries/core/src/lib/index.ts
@@ -1,4 +1,6 @@
 export * from './popover';
 export * from './select';
 export * from './user';
+export * from './api';
+export * from './rdd';
 export { LuModule } from './module';
--- packages/ng/libraries/core/src/lib/module.ts
+++ packages/ng/libraries/core/src/lib/module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { LuPopoverModule } from './popover';
 import { LuUserModule } from './user';
 import { LuSelectModule } from './select';
+import { LuApiModule } from './api';
+import { LuRddModule } from './rdd';
 
 @NgModule({
 	imports: [
 		LuUserModule,
 		LuPopoverModule,
 		LuSelectModule,
+		LuApiModule,
+		LuRddModule,
 	],
 	exports: [
 		LuUserModule,
 		LuPopoverModule,
 		LuSelectModule,
+		LuApiModule,
+		LuRddModule,
 	],
 })
 export class LuModule {}
