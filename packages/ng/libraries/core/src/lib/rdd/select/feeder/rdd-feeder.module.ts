f67be1f3941b239f5d40aaee827c9d2bc4dbf8d7
Guillaume Nury <GuillaumeNury@users.noreply.github.com>
2018-05-15 14:24:49 +0200

    Add missing import

--- packages/ng/libraries/core/src/lib/rdd/select/feeder/rdd-feeder.module.ts
+++ packages/ng/libraries/core/src/lib/rdd/select/feeder/rdd-feeder.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RDDApiFeederComponent } from './rdd-feeder.component';
 
 @NgModule({
-	imports: [],
+	imports: [HttpClientModule],
 	declarations: [RDDApiFeederComponent],
 	exports: [RDDApiFeederComponent],
 })
