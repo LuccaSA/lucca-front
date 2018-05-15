d712304b624f80224854ca14ce1f5d97871dedca
GuillaumeNury <nury.guillaume@gmail.com>
2018-05-15 11:29:01 +0200

    code review

--- packages/ng/libraries/core/src/lib/animation/animation.factory.ts
+++ packages/ng/libraries/core/src/lib/animation/animation.factory.ts
@@ -5,29 +5,29 @@ import { luScalingAnimation } from './scale.animation';
 import { luSlidingAnimation } from './slide.animation';
 
 export type LuAnimationType = 'scale' | 'slide' | 'fade';
-export const DEFAULT_LF_ANIMATION_TIMING = '250ms 0ms ease-out';
+export const LU_DEFAULT_ANIMATION_TIMING = '250ms 0ms ease-out';
 
-export function LuFadeAnimationFactory(
-	animationInTiming: string = DEFAULT_LF_ANIMATION_TIMING,
-	animationOutTiming: string = DEFAULT_LF_ANIMATION_TIMING,
+export function luFadeAnimationFactory(
+	animationInTiming: string = LU_DEFAULT_ANIMATION_TIMING,
+	animationOutTiming: string = LU_DEFAULT_ANIMATION_TIMING,
 	triggerName: string = 'fadeAnimation',
 ): AnimationTriggerMetadata {
 	return trigger(triggerName, [
 		...luFadingAnimation(animationInTiming, animationOutTiming),
 	]);
 }
-export function LuScaleAnimationFactory(
-	animationInTiming: string = DEFAULT_LF_ANIMATION_TIMING,
-	animationOutTiming: string = DEFAULT_LF_ANIMATION_TIMING,
+export function luScaleAnimationFactory(
+	animationInTiming: string = LU_DEFAULT_ANIMATION_TIMING,
+	animationOutTiming: string = LU_DEFAULT_ANIMATION_TIMING,
 	triggerName: string = 'scaleAnimation',
 ): AnimationTriggerMetadata {
 	return trigger(triggerName, [
 		...luScalingAnimation(animationInTiming, animationOutTiming),
 	]);
 }
-export function LuSlideAnimationFactory(
-	animationInTiming: string = DEFAULT_LF_ANIMATION_TIMING,
-	animationOutTiming: string = DEFAULT_LF_ANIMATION_TIMING,
+export function luSlideAnimationFactory(
+	animationInTiming: string = LU_DEFAULT_ANIMATION_TIMING,
+	animationOutTiming: string = LU_DEFAULT_ANIMATION_TIMING,
 	triggerName: string = 'slideAnimation',
 ): AnimationTriggerMetadata {
 	return trigger(triggerName, [
