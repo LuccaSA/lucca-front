1ce5023bf97b3d9ba6cbbd0bbb36620a68ba5c33
lucienbertin <me@lucienbertin.com>
2018-07-25 15:01:45 +0200

    clean imports

--- packages/ng/libraries/core/src/lib/popover/trigger/popover-trigger.directive.ts
+++ packages/ng/libraries/core/src/lib/popover/trigger/popover-trigger.directive.ts
@@ -5,39 +5,25 @@ import {
 	EventEmitter,
 	Input,
 	OnDestroy,
-	Optional,
 	Output,
 	ViewContainerRef,
 	HostListener,
 } from '@angular/core';
 
-import { isFakeMousedownFromScreenReader } from '@angular/cdk/a11y';
-import { Direction } from '@angular/cdk/bidi';
 import {
-	ConnectedPositionStrategy,
-	OriginConnectionPosition,
 	Overlay,
-	OverlayConnectionPosition,
-	OverlayRef,
-	OverlayConfig,
-	HorizontalConnectionPos,
-	VerticalConnectionPos,
 } from '@angular/cdk/overlay';
-import { TemplatePortal } from '@angular/cdk/portal';
 
-import { Subscription } from 'rxjs/Subscription';
 
 import {
 	ILuPopoverTrigger, ALuPopoverTrigger,
 } from './popover-trigger.model';
 import {
 	ILuPopoverPanel,
-	LuPopoverPosition,
 } from '../panel/popover-panel.model';
 import {
 	ILuPopoverTarget,
 } from '../target/popover-target.model';
-import { throwLuPopoverMissingError } from '../popover.errors';
 
 /**
 * This directive is intended to be used in conjunction with an lu-popover tag.  It is
