c7e7a6a5b73a17a429358bc99fc6c366a4d97923
lucienbertin <me@lucienbertin.com>
2018-07-27 15:18:14 +0200

    use user-feeder in user-select

--- packages/ng/libraries/core/src/lib/user/select/input/user-select-input.module.ts
+++ packages/ng/libraries/core/src/lib/user/select/input/user-select-input.module.ts
@@ -3,12 +3,14 @@ import { LuUserSelectInputComponent } from './user-select-input.component';
 import { CommonModule } from '@angular/common';
 import { LuUserDisplayModule } from '../../display/index';
 import { LuOptionOperatorModule, LuOptionItemModule, LuOptionPickerModule } from '../../../option/index';
+import { LuUserOperatorModule } from '../operator/index';
 
 @NgModule({
 	imports: [
 		LuUserDisplayModule,
 		LuOptionItemModule,
 		LuOptionOperatorModule,
+		LuUserOperatorModule,
 		LuOptionPickerModule,
 		CommonModule,
 	],
