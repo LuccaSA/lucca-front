cbf98af086980421a0c029b79e2cee816f4560c5
lucienbertin <me@lucienbertin.com>
2018-07-31 16:37:17 +0200

    add option disaplayer - temp cuz it has nothing to do with option

---
+++ packages/ng/libraries/core/src/lib/option/displayer/option-displayer.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { LuOptionDisplayerComponent } from './option-displayer.component';
+
+@NgModule({
+	declarations: [
+		LuOptionDisplayerComponent,
+	],
+	exports: [
+		LuOptionDisplayerComponent,
+	],
+})
+export class LuOptionDisplayerModule {}
