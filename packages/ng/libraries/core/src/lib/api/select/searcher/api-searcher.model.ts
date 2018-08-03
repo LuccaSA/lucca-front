5ee4c943720432bcfcad57a64bf057bf9e532bae
lucienbertin <me@lucienbertin.com>
2018-08-03 11:02:43 +0200

    add api-paged searcher

--- packages/ng/libraries/core/src/lib/api/select/searcher/api-searcher.model.ts
+++ packages/ng/libraries/core/src/lib/api/select/searcher/api-searcher.model.ts
@@ -5,9 +5,12 @@ import { ILuApiOptionFeeder, ILuApiFeederService, ALuApiFeederService } from '..
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/withLatestFrom';
 import { merge } from 'rxjs/observable/merge';
+import { ILuApiPagerService } from '../pager';
 
 export interface ILuApiOptionSearcher<T extends IApiItem = IApiItem> extends ILuApiOptionFeeder<T> {}
 export interface ILuApiSearcherService<T extends IApiItem = IApiItem> extends ILuApiFeederService<T> {
@@ -21,14 +24,14 @@ export abstract class ALuApiOptionSearcher<T extends IApiItem = IApiItem> implem
 	protected _results$: Observable<T[]>;
 	protected _clue$: Observable<string>;
 	set clue$(clue$: Observable<string>) {
-		this._clue$ = clue$;
-		this.initObservables();
+		this.initObservables(clue$);
 	}
 	constructor(protected service: ILuApiSearcherService<T>) {}
 	onOpen() {
 		this.resetClue();
 	}
-	protected initObservables() {
+	protected initObservables(clue$) {
+		this._clue$ = clue$;
 		this._results$ = this._clue$.switchMap(clue => this.service.searchAll(clue).catch(err => of([])));
 
 		this._results$.subscribe(items => this.outOptions$.next(items));
@@ -53,5 +56,79 @@ implements ILuApiSearcherService<T> {
 		const urlSafeClue = encodeURIComponent(clue);
 		const url = `${this.url}&${this._searchProperty}=like,${urlSafeClue}`;
 		return this._get(url);
+	}
+}
+// paged
+const MAGIC_PAGE_SIZE = 20;
+export interface ILuApiOptionPagedSearcher<T extends IApiItem = IApiItem> extends ILuApiOptionSearcher<T> {}
+export interface ILuApiPagedSearcherService<T extends IApiItem = IApiItem> extends ILuApiSearcherService<T>, ILuApiPagerService<T> {
+	searchPaged(clue: string, page: number): Observable<T[]>;
+}
+export abstract class ALuApiPagedSearcherService<T extends IApiItem = IApiItem>
+extends ALuApiSearcherService<T>
+implements ILuApiPagedSearcherService<T> {
+	constructor(protected http: HttpClient) { super(http); }
+	getPaged(page = 0) {
+		const paging = `paging=${page * MAGIC_PAGE_SIZE},${MAGIC_PAGE_SIZE}`;
+		const url = `${this.url}&${paging}`;
+		return this._get(url);
+	}
+	searchPaged(clue = '', page = 0) {
+		if (!clue) {
+			return this.getPaged(page);
+		}
+		const urlSafeClue = encodeURIComponent(clue);
+		const paging = `paging=${page * MAGIC_PAGE_SIZE},${MAGIC_PAGE_SIZE}`;
+		const url = `${this.url}&${this._searchProperty}=like,${urlSafeClue}&${paging}`;
+		return this._get(url);
+	}
+}
+export abstract class ALuApiOptionPagedSearcher<T extends IApiItem = IApiItem>
+extends ALuApiOptionSearcher<T>
+implements ILuApiOptionPagedSearcher<T> {
+	outOptions$ = new BehaviorSubject<T[]>([]);
+	loading$: Observable<boolean>;
+	protected _loading = false;
+	protected _page$ = new BehaviorSubject<number>(undefined);
+
+	constructor(protected service: ILuApiPagedSearcherService<T>) { super(service); }
+	onOpen() {
+		this.resetClue();
+		this.resetPage();
+	}
+	onScrollBottom() {
+		if (!this._loading) {
+			this._page$.next(this._page$.value + 1);
+		}
+	}
+	protected initObservables(clue$) {
+		this._clue$ = clue$.do(c => this._page$.next(0));
+		this._results$ =
+		merge(
+			this._clue$
+			.withLatestFrom(this._page$)
+			.switchMap(([clue, page]) => this.service.searchPaged(clue, page).catch(err => of([]))),
+			this._page$
+			.withLatestFrom(this._clue$)
+			.switchMap(([page, clue]) => this.service.searchPaged(clue, page).catch(err => of([]))),
+		);
+
+		this._results$
+		.withLatestFrom(this._page$)
+		.subscribe(([items, page]) => {
+			if (page === 0) {
+				this.outOptions$.next([...items]);
+			} else {
+				this.outOptions$.next([...this.outOptions$.value, ...items]);
+			}
+		});
+		this.loading$ = merge(
+			this._clue$.mapTo(true),
+			this._results$.mapTo(false),
+		).do(l => this._loading = l);
+	}
+	abstract resetClue();
+	resetPage() {
+		this._page$.next(0);
 	}
 }
