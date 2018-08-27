e39e6d7cf500d9b115218b86a3bdad57be960b8f
sanlucca <sgoldscheider@lucca.fr>
2018-08-27 16:58:29 +0200

    [upd] demo multiple

--- packages/ng/docs/demo/src/app/select/multiple/multiple.ts
+++ packages/ng/docs/demo/src/app/select/multiple/multiple.ts
@@ -13,6 +13,8 @@ import {
 export class MultipleSelectComponent {
 	red =	{ id: 1, name: 'red' };
 	green = { id: 2, name: 'green' };
+	blue = { id: 3, name: 'blue' };
+	yellow =	{ id: 4, name: 'yellow' };
 	items = [this.red];
 	colors = [];
 }
