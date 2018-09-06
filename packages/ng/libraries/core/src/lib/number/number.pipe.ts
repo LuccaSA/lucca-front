f1987ea4a60d7c065d91c24c08acc5f2e790970c
lucienbertin <me@lucienbertin.com>
2018-09-06 14:40:57 +0200

    number pipe added precision parameters - defaults to 2

--- packages/ng/libraries/core/src/lib/number/number.pipe.ts
+++ packages/ng/libraries/core/src/lib/number/number.pipe.ts
@@ -7,13 +7,18 @@ import { formatNumber, getLocaleNumberSymbol, NumberSymbol } from '@angular/comm
 })
 export class LuNumberPipe implements PipeTransform {
 	constructor(@Inject(LOCALE_ID) protected locale) {}
-	transform(number: number) {
-		const formatted = formatNumber(number, this.locale, '1.2-2');
+	transform(number: number, precision = 2) {
+		const formatted = formatNumber(number, this.locale, `1.${precision}-${precision}`);
 		const separator = getLocaleNumberSymbol(this.locale, NumberSymbol.Decimal);
 		const split = formatted.split(separator);
 		const integral = split[0];
 		const decimal = split[1];
 		const hideDecimal = Math.round(number) === number;
-		return `${integral}<span class="decimal-part${hideDecimal ? ' u-hidden' : ''}">${separator}${decimal}</span>`;
+		if (precision > 0) {
+			return `${integral}<span class="decimal-part${hideDecimal ? ' u-hidden' : ''}">${separator}${decimal}</span>`;
+		} else {
+			return integral;
+		}
+
 	}
 }
