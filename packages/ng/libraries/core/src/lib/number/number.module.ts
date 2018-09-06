8abbf738af7e384c63d5cfc37e3a6a4359149539
lucienbertin <me@lucienbertin.com>
2018-09-06 14:25:56 +0200

    number pipe that wraps decimal and separator in a span

--- packages/ng/libraries/core/src/lib/number/number.module.ts
+++ packages/ng/libraries/core/src/lib/number/number.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LuNumberPipe } from './number.pipe';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+	imports: [CommonModule],
 	declarations: [LuNumberPipe],
 	exports: [LuNumberPipe],
 })
--- packages/ng/libraries/core/src/lib/number/number.pipe.ts
+++ packages/ng/libraries/core/src/lib/number/number.pipe.ts
@@ -1,11 +1,19 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, Inject, LOCALE_ID } from '@angular/core';
+import { formatNumber, getLocaleNumberSymbol, NumberSymbol } from '@angular/common';
 
 @Pipe({
 	name: 'luNumber',
 	pure: true,
 })
 export class LuNumberPipe implements PipeTransform {
+	constructor(@Inject(LOCALE_ID) protected locale) {}
 	transform(number: number) {
-		return `<b>${number}</b>`;
+		const formatted = formatNumber(number, this.locale, '1.2-2');
+		const separator = getLocaleNumberSymbol(this.locale, NumberSymbol.Decimal);
+		const split = formatted.split(separator);
+		const integral = split[0];
+		const decimal = split[1];
+		const hideDecimal = Math.round(number) === number;
+		return `${integral}<span class="decimal-part${hideDecimal ? ' u-hidden' : ''}">${separator}${decimal}</span>`;
 	}
 }
