e61c4a22467821808dd6b81cb168fb9d546fd28b
sanlucca <sgoldscheider@lucca.fr>
2018-09-26 15:22:20 +0200

    [upd] Demo for tooltip

--- packages/ng/docs/demo/src/app/navigation/navigation.router.ts
+++ packages/ng/docs/demo/src/app/navigation/navigation.router.ts
@@ -5,6 +5,7 @@ import { apiRoutes } from '../api/api.router';
 import { userRoutes } from '../user/user.router';
 import { selectRoutes } from '../select/select.router';
 import { numberRoutes } from '../number/number.router';
+import { tooltipRoutes } from '../tooltip/tooltip.router';
 export const featuresRoutes = [
 	...numberRoutes,
 	...formlyRoutes,
@@ -13,4 +14,5 @@ export const featuresRoutes = [
 	...apiRoutes,
 	...userRoutes,
 	...selectRoutes,
+	...tooltipRoutes,
 ];
