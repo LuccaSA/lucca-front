b780fc0ab227b18ed79f7ac6105fc7e91455f94a
lucienbertin <me@lucienbertin.com>
2018-10-04 16:35:15 +0200

    fix gulpfile of icons after migrating to gulp ^4

--- packages/icons/gulpfile.js
+++ packages/icons/gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
-
 let gulp = require('gulp');
+let runSequence = require('run-sequence');
 let sass = require('gulp-sass');
 let clean = require('gulp-clean');
 let rename = require('gulp-rename');
@@ -30,12 +30,14 @@ gulp.task('sass:dist', () => {
 	.pipe(gulp.dest(OUT_DIR));
 });
 
-gulp.task('prepare-dist-folder', ['dist:clean'], () => {
+gulp.task('copy:font', () => {
 	return gulp.src(['./font/lucca-icons.woff', './font/lucca-icons.eot', './font/lucca-icons.svg', './font/lucca-icons.ttf'])
 	.pipe(gulp.dest(OUT_DIR));
 });
 
-gulp.task('serve', ['prepare-dist-folder', 'sass:dist'], () => {
+gulp.task('build', gulp.series('dist:clean', 'copy:font', 'sass:dist'));
+
+gulp.task('serve', () => {
 	browserSync.init({
 		server: {
 			baseDir: DEMO_DIR,
@@ -49,5 +51,4 @@ gulp.task('serve', ['prepare-dist-folder', 'sass:dist'], () => {
 	gulp.watch(["./demo/*.html", "./demo/*.css"], browserSync.reload);
 });
 
-gulp.task('dist', ['prepare-dist-folder', 'sass:dist']);
-gulp.task('default', ['dist']);
+gulp.task('start', gulp.series('build', 'serve'));
