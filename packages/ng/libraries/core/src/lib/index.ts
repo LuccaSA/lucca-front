095247a89bc7f7d47f8b8b2aa6f66f01bc508cfa
lucienbertin <me@lucienbertin.com>
2018-10-31 16:29:52 +0100

    reordering lines

--- packages/ng/libraries/core/src/lib/index.ts
+++ packages/ng/libraries/core/src/lib/index.ts
@@ -1,14 +1,13 @@
-export * from './api/index';
-export * from './popover/index';
-export * from './select/index';
-export * from './user/index';
-export * from './api/index';
 export * from './animation/index';
-export * from './option/index';
+export * from './api/index';
 export * from './input/index';
-export * from './scroll/index';
 export * from './number/index';
-export * from './tooltip/index';
+export * from './option/index';
+export * from './popover/index';
 export * from './safe-content/index';
+export * from './scroll/index';
+export * from './select/index';
+export * from './tooltip/index';
+export * from './user/index';
 
 export { LuModule } from './module';
