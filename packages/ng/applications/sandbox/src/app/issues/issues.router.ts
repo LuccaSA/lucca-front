7fb8acf45841b93dd30343babae755df05675f92
lucienbertin <me@lucienbertin.com>
2019-04-10 16:22:31 +0200

    minor

--- packages/ng/applications/sandbox/src/app/issues/issues.router.ts
+++ packages/ng/applications/sandbox/src/app/issues/issues.router.ts
@@ -1,14 +1,11 @@
-import { BogusModule } from './bogus';
 import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { BogusModule } from './bogus';
 
 const routes: Routes = [
 	{ path: 'bogus', loadChildren: () => BogusModule },
 ];
-
-
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
