56ebf74afe11ac15eea74b095da373aa3f5ea60a
lucienbertin <me@lucienbertin.com>
2019-04-15 14:39:16 +0200

    local override of translations

--- packages/ng/applications/sandbox/src/app/issues/poc-translate/poc-translate.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-translate/poc-translate.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { PocTranslateComponent } from './poc-translate.component';
-import { LuUserDisplayModule, LuUserSelectModule } from '@lucca-front/ng';
-
+import { LolModule } from './lol.module';
+import { lolOverrides } from './lol.override';
+import { LOL_TRANSLATIONS_TOKEN } from './lol.translate';
 
 @NgModule({
 	declarations: [
 		PocTranslateComponent,
 	],
 	imports: [
-
 		RouterModule.forChild([
 			{ path: '', component: PocTranslateComponent },
 		]),
-		LuUserDisplayModule,
-		LuUserSelectModule,
+		LolModule,
 	],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'fr-FR' },
+		// { provide: LOCALE_ID, useValue: 'de-DE' },
+		{ provide: LOL_TRANSLATIONS_TOKEN, useValue: lolOverrides },
+	]
 })
 export class PocTranslateModule {}
