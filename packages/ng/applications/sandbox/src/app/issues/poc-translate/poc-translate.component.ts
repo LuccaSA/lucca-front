32764a1f967f37eb23e3cb79bad464c9b1dd3f34
lucienbertin <me@lucienbertin.com>
2019-04-15 16:24:08 +0200

    minor - display current locale

--- packages/ng/applications/sandbox/src/app/issues/poc-translate/poc-translate.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-translate/poc-translate.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
 	selector: 'lu-poc-translate',
 	templateUrl: './poc-translate.component.html'
 })
 export class PocTranslateComponent {
+	constructor(@Inject(LOCALE_ID) public locale) {}
 }
