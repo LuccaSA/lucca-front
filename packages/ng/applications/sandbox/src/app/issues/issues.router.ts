b0b61daaaafe640960be5b43f85c4348c50bb767
lucienbertin <me@lucienbertin.com>
2019-04-17 15:44:46 +0200

    sand - minor enh, root route displays all currently available issues

--- packages/ng/applications/sandbox/src/app/issues/issues.router.ts
+++ packages/ng/applications/sandbox/src/app/issues/issues.router.ts
@@ -4,15 +4,31 @@ import { RouterModule } from '@angular/router';
 import { PocTranslateModule } from './poc-translate';
 import { RefactoOverlaysModule } from './refacto-overlays';
 import { RefactoOverlaysTooltipModule } from './refacto-overlays-tooltip';
+import { CommonModule } from '@angular/common';
+import { IssuesComponent, ISSUES_INDEX_TOKEN } from './issues.component';
 
 const routes: Routes = [
+	{ path: '', component: IssuesComponent },
 	{ path: 'poc-translate', loadChildren: () => PocTranslateModule},
 	{ path: 'refacto-overlays', loadChildren: () => RefactoOverlaysModule},
 	{ path: 'refacto-overlays-tooltip', loadChildren: () => RefactoOverlaysTooltipModule},
 ];
+const issues = [ ...routes].map(r => r.path);
+issues.shift();
 
 @NgModule({
-	imports: [RouterModule.forChild(routes)],
-	exports: [RouterModule]
+	imports: [
+		RouterModule.forChild(routes),
+		CommonModule,
+	],
+	declarations: [
+		IssuesComponent,
+	],
+	exports: [
+		RouterModule,
+	],
+	providers: [
+		{ provide: ISSUES_INDEX_TOKEN, useValue: issues },
+	]
 })
 export class IssuesRouter { }
