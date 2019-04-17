758da8e9e01630bcf87dd02a7f1964bfd3c65660
lucienbertin <me@lucienbertin.com>
2019-04-17 16:09:47 +0200

    fix tooltip

--- packages/ng/libraries/core/src/lib/overlay/tooltip/panel/tooltip-panel.component.ts
+++ packages/ng/libraries/core/src/lib/overlay/tooltip/panel/tooltip-panel.component.ts
@@ -7,20 +7,20 @@ import { ALuPopoverPanel, ILuPopoverPanel, luTransformPopover } from '../../popo
 	styleUrls: ['./tooltip-panel.component.scss'],
 	animations: [luTransformPopover],
 })
-export class LuTooltipPanelComponent extends ALuPopoverPanel implements ILuPopoverPanel, OnInit {
+export class LuTooltipPanelComponent extends ALuPopoverPanel implements ILuPopoverPanel {
 
 	@HostBinding('@transformPopover') animationState = 'enter';
 	@HostBinding('class') get classes() {
-		if (this._classList['lu-popover-above']) {
+		if (this.panelClasses['lu-popover-above']) {
 			return 'lu-tooltip-above';
 		}
-		if (this._classList['lu-popover-below']) {
+		if (this.panelClasses['lu-popover-below']) {
 			return 'lu-tooltip-below';
 		}
-		if (this._classList['lu-popover-before']) {
+		if (this.panelClasses['lu-popover-before']) {
 			return 'lu-tooltip-before';
 		}
-		if (this._classList['lu-popover-after']) {
+		if (this.panelClasses['lu-popover-after']) {
 			return 'lu-tooltip-after';
 		}
 		return '';
@@ -34,8 +34,6 @@ export class LuTooltipPanelComponent extends ALuPopoverPanel implements ILuPopov
 	constructor(private _changeDetectorRef: ChangeDetectorRef) {
 		super();
 	}
-
-	ngOnInit(): void { }
 
 	_emitCloseEvent(): void {
 		this.close.emit();
