cfc375a0531a274c856cedacced8601ab0fe2117
lucienbertin <me@lucienbertin.com>
2019-04-19 17:06:34 +0200

    u7ncomment all feature except api and user

--- packages/ng/libraries/core/src/lib/module.ts
+++ packages/ng/libraries/core/src/lib/module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 // import { LuApiModule } from './api/index';
 import { LuInputModule } from './input/index';
-// import { LuNumberModule } from './number/index';
+import { LuNumberModule } from './number/index';
 import { LuOptionModule } from './option/index';
-// import { LuSafeContentModule } from './safe-content/index';
-// import { LuScrollModule } from './scroll/index';
+import { LuSafeContentModule } from './safe-content/index';
+import { LuScrollModule } from './scroll/index';
 import { LuSelectModule } from './select/index';
 // import { LuUserModule } from './user/index';
 import { LuOverlayModule } from './overlay/index';
@@ -13,21 +13,21 @@ import { LuOverlayModule } from './overlay/index';
 	imports: [
 		// LuApiModule,
 		LuInputModule,
-		// LuNumberModule,
+		LuNumberModule,
 		LuOptionModule,
-		// LuSafeContentModule,
-		// LuScrollModule,
+		LuSafeContentModule,
+		LuScrollModule,
 		LuSelectModule,
 		// LuUserModule,
 		LuOverlayModule,
 	],
 	exports: [
 		// LuApiModule,
-		// LuInputModule,
-		// LuNumberModule,
+		LuInputModule,
+		LuNumberModule,
 		LuOptionModule,
-		// LuSafeContentModule,
-		// LuScrollModule,
+		LuSafeContentModule,
+		LuScrollModule,
 		LuSelectModule,
 		// LuUserModule,
 		LuOverlayModule,
