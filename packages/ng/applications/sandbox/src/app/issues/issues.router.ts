7ab348ae4095e0c8319a6c5e6b9850857dd4e16d
lucienbertin <me@lucienbertin.com>
2019-04-23 17:14:20 +0200

    sand - add issue to check api and user select

--- packages/ng/applications/sandbox/src/app/issues/issues.router.ts
+++ packages/ng/applications/sandbox/src/app/issues/issues.router.ts
@@ -12,6 +12,7 @@ import { RefactoTooltipModule } from './refacto-tooltip';
 import { RefactorSelectModule } from './refactor-select';
 import { RefactorApiSelectModule } from './refactor-api-select';
 import { SplitOptionPickerModule } from './split-option-picker';
+import { SplitOptionPickerApiAuserModule } from './split-option-picker-api-auser';
 
 const routes: Routes = [
 	{ path: '', component: IssuesComponent },
@@ -24,6 +25,7 @@ const routes: Routes = [
 	{ path: 'refactor-select', loadChildren: () => RefactorSelectModule},
 	{ path: 'refactor-api-select', loadChildren: () => RefactorApiSelectModule},
 	{ path: 'split-option-picker', loadChildren: () => SplitOptionPickerModule},
+	{ path: 'split-option-picker-api-auser', loadChildren: () => SplitOptionPickerApiAuserModule},
 ];
 const issues = [ ...routes].map(r => r.path);
 issues.shift();
