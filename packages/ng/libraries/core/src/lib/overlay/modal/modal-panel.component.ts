3621587d05fe53c5265971a50fb047b0944c34bc
lucienbertin <me@lucienbertin.com>
2019-05-14 16:28:15 +0200

    minor

--- packages/ng/libraries/core/src/lib/overlay/modal/modal-panel.component.ts
+++ packages/ng/libraries/core/src/lib/overlay/modal/modal-panel.component.ts
@@ -7,10 +7,13 @@ import { PortalOutlet, CdkPortalOutlet, Portal } from '@angular/cdk/portal';
 	templateUrl: './modal-panel.component.html',
 	styleUrls: ['./modal-panel.component.scss']
 })
-export class LuModalPanelComponent implements PortalOutlet, OnDestroy {
+export class LuModalPanelComponent<T = any> implements PortalOutlet, OnDestroy {
 	@ViewChild('outlet', { read: CdkPortalOutlet }) protected _outlet: PortalOutlet;
-	attach<T>(portal: Portal<T>) {
-		return this._outlet.attach(portal) as ComponentRef<T>;
+	protected _componentInstance: T;
+	attach<U extends T = T>(portal: Portal<U>) {
+		const ref = this._outlet.attach(portal) as ComponentRef<U>;
+		this._componentInstance = ref.instance;
+		return ref;
 	}
 	detach() {
 		return this._outlet.detach();
