daca068f87fe3e96cc6b436b81516b1e143ab4e9
lucienbertin <me@lucienbertin.com>
2019-05-20 16:58:49 +0200

    add modal translations and intl injectables for thecancel button in the modal footer

--- packages/ng/libraries/core/src/lib/overlay/modal/modal-panel.component.ts
+++ packages/ng/libraries/core/src/lib/overlay/modal/modal-panel.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ComponentRef, OnDestroy } from '@angular/core';
 import { PortalOutlet, CdkPortalOutlet, Portal } from '@angular/cdk/portal';
 import { ILuModalContent } from './modal.model';
 import { ALuModalRef } from './modal-ref.model';
+import { LuModalIntl } from './modal.intl';
 
 
 @Component({
@@ -17,6 +18,7 @@ export class LuModalPanelComponent<T extends ILuModalContent = ILuModalContent> 
 	}
 	constructor(
 		protected _ref: ALuModalRef<LuModalPanelComponent>,
+		public intl: LuModalIntl,
 	) {}
 	attach<U extends T = T>(portal: Portal<U>) {
 		const ref = this._outlet.attach(portal) as ComponentRef<U>;
--- packages/ng/libraries/core/src/lib/overlay/modal/modal.module.ts
+++ packages/ng/libraries/core/src/lib/overlay/modal/modal.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { LuModal } from './modal.service';
-import { LU_MODAL_CONFIG } from './modal.token';
+import { LU_MODAL_CONFIG, LU_MODAL_TRANSLATIONS } from './modal.token';
 import { luDefaultModalConfig } from './modal-config.default';
 import { LuModalPanelComponent } from './modal-panel.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
 import { LU_POPUP_REF_FACTORY, LU_POPUP_CONFIG } from '../popup';
 import { LuModalRefFactory } from './modal-ref.factory';
+import { LuModalIntl } from './modal.intl';
+import { luModalTranslations } from './modal.translate';
 
 
 @NgModule({
@@ -19,9 +21,11 @@ import { LuModalRefFactory } from './modal-ref.factory';
 	exports: [LuModalPanelComponent],
 	providers: [
 		LuModal,
+		LuModalIntl,
 		{ provide: LU_POPUP_CONFIG, useValue: luDefaultModalConfig },
 		{ provide: LU_MODAL_CONFIG, useValue: luDefaultModalConfig },
 		{ provide: LU_POPUP_REF_FACTORY, useClass: LuModalRefFactory },
+		{ provide: LU_MODAL_TRANSLATIONS, useValue: luModalTranslations },
 	]
 })
 export class LuModalModule {}
