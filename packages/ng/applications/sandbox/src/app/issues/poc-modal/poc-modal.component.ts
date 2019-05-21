94affa65607fdda54646cdaa36040ce4ed66cf18
lucienbertin <me@lucienbertin.com>
2019-05-21 10:51:40 +0200

    sand - mnodal with footer

--- packages/ng/applications/sandbox/src/app/issues/poc-modal/poc-modal.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-modal/poc-modal.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 import { LuModal, ALuModalRef, ILuModalContent } from '@lucca-front/ng';
+import { timer, Subject } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 
 @Component({
 	selector: 'lu-poc-modal',
 	templateUrl: './poc-modal.component.html'
 })
 export class PocModalComponent {
+	result$ = new Subject();
 	constructor(private _luModal: LuModal) {}
 	open() {
-		this._luModal.open(PocModalInsideComponent);
+		this._luModal.open(PocModalInsideComponent).onClose.subscribe(r => this.result$.next(r));
 	}
 }
 @Component({
 	selector: 'lu-poc-modal-inside',
 	template: `
 		only the content in this injected component
+		<div class="textfield">
+			<input class="textfield-input" [(ngModel)]="result">
+			<label class="textfield-label">result</label>
+		</div>
+		<div class="checkbox">
+			<input class="checkbox-input" type="checkbox" name="checkbox" id="checkbox" [(ngModel)]="submitDisabled">
+			<label class="checkbox-label" for="checkbox">disable submit</label>
+		</div>
 	`,
 })
 export class PocModalInsideComponent implements ILuModalContent {
 	title = 'title of the component';
+	submitLabel = 'submit';
+	submitDisabled = false;
+
+	result = 0;
+	submitAction = () => timer(1500).pipe(mapTo(this.result));
 	constructor(
 		protected _ref: ALuModalRef<PocModalInsideComponent>,
 	) {}
--- packages/ng/applications/sandbox/src/app/issues/poc-modal/poc-modal.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-modal/poc-modal.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PocModalComponent, PocModalInsideComponent } from './poc-modal.component';
 import { LuModalModule } from '@lucca-front/ng';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -14,6 +16,8 @@ import { LuModalModule } from '@lucca-front/ng';
 	entryComponents: [PocModalInsideComponent],
 	imports: [
 		LuModalModule,
+		FormsModule,
+		CommonModule,
 		RouterModule.forChild([
 			{ path: '', component: PocModalComponent },
 		]),
