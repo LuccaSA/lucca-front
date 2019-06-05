7f494feae1f59a6a80085e34ed8d3aefdc0957f5
lucienbertin <me@lucienbertin.com>
2019-06-05 11:58:43 +0200

    sand - some shenanigans

--- packages/ng/applications/sandbox/src/app/issues/poc-tree/poc-tree.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-tree/poc-tree.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
 	templateUrl: './poc-tree.component.html'
 })
 export class PocTreeComponent {
-	node1 = { id: 1, name: 'root' };
-	node11 = { id: 11, name: 'node' };
-	node111 = { id: 111, name: 'leaf' };
+	node1 = { id: 1, name: 'root 1' };
+	node11 = { id: 11, name: 'node 1.1' };
+	node111 = { id: 111, name: 'leaf 1.1.1' };
+	node12 = { id: 11, name: 'node 1.2' };
 
 }
