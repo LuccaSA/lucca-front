11882d2c9ea94fadf4b9a6ca514c5684c8ae74fe
lucienbertin <me@lucienbertin.com>
2019-06-10 16:03:55 +0200

    sand - tree picker advanced with operators

--- packages/ng/applications/sandbox/src/app/issues/tree-picker-advanced/tree-picker-advanced.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/tree-picker-advanced/tree-picker-advanced.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ILuTree } from '@lucca-front/ng';
 
 @Component({
 	selector: 'lu-tree-picker-advanced',
@@ -9,6 +10,17 @@ export class TreePickerAdvancedComponent {
 	node11 = { id: 11, name: 'node 1.1' };
 	node111 = { id: 111, name: 'leaf 1.1.1' };
 	node12 = { id: 11, name: 'node 1.2' };
+	node2 = { id: 11, name: 'root 2' };
 
 	collection = [];
+
+	options: ILuTree[] = [
+		{ node: this.node1, children: [
+			{ node: this.node11, children: [
+				{node: this.node111, children: [] }
+			]},
+			{ node: this.node12, children: [] },
+		]},
+		{ node: this.node2, children: [] },
+	];
 }
