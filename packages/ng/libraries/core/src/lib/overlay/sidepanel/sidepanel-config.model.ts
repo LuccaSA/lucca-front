613db5b4caee7092113ea477c3abb929a811c21f
lucienbertin <me@lucienbertin.com>
2019-06-14 15:01:59 +0200

    restrict sidepanel position to right or left

--- packages/ng/libraries/core/src/lib/overlay/sidepanel/sidepanel-config.model.ts
+++ packages/ng/libraries/core/src/lib/overlay/sidepanel/sidepanel-config.model.ts
@@ -1,3 +1,5 @@
 import { ILuModalConfig } from '../modal/index';
 
-export interface ILuSidepanelConfig extends ILuModalConfig {}
+export interface ILuSidepanelConfig extends ILuModalConfig {
+	position?: 'left' | 'right';
+}
