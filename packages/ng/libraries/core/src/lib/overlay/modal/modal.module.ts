009a109bbdbfdcca1c93d3d520698fc8bd34f3eb
lucienbertin <me@lucienbertin.com>
2019-07-12 17:20:22 +0200

    fix modal/sidepanel with more injection tokens

--- packages/ng/libraries/core/src/lib/overlay/modal/modal.module.ts
+++ packages/ng/libraries/core/src/lib/overlay/modal/modal.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { LuModal } from './modal.service';
-import { LU_MODAL_CONFIG, LU_MODAL_TRANSLATIONS } from './modal.token';
+import { LU_MODAL_CONFIG, LU_MODAL_TRANSLATIONS, LU_MODAL_REF_FACTORY } from './modal.token';
 import { luDefaultModalConfig } from './modal-config.default';
 import { LuModalPanelComponent } from './modal-panel.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
-import { LU_POPUP_REF_FACTORY, LU_POPUP_CONFIG } from '../popup/index';
 import { LuModalRefFactory } from './modal-ref.factory';
 import { LuModalIntl } from './modal.intl';
 import { luModalTranslations } from './modal.translate';
@@ -24,9 +23,8 @@ import { CommonModule } from '@angular/common';
 	providers: [
 		LuModal,
 		LuModalIntl,
-		{ provide: LU_POPUP_CONFIG, useValue: luDefaultModalConfig },
 		{ provide: LU_MODAL_CONFIG, useValue: luDefaultModalConfig },
-		{ provide: LU_POPUP_REF_FACTORY, useClass: LuModalRefFactory },
+		{ provide: LU_MODAL_REF_FACTORY, useClass: LuModalRefFactory },
 		{ provide: LU_MODAL_TRANSLATIONS, useValue: luModalTranslations },
 	]
 })
