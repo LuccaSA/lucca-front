f7aeb61f83345540e378d1fe200e857df952a273
lucienbertin <me@lucienbertin.com>
2019-07-23 15:10:18 +0200

    minor typo

--- packages/ng/libraries/core/src/lib/user/select/input/user-select-input.token.ts
+++ packages/ng/libraries/core/src/lib/user/select/input/user-select-input.token.ts
@@ -1,3 +1,3 @@
 import { InjectionToken } from '@angular/core';
 
-export const LU_USER_SELECT_INPUT_TRANSLATIONS = new InjectionToken('LuUserSelectTranslationsTranslations');
+export const LU_USER_SELECT_INPUT_TRANSLATIONS = new InjectionToken('LuUserSelectTranslations');
