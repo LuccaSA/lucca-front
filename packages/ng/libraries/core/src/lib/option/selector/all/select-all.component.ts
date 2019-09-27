caf0c721e3e2bf1090a5912d5c34a3f87436d4cb
lucienbertin <me@lucienbertin.com>
2019-09-27 16:08:19 +0200

    select-all component kinda works, need to plug the picker advanced now

--- packages/ng/libraries/core/src/lib/option/selector/all/select-all.component.ts
+++ packages/ng/libraries/core/src/lib/option/selector/all/select-all.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, forwardRef, Input, HostBinding } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ALuOptionSelector } from '../option-selector.model';
+import { ChangeDetectionStrategy, Component, forwardRef, HostBinding } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { ALuOptionSelector, ILuOptionSelector } from '../option-selector.model';
 import { tap } from 'rxjs/operators';
 import { ALuOptionOperator } from '../../operator/option-operator.model';
 @Component({
 	selector: 'lu-option-select-all',
-	template: `lol`,
+	templateUrl: './select-all.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	providers: [
 		{
@@ -20,12 +20,21 @@ import { ALuOptionOperator } from '../../operator/option-operator.model';
 		},
 	],
 })
-export class LuOptionSelectAllComponent<T = any> extends ALuOptionOperator<T> {
+export class LuOptionSelectAllComponent<T = any> extends ALuOptionOperator<T> implements ILuOptionSelector<T> {
+	multiple = true;
+	onSelectValue = new Subject<T | T[]>();
+
 	@HostBinding('class.position-fixed') fixed = true;
 	options;
 	set inOptions$(in$: Observable<T[]>) {
 		this.outOptions$ = in$.pipe(
 			tap(options => this.options = options)
 		);
+	}
+	selectAll() {
+		this.onSelectValue.next([...this.options]);
+	}
+	setValue(values: T | T[]): void {
+		
 	}
 }
