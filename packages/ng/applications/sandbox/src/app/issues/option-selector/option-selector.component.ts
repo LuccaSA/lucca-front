a67a20ef9c7a01f4638906afd6c9c3cada413cc9
lucienbertin <me@lucienbertin.com>
2019-10-16 16:26:36 +0200

    sand - add stuff for a tree option selecttor

--- packages/ng/applications/sandbox/src/app/issues/option-selector/option-selector.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/option-selector/option-selector.component.ts
@@ -1,4 +1,12 @@
+/* tslint:disable */
 import { Component } from '@angular/core';
+
+const node1 = { id: 1, name: 'root 1' };
+const node11 = { id: 11, name: 'node 1.1' };
+const node111 = { id: 111, name: 'leaf 1.1.1' };
+const node12 = { id: 11, name: 'node 1.2' };
+const node2 = { id: 2, name: 'root 2' };
+const node21 = { id: 21, name: 'node 2.1' };
 
 @Component({
 	selector: 'lu-option-selector',
@@ -162,4 +170,50 @@ export class OptionSelectorComponent {
 		{ name: 'yellow', code: '#ffff00' },
 		{ name: 'yellowgreen', code: '#9acd32' },
 	];
+	options = [{
+		value: node1,
+		children: [
+			{ value: node11, children: [ { value: node111 } ] },
+			{ value: node12, children: [] },
+		]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}];
 }
