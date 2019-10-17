07566ccf566c80dfa2bf8acc1a442a4a3be0f055
lucienbertin <me@lucienbertin.com>
2019-10-17 18:20:34 +0200

    added build:publish script for pck scss

--- packages/scss/gulpfile.js
+++ packages/scss/gulpfile.js
@@ -10,7 +10,7 @@ let browserSync = require('browser-sync').create();
 
 const DEMO_DIR = './demo';
 const OUT_DIR = './dist';
-const DEPLOY_DIR = '../../demo/scss';
+const PUBLISH_DIR = '../../demo/scss';
 const SASS_OPTIONS_DEBUG = {
 	outputStyle: 'extended',
 	sourceMapEmbed: true,
@@ -34,6 +34,10 @@ gulp.task('dist:clean', () => {
 	return gulp.src(OUT_DIR, { read: false, allowEmpty: true })
 	.pipe(clean());
 });
+gulp.task('publish:clean', () => {
+	return gulp.src(PUBLISH_DIR, { read: false, allowEmpty: true })
+	.pipe(clean());
+});
 
 // gulp.task('serve', ['scss-lint', 'dist:clean', 'sass:debug'], () => {
 gulp.task('serve', () => {
@@ -42,8 +46,8 @@ gulp.task('serve', () => {
 			baseDir: DEMO_DIR,
 			index: "demo.html",
 			routes: {
-				"/scss/lucca-front.css": "./dist/lucca-front.css",
-				"/scss/demo.css": "./demo/demo.css",
+				"./lucca-front.css": "./dist/lucca-front.css",
+				"./demo.css": "./demo/demo.css",
 				"/icons": "../icons"
 			}
 		}
@@ -69,13 +73,7 @@ gulp.task('sass:dist', () => {
 	.pipe(autoprefixer(AUTOPREFIXER_OPTIONS))
 	.pipe(gulp.dest(OUT_DIR));
 });
-gulp.task('sass:deploy', () => {
-	return gulp.src('./src/main.dist.scss')
-	.pipe(sass(SASS_OPTIONS_DIST).on('error', sass.logError))
-	.pipe(rename('lucca-front.css'))
-	.pipe(autoprefixer(AUTOPREFIXER_OPTIONS))
-	.pipe(gulp.dest(DEPLOY_DIR));
-});
+
 
 gulp.task('sass:lint', () => {
 	return gulp.src(["./src/*.scss", "./src/**/*.scss"])
@@ -86,8 +84,17 @@ gulp.task('sass:lint', () => {
 	}));
 });
 
+gulp.task('publish:demo', () => {
+	return gulp.src(['./demo/**'])
+	.pipe(gulp.dest(PUBLISH_DIR))
+});
+gulp.task('publish:dist', () => {
+	return gulp.src(['./dist/**'])
+	.pipe(gulp.dest(PUBLISH_DIR))
+});
+
 gulp.task('build', gulp.series('dist:clean', 'sass:dist'));
-gulp.task('deploy', gulp.series('sass:deploy'));
 gulp.task('debug', gulp.parallel('sass:lint', 'sass:dist'));
 gulp.task('start', gulp.series('debug', 'serve'));
 
+gulp.task('build:publish', gulp.series('publish:clean', 'dist:clean', 'sass:dist', 'publish:demo', 'publish:dist'));
