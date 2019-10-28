d234d8fb0e65083f16a3e0e04aa750da42172faa
Ziri Belabbas <zbelabbas@lucca.fr>
2019-10-28 15:45:37 +0100

    add dummy pipe for IE11

--- packages/ng/libraries/core/src/lib/remove-diacritics/dummy-remove-diacritics.pipe.ts
+++ packages/ng/libraries/core/src/lib/remove-diacritics/dummy-remove-diacritics.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'luRemoveDiacritics' })
-export class LuRemoveDiacriticsPipe implements PipeTransform {
+export class LuDummyRemoveDiacriticsPipe implements PipeTransform {
 
 	constructor() { }
 
 	public transform(value: any): string {
-		return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+		return value;
 	}
 
 }
--- packages/ng/libraries/core/src/lib/remove-diacritics/remove-diacritics.module.ts
+++ packages/ng/libraries/core/src/lib/remove-diacritics/remove-diacritics.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuRemoveDiacriticsPipe } from './remove-diacritics.pipe';
+import { LuDummyRemoveDiacriticsPipe } from './dummy-remove-diacritics.pipe';
 
 @NgModule({
 	imports: [CommonModule],
 	declarations: [LuRemoveDiacriticsPipe],
-	exports: [LuRemoveDiacriticsPipe],
-	providers: [],
+	providers: [
+		{
+			provide: LuRemoveDiacriticsModule,
+			useClass: String.prototype.normalize == null ? LuDummyRemoveDiacriticsPipe : LuRemoveDiacriticsPipe,
+		}
+	],
 })
 export class LuRemoveDiacriticsModule { }
