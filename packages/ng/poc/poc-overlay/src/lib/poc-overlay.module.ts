ca618441842d914c6e07db79e411d305a3951b8f
lucienbertin <me@lucienbertin.com>
2019-11-05 10:41:38 +0100

    poc overlay module imports poc core module

--- packages/ng/poc/poc-overlay/src/lib/poc-overlay.module.ts
+++ packages/ng/poc/poc-overlay/src/lib/poc-overlay.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PocOverlayComponent } from './poc-overlay.component';
+import { PocCoreModule } from '@lf/poc/core';
 
 
 
 @NgModule({
-  declarations: [PocOverlayComponent],
-  imports: [
-  ],
-  exports: [PocOverlayComponent]
+	declarations: [PocOverlayComponent],
+	imports: [
+		PocCoreModule,
+	],
+	exports: [PocOverlayComponent]
 })
 export class PocOverlayModule { }
