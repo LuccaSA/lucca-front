e3002f993c147014c2df8707390a38e4a4350664
lucienbertin <me@lucienbertin.com>
2019-11-13 17:36:22 +0100

    added root library that just exposes all secondary entry points

--- packages/ng/libraries/root/src/lib/overlay/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './modal/index';
-export * from './popover/index';
-export * from './popup/index';
-export * from './sidepanel/index';
-export * from './tooltip/index';
-export * from './overlay.module';
--- packages/ng/libraries/root/src/lib/tree/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './option/index';
-
-export * from './tree.model';
-export * from './tree.module';
