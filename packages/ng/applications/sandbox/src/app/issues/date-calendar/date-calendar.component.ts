5a66d1cb8bffe3389b7dab0415fc514c66b1e7a2
lucienbertin <me@lucienbertin.com>
2019-11-18 14:31:08 +0100

    sand - test with date init in different year

--- packages/ng/applications/sandbox/src/app/issues/date-calendar/date-calendar.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-calendar/date-calendar.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'sand-date-calendar',
 	templateUrl: './date-calendar.component.html'
 })
-export class DateCalendarComponent {
+export class DateCalendarComponent implements OnInit {
 	date = new Date();
+	ngOnInit() {
+		// this.date.setFullYear(2016);
+	}
 }
