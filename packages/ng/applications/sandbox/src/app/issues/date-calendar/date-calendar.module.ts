ec86bbba9bc051a6c1f791586a79012eb9e8b575
lucienbertin <me@lucienbertin.com>
2019-11-18 13:40:17 +0100

    sznd - different locale for the datepicker calendar

--- packages/ng/applications/sandbox/src/app/issues/date-calendar/date-calendar.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-calendar/date-calendar.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { DateCalendarComponent } from './date-calendar.component';
 import { LuDateModule } from '@lucca-front/ng/date';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 
+import localeFr from '@angular/common/locales/fr';
+import localeGb from '@angular/common/locales/en-GB';
 
+registerLocaleData(localeFr);
+registerLocaleData(localeGb);
 
 @NgModule({
 	declarations: [
@@ -20,5 +24,10 @@ import { CommonModule } from '@angular/common';
 			{ path: '', component: DateCalendarComponent },
 		]),
 	],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'fr-FR' }
+		// { provide: LOCALE_ID, useValue: 'en-GB' }
+		// { provide: LOCALE_ID, useValue: 'en-US' }
+	]
 })
 export class DateCalendarModule {}
