4b96fb35e7553f90aba038211e9d425990284f41
lucienbertin <me@lucienbertin.com>
2019-11-21 17:36:33 +0100

    sand - integration inside a form

--- packages/ng/applications/sandbox/src/app/issues/date-input/date-input.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-input/date-input.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'sand-date-input',
 	templateUrl: './date-input.component.html'
 })
-export class DateInputComponent {
+export class DateInputComponent implements OnInit {
 	date = new Date();
-	random() {
-		this.date = new Date(this.date);
-		this.date.setDate(Math.ceil(Math.random() * 1));
+	form = new FormGroup({
+		date: new FormControl(),
+	});
+	submit() {
+		console.log(this.form.value);
+	}
+	ngOnInit() {
+		this.form.valueChanges.subscribe(v => {
+			const f = this.form;
+		})
 	}
 }
--- packages/ng/applications/sandbox/src/app/issues/date-input/date-input.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-input/date-input.module.ts
@@ -2,7 +2,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { DateInputComponent } from './date-input.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LuDateModule } from '@lucca-front/ng/date';
 
 import localeFr from '@angular/common/locales/fr';
@@ -22,15 +22,16 @@ registerLocaleData(localeDe);
 		FormsModule,
 		LuDateModule,
 		CommonModule,
+		ReactiveFormsModule,
 		RouterModule.forChild([
 			{ path: '', component: DateInputComponent },
 		]),
 	],
 	providers: [
-		{ provide: LOCALE_ID, useValue: 'fr-FR' },
+		// { provide: LOCALE_ID, useValue: 'fr-FR' },
 		// { provide: LOCALE_ID, useValue: 'de-DE' },
 		// { provide: LOCALE_ID, useValue: 'en-GB' },
-		// { provide: LOCALE_ID, useValue: 'en-US' },
+		{ provide: LOCALE_ID, useValue: 'en-US' },
 	]
 })
 export class DateInputModule {}
