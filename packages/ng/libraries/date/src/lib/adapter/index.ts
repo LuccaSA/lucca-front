426779a53032604880881cf9211c8dd943ef9d59
lucienbertin <me@lucienbertin.com>
2019-11-26 11:42:16 +0100

    added luDate pipe

--- packages/ng/libraries/date/src/lib/adapter/index.ts
+++ packages/ng/libraries/date/src/lib/adapter/index.ts
@@ -1,3 +1,5 @@
 export * from './date-granularity.enum';
 export * from './date-adapter.interface';
 export * from './date-adapter.class';
+export * from './date-adapter.module';
+export * from './date-adapter.pipe';
--- packages/ng/libraries/date/src/lib/date.module.ts
+++ packages/ng/libraries/date/src/lib/date.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { LuCalendarInputModule } from './calendar/index';
 import { LuDatePickerModule } from './picker/index';
 import { LuDateInputModule } from './input/index';
+import { LuDateAdapterModule } from './adapter/index';
 
 @NgModule({
 	imports: [
 		LuCalendarInputModule,
 		LuDatePickerModule,
 		LuDateInputModule,
+		LuDateAdapterModule,
 	],
 	exports: [
 		LuCalendarInputModule,
 		LuDatePickerModule,
 		LuDateInputModule,
+		LuDateAdapterModule,
 	]
 })
 export class LuDateModule {}
