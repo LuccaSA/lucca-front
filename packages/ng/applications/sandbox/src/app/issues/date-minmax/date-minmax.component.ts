862c033794fe70c23d9f4c0f41b95e9e60fd0408
lucienbertin <me@lucienbertin.com>
2019-11-26 16:20:46 +0100

    sand - min max validation

--- packages/ng/applications/sandbox/src/app/issues/date-minmax/date-minmax.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-minmax/date-minmax.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'lu-date-minmax',
 	templateUrl: './date-minmax.component.html'
 })
 export class DateMinmaxComponent {
+	date = new Date();
+	min = new Date();
+	max = new Date();
+	form = new FormGroup({
+		date: new FormControl(new Date()),
+	});
 }
--- packages/ng/applications/sandbox/src/app/issues/date-minmax/date-minmax.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-minmax/date-minmax.module.ts
@@ -1,19 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { DateMinmaxComponent } from './date-minmax.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LuDateModule, LuNativeDateModule } from '@lucca-front/ng/date';
 
+import localeFr from '@angular/common/locales/fr';
+import localeGb from '@angular/common/locales/en-GB';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData, CommonModule } from '@angular/common';
 
+registerLocaleData(localeFr);
+registerLocaleData(localeGb);
+registerLocaleData(localeDe);
 
 @NgModule({
 	declarations: [
 		DateMinmaxComponent,
 	],
 	imports: [
+		LuDateModule,
+		LuNativeDateModule,
+		FormsModule,
+		CommonModule,
+		ReactiveFormsModule,
 
 		RouterModule.forChild([
 			{ path: '', component: DateMinmaxComponent },
 		]),
 	],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'fr-FR' },
+		// { provide: LOCALE_ID, useValue: 'de-DE' },
+		// { provide: LOCALE_ID, useValue: 'en-GB' },
+		// { provide: LOCALE_ID, useValue: 'en-US' },
+	]
 })
 export class DateMinmaxModule {}
