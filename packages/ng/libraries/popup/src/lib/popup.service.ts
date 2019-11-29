b3e97a21bb0215e91670f876022730bdbdf4b1d8
lucienbertin <me@lucienbertin.com>
2019-11-29 18:09:35 +0100

    fix build again ?

--- packages/ng/libraries/popup/src/lib/popup.service.ts
+++ packages/ng/libraries/popup/src/lib/popup.service.ts
@@ -9,7 +9,7 @@ import { ILuPopupContent } from './popup.model';
 export class LuPopup<C extends ILuPopupConfig = ILuPopupConfig> {
 	constructor(
 		@Inject(LU_POPUP_REF_FACTORY) protected _factory: ILuPopupRefFactory,
-		@Inject(LU_POPUP_CONFIG) protected _config: C,
+		@Inject(LU_POPUP_CONFIG) protected _config: ILuPopupConfig,
 	) {}
 
 	open<T extends ILuPopupContent = ILuPopupContent, D = any, R = any>(component: ComponentType<T>, data: D = undefined, config: C = {} as any): ILuPopupRef<T, D, R> {
