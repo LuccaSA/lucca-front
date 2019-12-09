5177438c5ed86d8e9ddd4cd5981022898be6f8e2
lucienbertin <me@lucienbertin.com>
2019-12-09 17:07:34 +0100

    sand - tree opersators

--- packages/ng/applications/sandbox/src/app/issues/ivy-tree/ivy-tree.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/ivy-tree/ivy-tree.component.ts
@@ -4,6 +4,8 @@ const node1 = { id: 1, name: 'root 1' };
 const node11 = { id: 11, name: 'node 1.1' };
 const node111 = { id: 111, name: 'leaf 1.1.1' };
 const node12 = { id: 11, name: 'node 1.2' };
+const node2 = { id: 2, name: 'root 2' };
+const node21 = { id: 21, name: 'node 2.1' };
 
 @Component({
 	selector: 'sand-ivy-tree',
@@ -17,6 +19,20 @@ export class IvyTreeComponent {
 			{ value: node12, children: [] },
 		]
 	};
-	item = null;
+	options = [{
+		value: node1,
+		children: [
+			{ value: node11, children: [ { value: node111 } ] },
+			{ value: node12, children: [] },
+		]
+	}, {
+		value: node2,
+		children: [ { value: node21 }]
+	}];
+	item;
 	collection = [];
+
+	searchFn(o, c) {
+		return o.name.startsWith(c);
+	}
 }
