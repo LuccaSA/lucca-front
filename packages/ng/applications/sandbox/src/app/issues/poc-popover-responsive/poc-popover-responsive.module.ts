81a57365e91c83f3e3d14ae49b049ea4ff9dc61c
lucienbertin <me@lucienbertin.com>
2019-12-16 16:21:58 +0100

    sand - add select in poc responsive issue

--- packages/ng/applications/sandbox/src/app/issues/poc-popover-responsive/poc-popover-responsive.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/poc-popover-responsive/poc-popover-responsive.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PocPopoverResponsiveComponent } from './poc-popover-responsive.component';
 import { LuPopoverModule } from '@lucca-front/ng/popover';
+import { LuSelectModule } from '@lucca-front/ng/select';
+import { LuInputModule } from '@lucca-front/ng/input';
+import { LuOptionModule } from '@lucca-front/ng/option';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +16,10 @@ import { LuPopoverModule } from '@lucca-front/ng/popover';
 	],
 	imports: [
 		LuPopoverModule,
-
+		LuSelectModule,
+		LuInputModule,
+		LuOptionModule,
+		FormsModule,
 		RouterModule.forChild([
 			{ path: '', component: PocPopoverResponsiveComponent },
 		]),
