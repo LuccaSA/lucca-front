4ebddeb4727aeafd0e05ff5fa137e05acd836cde
lucien bertin <me@lucienbertin.com>
2020-05-20 12:11:19 +0200

    [ng] dropdown add dorpdown item directive

---
+++ packages/ng/libraries/dropdown/src/lib/item/dropdown-item.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, forwardRef } from '@angular/core';
+import { ALuDropdownItem } from './dropdown-item.model';
+
+@Directive({
+	selector: '[luDropdownItem]',
+	exportAs: 'LuDropdownItem',
+	providers: [
+		{
+			provide: ALuDropdownItem,
+			useExisting: forwardRef(() => LuDropdownItemDirective),
+			multi: true,
+		}
+	]
+})
+export class LuDropdownItemDirective extends ALuDropdownItem {}
