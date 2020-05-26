38210548b7bf98454041002737812718a9150f83
lucien bertin <me@lucienbertin.com>
2020-05-26 12:15:31 +0200

    [sand] fix all transpile issues

--- packages/ng/applications/sandbox/src/app/issues/date-input/date-input.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-input/date-input.module.ts
@@ -3,7 +3,8 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DateInputComponent } from './date-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LuDateModule, LuNativeDateModule } from '@lucca-front/ng/date';
+import { LuDateModule } from '@lucca-front/ng/date';
+import { ALuDateAdapter, LuNativeDateAdapter } from '@lucca-front/ng/core';
 
 import localeFr from '@angular/common/locales/fr';
 import localeGb from '@angular/common/locales/en-GB';
@@ -21,7 +22,6 @@ registerLocaleData(localeDe);
 	imports: [
 		FormsModule,
 		LuDateModule,
-		LuNativeDateModule,
 		CommonModule,
 		ReactiveFormsModule,
 		RouterModule.forChild([
@@ -33,6 +33,7 @@ registerLocaleData(localeDe);
 		// { provide: LOCALE_ID, useValue: 'de-DE' },
 		// { provide: LOCALE_ID, useValue: 'en-GB' },
 		{ provide: LOCALE_ID, useValue: 'en-US' },
+		{ provide: ALuDateAdapter, useClass: LuNativeDateAdapter },
 	]
 })
 export class DateInputModule {}
--- packages/ng/applications/sandbox/src/app/issues/date-utc/date-utc.module.ts
+++ packages/ng/applications/sandbox/src/app/issues/date-utc/date-utc.module.ts
@@ -2,7 +2,8 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { DateUtcComponent } from './date-utc.component';
-import { LuDateModule, LuNativeUTCDateModule, LuNativeDateModule } from '@lucca-front/ng/date';
+import { ALuDateAdapter, LuNativeDateAdapter, LU_NATIVE_DATE_ADAPTER_OPTIONS } from '@lucca-front/ng/core';
+import { LuDateModule } from '@lucca-front/ng/date';
 
 import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
@@ -18,8 +19,6 @@ registerLocaleData(localeGb);
 	],
 	imports: [
 		LuDateModule,
-		LuNativeDateModule,
-		// LuNativeUTCDateModule,
 		FormsModule,
 		RouterModule.forChild([
 			{ path: '', component: DateUtcComponent },
@@ -29,6 +28,8 @@ registerLocaleData(localeGb);
 		// { provide: LOCALE_ID, useValue: 'fr-FR' },
 		{ provide: LOCALE_ID, useValue: 'en-GB' },
 		// { provide: LOCALE_ID, useValue: 'en-US' },
+		{ provide: LU_NATIVE_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+		{ provide: ALuDateAdapter, useClass: LuNativeDateAdapter },
 	]
 })
 export class DateUtcModule {}
