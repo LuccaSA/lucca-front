80ecaec357ad5aa11349d83fdc26a38d2f77f58c
lucien bertin <me@lucienbertin.com>
2020-05-27 12:21:20 +0200

    [ng] fix build

--- packages/ng/libraries/option/src/lib/operator/index.ts
+++ packages/ng/libraries/option/src/lib/operator/index.ts
@@ -2,6 +2,7 @@ export * from './feeder/index';
 export * from './pager/index';
 export * from './searcher/index';
 export * from './for-options/index';
+export * from './placeholder/index';
 
 export * from './option-operator.module';
 export * from './option-operator.model';
