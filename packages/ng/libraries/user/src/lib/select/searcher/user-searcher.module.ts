871869f57237b8a8d49566f7340dc6c8aafba7f9
lucien bertin <me@lucienbertin.com>
2020-06-02 17:54:58 +0200

    fixed now it kinda works

--- packages/ng/libraries/user/src/lib/select/searcher/user-searcher.module.ts
+++ packages/ng/libraries/user/src/lib/select/searcher/user-searcher.module.ts
@@ -3,12 +3,14 @@ import { LuUserPagedSearcherComponent } from './user-searcher.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LuOptionPlaceholderModule } from '@lucca-front/ng/option';
 
 @NgModule({
 	imports: [
 		HttpClientModule,
 		ReactiveFormsModule,
 		CommonModule,
+		LuOptionPlaceholderModule,
 	],
 	declarations: [
 		LuUserPagedSearcherComponent,
