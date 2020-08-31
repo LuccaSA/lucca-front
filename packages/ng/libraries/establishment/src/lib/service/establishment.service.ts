35e9f6ad226dce6f933c9581975d177be5d2d190
lucien bertin <me@lucienbertin.com>
2020-08-31 14:56:39 +0200

    ng - ets service is generic v4 service with specific api

--- packages/ng/libraries/establishment/src/lib/service/establishment.service.ts
+++ packages/ng/libraries/establishment/src/lib/service/establishment.service.ts
@@ -1,41 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ALuEstablishmentService } from './establishment-service.model';
 import { ILuEstablishment } from '../establishment.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { LuApiV4Service } from '@lucca-front/ng/api';
 
 @Injectable()
-export class LuEstablishmentService extends ALuEstablishmentService<ILuEstablishment> {
-	constructor(protected _http: HttpClient) { super(); }
-
-	getAll(filters: string[] = []): Observable<ILuEstablishment[]> {
-		const url = [`/organization/structure/api/establishments`, ...filters.join('&')].join('?');
-
-		return this._http.get<{ items: ILuEstablishment[] }>(url).pipe(
-			map(res => res.items),
-		);
-	}
-	getPaged(page: number = 0, filters: string[] = []): Observable<ILuEstablishment[]> {
-		const url = [`/organization/structure/api/establishments`, [`page=${page + 1}`, ...filters].join('&')].join('?');
-		return this._http.get<{ items: ILuEstablishment[] }>(url).pipe(
-			map(res => res.items),
-		);
-	}
-	searchAll(clue: string = '', filters: string[] = []): Observable<ILuEstablishment[]> {
-		if (!clue) { return this.getAll(filters); }
-		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const url = [`/organization/structure/api/establishments`, [`search=${urlSafeClues}`, ...filters].join('&')].join('?');
-		return this._http.get<{ items: ILuEstablishment[] }>(url).pipe(
-			map(res => res.items),
-		);
-	}
-	searchPaged(clue: string = '', page: number = 0, filters: string[] = []): Observable<ILuEstablishment[]> {
-		if (!clue) { return this.getPaged(page, filters); }
-		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const url = [`/organization/structure/api/establishments`, [`search=${urlSafeClues}`, `page=${page + 1}`, ...filters].join('&')].join('?');
-		return this._http.get<{ items: ILuEstablishment[] }>(url).pipe(
-			map(res => res.items),
-		);
-	}
+export class LuEstablishmentService extends LuApiV4Service<ILuEstablishment> {
+	protected _api = `/organization/structure/api/establishments`;
+	constructor(protected _http: HttpClient) { super(_http); }
 }
