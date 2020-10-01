811fbf84a5fde13db6a591b1743346eaf89f12a6
lucien bertin <me@lucienbertin.com>
2020-10-01 16:44:11 +0200

    [ng] ets supports @input filters

--- packages/ng/libraries/establishment/src/lib/service/establishment.service.ts
+++ packages/ng/libraries/establishment/src/lib/service/establishment.service.ts
@@ -6,5 +6,6 @@ import { LuApiV4Service } from '@lucca-front/ng/api';
 @Injectable()
 export class LuEstablishmentService extends LuApiV4Service<ILuEstablishment> {
 	protected _api = `/organization/structure/api/establishments`;
+
 	constructor(protected _http: HttpClient) { super(_http); }
 }
