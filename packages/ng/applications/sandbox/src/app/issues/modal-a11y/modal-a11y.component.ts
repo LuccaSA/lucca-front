7f02e350e21df75671c0c3ee0cfaeb45a2e01c33
lucien bertin <me@lucienbertin.com>
2020-12-22 10:48:55 +0100

    sand - some testin

--- packages/ng/applications/sandbox/src/app/issues/modal-a11y/modal-a11y.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/modal-a11y/modal-a11y.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ILuModalConfig, LuModal } from '@lucca-front/ng/modal';
+import { LuModal } from '@lucca-front/ng/modal';
 import { LuSidepanel } from '@lucca-front/ng/sidepanel';
 const MAGIC_POSITIONS = [
 	'center',
@@ -18,11 +18,14 @@ const MAGIC_SIZES = [
 	template: `
 	<p>content of the modal component</p>
 	<button class="button" (click)="openModal()">more modals</button>
+	<button class="button" (click)="incr()">incr</button>
 	`
 })
 export class BasicModalContent {
 	title = 'title';
 	submitAction = () => true;
+	// submitPalette = 'warning';
+	submitCounter = 0;
 	constructor(
 		private _modal: LuModal,
 	) {}
@@ -34,6 +37,9 @@ export class BasicModalContent {
 		config.size = MAGIC_SIZES[randSize];
 
 		this._modal.open(BasicModalContent, undefined, config);
+	}
+	incr() {
+		this.submitCounter++;
 	}
 }
 @Component({
