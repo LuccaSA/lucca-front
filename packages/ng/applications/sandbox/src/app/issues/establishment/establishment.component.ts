87fe181d8180ad8036a5124baf0f5f3b8d78d982
Bastien G <bguillon@lucca.fr>
2021-02-11 15:26:02 +0100

    enh custom display demo

--- packages/ng/applications/sandbox/src/app/issues/establishment/establishment.component.ts
+++ packages/ng/applications/sandbox/src/app/issues/establishment/establishment.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ILuEstablishment } from '@lucca-front/ng/establishment';
-import { of } from 'rxjs';
+import { ILuLegalUnit } from 'libraries/establishment/src';
+import { Observable, zip } from 'rxjs';
+import { map, publishReplay, refCount } from 'rxjs/operators';
+
+interface ICountry {
+	id: number;
+	name: string;
+}
 
 @Component({
 	selector: 'lu-establishment-issue',
 	templateUrl: './establishment.component.html'
 })
-export class EstablishmentComponent {
+export class EstablishmentComponent implements OnInit {
 	collection = [];
 	item;
 	item2;
 
-	customDisplayer = (ets: ILuEstablishment) => of(`${ets.name} (<i>${ets.code}</i>)`);
+	countriesByLegalUnitId$: Observable<Map<number, ICountry>>;
+
+	constructor(private readonly httpClient: HttpClient) { }
+
+	ngOnInit() {
+		this.countriesByLegalUnitId$ = zip(
+			this.httpClient.get<{ items: ICountry[] }>('/organization/structure/api/countries'),
+			this.httpClient.get<{ items: ILuLegalUnit[] }>('/organization/structure/api/legal-units')
+		).pipe(
+			map(([countriesResponse, legalUnitsResponse]) => {
+				const result = new Map<number, ICountry>();
+				legalUnitsResponse.items.forEach(legalUnit => {
+					result.set(legalUnit.id, countriesResponse.items.find(e => e.id === legalUnit.countryId))
+				});
+				return result;
+			}),
+			publishReplay(1),
+			refCount()
+		);
+		this.countriesByLegalUnitId$.subscribe();
+	}
+
+	customDisplayer = (ets: ILuEstablishment): Observable<string> => {
+		return this.countriesByLegalUnitId$.pipe(
+			map(countriesByLegalUnitId => {
+				const country = countriesByLegalUnitId.get(ets.legalUnitId);
+				return country ?
+					`${ets.name} (<i>${country.name}</i>)` :
+					ets.name;
+			})
+		);
+	}
 
 	trackBy(idx, item): string {
 		return item.code;
--- packages/ng/libraries/establishment/src/lib/establishment.model.ts
+++ packages/ng/libraries/establishment/src/lib/establishment.model.ts
@@ -1,6 +1,8 @@
 import { ILuApiItem } from '@lucca-front/ng/api';
 
-export interface ILuLegalUnit extends ILuApiItem { }
+export interface ILuLegalUnit extends ILuApiItem {
+	countryId: number;
+}
 
 export interface ILuEstablishment extends ILuApiItem {
 	code: string;
