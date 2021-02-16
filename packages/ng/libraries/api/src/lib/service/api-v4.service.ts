e17609a15a8f39b186eb058501fad32e4cf180af
Bastien G <bguillon@lucca.fr>
2021-02-16 11:23:00 +0100

    simplify service

--- packages/ng/libraries/api/src/lib/service/api-v4.service.ts
+++ packages/ng/libraries/api/src/lib/service/api-v4.service.ts
@@ -1,60 +1,54 @@
-import { Injectable } from '@angular/core';
-import { ILuApiItem } from '../api.model';
-import { ALuApiService } from './api-service.model';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ILuApiItem } from '../api.model';
+import { ALuApiService } from './api-service.model';
 
 @Injectable()
 export class LuApiV4Service<T extends ILuApiItem = ILuApiItem> extends ALuApiService<T> {
 	protected _api: string;
 	set api(api: string) { this._api = api; }
+
 	protected _filters: string[] = [];
 	set filters(filters: string[]) { this._filters = filters || []; }
+
 	protected _sort: string;
-	set sort(sort: string) { this._sort = sort; }
+	set sort(sort: string) { this._sort = `sort=${sort}`; }
 
 	constructor(protected _http: HttpClient) { super(); }
 
 	getAll(filters: string[] = []): Observable<T[]> {
-		const query = [...this._filters, ...filters]
-		if (this._sort != null) {
-			query.push(`sort=${this._sort}`);
-		}
+		const query = [...this._filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
 		);
 	}
 	getPaged(page: number = 0, filters: string[] = []): Observable<T[]> {
-		const query = [`page=${page + 1}`, ...this._filters, ...filters]
-		if (this._sort != null) {
-			query.push(`sort=${this._sort}`);
-		}
+		const query = [`page=${page + 1}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
 		);
 	}
 	searchAll(clue: string = '', filters: string[] = []): Observable<T[]> {
-		if (!clue) { return this.getAll(filters); }
-		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const query = [`search=${urlSafeClues}`, ...this._filters, ...filters];
-		if (this._sort != null) {
-			query.push(`sort=${this._sort}`);
+		if (!clue) {
+			return this.getAll(filters);
 		}
+		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
+		const query = [`search=${urlSafeClues}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
 		);
 	}
 	searchPaged(clue: string = '', page: number = 0, filters: string[] = []): Observable<T[]> {
-		if (!clue) { return this.getPaged(page, filters); }
-		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const query = [`search=${urlSafeClues}`, `page=${page + 1}`, ...this._filters, ...filters];
-		if (this._sort != null) {
-			query.push(`sort=${this._sort}`);
+		if (!clue) {
+			return this.getPaged(page, filters);
 		}
+		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
+		const query = [`search=${urlSafeClues}`, `page=${page + 1}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
