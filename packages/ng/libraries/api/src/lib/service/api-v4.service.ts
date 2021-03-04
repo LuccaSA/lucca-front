dd54990aefe33a04c06ddcf2a04f1f15313c4832
lucien bertin <me@lucienbertin.com>
2021-03-04 17:45:32 +0100

    feat(ng-ets) ets service can filter with scope thingies

--- packages/ng/libraries/api/src/lib/service/api-v4.service.ts
+++ packages/ng/libraries/api/src/lib/service/api-v4.service.ts
@@ -12,6 +12,7 @@ export class LuApiV4Service<T extends ILuApiItem = ILuApiItem> extends ALuApiSer
 
 	protected _filters: string[] = [];
 	set filters(filters: string[]) { this._filters = filters || []; }
+	get filters(): string[] { return this._filters; }
 
 	protected _sort: string;
 	set sort(sort: string) { this._sort = `sort=${sort}`; }
@@ -19,14 +20,14 @@ export class LuApiV4Service<T extends ILuApiItem = ILuApiItem> extends ALuApiSer
 	constructor(protected _http: HttpClient) { super(); }
 
 	getAll(filters: string[] = []): Observable<T[]> {
-		const query = [...this._filters, ...filters, this._sort].filter(f => !!f);
+		const query = [...this.filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
 		);
 	}
 	getPaged(page: number = 0, filters: string[] = []): Observable<T[]> {
-		const query = [`page=${page + 1}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
+		const query = [`page=${page + 1}`, ...this.filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
@@ -37,7 +38,7 @@ export class LuApiV4Service<T extends ILuApiItem = ILuApiItem> extends ALuApiSer
 			return this.getAll(filters);
 		}
 		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const query = [`search=${urlSafeClues}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
+		const query = [`search=${urlSafeClues}`, ...this.filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
@@ -48,7 +49,7 @@ export class LuApiV4Service<T extends ILuApiItem = ILuApiItem> extends ALuApiSer
 			return this.getPaged(page, filters);
 		}
 		const urlSafeClues = clue.split(' ').map(c => encodeURIComponent(c));
-		const query = [`search=${urlSafeClues}`, `page=${page + 1}`, ...this._filters, ...filters, this._sort].filter(f => !!f);
+		const query = [`search=${urlSafeClues}`, `page=${page + 1}`, ...this.filters, ...filters, this._sort].filter(f => !!f);
 		const url = [this._api, query.join('&')].join('?');
 		return this._http.get<{ items: T[] }>(url).pipe(
 			map(res => res.items),
