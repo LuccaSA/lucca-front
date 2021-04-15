6cc7414bd2d99ab5d961487d1f5bb6df60e6cb16
lucien bertin <me@lucienbertin.com>
2021-04-15 11:10:43 +0200

    one story as a html file

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,3 +1,5 @@
+import * as palettes from '!html-loader!./button-palettes.html';
+
 export default {
   title: 'SCSS/Button',
   argTypes: {
@@ -9,11 +11,9 @@ export default {
   },
 };
 
-const Template = ({ label, mod, palette, state, size, ...args }) => {
-  return `
+const Template = ({ label, mod, palette, state, size, ...args }) => `
   <button type="button" class="button ${mod} ${palette} ${state} ${size}">${label}</button>
-`;
-};
+`
 
 export const Sandbox = Template.bind({});
 Sandbox.args = {
@@ -24,28 +24,12 @@ Sandbox.args = {
   size: '',
 };
 
-export const Basic = () => `
+export const Mods = () => `
 <button class="button">Basique</button>
 <button class="button mod-outline">Blanc</button>
 <button class="button mod-link">Lien</button>
 `
-
-export const Palettes = () => `
-<div>
-  <button class="button palette-primary">Primary</button>
-  <button class="button palette-secondary">Secondary</button>
-  <button class="button palette-success">Success</button>
-  <button class="button palette-warning">Warning</button>
-  <button class="button palette-error">Error</button>
-</div>
-<div>
-  <button class="button mod-outline palette-primary">Primary</button>
-  <button class="button mod-outline palette-secondary">Secondary</button>
-  <button class="button mod-outline palette-success">Success</button>
-  <button class="button mod-outline palette-warning">Warning</button>
-  <button class="button mod-outline palette-error">Error</button>
-</div>
-`
+export const Palettes = () => palettes;
 
 export const Sizes = () => `
 <button class="button mod-smaller">Smaller</button>
