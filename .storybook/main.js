6bdaeb5de8b93448c5b030cdcef3a25214edf3ce
lucien bertin <me@lucienbertin.com>
2021-04-21 15:04:11 +0200

    add bogus spa for configuring storybook > info => Using angular project 'storybook' for configuring Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-knobs',
   ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,8 +3,6 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "./documentation.json";
 setCompodocJson(docJson);
 
-import '!style-loader!css-loader!sass-loader!./styles.scss';
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
