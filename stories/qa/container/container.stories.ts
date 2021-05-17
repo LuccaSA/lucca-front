4aff7ae2390fe011610d9eeb43fd733e8c5ff0b2
Ingrid <30234402+RipripKipkip@users.noreply.github.com>
2021-05-17 10:20:26 +0200

    nolog(sb) add qa/container stories (#1315)

---
+++ stories/qa/container/container.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'container-stories',
+	templateUrl: './container.stories.html',
+}) class ContainerStory {}
+
+export default {
+  title: 'QA/Container',
+  component: ContainerStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [ContainerStory]
+		})
+	]
+} as Meta;
+
+const template: Story<ContainerStory> = () => ({});
+
+export const basic = template.bind({});
