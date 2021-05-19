f4ff2a82a1b5714be6ef7ac898d34cc8215316a9
Ingrid Huber <ihuber@lucca.fr>
2021-05-19 15:19:29 +0200

    [sb] add table story

---
+++ stories/qa/table/table.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'table-stories',
+	templateUrl: './table.stories.html',
+}) class TableStory {}
+
+export default {
+  title: 'QA/Table',
+  component: TableStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [TableStory]
+		})
+	]
+} as Meta;
+
+const template: Story<TableStory> = () => ({});
+
+export const basic = template.bind({});
