8674f3ff9bd688a1a370c5b1a3f0db06c81cbc2c
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2021-05-20 18:15:11 +0200

    stories appelées différement

--- stories/icons/icons-svg.stories.ts
+++ stories/icons/icons-svg.stories.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Story, Meta, moduleMetadata } from '@storybook/angular';
-import files from '../../packages/icons/svg/selection.json';
+import { boolean, color, text, withKnobs } from '@storybook/addon-knobs';
+import files from './selection.json';
+
+export default { title: 'Icons/SVG', decorators: [withKnobs] };
 
 @Component({
 	selector: 'icons-svg-list-stories',
@@ -9,54 +12,72 @@ import files from '../../packages/icons/svg/selection.json';
 	styleUrls: ['./icons-svg.stories.scss'],
 }) class IconsSVGListStory {
 
-	@Input() set filter(query: string){
+	@Input() set filter(query: string) {
 		this.icons = files.icons.filter(icon => !query || icon.properties.name.includes(query));
-	};
-	@Input() rounded: boolean;
-	@Input() fill: string;
-	@Input() background: string;
+	}
+	@Input() rounded = false;
+	@Input() fill = '#000';
+	@Input() background = '#FFF';
 
-	files = files;
-
-	icons = [];
+	icons = files.icons;
 }
 
-export default {
-	title: 'Icons/SVG',
-	component: IconsSVGListStory,
-	argTypes: {
-		filter: {
-			control: {
-				type: 'text',
-			}
-		},
-		rounded: {
-			control: {
-				type: 'boolean',
-			}
-		},
-		fill: {
-			control: {
-				type: 'color',
-			}
-		},
-		background: {
-			control: {
-				type: 'color',
-			}
-		},
+export const IconsSVG = () => ({
+	decorators: [],
+	template: `
+		<icons-svg-list-stories [rounded]="rounded" [fill]="fill" [background]="background" [filter]="filter"></icons-svg-list-stories>
+	`,
+	moduleMetadata: {
+	  declarations: [IconsSVGListStory],
+	  // imports: [BrowserModule],
 	},
-	decorators: [
-		moduleMetadata({
-			entryComponents: [IconsSVGListStory],
-			imports: [BrowserModule],
-		})
-	]
-} as Meta;
+	props: {
+		filter: text('Filter',''),
+		rounded: boolean('Rounded', false),
+		fill: color('Fill','#000'),
+		background: color('Background','#FFF'),
+	}
+  });
 
-const template: Story<IconsSVGListStory> = (args: IconsSVGListStory) => ({
-	props: args,
-});
+IconsSVG.storyName = 'List';
 
-export const list = template.bind({});
-list.args = { rounded: false, filter: '', fill: '#000', background: '#FFF' };
+// export default {
+// 	title: 'Icons/SVG',
+// 	template: '<icons-svg-list-stories></icons-svg-list-stories>',
+// 	argTypes: {
+// 		filter: {
+// 			control: {
+// 				type: 'text',
+// 			}
+// 		},
+// 		rounded: {
+// 			control: {
+// 				type: 'boolean',
+// 			}
+// 		},
+// 		fill: {
+// 			control: {
+// 				type: 'color',
+// 			}
+// 		},
+// 		background: {
+// 			control: {
+// 				type: 'color',
+// 			}
+// 		},
+// 	},
+// 	moduleMetadata: {
+// 		declarations: [IconsSVGListStory],
+// 		imports: [BrowserModule],
+// 	},
+// 	decorators: [
+	
+// 	]
+// } as Meta;
+
+// const template: Story<IconsSVGListStory> = (args: IconsSVGListStory) => ({
+// 	props: args,
+// });
+
+// export const list = template.bind({});
+// list.args = { rounded: false, filter: '', fill: '#000', background: '#FFF' };
