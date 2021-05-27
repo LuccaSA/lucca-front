5988942161efba389605a7d170327fa9fa4fb0ce
Ingrid Huber <ihuber@lucca.fr>
2021-05-27 17:43:16 +0200

    [sb] add typography story

---
+++ stories/qa/typography/typography.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'typography-stories',
+	templateUrl: './typography.stories.html',
+}) class TypographyStory {}
+
+export default {
+  title: 'QA/Typography',
+  component: TypographyStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [TypographyStory]
+		})
+	]
+} as Meta;
+
+const template: Story<TypographyStory> = () => ({});
+
+export const basic = template.bind({});
