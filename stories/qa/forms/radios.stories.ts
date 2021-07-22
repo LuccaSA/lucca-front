347dcd3f4cf5c683b55e4a8318ce550a4b5a936f
Ingrid Huber <ihuber@lucca.fr>
2021-07-22 10:45:34 +0200

    [sb] add forms - radios

---
+++ stories/qa/forms/radios.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'forms-radios-stories',
+	templateUrl: './radios.stories.html'
+}) class RadiosStory {}
+
+export default {
+  title: 'QA/Forms/Radios',
+  component: RadiosStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [RadiosStory]
+		})
+	]
+} as Meta;
+
+const template: Story<RadiosStory> = () => ({});
+
+export const basic = template.bind({});
