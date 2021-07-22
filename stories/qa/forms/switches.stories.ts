d94af56b93f7c6d54d757719789c7f1e4c48e447
Ingrid Huber <ihuber@lucca.fr>
2021-07-22 16:07:55 +0200

    [sb] add forms - switches

---
+++ stories/qa/forms/switches.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'forms-switches-stories',
+	templateUrl: './switches.stories.html'
+}) class SwitchesStory {}
+
+export default {
+  title: 'QA/Forms/Switches',
+  component: SwitchesStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [SwitchesStory]
+		})
+	]
+} as Meta;
+
+const template: Story<SwitchesStory> = () => ({});
+
+export const basic = template.bind({});
