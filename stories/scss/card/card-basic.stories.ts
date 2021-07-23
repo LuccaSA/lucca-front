8f0fba575e8350907c0ac8917da46304d876b483
MarcFairbrother <mfairbrother@lucca.fr>
2021-07-23 17:36:40 +0200

    FIX code formatting

--- stories/scss/card/card-basic.stories.ts
+++ stories/scss/card/card-basic.stories.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Story, Meta, moduleMetadata } from '@storybook/angular';
 
-
 @Component({
 	selector: 'card-basic-stories',
 	templateUrl: './card-basic.stories.html',
-}) class CardBasicStory {
+})
+class CardBasicStory {
 	@Input() appearance: object = [];
 	@Input() cardTitle: string = '';
 	@Input() cardContent: string = '';
@@ -23,26 +23,26 @@ export default {
 		appearance: {
 			control: {
 				type: 'check',
-				options: ['mod-grey', 'mod-elevated', 'is-disabled']
-			}
+				options: ['mod-grey', 'mod-elevated', 'is-disabled'],
+			},
 		},
 		hasFooter: {
 			control: {
-				type: 'boolean'
-			}
+				type: 'boolean',
+			},
 		},
 		isClickable: {
 			control: {
-				type: 'boolean'
-			}
-		}
+				type: 'boolean',
+			},
+		},
 	},
 	decorators: [
 		moduleMetadata({
 			entryComponents: [CardBasicStory],
 			imports: [BrowserModule],
-		})
-	]
+		}),
+	],
 } as Meta;
 
 const template: Story<CardBasicStory> = (args: CardBasicStory) => ({
@@ -50,12 +50,12 @@ const template: Story<CardBasicStory> = (args: CardBasicStory) => ({
 });
 
 export const basic = template.bind({});
-basic.args = { 
+basic.args = {
 	appearance: [],
-    cardTitle: 'My Card',
-    cardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dapibus commodo ornare. Proin eget iaculis lacus, a varius erat. Nulla facilisi. Sed eget scelerisque urna. Etiam eget sem accumsan, venenatis purus nec, dignissim ex.',
-    hasFooter: false, 
+	cardTitle: 'My Card',
+	cardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dapibus commodo ornare. Proin eget iaculis lacus, a varius erat. Nulla facilisi. Sed eget scelerisque urna. Etiam eget sem accumsan, venenatis purus nec, dignissim ex.',
+	hasFooter: false,
 	isClickable: false,
 	mainButton: 'Confirm',
-	secondaryButton: 'Cancel'
+	secondaryButton: 'Cancel',
 };
