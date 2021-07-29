d2414f9fe464b2569bb05bde42cb83e6512268be
Naïs Alcaraz <nsalcaraz@gmail.com>
2021-07-29 11:32:17 +0200

    nolog(sb) add chip on scss (#1413)

---
+++ stories/scss/chip/chip.stories.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+
+@Component({
+	selector: 'chip-stories',
+	templateUrl: './chip.stories.html',
+}) class ChipStory {
+	@Input() label: string;
+	@Input() mod: string = '';
+}
+
+
+export default {
+	title: 'SCSS/Chip',
+	component: ChipStory,
+	argTypes: {
+		mod: {
+			control: {
+				type: 'radio',
+				options: ['', 'mod-unkillable']
+			}
+		},
+	},
+	decorators: [
+		moduleMetadata({
+			entryComponents: [ChipStory],
+			imports: [BrowserModule],
+		})
+	]
+} as Meta;
+
+const template: Story<ChipStory> = (args: ChipStory) => ({
+	props: args,
+});
+
+export const def = template.bind({});
+def.args = { label: 'label', mod: ''};
+
