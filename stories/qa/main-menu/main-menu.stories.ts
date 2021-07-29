a4a02da0d382e6331603426fdbfb275b656b4bc9
AlexiaAlarza <77682210+AlexiaAlarza@users.noreply.github.com>
2021-07-29 16:52:33 +0200

    nolog(sb) add main-menu story (#1408)
    * nolog(sb) add main-menu story
    
    * Fix remove href on link

---
+++ stories/qa/main-menu/main-menu.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'main-menu-stories',
+	templateUrl: './main-menu.stories.html',
+}) class MainMenuStory {}
+
+export default {
+  title: 'QA/Main Menu',
+  component: MainMenuStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [MainMenuStory]
+		})
+	]
+} as Meta;
+
+const template: Story<MainMenuStory> = () => ({});
+
+export const basic = template.bind({});
