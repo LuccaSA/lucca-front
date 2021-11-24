abd92aa58dca89c75b62a9b1bf7b3228cd0c86fe
Marc Fairbrother <mfairbrother@lucca.fr>
2021-11-24 17:03:24 +0100

    ADD story

---
+++ stories/scss/form/switch/switch.stories.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+
+
+@Component({
+	selector: 'switch-stories',
+	templateUrl: './switch.stories.html',
+}) class SwitchStory {
+	@Input() isChecked: boolean = false;
+	@Input() isDisabled: boolean = false;
+	@Input() isSmall: boolean = false;
+	@Input() isInline: boolean = false;
+}
+
+export default {
+	title: 'SCSS/Form/Switches',
+	component: SwitchStory,
+	argTypes: {
+		isChecked: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		isDisabled: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		isSmall: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		isInline: {
+			control: {
+				type: 'boolean',
+			},
+		}
+	},
+	decorators: [
+		moduleMetadata({
+			entryComponents: [SwitchStory],
+			imports: [BrowserModule, FormsModule, CommonModule],
+		})
+	]
+} as Meta;
+
+const template: Story<SwitchStory> = (args: SwitchStory) => ({
+	props: args,
+});
+
+export const basic = template.bind({});
+basic.args = {
+	isChecked: false,
+	isDisabled: false,
+	isSmall: false,
+	isInline: false
+};
