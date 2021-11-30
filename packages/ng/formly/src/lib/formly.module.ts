58691b7e4f5eac30e7180582c3b6d271dc728213
Corentin <2089620+shajz@users.noreply.github.com>
2021-11-30 17:50:35 +0100

    FIX formly component not declared

--- packages/ng/formly/src/lib/formly.module.ts
+++ packages/ng/formly/src/lib/formly.module.ts
@@ -1,40 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { FormlyModule } from '@ngx-formly/core';
-import { LU_FORMLY_CONFIG } from './formly.config';
-import { LuDateModule } from '@lucca-front/ng/date';
-import { LuSelectModule } from '@lucca-front/ng/select';
-import { LuInputModule } from '@lucca-front/ng/input';
-import { LuUserModule } from '@lucca-front/ng/user';
-import { LuOptionModule } from '@lucca-front/ng/option';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LuApiModule } from '@lucca-front/ng/api';
+import { LuDateModule } from '@lucca-front/ng/date';
 import { LuDepartmentModule } from '@lucca-front/ng/department';
 import { LuEstablishmentModule } from '@lucca-front/ng/establishment';
+import { LuInputModule } from '@lucca-front/ng/input';
+import { LuOptionModule } from '@lucca-front/ng/option';
 import { LuQualificationModule } from '@lucca-front/ng/qualification';
-
+import { LuSelectModule } from '@lucca-front/ng/select';
+import { LuUserModule } from '@lucca-front/ng/user';
+import { FormlyModule } from '@ngx-formly/core';
+import { LU_FORMLY_CONFIG } from './formly.config';
 import {
-	LuFormlyFieldInput,
-	LuFormlyFieldDate,
-	LuFormlyFieldTextarea,
-	LuFormlyFieldSelect,
-	LuFormlyFieldUser,
-	LuFormlyFieldApi,
-	LuFormlyFieldDepartment,
-	LuFormlyFieldCheckbox,
-	LuFormlyFieldRadios,
-	LuFormlyFieldEstablishment,
+	LuFormlyFieldApi, LuFormlyFieldCheckbox, LuFormlyFieldDate, LuFormlyFieldDepartment, LuFormlyFieldEstablishment, LuFormlyFieldInput, LuFormlyFieldRadios, LuFormlyFieldSelect, LuFormlyFieldTextarea, LuFormlyFieldUser, LuFormlyFieldQualification
 } from './types/index';
 import {
-	LuFormlyWrapperHelper,
-	LuFormlyWrapperTextfieldLayout,
-	LuFormlyWrapperSuffix,
-	LuFormlyWrapperIcon,
-	LuFormlyWrapperError,
-	LuFormlyErrorMessage,
-	LuFormlyWrapperRadiosfieldLayout,
-	LuFormlyWrapperCheckboxLayout,
+	LuFormlyErrorMessage, LuFormlyWrapperCheckboxLayout, LuFormlyWrapperError, LuFormlyWrapperHelper, LuFormlyWrapperIcon, LuFormlyWrapperRadiosfieldLayout, LuFormlyWrapperSuffix, LuFormlyWrapperTextfieldLayout
 } from './wrappers/index';
+
 
 /** HACK to avoid a 'Function calls are not supported in decorators' error */
 export const LuFormlyChild = FormlyModule.forChild(LU_FORMLY_CONFIG);
@@ -51,6 +35,7 @@ export const LuFormlyChild = FormlyModule.forChild(LU_FORMLY_CONFIG);
 		LuFormlyFieldApi,
 		LuFormlyFieldDepartment,
 		LuFormlyFieldEstablishment,
+		LuFormlyFieldQualification,
 		LuFormlyFieldRadios,
 		LuFormlyFieldCheckbox,
 
@@ -77,7 +62,6 @@ export const LuFormlyChild = FormlyModule.forChild(LU_FORMLY_CONFIG);
 		LuQualificationModule,
 		LuDateModule,
 
-		// FormlyModule.forChild(LU_FORMLY_CONFIG),
 		LuFormlyChild,
 	],
 })
