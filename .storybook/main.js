5c6b5b6edbd8f5c43e49d0a86b0948ec5359c8d9
Corentin <2089620+shajz@users.noreply.github.com>
2021-12-02 11:09:05 +0100

    FIX storybook start, build pending

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,23 +3,23 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
     '@storybook/addon-knobs',
   ],
-  "core": {
-    "builder": "webpack5"
+  core: {
+    builder: 'webpack5',
   },
-  // related to https://github.com/webpack/webpack/issues/13691
-  // TODO remove when fixed by webpack
-  "webpackFinal": async (config) => {
-    config.cache = false;
+  angularOptions: {
+    enableIvy: true,
+  },
+  webpackFinal: async (config) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
         configFile: path.resolve(__dirname, '../tsconfig.json'),
@@ -27,4 +27,4 @@ module.exports = {
     ];
     return config;
   },
-}
+};
