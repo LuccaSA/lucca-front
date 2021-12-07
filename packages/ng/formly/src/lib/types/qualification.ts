702e14674c3ecd00e2ed6c3a405a7a16b96122f2
Corentin <2089620+shajz@users.noreply.github.com>
2021-12-07 08:56:27 +0100

    REF override

--- packages/ng/formly/src/lib/types/qualification.ts
+++ packages/ng/formly/src/lib/types/qualification.ts
@@ -8,7 +8,7 @@ import { FieldType } from '@ngx-formly/core';
 	templateUrl: './qualification.html'
 })
 export class LuFormlyFieldQualification extends FieldType {
-	readonly formControl: FormControl;
+	override readonly formControl: FormControl;
 	focus() {
 		this.to._isFocused = true;
 	}
