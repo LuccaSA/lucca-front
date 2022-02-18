8e0792d783dfe6d535c6638997738701ce63ae08
Bastien G <bguillon@lucca.fr>
2022-02-18 18:44:08 +0100

    enh accessibility & translate close btn

--- packages/ng/modal/src/lib/modal.translate.ts
+++ packages/ng/modal/src/lib/modal.translate.ts
@@ -3,23 +3,28 @@ import { ILuTranslation } from '@lucca-front/ng/core';
 export interface ILuModalLabel {
 	submit: string;
 	cancel: string;
+	close: string;
 }
 export abstract class ALuModalLabel {
 	submit: string;
 	cancel: string;
+	close: string;
 }
 
 export const luModalTranslations: ILuTranslation<ILuModalLabel> = {
 	en: {
 		submit: 'Ok',
 		cancel: 'Cancel',
+		close: 'Close',
 	},
 	fr: {
 		submit: 'Ok',
 		cancel: 'Annuler',
+		close: 'Fermer',
 	},
 	es: {
 		submit: 'Ok',
 		cancel: 'Cancelar',
+		close: 'Cerrar',
 	},
 };
--- packages/ng/sidepanel/src/lib/sidepanel.translate.ts
+++ packages/ng/sidepanel/src/lib/sidepanel.translate.ts
@@ -3,23 +3,28 @@ import { ILuTranslation } from '@lucca-front/ng/core';
 export interface ILuSidepanelLabel {
 	submit: string;
 	cancel: string;
+	close: string;
 }
 export abstract class ALuSidepanelLabel {
 	submit: string;
 	cancel: string;
+	close: string;
 }
 
 export const luSidepanelTranslations: ILuTranslation<ILuSidepanelLabel> = {
 	en: {
 		submit: 'Ok',
 		cancel: 'Cancel',
+		close: 'Close',
 	},
 	fr: {
 		submit: 'Ok',
 		cancel: 'Annuler',
+		close: 'Fermer',
 	},
 	es: {
 		submit: 'Ok',
 		cancel: 'Cancelar',
+		close: 'Cerrar',
 	},
 };
