ba1c7e00e3bebac48413352b422fd875250a5e42
Guillaume Nury <nury.guillaume@gmail.com>
2022-02-24 14:02:17 +0100

    chore: cleanup storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,17 @@
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
-    '@storybook/addon-knobs',
   ],
   core: {
     builder: 'webpack5',
   },
   angularOptions: {
     enableIvy: true,
-  },
-  webpackFinal: async (config) => {
-    config.resolve.plugins = [
-      new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, '../tsconfig.json'),
-      }),
-    ];
-    return config;
   },
 };
