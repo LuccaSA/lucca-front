9cd0ef89c578446fded2a2bb59f3b4139dfb191e
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2022-02-25 17:22:22 +0100

    rwd for buttons group

--- stories/scss/button/button.stories.ts
+++ stories/scss/button/button.stories.ts
@@ -1,63 +1,64 @@
 import { Component, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 @Component({
-	selector: 'button-basic-stories',
-	templateUrl: './button-basic.stories.html',
-}) class ButtonBasicStory {
+	selector: 'button-stories',
+	templateUrl: './button.stories.html',
+	styleUrls: ['./button-stories.scss'],
+})
+class ButtonStory {
 	@Input() label: string;
 	@Input() mod: string = '';
 	@Input() palette: string = '';
 	@Input() state: string = '';
 	@Input() size: string = '';
+	@Input() group: boolean = false;
 	@Input() disabled: boolean = false;
-	// @Input() more: boolean = false; // lucca-icons needed
+	@Input() noFlexWrap: boolean = false;
+	@Input() more: boolean = false;
 }
 
 export default {
-	title: 'SCSS/Button/Basic',
-	component: ButtonBasicStory,
+	title: 'SCSS/Button',
+	component: ButtonStory,
 	argTypes: {
 		mod: {
-			options: ['', ' mod-outline', 'mod-link', 'mod-link mod-invert'],
+			options: ['', 'mod-outlined', 'mod-link', 'mod-link mod-invert'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 		palette: {
 			options: ['', 'palette-primary', 'palette-secondary', 'palette-grey', 'palette-success', 'palette-warning', 'palette-error'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 		state: {
 			options: ['', 'is-loading', 'is-error', 'is-success', 'is-disabled'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 		size: {
-			options: ['', 'mod-smaller', 'mod-small'],
+			options: ['', 'mod-small', 'mod-smaller'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
-		
 	},
 	decorators: [
 		moduleMetadata({
-			entryComponents: [ButtonBasicStory],
+			entryComponents: [ButtonStory],
 			imports: [BrowserModule],
-		})
-	]
+		}),
+	],
 } as Meta;
 
-const template: Story<ButtonBasicStory> = (args: ButtonBasicStory) => ({
+const template: Story<ButtonStory> = (args: ButtonStory) => ({
 	props: args,
 });
 
 export const def = template.bind({});
-def.args = { label: 'label', mod: '', size: '', state: '', palette: '', disabled: false };
-
+def.args = { label: 'label', mod: '', size: '', state: '', palette: '', disabled: false, group: false, noFlexWrap: false, more: false };
