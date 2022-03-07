abb94c4557a93e5a697e5785fd8ed58cca6fdb77
Jérémie Biron <jbiron@lucca.fr>
2022-03-07 18:16:22 +0100

    add box stories

--- stories/scss/box/box-toggle.stories.ts
+++ stories/scss/box/box-toggle.stories.ts
@@ -1,11 +1,11 @@
 import { Meta, Story } from '@storybook/angular';
 
-interface BoxBasicStory {
+interface BoxToggleStory {
 	mod: string;
 }
 
 export default {
-	title: 'SCSS/Box/Basic',
+	title: 'SCSS/Box/Toggle',
 	argTypes: {
 		mod: {
 			options: ['', 'mod-grey'],
@@ -16,17 +16,21 @@ export default {
 	},
 } as Meta;
 
-function getTemplate(args: BoxBasicStory): string {
+function getTemplate(args: BoxToggleStory): string {
 	const classes = [args.mod].filter(Boolean).join(' ');
 
 	return `
+	<div class="switch">
+	    <input class="switch-input" type="checkbox" id="boxSwitch" checked disabled>
+	    <label class="switch-label" for="boxSwitch">Switch</label>
+	</div>
 	<div class="box ${classes}">
 	    Jujubes toppin gvueoat cake cake lemon drops chupa chups sweet roll. Macaroon icing tootsie roll bonbon dragée carrot cake sweet roll. Pie gingerbread jelly beans cotton candy tart lollipop bonbon candy. Bonbon chocolate gingerbread pastry.
 	</div>
 	`
 }
 
-const Template: Story<BoxBasicStory> = (args: BoxBasicStory) => ({
+const Template: Story<BoxToggleStory> = (args: BoxToggleStory) => ({
 	props: args,
 	template: getTemplate(args),
 	styles: [`
@@ -39,5 +43,5 @@ const Template: Story<BoxBasicStory> = (args: BoxBasicStory) => ({
 	],
 });
 
-export const Basic = Template.bind({});
-Basic.args = { mod: '' };
+export const Toggle = Template.bind({});
+Toggle.args = { mod: '' };
