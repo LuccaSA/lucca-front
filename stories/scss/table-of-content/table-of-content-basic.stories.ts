08b6181dd2c42cda1ef5d905f301c835f74d90b3
Jérémie Biron <jbiron@lucca.fr>
2022-03-10 18:09:41 +0100

    table of content stories

---
+++ stories/scss/table-of-content/table-of-content-basic.stories.ts
@@ -0,0 +1,45 @@
+import { Meta, Story } from '@storybook/angular';
+
+interface TableOfContentBasicStory {
+	mod: string;
+}
+
+export default {
+	title: 'SCSS/TableOfContent/Basic',
+	argTypes: {
+		mod: {
+			options: ['', 'mod-grey'],
+			control: {
+				type: 'radio',
+			}
+		},
+	},
+} as Meta;
+
+function getTemplate(args: TableOfContentBasicStory): string {
+	const classes = [args.mod].filter(Boolean).join(' ');
+	return `
+	<nav class="tableOfContent ${classes}">
+		<a class="tableOfContent-item is-active">Section 1</a>
+		<a class="tableOfContent-item">Section 2</a>
+		<a class="tableOfContent-item">Section 3</a>
+		<a class="tableOfContent-item">Section 4</a>
+	</nav>
+	`
+}
+
+const Template: Story<TableOfContentBasicStory> = (args: TableOfContentBasicStory) => ({
+	props: args,
+	template: getTemplate(args),
+	styles: [`
+		:host {
+			display: block;
+		}`,
+		args.mod === ''
+			? ':host { background-color: #F3F5FC; margin: -30px -20px; padding: 30px 20px; }'
+			: ''
+	],
+});
+
+export const Basic = Template.bind({});
+Basic.args = { mod: '' };
