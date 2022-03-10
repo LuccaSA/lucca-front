b826909a20b45df3c1ee52fea86e3130d3abd2a9
Jérémie Biron <jbiron@lucca.fr>
2022-03-10 16:01:13 +0100

    timelines stories

---
+++ stories/scss/timelines/timelines-basic.stories.ts
@@ -0,0 +1,72 @@
+import { Meta, Story } from '@storybook/angular';
+
+interface TimelinesBasicStory {
+	state: string;
+	number: boolean;
+}
+
+export default {
+	title: 'SCSS/Timelines/Basic',
+	argTypes: {
+		state: {
+			options: ['', 'u-textWarning', 'u-textSuccess'],
+			control: {
+				type: 'radio',
+			}
+		},
+		number: {
+			control: {
+				type: 'boolean',
+			}
+		},
+	},
+} as Meta;
+
+function getTemplate(args: TimelinesBasicStory): string {
+	const classes = [args.state].filter(Boolean).join(' ');
+	const number = args.number ? `mod-number` : '';
+	return `
+	<ol class="timeline ${classes} ${number}">
+		<li class="timeline-step">
+			<span class="timeline-step-title">
+				<a href="#" class="timeline-step-title-action">Previous step</a>
+			</span>
+			<div class="gauge mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+		</li>
+		<li class="timeline-step" aria-current="step">
+			<span class="timeline-step-title">
+				Current step
+			</span>
+			<div class="gauge mod-thin">
+				<div class="gauge-bar" style="width: 50%"></div>
+			</div>
+		</li>
+		<li class="timeline-step">
+			<span class="timeline-step-title">
+				Next step
+			</span>
+			<div class="gauge mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+		</li>
+		<li class="timeline-step">
+			<span class="timeline-step-title">
+				Final step
+			</span>
+			<div class="gauge mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+		</li>
+	</ol>
+	`
+}
+
+const Template: Story<TimelinesBasicStory> = (args: TimelinesBasicStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Basic = Template.bind({});
+Basic.args = { state: '', number: false; };
---
+++ stories/scss/timelines/timelines-vertical.stories.ts
@@ -0,0 +1,69 @@
+import { Meta, Story } from '@storybook/angular';
+
+interface TimelinesVerticalStory {
+	number: boolean;
+}
+
+export default {
+	title: 'SCSS/Timelines/Vertical',
+	argTypes: {
+		number: {
+			control: {
+				type: 'boolean',
+			}
+		},
+	},
+} as Meta;
+
+function getTemplate(args: TimelinesVerticalStory): string {
+	const classes = [args.state].filter(Boolean).join(' ');
+	const number = args.number ? `mod-number mod-smallNumber` : '';
+	return `
+	<section class="timeline mod-vertical ${number}">
+		<div class="timeline-step">
+			<h2 class="timeline-step-title">
+				<a href="#" class="timeline-step-title-action">Previous step</a>
+			</h2>
+			<div class="gauge mod-vertical mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+			…
+		</div>
+		<div class="timeline-step" aria-current="step">
+			<h2 class="timeline-step-title">
+				Current step
+			</h2>
+			<div class="gauge mod-vertical mod-thin">
+				<div class="gauge-bar" style="height: 50%"></div>
+			</div>
+			…
+		</div>
+		<div class="timeline-step">
+			<h2 class="timeline-step-title">
+				Next step
+			</h2>
+			<div class="gauge mod-vertical mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+			…
+		</div>
+		<div class="timeline-step">
+			<h2 class="timeline-step-title">
+				Last step
+			</h2>
+			<div class="gauge mod-vertical mod-thin">
+				<div class="gauge-bar"></div>
+			</div>
+			…
+		</div>
+	</section>
+	`
+}
+
+const Template: Story<TimelinesVerticalStory> = (args: TimelinesVerticalStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Vertical = Template.bind({});
+Vertical.args = {number: false};
