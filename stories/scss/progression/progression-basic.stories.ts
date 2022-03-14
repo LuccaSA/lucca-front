8f330cbccb6e2a7990ed3c0cc09d44c3e6a61302
Jérémie Biron <jbiron@lucca.fr>
2022-03-14 15:52:50 +0100

    progression

---
+++ stories/scss/progression/progression-basic.stories.ts
@@ -0,0 +1,41 @@
+import { Meta, Story } from '@storybook/angular';
+
+interface ProgressionBasicStory {
+	status: string;
+	indeterminate: boolean;
+}
+
+export default {
+	title: 'SCSS/Progression/Basic',
+	argTypes: {
+		status: {
+			options: ['', 'is-success', 'is-error'],
+			control: {
+				type: 'radio',
+			}
+		},
+		indeterminate: {
+			control: {
+				type: 'boolean',
+			}
+		},
+	},
+} as Meta;
+
+function getTemplate(args: ProgressionBasicStory): string {
+	const classes = [args.status].filter(Boolean).join(' ');
+	const indeterminate = args.indeterminate ? `mod-indeterminate` : '';
+	return `
+	<div class="progress ${classes} ${indeterminate}">
+		<div class="progress-bar" style="width: 50%"></div>
+	</div>
+	`
+}
+
+const Template: Story<ProgressionBasicStory> = (args: ProgressionBasicStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Basic = Template.bind({});
+Basic.args = { status: '', indeterminate: false };
