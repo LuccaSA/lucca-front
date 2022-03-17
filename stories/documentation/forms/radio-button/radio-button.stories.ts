26860b7526d64da0cb62c048a5e29b90fd7767fc
Jérémie Biron <jbiron@lucca.fr>
2022-03-17 11:46:35 +0100

    radio buttons stories

--- stories/documentation/forms/radio-button/radio-button.stories.ts
+++ stories/documentation/forms/radio-button/radio-button.stories.ts
@@ -1,53 +1,59 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular';
 
-
-@Component({
-	selector: 'radioButton-stories',
-	templateUrl: './radio-button.stories.html',
-}) class RadioButtonStory {
-	@Input() buttonsCount: number;
-	@Input() indexDisabled: number;
-	@Input() indexChecked: number;
-	@Input() palette: string = '';
-	@Input() isModSmall: boolean;
-	get buttonsArray() {
-		return Array(this.buttonsCount).keys();
-	}
+interface RadiosButtonsBasicStory {
+	small: boolean;
+	disabled: boolean;
+	palette: string;
 }
 
 export default {
-	title: 'Documentation/Forms/radioButtons',
-	component: RadioButtonStory,
+	title: 'Documentation/Forms/Radio Buttons/Basic',
 	argTypes: {
 		palette: {
 			options: ['', 'palette-primary', 'palette-secondary', 'palette-grey', 'palette-success', 'palette-warning', 'palette-error'],
 			control: {
 				type: 'radio',
 			}
-		}
+		},
+		small: {
+			control: {
+				type: 'boolean',
+			}
+		},
+		disabled: {
+			control: {
+				type: 'boolean',
+			}
+		},
 	},
-	decorators: [
-		moduleMetadata({
-			entryComponents: [RadioButtonStory],
-			imports: [BrowserModule, FormsModule, CommonModule],
-		})
-	]
 } as Meta;
 
+function getTemplate(args: RadiosButtonsBasicStory): string {
+	const classes = [args.palette].filter(Boolean).join(' ');
+	const small = args.small ? `mod-small` : '';
+	const disabled = args.disabled ? `disabled` : '';
+	return `
+	<div class="radioButtons ${classes} ${small}">
+		<label class="radioButtons-item">
+			<input type="radio" name="radioButtonsID" class="radioButtons-item-input" checked>
+			<span class="radioButtons-item-label">Bouton 1</span>
+		</label>
+		<label class="radioButtons-item">
+			<input type="radio" name="radioButtonsID" class="radioButtons-item-input">
+			<span class="radioButtons-item-label">Bouton 2</span>
+		</label>
+		<label class="radioButtons-item">
+			<input type="radio" name="radioButtonsID" class="radioButtons-item-input" ${disabled}>
+			<span class="radioButtons-item-label">Bouton 3</span>
+		</label>
+	</div>
+	`
+}
 
-const template: Story<RadioButtonStory> = (args: RadioButtonStory) => ({
+const Template: Story<RadiosButtonsBasicStory> = (args: RadiosButtonsBasicStory) => ({
 	props: args,
+	template: getTemplate(args),
 });
 
-export const basic = template.bind({});
-basic.args = {
-	buttonsCount: 3,
-	indexChecked: 0,
-	indexDisabled: 0,
-	palette: '',
-	isModSmall: false
-};
+export const Basic = Template.bind({});
+Basic.args = { palette: '', small: false, disabled: false };
