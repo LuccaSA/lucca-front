699733b34e8cfbf89f62ebc2284a339e903b9bfe
Naïs Alcaraz <naisalcaraz@MacBook-Pro-de-Nais.local>
2022-03-18 16:54:38 +0100

    End of work

--- stories/documentation/overlays/popover/popover.stories.ts
+++ stories/documentation/overlays/popover/popover.stories.ts
@@ -1,23 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LuPopoverModule } from '@lucca-front/ng/popover';
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 @Component({
 	selector: 'popover-stories',
 	template: `
-<button class="button mod-outline">don't click me</button>
-<button class="button"
-	[luPopover]="popover"
-	[luPopoverPosition]="position"
-	[luPopoverAlignment]="alignement"
-	[luPopoverTrigger]="trigger"
->{{trigger}} me</button>
-<button class="button mod-outline">don't click me either</button>
-<lu-popover #popover>{{popoverContent}}</lu-popover>
-`,
-}) class PopoverStory {
+		<button type="button" class="button mod-outline">don't click me</button>
+		<button type="button" class="button" [luPopover]="popover" [luPopoverPosition]="position" [luPopoverAlignment]="alignement" [luPopoverTrigger]="trigger">{{ trigger }} me</button>
+		<button type="button" class="button mod-outline">don't click me either</button>
+		<lu-popover #popover>{{ popoverContent }}</lu-popover>
+	`,
+})
+class PopoverStory {
 	@Input() popoverContent: string;
 	@Input() position: string;
 	@Input() alignment: string;
@@ -25,41 +20,38 @@ import { Story, Meta, moduleMetadata } from '@storybook/angular';
 }
 
 export default {
-  title: 'Documentation/Overlays/Popover',
-  component: PopoverStory,
+	title: 'Documentation/Overlays/Popover',
+	component: PopoverStory,
 	argTypes: {
 		position: {
 			options: ['above', 'below', 'before', 'after'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 		alignment: {
 			options: ['top', 'bottom', 'left', 'right', 'center'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 		trigger: {
 			options: ['none', 'hover', 'focus', 'click'],
 			control: {
 				type: 'radio',
-			}
+			},
 		},
 	},
 	decorators: [
 		moduleMetadata({
 			entryComponents: [PopoverStory],
-			imports: [
-				LuPopoverModule,
-				BrowserAnimationsModule,
-			]
-		})
-	]
+			imports: [LuPopoverModule, BrowserAnimationsModule],
+		}),
+	],
 } as Meta;
 
 const template: Story<PopoverStory> = (args: PopoverStory) => ({
-  props: args,
+	props: args,
 });
 
 export const basic = template.bind({});
@@ -68,7 +60,7 @@ basic.args = {
 	position: 'below',
 	alignment: 'center',
 	trigger: 'click',
-}
+};
 
 export const hover = template.bind({});
 hover.args = {
@@ -76,7 +68,7 @@ hover.args = {
 	position: 'below',
 	alignment: 'center',
 	trigger: 'hover',
-}
+};
 
 export const focus = template.bind({});
 focus.args = {
@@ -84,4 +76,4 @@ focus.args = {
 	position: 'below',
 	alignment: 'center',
 	trigger: 'focus',
-}
+};
