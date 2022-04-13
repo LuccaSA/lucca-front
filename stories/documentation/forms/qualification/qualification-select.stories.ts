b3bbdb749cf9d4e6c1efbd370066ee2d886e835f
Guillaume Nury <nury.guillaume@gmail.com>
2022-04-13 17:53:23 +0200

    feat(ng): qualification select for zero height

--- stories/documentation/forms/qualification/qualification-select.stories.ts
+++ stories/documentation/forms/qualification/qualification-select.stories.ts
@@ -1,48 +1,79 @@
 import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LuApiSelectModule } from '@lucca-front/ng/api';
-import { LuQualificationSelectModule } from '@lucca-front/ng/qualification';
-import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { LuQualificationSelectInputComponent, LuQualificationSelectModule } from '@lucca-front/ng/qualification';
+import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
 
 @Component({
 	selector: 'qualification-picker-stories',
 	template: `
-<section class="section">
-	<label class="textfield">
-		<lu-qualification-select placeholder="Select a qualification" class="textfield-input" ></lu-qualification-select>
-		<span class="textfield-label">Qualification</span>
-	</label>
-	<br/>
-	<label class="textfield">
-		<lu-qualification-select placeholder="Select a qualification" [multiple]="true" class="textfield-input" ></lu-qualification-select>
-		<span class="textfield-label">Qualifications</span>
-	</label>
-</section>
+	<section class="section">
+		<label class="textfield">
+			<lu-qualification-select placeholder="Select a qualification" class="textfield-input" ></lu-qualification-select>
+			<span class="textfield-label">Qualification</span>
+		</label>
+		<br/>
+		<label class="textfield">
+			<lu-qualification-select placeholder="Select a qualification" [multiple]="true" class="textfield-input" ></lu-qualification-select>
+			<span class="textfield-label">Qualifications</span>
+		</label>
+	</section>
 `,
 })
 class QualificationSelectStory { }
 
 export default {
 	title: 'Documentation/Forms/QualificationPicker',
-	component: QualificationSelectStory,
-	argTypes: {
-	},
+	component: LuQualificationSelectInputComponent,
 	decorators: [
+		componentWrapperDecorator(QualificationSelectStory),
 		moduleMetadata({
-			entryComponents: [QualificationSelectStory],
+			declarations: [QualificationSelectStory],
 			imports: [
 				LuQualificationSelectModule,
-				LuApiSelectModule,
 				BrowserAnimationsModule,
 			]
 		})
 	]
 } as Meta;
 
-const template: Story<QualificationSelectStory> = (args: QualificationSelectStory) => ({
-	props: args,
+const template: Story<QualificationSelectStory> = props => ({
+	props,
 });
 
-export const basic = template.bind({});
-basic.args = {
+export const Basic = template.bind({});
+Basic.parameters = {
+	// Disable controls as they are not modifiable because of ComponentWrapper
+	controls: { include: [] },
+	docs: {
+		source: {
+			language: 'ts',
+			code: `
+/* 1. Importer LuQualificationSelectModule */
+import { LuQualificationSelectModule } from '@lucca-front/ng/api';
+
+@NgModule({
+	imports: [LuQualificationSelectModule]
+})
+class QualificationSelectStoriesModule {}
+
+/* 2. Use it */
+@Component({
+	selector: 'qualification-select-story',
+	template: \`
+		<section class="section">
+			<label class="textfield">
+				<lu-qualification-select placeholder="Select a qualification" class="textfield-input" ></lu-qualification-select>
+				<span class="textfield-label">Qualification</span>
+			</label>
+			<br/>
+			<label class="textfield">
+				<lu-qualification-select placeholder="Select a qualification" [multiple]="true" class="textfield-input" ></lu-qualification-select>
+				<span class="textfield-label">Qualifications</span>
+			</label>
+		</section>
+	\`
+})
+class QualificationSelectStory { }`
+		}
+	}
 }
