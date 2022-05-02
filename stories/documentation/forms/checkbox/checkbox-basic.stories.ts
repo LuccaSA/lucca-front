5e8a7fe2fcafa044da2bbd8191b29acce3fa6408
Jeremie <jbiron@lucca.fr>
2022-05-02 14:32:18 +0200

    [DS to LF] Checkbox (#1611)
    * ds to lf checkbox
    
    * white variable
    
    * active box shadow

--- stories/documentation/forms/checkbox/checkbox-basic.stories.ts
+++ stories/documentation/forms/checkbox/checkbox-basic.stories.ts
@@ -3,20 +3,14 @@ import { Meta, Story } from '@storybook/angular';
 interface CheckboxBasicStory {
 	row: boolean;
 	disabled: boolean;
-	palette: string;
 	size: string;
 	required: boolean;
+	grey: boolean;
 }
 
 export default {
 	title: 'Documentation/Forms/Checkboxes/Basic',
 	argTypes: {
-		palette: {
-			options: ['', 'palette-primary', 'palette-secondary', 'palette-grey', 'palette-success', 'palette-warning', 'palette-error'],
-			control: {
-				type: 'radio',
-			}
-		},
 		size: {
 			options: ['', 'mod-small', 'mod-big'],
 			control: {
@@ -38,12 +32,18 @@ export default {
 				type: 'boolean',
 			}
 		},
+		grey: {
+			control: {
+				type: 'boolean',
+			}
+		},
 	},
 } as Meta;
 
 function getTemplate(args: CheckboxBasicStory): string {
-	const classes = [args.size, args.palette].filter(Boolean).join(' ');
+	const classes = [args.size].filter(Boolean).join(' ');
 	const row = args.row ? `mod-row` : '';
+	const grey = args.grey ? `mod-grey` : '';
 	const disabled = args.disabled ? `disabled` : '';
 	const required = args.required ? `aria-required="true"` : '';
 	return `
@@ -51,13 +51,13 @@ function getTemplate(args: CheckboxBasicStory): string {
 		<legend class="checkboxesfield-label">Liste de checkboxes</legend>
 		<div class="checkboxesfield-input ${row}">
 			<div>
-				<label class="checkbox ${classes}">
+				<label class="checkbox ${classes} ${grey}">
 					<input class="checkbox-input" type="checkbox" name="checkboxList1" ${disabled} ${required} checked>
 					<span class="checkbox-label">checkbox</span>
 				</label>
 			</div>
 			<div>
-				<label class="checkbox ${classes}">
+				<label class="checkbox ${classes} ${grey}">
 					<input class="checkbox-input" type="checkbox" name="checkboxList1" ${disabled} ${required}>
 					<span class="checkbox-label">checkbox</span>
 				</label>
@@ -73,4 +73,4 @@ const Template: Story<CheckboxBasicStory> = (args: CheckboxBasicStory) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = { palette: '', size: '', row: false, disabled: false, required: false };
+Basic.args = { grey: false, size: '', row: false, disabled: false, required: false };
