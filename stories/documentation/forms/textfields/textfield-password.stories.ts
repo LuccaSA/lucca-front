65bbbe88e288672e99d7fb705aa06fe953385855
Jérémie Biron <jbiron@lucca.fr>
2022-05-13 12:12:06 +0200

    password story

--- stories/documentation/forms/textfields/textfield-password.stories.ts
+++ stories/documentation/forms/textfields/textfield-password.stories.ts
@@ -11,29 +11,52 @@ export default {
 
 function getTemplate(args: TextfieldPasswordStory): string {
 	return `
-	<label class="textfield mod-password">
-		<input class="textfield-input" type="password" placeholder="placeholder" value="azerty12345#@!%;" />
-		<span class="textfield-label">Password label</span>
-		<span class="textfield-suffix">
-			<a
-				href="#"
-				onClick="event.preventDefault()"
-				role="button"
-				aria-pressed="false"
-				class="textfield-suffix-action"
-			>
-				<span aria-hidden="true" class="lucca-icon icon-watch"></span>
-				<span aria-hidden="true" class="lucca-icon icon-unwatch"></span>
-				<span class="u-mask">Show password</span>
-			</a>
-			</span>
-	</label>
+		<label class="textfield mod-password">
+			<input class="textfield-input" type="password" placeholder="placeholder" value="azerty12345#@!%;" />
+			<span class="textfield-label">Password label</span>
+			<span class="textfield-suffix">
+				<a
+					href="#"
+					onClick="event.preventDefault()"
+					role="button"
+					aria-pressed="false"
+					class="textfield-suffix-action"
+				>
+				  <span aria-hidden="true" class="lucca-icon icon-watch"></span>
+				  <span aria-hidden="true" class="lucca-icon icon-unwatch"></span>
+				  <span class="u-mask">Show password</span>
+				</a>
+			  </span>
+		</label>
+		<label class="textfield mod-password">
+			<input class="textfield-input" type="text" placeholder="placeholder" value="azerty12345#@!%;" />
+			<span class="textfield-label">Password label</span>
+			<span class="textfield-suffix">
+				<a
+				  href="#"
+				  onClick="event.preventDefault()"
+				  role="button"
+				  aria-pressed="true"
+				  class="textfield-suffix-action"
+				>
+				  <span aria-hidden="true" class="lucca-icon icon-watch"></span>
+				  <span aria-hidden="true" class="lucca-icon icon-unwatch"></span>
+				  <span class="u-mask">Show password</span>
+				</a>
+			  </span>
+		</label>
 	`
 }
 
 const Template: Story<TextfieldPasswordStory> = (args: TextfieldPasswordStory) => ({
 	props: args,
 	template: getTemplate(args),
+	styles: [`
+		.textfield {
+			float: left;
+			margin-right: 1rem;
+		}`,
+	]
 });
 
 export const Password = Template.bind({});
