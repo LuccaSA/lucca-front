f6f346786ba54f2d10a05106a9c37f3111bad0f2
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-02 10:33:12 +0200

    fix(schematics): lint issue

--- packages/ng/schematics/migrations/css-vars/index.ts
+++ packages/ng/schematics/migrations/css-vars/index.ts
@@ -2,6 +2,7 @@ import { Rule, Tree } from '@angular-devkit/schematics';
 
 export default (): Rule => {
 	return (tree: Tree) => {
+		// eslint-disable-next-line no-console
 		console.log(tree);
 	};
 };
