5bfc37e8d5e2270ef5013ced5043d9fba4d54cf3
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-08 19:24:32 +0200

    feat(schematics): handle new entry points and remove scss placeholders

---
+++ packages/ng/schematics/migrations/css-vars/updaters/main-import.ts
@@ -0,0 +1,45 @@
+import { AtRule, Node, Root } from 'postcss';
+import { addImport, removeImportNode } from '../utils';
+
+export function updateMainImport(root: Root) {
+	let hasMainScss = false;
+	let hasMainNg = false;
+
+	root.walkAtRules('import', (atRule) => {
+		if (atRule.params.includes('scss/src/theming.overridable')) {
+			removeImportNode(atRule, 'scss/src/theming.overridable');
+		}
+		if (atRule.params.includes('scss/src/main.overridable')) {
+			removeImportNode(atRule, 'scss/src/main.overridable');
+			hasMainScss = true;
+		}
+		if (atRule.params.includes('ng/style/main.overridable')) {
+			removeImportNode(atRule, 'ng/style/main.overridable');
+			hasMainNg = true;
+		}
+	});
+
+	if (hasMainScss) {
+		addForwardRule(root, '@lucca-front/scss/src/commons');
+		addForwardRule(root, '@lucca-front/scss/src/components');
+	}
+	if (hasMainNg) {
+		addForwardRule(root, '@lucca-front/ng/style/main');
+	}
+}
+
+function addForwardRule(root: Root, name: string) {
+	let lastForwardRule: AtRule | undefined;
+
+	root.walkAtRules('forward', (rule) => {
+		lastForwardRule = rule;
+	});
+
+	let afterNode: Node | undefined = lastForwardRule;
+
+	if (!afterNode && root.first?.type === 'comment') {
+		afterNode = root.first;
+	}
+
+	addImport(root, new AtRule({ name: 'forward', params: `'${name}'` }), afterNode);
+}
