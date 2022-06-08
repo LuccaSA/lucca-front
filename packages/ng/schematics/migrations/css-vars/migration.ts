fdd1412d2d418329cb80d6ac1783e863887d9131
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-08 19:24:32 +0200

    feat(schematics): handle new entry points and remove scss placeholders

--- packages/ng/schematics/migrations/css-vars/migration.ts
+++ packages/ng/schematics/migrations/css-vars/migration.ts
@@ -4,7 +4,9 @@ import { updateColorMixin } from './updaters/color';
 import { updateElevation } from './updaters/elevation';
 import { updateGetSetFunctions } from './updaters/get-set';
 import { removeIE11ThemeSupport } from './updaters/ie11';
+import { updateMainImport } from './updaters/main-import';
 import { replaceMixin } from './updaters/mixins';
+import { removeScssPlaceholders } from './updaters/remove-scss-placeholder';
 import { updateThemeMixin } from './updaters/theme';
 import { removeContainerIfEmpty, removeImportNode } from './utils';
 
@@ -15,11 +17,13 @@ export function migrateFile(content: string): string {
 		['@lucca-front/scss/src/mixins', '@lucca-front/icons/src/mixins', 'theming', 'theme'].some((name) => removeImportNode(rule, name));
 	});
 
+	removeScssPlaceholders(root);
 	removeIE11ThemeSupport(root);
 	updateColorMixin(root);
 	updateThemeMixin(root);
 	updateGetSetFunctions(root);
 	updateElevation(root);
+	updateMainImport(root);
 
 	root.walkAtRules('include', (atRule) => {
 		if (atRule.params === 'ie11') {
