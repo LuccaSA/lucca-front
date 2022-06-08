e7667bd41fd598f6ef89797a467c0067772506f5
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-08 17:38:00 +0200

    fix(schematics): do not migrate node_modules

--- packages/ng/schematics/migrations/css-vars/index.ts
+++ packages/ng/schematics/migrations/css-vars/index.ts
@@ -4,7 +4,7 @@ import { migrateFile } from './migration';
 export default (): Rule => {
 	return (tree: Tree) => {
 		tree.visit((path, entry) => {
-			if (!path.endsWith('.scss') || !entry) {
+			if (!path.endsWith('.scss') || path.includes('node_modules') || !entry) {
 				return;
 			}
 
