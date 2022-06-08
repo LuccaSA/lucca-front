0c45a9c47db965dd4e44e0cd5854b7d0c16c5e87
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-08 17:04:56 +0200

    feat(schematics): ng update for lf next

--- packages/ng/schematics/migrations/css-vars/index.ts
+++ packages/ng/schematics/migrations/css-vars/index.ts
@@ -1,8 +1,14 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
+import { migrateFile } from './migration';
 
 export default (): Rule => {
 	return (tree: Tree) => {
-		// eslint-disable-next-line no-console
-		console.log(tree);
+		tree.visit((path, entry) => {
+			if (!path.endsWith('.scss') || !entry) {
+				return;
+			}
+
+			tree.overwrite(path, migrateFile(entry.content.toString()));
+		});
 	};
 };
