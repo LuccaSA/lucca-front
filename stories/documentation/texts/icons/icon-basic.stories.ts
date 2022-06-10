7e12bc20bae8a85fd3edecc7d5ab8f63fbeb15f0
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-10 12:48:43 +0200

    feat: add filter in icon story

--- stories/documentation/texts/icons/icon-basic.stories.ts
+++ stories/documentation/texts/icons/icon-basic.stories.ts
@@ -1,40 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-import * as icons from '@lucca-front/icons';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as icons from '@lucca-front/icons';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 @Component({
 	selector: 'icon-basic-stories',
 	templateUrl: './icon-basic.stories.html',
 }) class IconStory {
 	icons: string[] = icons.default;
+	filter = '';
+
+	public updateIcons(filter: string) {
+		this.icons = filter ? icons.default.filter((icon: string) => icon.toLowerCase().includes(filter.toLowerCase())) : icons.default;
+	}
+
 	public camelize(str): string{
 		let arr = str.split('_');
 		let capital = arr.map((item, index) => index ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item.toLowerCase());
 		let capitalString = capital.join("");
 		return capitalString;
 	}
+
+	public copyIcon(icon: string): void {
+		navigator.clipboard.writeText(`<span aria-hidden="true" class="lucca-icon icon-${this.camelize(icon)}"></span>`)
+	}
 }
 
 export default {
-  title: 'Documentation/Texts/Icons/List',
-  component: IconStory,
+	title: 'Documentation/Texts/Icons/List',
+	component: IconStory,
 	decorators: [
 		moduleMetadata({
-			entryComponents: [IconStory],
-			imports: [CommonModule],
+			imports: [CommonModule, FormsModule],
 		})
 	]
 } as Meta;
 
-const template: Story<IconStory> = () => ({});
+const template: Story<IconStory> = (args) => ({ props: args });
 
 export const basic = template.bind({});
 
 const code = `<span aria-hidden="true" class="lucca-icon icon-heart"></span>`;
 
 basic.parameters = {
-	controls: { include: [] },
 	docs: {
 		source: {
 			language: 'html',
