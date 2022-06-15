f1bd25cd76df4d009e338ca9f430fcdab4a4f686
Guillaume Nury <nury.guillaume@gmail.com>
2022-06-15 16:41:14 +0200

    feat(schematics): replace fakeborder

--- packages/ng/schematics/migrations/css-vars/updaters/theme.ts
+++ packages/ng/schematics/migrations/css-vars/updaters/theme.ts
@@ -1,23 +1,40 @@
-import { Root } from 'postcss';
+import { Declaration, Root } from 'postcss';
+import { FunctionNode } from 'postcss-value-parser';
 import { commentNode, ValueNode } from '../utils';
 
 export function updateThemeMixin(root: Root) {
 	root.walkDecls((decl) => {
 		const valueNode = new ValueNode(decl.value);
-		valueNode.walkFunction('_theme', (funcNode) => {
-			const isUnsupported = funcNode.nodes.length > 1;
-			if (isUnsupported) {
-				commentNode(decl, 'Utilisation de _theme non gérée par la migration automatique');
-				return false;
-			}
 
-			const themeVar = funcNode.nodes[0].value.replace(/\./g, '-');
-			funcNode.value = 'var';
-			funcNode.nodes = new ValueNode(`--${themeVar}`).nodes;
+		// _theme("spacings.smallest") => var(--spacings-smallest)
+		valueNode.walkFunction('_theme', (funcNode) =>
+			updateThemeNode(funcNode, decl, (themeVar) => {
+				funcNode.value = 'var';
+				funcNode.nodes = new ValueNode(`--${themeVar}`).nodes;
+			}),
+		);
 
-			return undefined;
-		});
+		// -_theme("spacings.smallest") => calc(var(--spacings-smallest) * -1)
+		valueNode.walkFunction('-_theme', (funcNode) =>
+			updateThemeNode(funcNode, decl, (themeVar) => {
+				funcNode.value = 'calc';
+				funcNode.nodes = new ValueNode(`var(--${themeVar}) * -1`).nodes;
+			}),
+		);
 
 		decl.value = valueNode.toString();
 	});
+}
+
+function updateThemeNode(funcNode: FunctionNode, declaration: Declaration, updater: (themeVar: string) => unknown): false | void {
+	const isUnsupported = funcNode.nodes.length > 1;
+	if (isUnsupported) {
+		commentNode(declaration, 'Utilisation de _theme non gérée par la migration automatique');
+		return false;
+	}
+
+	const themeVar = funcNode.nodes[0].value.replace(/\./g, '-');
+	updater(themeVar);
+
+	return undefined;
 }
