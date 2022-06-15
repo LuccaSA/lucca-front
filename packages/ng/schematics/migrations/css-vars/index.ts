4204a6718992c0a4ddd3e422b84ce3cf47fb2626
LucasPerrault <lperrault@lucca.fr>
2022-06-15 16:25:35 +0200

    fix add migrate angular json file func

--- packages/ng/schematics/migrations/css-vars/index.ts
+++ packages/ng/schematics/migrations/css-vars/index.ts
@@ -1,11 +1,15 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
-import { migrateFile } from './migration';
+import { migrateAngularJsonFile, migrateFile } from './migration';
 
 export default (): Rule => {
 	return (tree: Tree) => {
 		tree.visit((path, entry) => {
 			if (!path.endsWith('.scss') || path.includes('node_modules') || !entry) {
 				return;
+			}
+
+			if (path === 'angular.json') {
+				tree.overwrite(path, migrateAngularJsonFile(entry.content.toString()));
 			}
 
 			tree.overwrite(path, migrateFile(entry.content.toString()));
