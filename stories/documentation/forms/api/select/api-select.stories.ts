c31db4fbb3d8d1ef0fb16cc47fd33b92f761da5a
Corentin <2089620+shajz@users.noreply.github.com>
2022-07-04 12:24:46 +0200

    FIX issues with index.ts and httpClientModule

--- stories/documentation/forms/api/select/api-select.stories.ts
+++ stories/documentation/forms/api/select/api-select.stories.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LuApiModule, LuApiSelectInputComponent } from '@lucca-front/ng/api';
@@ -34,7 +35,7 @@ export default {
 		componentWrapperDecorator(ApiSelectStory),
 		moduleMetadata({
 			declarations: [ApiSelectStory],
-			imports: [LuApiModule, BrowserAnimationsModule],
+			imports: [HttpClientModule, LuApiModule, BrowserAnimationsModule],
 		}),
 	],
 } as Meta;
