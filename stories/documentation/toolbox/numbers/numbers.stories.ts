80ad1f112afad3edd0dea0cac32c1bdf5d91934e
Maxime GENEVIER <mgenevier@lucca.fr>
2022-08-17 17:03:47 +0200

    [ADD] Numbers stories

--- stories/documentation/toolbox/numbers/numbers.stories.ts
+++ stories/documentation/toolbox/numbers/numbers.stories.ts
@@ -1,54 +1,22 @@
 import { Component } from '@angular/core';
-import { LuNumberModule, LuNumberPipe } from '@lucca-front/ng/number';
-import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
+import { LuNumberModule } from '@lucca-front/ng/number';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 @Component({
 	selector: 'numbers-stories',
-	template: `
-		<table class="table">
-			<thead class="table-head">
-				<tr class="table-head-row">
-					<th class="table-head-row-cell">Number</th>
-					<th class="table-head-row-cell u-textRight">Value</th>
-					<th class="table-head-row-cell u-textRight">Value aligned</th>
-				</tr>
-			</thead>
-			<tbody class="table-body">
-				<tr class="table-body-row">
-					<td class="table-body-row-cell">π</td>
-					<td class="table-body-row-cell u-textRight">{{ pi }}</td>
-					<td class="table-body-row-cell u-textRight">{{ pi | luNumber }}</td>
-				</tr>
-				<tr class="table-body-row">
-					<td class="table-body-row-cell">ten</td>
-					<td class="table-body-row-cell u-textRight">{{ ten }}</td>
-					<td class="table-body-row-cell u-textRight">{{ ten | luNumber }}</td>
-				</tr>
-				<tr class="table-body-row">
-					<td class="table-body-row-cell"><span>e</span></td>
-					<td class="table-body-row-cell u-textRight">{{ e }}</td>
-					<td class="table-body-row-cell u-textRight">{{ e | luNumber }}</td>
-				</tr>
-				<tr class="table-body-row">
-						<td class="table-body-row-cell">milliers</td>
-						<td class="table-body-row-cell u-textRight">{{ thousands }}</td>
-						<td class="table-body-row-cell u-textRight">{{ thousands | luNumber }}</td>
-					</tr>
-			</tbody>
-		</table>
-	`,
+	templateUrl: './numbers.stories.html',
 })
 class NumbersStory {
-
+	public pi = 3.141592;
+	public ten = 10;
+	public e = 2.71727;
+	public thousands = 12345;
 }
 
 export default {
-	title: 'Documentation/Users/Picture/Basic',
-	component: LuNumberPipe,
-	argTypes: {
-	},
+	title: 'Documentation/Toolbox/Numbers/Basic',
+	component: NumbersStory,
 	decorators: [
-		componentWrapperDecorator(NumbersStory, (props: NumbersStory) => ({  })),
 		moduleMetadata({
 			imports: [LuNumberModule],
 			declarations: [NumbersStory],
@@ -60,10 +28,6 @@ const template: Story<NumbersStory> = (args: NumbersStory) => ({
 	props: args,
 });
 
-export const Basic = template.bind({});
-Basic.args = {
-};
-
 const code = `
 /* 1. Importer LuNumberModule */
 import { LuNumberModule } from '@lucca-front/ng/number';
@@ -73,8 +37,53 @@ import { LuNumberModule } from '@lucca-front/ng/number';
 })
 class StoriesModule {}
 
-/* 2. Utiliser le pipe luNumber */
+/* 2. Template */
+<table class="table">
+	<thead class="table-head">
+		<tr class="table-head-row">
+			<th class="table-head-row-cell">Number</th>
+			<th class="table-head-row-cell u-textRight">Value</th>
+			<th class="table-head-row-cell u-textRight">Value aligned</th>
+		</tr>
+	</thead>
+	<tbody class="table-body">
+		<tr class="table-body-row">
+			<td class="table-body-row-cell">π</td>
+			<td class="table-body-row-cell u-textRight">{{ pi }}</td>
+			<td class="table-body-row-cell u-textRight" [innerHtml]="pi | luNumber"></td>
+		</tr>
+		<tr class="table-body-row">
+			<td class="table-body-row-cell">Dix</td>
+			<td class="table-body-row-cell u-textRight">{{ ten }}</td>
+			<td class="table-body-row-cell u-textRight" [innerHtml]="ten | luNumber"></td>
+		</tr>
+		<tr class="table-body-row">
+			<td class="table-body-row-cell"><span>e</span></td>
+			<td class="table-body-row-cell u-textRight">{{ e }}</td>
+			<td class="table-body-row-cell u-textRight" [innerHtml]="e | luNumber"></td>
+		</tr>
+		<tr class="table-body-row">
+			<td class="table-body-row-cell">Milliers</td>
+			<td class="table-body-row-cell u-textRight">{{ thousands }}</td>
+			<td class="table-body-row-cell u-textRight" [innerHtml]="thousands | luNumber"></td>
+		</tr>
+	</tbody>
+</table>
+
+/* 3. Utiliser le pipe luNumber */
+@Component({
+	selector: 'numbers-stories',
+	templateUrl: './numbers.stories.html',
+})
+class NumbersStory {
+	public pi = 3.141592;
+	public ten = 10;
+	public e = 2.71727;
+	public thousands = 12345;
+}
 `;
+export const Basic = template.bind({});
+Basic.args = {};
 
 Basic.parameters = {
 	// Disable controls as they are not modifiable because of ComponentWrapper
