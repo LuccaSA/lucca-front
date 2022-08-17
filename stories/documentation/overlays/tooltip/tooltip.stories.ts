fe403958a645c0896884e0a79a6a204291dd45b6
Adele Guillerot <aguillerot@lucca.fr>
2022-08-17 17:11:07 +0200

    Edit lu tooltip stories

--- stories/documentation/overlays/tooltip/tooltip.stories.ts
+++ stories/documentation/overlays/tooltip/tooltip.stories.ts
@@ -1,25 +1,57 @@
 import { Component, Input } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LuTooltipModule } from '@lucca-front/ng/tooltip';
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
+import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
+import { LuPopoverPosition } from '../../../../packages/ng/popover/src/lib';
 
 
 @Component({
 	selector: 'tooltip-stories',
-	templateUrl: './tooltip.stories.html',
+	template: `<button luTooltip='so you can see me'
+	[luTooltipEnterDelay]='luTooltipEnterDelay'
+	[luTooltipLeaveDelay]='luTooltipLeaveDelay'
+	[luTooltipPosition]='luTooltipPosition'
+	[luTooltipDisabled]='luTooltipDisabled'>
+		Come over here
+	</button>`,
 }) class TooltipStory {
+	@Input() luTooltipEnterDelay: number;
+	@Input() luTooltipLeaveDelay: number;
+	@Input() luTooltipDisabled: boolean;
+	@Input() luTooltipPosition: LuPopoverPosition;
 }
 
 export default {
-  title: 'Documentation/Overlays/Tooltip/Focus',
+  title: 'Documentation/Overlays/Tooltip/Basic',
   component: TooltipStory,
+	argTypes: {
+		luTooltipEnterDelay: {
+			control: {type: 'number'},
+		},
+		luTooltipLeaveDelay: {
+			control: {type: 'number'},
+		},
+		luTooltipDisabled: {
+			control: {type: 'boolean'},
+		},
+		luTooltipPosition: {
+			control: 'inline-radio', options: ['above', 'below', 'before', 'after'],
+		},
+	},
 	decorators: [
+		componentWrapperDecorator(TooltipStory, (props: TooltipStory) =>
+			({
+				luTooltipEnterDelay: props.luTooltipEnterDelay,
+				luTooltipLeaveDelay: props.luTooltipLeaveDelay,
+				luTooltipDisabled: props.luTooltipDisabled,
+				luTooltipPosition: props.luTooltipPosition
+			})),
 		moduleMetadata({
-			entryComponents: [TooltipStory],
 			imports: [
 				LuTooltipModule,
 				BrowserAnimationsModule,
-			]
+			],
+			declarations: [TooltipStory],
 		})
 	]
 } as Meta;
@@ -28,5 +60,51 @@ const template: Story<TooltipStory> = (args: TooltipStory) => ({
   props: args,
 });
 
-export const basic = template.bind({});
-basic.args = {}
+export const Basic = template.bind({});
+Basic.args = {
+	luTooltipEnterDelay: 50,
+	luTooltipLeaveDelay: 50,
+	luTooltipDisabled: false,
+	luTooltipPosition: 'below',
+}
+
+const code = `
+/* 1. Importer LuTooltipModule */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LuTooltipModule } from '@lucca-front/ng/user';
+
+@NgModule({
+	imports: [LuUserPictureModule, BrowserAnimationsModule]
+})
+class StoriesModule {}
+
+/* 2. Utiliser luTooltip */
+@Component({
+	selector: 'luTooltip-stories',
+	template:
+	\`<button luTooltip='so you can see me'
+	[luTooltipEnterDelay]='luTooltipEnterDelay'
+	[luTooltipLeaveDelay]='luTooltipLeaveDelay'
+	[luTooltipPosition]='luTooltipPosition'
+	[luTooltipDisabled]='luTooltipDisabled'>
+		Come over here
+	</button>\`,
+})
+class TooltipStory {
+  @Input() luTooltipEnterDelay: number;
+  @Input() luTooltipLeaveDelay: number;
+  @Input() luTooltipDisabled: boolean;
+  @Input() luTooltipPosition: LuPopoverPosition;
+}`;
+
+Basic.parameters = {
+	// Disable controls as they are not modifiable because of ComponentWrapper
+	controls: { include: ['luTooltipEnterDelay', 'luTooltipLeaveDelay', 'luTooltipDisabled', 'luTooltipPosition'] },
+	docs: {
+		source: {
+			language: 'ts',
+			type: 'code',
+			code,
+		},
+	},
+};
