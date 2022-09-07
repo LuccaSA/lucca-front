9bbe0426c5a11019fe4b50d78078b9c140080ec4
Guillaume Nury <nury.guillaume@gmail.com>
2022-09-07 16:20:01 +0200

    fix(select2): build issue

--- packages/ng/select2/src/lib/input/select-input.component.ts
+++ packages/ng/select2/src/lib/input/select-input.component.ts
@@ -20,7 +20,7 @@ import {
 	OnDestroy,
 	OnInit,
 	Output,
-	TemplateRef
+	TemplateRef,
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ReplaySubject, Subject, takeUntil } from 'rxjs';
