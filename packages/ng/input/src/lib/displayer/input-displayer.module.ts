dac4eea28822004239c14f7f7f8cf0bb9981eec3
Benjamin <bsabardu@lucca.fr>
2022-09-22 12:01:48 +0200

    standalone input displayer directive

--- packages/ng/input/src/lib/displayer/input-displayer.module.ts
+++ packages/ng/input/src/lib/displayer/input-displayer.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { LuInputDisplayerDirective } from './input-displayer.directive';
 
 @NgModule({
-	imports: [],
-	declarations: [LuInputDisplayerDirective],
+	imports: [LuInputDisplayerDirective],
 	exports: [LuInputDisplayerDirective],
 })
 export class LuInputDisplayerModule {}
