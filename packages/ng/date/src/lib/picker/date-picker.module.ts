90a2cf3235e039e72f740249a2fd6a5812a1a354
Benjamin <bsabardu@lucca.fr>
2022-09-22 16:06:21 +0200

    standalone date picker

--- packages/ng/date/src/lib/picker/date-picker.module.ts
+++ packages/ng/date/src/lib/picker/date-picker.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { LuCalendarInputModule } from '../calendar/index';
 import { LuDatePickerComponent } from './date-picker.component';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { A11yModule } from '@angular/cdk/a11y';
-import { LuDateInputModule } from '../input/index';
 
 @NgModule({
-	imports: [FormsModule, CommonModule, LuCalendarInputModule, LuDateInputModule, OverlayModule, A11yModule],
+	imports: [LuDatePickerComponent],
 	exports: [LuDatePickerComponent],
-	declarations: [LuDatePickerComponent],
 })
 export class LuDatePickerModule {}
