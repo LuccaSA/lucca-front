17b739b92eb90f17c287be71f37418cc0bae79fb
Benjamin <bsabardu@lucca.fr>
2022-11-02 17:25:28 +0100

    handle option module

--- packages/ng/option/src/lib/option.module.ts
+++ packages/ng/option/src/lib/option.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { LuOptionItemComponent } from './item/index';
-import { LuOptionOperatorModule } from './operator/index';
-import { LuOptionPickerModule } from './picker/index';
+import { LuForGroupsDirective, LuForOptionsDirective, LuOptionFeederComponent, LuOptionOperatorModule, LuOptionPagerComponent, LuOptionSearcherComponent } from './operator/index';
+import { LuOptionPickerAdvancedComponent, LuOptionPickerComponent } from './picker/index';
 import { LuOptionPlaceholderComponent } from './placeholder/index';
 import { LuOptionSelectAllComponent } from './selector/index';
 
 @NgModule({
-	imports: [LuOptionItemComponent, LuOptionPickerModule, LuOptionOperatorModule, LuOptionSelectAllComponent, LuOptionPlaceholderComponent],
-	exports: [LuOptionItemComponent, LuOptionPickerModule, LuOptionOperatorModule, LuOptionSelectAllComponent, LuOptionPlaceholderComponent],
+	imports: [
+		LuOptionItemComponent,
+		LuOptionPickerComponent,
+		LuOptionPickerAdvancedComponent,
+		LuOptionItemComponent,
+		LuOptionPagerComponent,
+		LuOptionFeederComponent,
+		LuOptionSearcherComponent,
+		LuForOptionsDirective,
+		LuForGroupsDirective,
+		LuOptionSelectAllComponent,
+		LuOptionPlaceholderComponent,
+	],
+	exports: [LuOptionItemComponent, LuOptionPickerComponent, LuOptionOperatorModule, LuOptionSelectAllComponent, LuOptionPlaceholderComponent],
 })
 export class LuOptionModule {}
