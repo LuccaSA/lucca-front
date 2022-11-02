b69d8a6944244ade7a45ea2dc6b1f8588bfddccb
Benjamin <bsabardu@lucca.fr>
2022-11-02 17:30:11 +0100

    api select module

--- packages/ng/api/src/lib/select/api-select.module.ts
+++ packages/ng/api/src/lib/select/api-select.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LuApiFeederComponent } from './feeder/index';
 import { LuApiSelectInputComponent } from './input/index';
-import { LuApiPagerComponent, LuApiPagerModule } from './pager/index';
-import { LuApiPagedSearcherComponent, LuApiSearcherComponent, LuApiSearcherModule } from './searcher/index';
+import { LuApiPagerComponent } from './pager/index';
+import { LuApiPagedSearcherComponent, LuApiSearcherComponent } from './searcher/index';
 
 @NgModule({
 	imports: [LuApiFeederComponent, LuApiPagedSearcherComponent, LuApiSearcherComponent, LuApiPagerComponent, LuApiSelectInputComponent],
-	exports: [LuApiFeederComponent, LuApiSearcherModule, LuApiPagerModule, LuApiSelectInputComponent],
+	exports: [LuApiFeederComponent, LuApiPagedSearcherComponent, LuApiSearcherComponent, LuApiPagerComponent, LuApiSelectInputComponent],
 })
 export class LuApiSelectModule {}
