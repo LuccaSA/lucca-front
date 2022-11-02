9e15057c74a67785a54fc3ac4470c797a488419d
Benjamin <bsabardu@lucca.fr>
2022-11-02 17:25:17 +0100

    tree modules

--- packages/ng/option/src/lib/picker/tree-option-picker.module.ts
+++ packages/ng/option/src/lib/picker/tree-option-picker.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { LuTreeOptionPickerComponent } from './tree-option-picker.component';
-import { CommonModule } from '@angular/common';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { LuScrollModule } from '@lucca-front/ng/scroll';
 import { A11yModule } from '@angular/cdk/a11y';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { LuScrollDirective } from '@lucca-front/ng/scroll';
 import { LuTreeOptionPickerAdvancedComponent } from './tree-option-picker-advanced.component';
+import { LuTreeOptionPickerComponent } from './tree-option-picker.component';
 
 @NgModule({
-	imports: [CommonModule, OverlayModule, LuScrollModule, A11yModule],
-	declarations: [LuTreeOptionPickerComponent, LuTreeOptionPickerAdvancedComponent],
+	imports: [CommonModule, OverlayModule, LuScrollDirective, A11yModule, LuTreeOptionPickerAdvancedComponent, LuTreeOptionPickerComponent],
 	exports: [LuTreeOptionPickerComponent, LuTreeOptionPickerAdvancedComponent],
 })
 export class LuTreeOptionPickerModule {}
