7b8c0f7e572ee2c127fb3ef337184d18e77d3429
Benjamin <bsabardu@lucca.fr>
2022-11-03 10:39:26 +0100

    delete user select comment

--- packages/ng/user/src/lib/select/input/user-select-input.module.ts
+++ packages/ng/user/src/lib/select/input/user-select-input.module.ts
@@ -2,17 +2,6 @@ import { NgModule } from '@angular/core';
 import { LuUserSelectInputComponent } from './user-select-input.component';
 
 @NgModule({
-	// imports: [
-	// 	CommonModule,
-	// 	LuUserDisplayModule,
-	// 	LuOptionOperatorModule,
-	// 	LuOptionPickerModule,
-	// 	LuInputClearerComponent,
-	// 	LuUserSearcherModule,
-	// 	LuInputDisplayerModule,
-	// 	LuUserHomonymsModule,
-	// LuUserMeOptionModule,
-	// ],
 	imports: [LuUserSelectInputComponent],
 	exports: [LuUserSelectInputComponent],
 })
