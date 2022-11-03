12aa8c0120640274834a18379af3e00cdbb0ade6
Benjamin <bsabardu@lucca.fr>
2022-11-03 10:02:31 +0100

    popover

--- packages/ng/popover/src/lib/trigger/popover-trigger.module.ts
+++ packages/ng/popover/src/lib/trigger/popover-trigger.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OverlayModule } from '@angular/cdk/overlay';
 import { LuPopoverTriggerDirective } from './popover-trigger.directive';
-import { LuPopoverPanelModule } from '../panel/index';
-import { LuPopoverTargetModule } from '../target/index';
 
 @NgModule({
-	imports: [CommonModule, OverlayModule, LuPopoverPanelModule, LuPopoverTargetModule],
-	declarations: [LuPopoverTriggerDirective],
+	imports: [LuPopoverTriggerDirective],
 	exports: [LuPopoverTriggerDirective],
 })
 export class LuPopoverTriggerModule {}
