72d81a12faf482eaf0d258b3cef100dd11e271c0
Benjamin <bsabardu@lucca.fr>
2022-11-03 11:12:48 +0100

    user select homonyms module

--- to-migrate/ng/user-select-homonyms/user-select-homonyms.module.ts
+++ to-migrate/ng/user-select-homonyms/user-select-homonyms.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
-
-import { RouterModule } from '@angular/router';
-import { LuUserModule } from '@lucca-front/ng/user';
 import { UserSelectHomonymsComponent } from './user-select-homonyms.component';
 
-// needed to reroute api calls to prisme-proxy
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RedirectModule } from '../../redirect';
-
 @NgModule({
-	declarations: [UserSelectHomonymsComponent],
-	imports: [FormsModule, CommonModule, LuUserModule, HttpClientModule, RedirectModule, RouterModule.forChild([{ path: '', component: UserSelectHomonymsComponent }])],
+	imports: [UserSelectHomonymsComponent],
+	exports: [UserSelectHomonymsComponent],
 })
 export class UserSelectHomonymsModule {}
