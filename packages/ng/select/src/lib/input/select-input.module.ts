30056f2730dba553cc4beee1d90b84c6e71c9fb5
Benjamin <bsabardu@lucca.fr>
2022-11-17 17:45:02 +0100

    fix lint

--- packages/ng/select/src/lib/input/select-input.module.ts
+++ packages/ng/select/src/lib/input/select-input.module.ts
@@ -1,5 +1,3 @@
-import { OverlayModule } from '@angular/cdk/overlay';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LuSelectInputComponent } from './select-input.component';
 
