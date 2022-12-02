fc50152d1f5e686f36a530854ce6754430ddd820
Guillaume Nury <gnury@lucca.fr>
2022-12-02 09:27:11 +0100

    ref(ng): rework injector as old syntax breaks tests

--- packages/ng/popup/src/lib/popup.service.ts
+++ packages/ng/popup/src/lib/popup.service.ts
@@ -1,12 +1,13 @@
 import { ComponentType } from '@angular/cdk/overlay';
-import { Inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ILuPopupConfig } from './popup-config.model';
-import { ILuPopupRef, ILuPopupRefFactory } from './popup-ref.model';
+import { ILuPopupRef } from './popup-ref.model';
 import { LU_POPUP_CONFIG, LU_POPUP_REF_FACTORY } from './popup.token';
 
 @Injectable()
 export class LuPopup<C extends ILuPopupConfig = ILuPopupConfig> {
-	constructor(@Inject(LU_POPUP_REF_FACTORY) protected _factory: ILuPopupRefFactory, @Inject(LU_POPUP_CONFIG) protected _config: ILuPopupConfig) {}
+	protected _factory = inject(LU_POPUP_REF_FACTORY);
+	protected _config = inject(LU_POPUP_CONFIG);
 
 	open<T, D, R>(component: ComponentType<T>, data: D = undefined, config: C = {} as C): ILuPopupRef<T, D, R> {
 		const extendedConfig = { ...this._config, ...config };
--- packages/ng/popup/src/lib/popup.token.ts
+++ packages/ng/popup/src/lib/popup.token.ts
@@ -2,7 +2,16 @@ import { InjectionToken } from '@angular/core';
 import { ILuPopupConfig } from './popup-config.model';
 import { ILuPopupRefFactory } from './popup-ref.model';
 
-export const LU_POPUP_REF_FACTORY = new InjectionToken<ILuPopupRefFactory>('LuPopupRefFactory');
+export const LU_POPUP_REF_FACTORY = new InjectionToken<ILuPopupRefFactory>('LuPopupRefFactory', {
+	factory: () => ({
+		forge() {
+			throw new Error('[LuPopupRefFactory] Cannot call forge, LuPopupRefFactory needs to be overriden.');
+		},
+	}),
+});
+
 /** Injection token that can be used to access the data that was passed in to a dialog. */
 export const LU_POPUP_DATA = new InjectionToken<unknown>('LuPopupData');
-export const LU_POPUP_CONFIG = new InjectionToken<ILuPopupConfig>('LuPopupDefaultConfig');
+export const LU_POPUP_CONFIG = new InjectionToken<ILuPopupConfig>('LuPopupDefaultConfig', {
+	factory: () => ({}),
+});
