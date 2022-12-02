4d9221a6aea2513f9e50fb20ebf3a54ebb9ef381
Mathieu Bellange <mbellange@lucca.fr>
2022-12-02 10:32:00 +0100

    Fix linting
    remove console.log

--- packages/ng/modal/src/lib/modal.utils.ts
+++ packages/ng/modal/src/lib/modal.utils.ts
@@ -3,7 +3,6 @@ import { ApplicationRef } from '@angular/core';
 export function setAriaHiddenOnApplicationRoot(applicationRef: ApplicationRef, hidden: boolean): void {
 	for (const component of applicationRef.components) {
 		const nativeElement = component.location.nativeElement as unknown;
-		console.log({ nativeElement, is: nativeElement instanceof HTMLElement });
 
 		if (nativeElement instanceof HTMLElement) {
 			nativeElement.ariaHidden = hidden ? 'true' : undefined;
