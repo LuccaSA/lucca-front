1c3cf322e416b982822cd935fbe2165c5fc7ccef
Guillaume Nury <gnury@lucca.fr>
2022-12-02 17:37:21 +0100

    fix(ng): lint issues

--- packages/ng/core/src/lib/translate/intl.model.ts
+++ packages/ng/core/src/lib/translate/intl.model.ts
@@ -3,9 +3,22 @@ import { ILuTranslation } from './translation.model';
 
 export type ILuIntl<T> = { [P in keyof T]: string };
 
+function isSupportedLang<T>(locale: string, translations: ILuTranslation<T>): locale is keyof ILuTranslation<T> {
+	return locale in translations;
+}
+
 export function getIntl<T>(translationsToken: InjectionToken<ILuTranslation<T>>): T {
 	const locale = inject(LOCALE_ID);
 	const translations = inject(translationsToken);
 
-	return translations[locale] || translations[locale.substring(0, 2)] || translations['en'];
+	if (isSupportedLang(locale, translations)) {
+		return translations[locale];
+	}
+
+	const shortLocale = locale.substring(0, 2);
+	if (isSupportedLang(shortLocale, translations)) {
+		return translations[shortLocale];
+	}
+
+	return translations['en'];
 }
--- packages/ng/schematics/migrations/css-vars/tests/flex-order.component.input.ts
+++ packages/ng/schematics/migrations/css-vars/tests/flex-order.component.input.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'lu-test',
 	template: `<div [class.u-order1]="isCurrentUser">
-			<span class="mdr u-order1"> {{ 'EXPORT_CARD_TITLE_DATE' | transloco: { createdAt: date | date: 'medium' } }} </span>
-			<span class="u-order-1"> {{ 'EXPORT_CARD_TITLE_DATE' | transloco: { createdAt: date | date: 'medium' } }} </span>
+			<span class="mdr u-order1"> {{ 'EXPORT_CARD_TITLE_DATE' | transloco : { createdAt: date | date : 'medium' } }} </span>
+			<span class="u-order-1"> {{ 'EXPORT_CARD_TITLE_DATE' | transloco : { createdAt: date | date : 'medium' } }} </span>
 		</div>
 
 		<span [ngClass]="{ 'u-textDefault u-order-1': true }"></span>`,
