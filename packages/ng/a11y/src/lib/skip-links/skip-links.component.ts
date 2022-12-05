f8fa854dac2a78e43dbb56c1ad4a7dd3c9ae07fe
Corentin <2089620+shajz@users.noreply.github.com>
2022-12-05 14:18:37 +0100

    FIX injection

--- packages/ng/a11y/src/lib/skip-links/skip-links.component.ts
+++ packages/ng/a11y/src/lib/skip-links/skip-links.component.ts
@@ -1,15 +1,17 @@
 import { DOCUMENT } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { LuSkipLinksIntl } from './skip-links.intl';
+import { ILuSkipLinksLabel } from './skip-links.translate';
 
 @Component({
 	selector: 'lu-skip-links',
 	templateUrl: './skip-links.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
+	providers: [LuSkipLinksIntl],
 })
 export class LuSkipLinksComponent {
-	constructor(@Inject(DOCUMENT) protected document: Document, protected intl: LuSkipLinksIntl) {}
+	constructor(@Inject(DOCUMENT) protected document: Document, @Inject(LuSkipLinksIntl) protected intl: ILuSkipLinksLabel) {}
 
 	anchor(hash: string, e: Event) {
 		e.preventDefault();
--- packages/ng/a11y/src/lib/skip-links/skip-links.intl.ts
+++ packages/ng/a11y/src/lib/skip-links/skip-links.intl.ts
@@ -3,7 +3,7 @@ import { ALuIntl, ILuTranslation } from '@lucca-front/ng/core';
 import { LU_SKIP_LINKS_TRANSLATIONS } from './skip-links.token';
 import { ILuSkipLinksLabel } from './skip-links.translate';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class LuSkipLinksIntl extends ALuIntl<ILuSkipLinksLabel> {
 	constructor(@Inject(LU_SKIP_LINKS_TRANSLATIONS) translations: ILuTranslation<ILuSkipLinksLabel>, @Inject(LOCALE_ID) locale: string) {
 		super(translations, locale);
