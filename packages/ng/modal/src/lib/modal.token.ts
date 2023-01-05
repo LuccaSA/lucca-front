90bb7620c2b3936ed1453928e8fe10ba0c1e2f1f
Guillaume Nury <gnury@lucca.fr>
2023-01-05 14:30:29 +0100

    fix(sidepanel): Sidepanel where broken because of the new inject sythaxe

--- packages/ng/modal/src/lib/modal.token.ts
+++ packages/ng/modal/src/lib/modal.token.ts
@@ -1,8 +1,16 @@
-import { InjectionToken } from '@angular/core';
-import { ILuPopupRefFactory } from '@lucca-front/ng/popup';
+import { inject, InjectionToken } from '@angular/core';
+import { ILuPopupRefFactory, LU_POPUP_CONFIG } from '@lucca-front/ng/popup';
 import { ILuModalConfig } from './modal-config.model';
 
 /** Injection token that can be used to access the data that was passed in to a dialog. */
 export const LU_MODAL_DATA = new InjectionToken<unknown>('LuModalData');
-export const LU_MODAL_CONFIG = new InjectionToken<ILuModalConfig>('LuModalDefaultConfig');
-export const LU_MODAL_REF_FACTORY = new InjectionToken<ILuPopupRefFactory>('LuModalRefFactory');
+export const LU_MODAL_CONFIG = new InjectionToken<ILuModalConfig>('LuModalDefaultConfig', {
+	factory: () => inject(LU_POPUP_CONFIG),
+});
+export const LU_MODAL_REF_FACTORY = new InjectionToken<ILuPopupRefFactory>('LuModalRefFactory', {
+	factory: () => ({
+		forge() {
+			throw new Error('[LuModalRefFactory] Cannot call forge, LuModalRefFactory needs to be overriden.');
+		},
+	}),
+});
