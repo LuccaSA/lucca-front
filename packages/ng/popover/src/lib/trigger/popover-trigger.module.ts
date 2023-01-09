84e7b70d39aab7149dbfcd1a09464c67211538fa
Louis AMSTUTZ <louis.amstutz@gmail.com>
2023-01-09 12:01:44 +0100

    reimport OverlayModule in PopoverTriggerModdule

--- packages/ng/popover/src/lib/trigger/popover-trigger.module.ts
+++ packages/ng/popover/src/lib/trigger/popover-trigger.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LuPopoverTriggerDirective } from './popover-trigger.directive';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
-	imports: [LuPopoverTriggerDirective],
+	imports: [LuPopoverTriggerDirective, OverlayModule],
 	exports: [LuPopoverTriggerDirective],
 })
 export class LuPopoverTriggerModule {}
