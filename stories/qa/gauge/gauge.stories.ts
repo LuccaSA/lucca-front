d183e232c7c274c5b6ccb5f1455f187a469a2a13
mbriand <mbriand@lucca.fr>
2023-03-15 15:34:25 +0100

    standalone and fix lint for stories qa

--- stories/qa/gauge/gauge.stories.ts
+++ stories/qa/gauge/gauge.stories.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Story, Meta, moduleMetadata } from '@storybook/angular';
 
 @Component({
+	standalone: true,
 	selector: 'gauge-stories',
 	templateUrl: './gauge.stories.html',
 }) class GaugeStory {}
