8b24034ba5df67b2250702d58f35d1056b1fc239
Guillaume Nury <gnury@lucca.fr>
2023-03-23 17:20:32 +0100

    feat(multi-select): add select all

--- packages/ng/multi-select/src/lib/input/panel-ref.factory.ts
+++ packages/ng/multi-select/src/lib/input/panel-ref.factory.ts
@@ -1,12 +1,12 @@
 import { Overlay, OverlayConfig, OverlayPositionBuilder, OverlayRef, ScrollStrategyOptions } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
-import { ComponentRef, ElementRef, inject, Injectable, Injector } from '@angular/core';
-import { LuSelectPanelRef } from '@lucca-front/ng/core-select';
+import { ChangeDetectorRef, ComponentRef, ElementRef, inject, Injectable, Injector } from '@angular/core';
 import { takeUntil } from 'rxjs';
 import { LuMultiSelectPanelComponent } from '../panel';
 import { ILuMultiSelectPanelData, MULTI_SELECT_PANEL_DATA } from '../select.model';
+import { LuMultiSelectPanelRef } from './panel.model';
 
-class SelectPanelRef<T> extends LuSelectPanelRef<T, T[]> {
+class MultiSelectPanelRef<T> extends LuMultiSelectPanelRef<T> {
 	instance: LuMultiSelectPanelComponent<T>;
 	private panelRef: ComponentRef<LuMultiSelectPanelComponent<T>>;
 	private portalRef: ComponentPortal<LuMultiSelectPanelComponent<T>>;
@@ -16,7 +16,7 @@ class SelectPanelRef<T> extends LuSelectPanelRef<T, T[]> {
 
 		const injector = Injector.create({
 			providers: [
-				{ provide: LuSelectPanelRef, useValue: this },
+				{ provide: LuMultiSelectPanelRef, useValue: this },
 				{ provide: MULTI_SELECT_PANEL_DATA, useValue: panelData },
 			],
 			parent: parentInjector,
@@ -36,6 +36,12 @@ class SelectPanelRef<T> extends LuSelectPanelRef<T, T[]> {
 		this.valueChanged.emit(value);
 	}
 
+	updateSelectedOptions(selectedOptions: T[]): void {
+		this.instance.selectedOptions = selectedOptions;
+		// Run change detection on the panel component
+		this.panelRef.injector.get(ChangeDetectorRef).markForCheck();
+	}
+
 	override close(): void {
 		super.close();
 		this.panelRef.destroy();
@@ -51,11 +57,10 @@ export class LuMultiSelectPanelRefFactory {
 	protected scrollStrategies = inject(ScrollStrategyOptions);
 	protected parentInjector = inject(Injector);
 
-	buildPanelRef<T>(panelData: ILuMultiSelectPanelData<T>, overlayConfigOverride: OverlayConfig = {}): LuSelectPanelRef<T, T[]> {
+	buildPanelRef<T>(panelData: ILuMultiSelectPanelData<T>, overlayConfigOverride: OverlayConfig = {}): LuMultiSelectPanelRef<T> {
 		const overlayConfig = this.buildOverlayConfig(overlayConfigOverride);
 		const overlayRef = this.overlay.create(overlayConfig);
-
-		return new SelectPanelRef(overlayRef, this.parentInjector, panelData);
+		return new MultiSelectPanelRef(overlayRef, this.parentInjector, panelData);
 	}
 
 	protected buildOverlayConfig(overlayConfigOverride: OverlayConfig = {}): OverlayConfig {
