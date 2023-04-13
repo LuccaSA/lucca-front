a59acb31f9aba97a36210194635d429d0a3af148
Guillaume Nury <gnury@luccasoftware.com>
2023-04-13 18:33:32 +0200

    ref(multi-select): rework chips visibility to avoid refresh issues

--- packages/ng/multi-select/src/lib/displayer/default-displayer.component.ts
+++ packages/ng/multi-select/src/lib/displayer/default-displayer.component.ts
@@ -3,7 +3,7 @@ import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostBinding, H
 import { getIntl } from '@lucca-front/ng/core';
 import { ILuOptionContext, LU_OPTION_CONTEXT, ɵLuOptionOutletDirective } from '@lucca-front/ng/core-select';
 import { LuTooltipModule } from '@lucca-front/ng/tooltip';
-import { Observable, ReplaySubject, Subject, combineLatest, concatMap, debounceTime, defer, distinctUntilChanged, map, merge, startWith, takeUntil, throttleTime, withLatestFrom } from 'rxjs';
+import { Observable, ReplaySubject, Subject, combineLatest, concatMap, debounceTime, defer, distinctUntilChanged, map, merge, share, startWith, takeUntil, throttleTime, withLatestFrom } from 'rxjs';
 import { LuMultiSelectInputComponent } from '../input';
 import { LU_MULTI_SELECT_DISPLAYER_TRANSLATIONS } from './default-displayer.translate';
 
@@ -12,33 +12,35 @@ import { LU_MULTI_SELECT_DISPLAYER_TRANSLATIONS } from './default-displayer.tran
 	standalone: true,
 	imports: [AsyncPipe, LuTooltipModule, NgIf, NgFor, NgPlural, NgPluralCase, ɵLuOptionOutletDirective],
 	template: `
-		<div class="chips-container" *ngIf="{ options: context.option$ | async, visibleChips: (visibleChips$ | async) || [] } as vm">
-			<div
-				#chip
-				*ngFor="let option of vm.options; let index = index"
-				class="chip lu-multiselect-chip"
-				[class.mod-unkillable]="disabled"
-				[attr.aria-hidden]="index >= vm.visibleChips.length ? 'true' : undefined"
-			>
-				<ng-container *luOptionOutlet="select.valueTpl || select.optionTpl; value: option"></ng-container>
-				<button
-					*ngIf="!disabled"
-					type="button"
-					class="chip-kill lu-multiselect-chip-kill"
-					(click)="unselectOption(option, $event)"
-					[attr.tabindex]="index >= vm.visibleChips.length ? -1 : undefined"
-				></button>
+		<ng-container *ngIf="vm$ | async as vm">
+			<div class="chips-container">
+				<div
+					#chip
+					*ngFor="let option of vm.options; let index = index"
+					class="chip lu-multiselect-chip"
+					[class.mod-unkillable]="disabled"
+					[attr.aria-hidden]="index >= vm.chipsByVisibility.visible.length ? 'true' : undefined"
+				>
+					<ng-container *luOptionOutlet="select.valueTpl || select.optionTpl; value: option"></ng-container>
+					<button
+						*ngIf="!disabled"
+						type="button"
+						class="chip-kill lu-multiselect-chip-kill"
+						(click)="unselectOption(option, $event)"
+						[attr.tabindex]="index >= vm.chipsByVisibility.visible.length ? -1 : undefined"
+					></button>
+				</div>
 			</div>
-		</div>
-		<div class="lu-multiselect-counter" #overflow>
-			<ng-container *ngIf="hiddenElementsCount$ | async as count">
-				<div class="chip mod-unkillable lu-multiselect-counter-chip" aria-hidden="true" [luTooltip]="intl.showResultsDetails">+ {{ count }}</div>
-				<span class="u-mask" [ngPlural]="count">
-					<ng-template ngPluralCase="=1">{{ intl.otherResult }}</ng-template>
-					<ng-template ngPluralCase="other">{{ intl.otherResults }}</ng-template>
-				</span>
-			</ng-container>
-		</div>
+			<div class="lu-multiselect-counter" #overflow>
+				<ng-container *ngIf="vm.chipsByVisibility.hidden.length as count">
+					<div class="chip mod-unkillable lu-multiselect-counter-chip" aria-hidden="true" [luTooltip]="intl.showResultsDetails">+ {{ count }}</div>
+					<span class="u-mask" [ngPlural]="count">
+						<ng-template ngPluralCase="=1">{{ intl.otherResult }}</ng-template>
+						<ng-template ngPluralCase="other">{{ intl.otherResults }}</ng-template>
+					</span>
+				</ng-container>
+			</div>
+		</ng-container>
 	`,
 	styleUrls: ['./default-displayer.component.scss'],
 })
@@ -71,35 +73,55 @@ export class LuMultiSelectDefaultDisplayerComponent<T> implements AfterViewInit,
 	maxElementsWidth$ = new ReplaySubject<number>(1);
 	context = inject<ILuOptionContext<T[]>>(LU_OPTION_CONTEXT);
 
-	private throttledResize$ = this.resize$.pipe(throttleTime(50), startWith(undefined));
+	private throttledResize$ = this.resize$.pipe(throttleTime(50));
 
 	private counterWidth$ = merge(
 		defer(() => this.hiddenElementsCount$),
 		this.throttledResize$,
 	).pipe(
 		debounceTime(0),
-		map(() => this.overflowCountContainer.nativeElement.offsetWidth),
+		withLatestFrom(defer(() => this.hiddenElementsCount$)),
+		map(([, count]) => (count ? this.overflowCountContainer?.nativeElement.offsetWidth || 0 : 0)),
 		distinctUntilChanged(),
+		share(),
 	);
 
-	visibleChips$ = merge(this.chips$, this.throttledResize$, this.counterWidth$).pipe(
+	chipsByVisibility$ = merge(this.chips$, this.throttledResize$, this.counterWidth$).pipe(
 		debounceTime(0),
 		withLatestFrom(this.chips$),
 		map(([, chips]) => {
 			const baseOffsetTop = this.elementRef.nativeElement.offsetTop;
-			return chips.filter((chip) => chip.offsetTop <= baseOffsetTop);
+			const firstHiddenIndex = chips.findIndex((chip) => chip.offsetTop > baseOffsetTop);
+
+			return firstHiddenIndex !== -1
+				? {
+						visible: chips.slice(0, firstHiddenIndex),
+						hidden: chips.slice(firstHiddenIndex),
+				  }
+				: {
+						visible: chips,
+						hidden: [],
+				  };
 		}),
-		distinctUntilChanged((previous, current) => previous.length === current.length && previous.every((chip, index) => chip === current[index])),
 		takeUntil(this.destroyed$),
+		share(),
 	);
 
-	hiddenElementsCount$: Observable<number> = combineLatest([this.chips$, this.visibleChips$]).pipe(map(([chips, visbleChips]) => chips.length - visbleChips.length));
+	hiddenElementsCount$: Observable<number> = this.chipsByVisibility$.pipe(map(({ hidden }) => hidden.length));
 
 	@HostBinding('style.--hidden-option-count-width.px')
-	public hiddenOptionCountWidthCssVar = 0;
+	hiddenOptionCountWidthCssVar = 0;
 
 	@HostBinding('style.--hidden-option-count-offset-left.px')
-	public hiddenOptionCountOffsetLeftCssVar = 0;
+	hiddenOptionCountOffsetLeftCssVar = 0;
+
+	vm$ = combineLatest([this.context.option$, this.chipsByVisibility$]).pipe(
+		debounceTime(0),
+		map(([options, chipsByVisibility]) => ({
+			options,
+			chipsByVisibility,
+		})),
+	);
 
 	@HostListener('window:resize')
 	protected onResize() {
@@ -112,11 +134,11 @@ export class LuMultiSelectDefaultDisplayerComponent<T> implements AfterViewInit,
 			this.cdr.markForCheck();
 		});
 
-		this.visibleChips$
+		this.chipsByVisibility$
 			.pipe(
 				takeUntil(this.destroyed$),
-				map((chips) => {
-					const lastChip = chips[chips.length - 1];
+				map(({ visible }) => {
+					const lastChip = visible[visible.length - 1];
 					return lastChip ? lastChip.offsetLeft + lastChip.offsetWidth : 0;
 				}),
 				distinctUntilChanged(),
