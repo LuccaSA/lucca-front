fcb6d481966416719c2e70e78cc0108a6692c223
Jérémie Biron <jbiron@lucca.fr>
2023-04-24 16:00:04 +0200

    Callout update style

--- stories/documentation/feedback/callout/callout-killable.stories.ts
+++ stories/documentation/feedback/callout/callout-killable.stories.ts
@@ -1,27 +1,20 @@
 import { Meta, Story } from '@storybook/angular';
 
 interface CalloutKillableStory {
-	palette: string;
 }
 
 export default {
 	title: 'Documentation/Feedback/Callout/Killable',
 	argTypes: {
-		palette: {
-			options: ['', 'palette-primary', 'palette-secondary', 'palette-grey', 'palette-success', 'palette-warning', 'palette-error'],
-			control: {
-				type: 'select',
-			}
-		},
 	},
 } as Meta;
 
 function getTemplate(args: CalloutKillableStory): string {
-	const classes = [args.palette].filter(Boolean).join(' ');
 	return `
-	<div class="callout ${classes}">
+	<div class="callout">
 		<div class="callout-content">
-			<div class="callout-content-description">Je suis un callout supprimable</div>
+			<strong class="callout-content-title">Feedback or informations</strong>
+			<div class="callout-content-description">Caesarem fama studio memorabili ut latius abscessere amplam Nebridius equitum.</div>
 		</div>
 		<button type="button" class="callout-kill"></button>
 	</div>
@@ -34,4 +27,4 @@ const Template: Story<CalloutKillableStory> = (args: CalloutKillableStory) => ({
 });
 
 export const Killable = Template.bind({});
-Killable.args = { palette: '' };
+Killable.args = { };
