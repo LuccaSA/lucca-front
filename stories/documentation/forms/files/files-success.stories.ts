189332f5eaece1e186f2a09d38ba5a0c1af519ab
Jérémie Biron <jbiron@lucca.fr>
2023-05-23 15:51:01 +0200

    file update ui

--- stories/documentation/forms/files/files-success.stories.ts
+++ stories/documentation/forms/files/files-success.stories.ts
@@ -1,25 +1,17 @@
 import { Meta, Story } from '@storybook/angular';
 
 interface FilesSuccessStory {
-	s: boolean;
 }
 
 export default {
 	title: 'Documentation/Forms/Files/Success',
 	argTypes: {
-		s: {
-			control: {
-				type: 'boolean',
-			},
-			description: 'Taille : Small',
-		},
 	},
 } as Meta;
 
 function getTemplate(args: FilesSuccessStory): string {
-	const s = args.s ? `mod-S` : '';
 	return `
-	<label class="file ${s}">
+	<label class="file">
 		<input class="file-input is-uploaded" type="file" title="" accept=".xls,.xlsx,.csv" />
 		<span role="text" class="file-content">
 			<span aria-hidden="true" class="file-icon lucca-icon icon-success u-textSuccess"></span>
@@ -29,7 +21,7 @@ function getTemplate(args: FilesSuccessStory): string {
 				<span class="file-name">filename.xls</span>
 			</span>
 			<span class="file-or">It’s the wrong file?</span>
-			<span class="file-button button mod-outline">Select another file</span>
+			<span class="file-button button mod-outlined">Select another file</span>
 			<span class="u-mask"> – </span>
 			<span class="file-formats u-marginTopXS">Supported file formats: .xls, .xlsx .csv</span>
 			<span class="file-progress progress">
@@ -37,13 +29,36 @@ function getTemplate(args: FilesSuccessStory): string {
 			</span>
 		</span>
 	</label>
-	`;
+
+	<label class="file mod-S">
+		<input class="file-input is-uploaded" type="file" title="" accept=".xls,.xlsx,.csv" />
+		<span role="text" class="file-content">
+			<span aria-hidden="true" class="file-icon lucca-icon icon-success u-textSuccess"></span>
+			<span aria-hidden="true" class="file-icon lucca-icon icon-error u-textError"></span>
+			<span class="file-titleName">
+				<span class="file-title u-marginBottom0">File uploaded</span>
+				<span class="file-formats">Supported file formats: .xls, .xlsx .csv</span>
+				<span class="file-name">filename.xls</span>
+			</span>
+			<span class="file-or">It’s the wrong file?</span>
+			<span class="file-button button mod-outlined mod-S">Select another file</span>
+			<span class="file-progress progress">
+				<span class="progress-bar" style="width: 50%"></span>
+			</span>
+		</span>
+	</label>
+	`
 }
 
 const Template: Story<FilesSuccessStory> = (args: FilesSuccessStory) => ({
 	props: args,
 	template: getTemplate(args),
+	styles: [
+		`.file {
+			margin-bottom: 1rem;
+		}`,
+	],
 });
 
 export const Success = Template.bind({});
-Success.args = { s: false };
+Success.args = { };
