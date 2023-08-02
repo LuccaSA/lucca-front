b90ed16600569c72a64da2ed151e17e802c30e66
Jérémie Biron <jbiron@lucca.fr>
2023-08-02 11:30:43 +0200

    is-critical > is-error

--- stories/documentation/forms/inline-message/inline-message-status.stories.ts
+++ stories/documentation/forms/inline-message/inline-message-status.stories.ts
@@ -20,7 +20,7 @@ function getTemplate(args: InlineMessageStatusStory): string {
 	const s = args.s ? `mod-S` : '';
 	return `<div class="inlineMessage is-success ${s}"><span aria-hidden="true" class="lucca-icon icon-success"></span>Inline message</div>
 <div class="inlineMessage is-warning ${s}"><span aria-hidden="true" class="lucca-icon icon-warning"></span>Inline message</div>
-<div class="inlineMessage is-critical ${s}"><span aria-hidden="true" class="lucca-icon icon-error"></span>Inline message</div>`;
+<div class="inlineMessage is-error ${s}"><span aria-hidden="true" class="lucca-icon icon-error"></span>Inline message</div>`;
 }
 
 const Template: Story<InlineMessageStatusStory> = (args: InlineMessageStatusStory) => ({
