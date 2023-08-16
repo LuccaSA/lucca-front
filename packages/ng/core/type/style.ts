ea02bc1f5bda6aee96d72d92aeb48a35c2678a4a
Flavien Normand <contact@flavien-normand.fr>
2023-08-16 10:51:06 +0200

    chore: remove unused type SizeMS

--- packages/ng/core/type/style.ts
+++ packages/ng/core/type/style.ts
@@ -2,8 +2,3 @@
  * Available CSS palettes
  */
 export type Palette = 'success' | 'warning' | 'error' | 'primary' | 'grey' | 'none';
-
-/**
- * Size that accepts only medium or small
- */
-export type SizeMS = 'm' | 's';
