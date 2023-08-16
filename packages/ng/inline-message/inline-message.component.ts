0f653cafbeb2c25437ec90d6f7cada82f1e5a124
Flavien Normand <contact@flavien-normand.fr>
2023-08-16 13:17:12 +0200

    feat(new-badge): angular wrapper for inline message
    related to #2186
    
    We should really consider using the new Story format from the beginning.

---
+++ packages/ng/inline-message/inline-message.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'lu-inline-message',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './inline-message.component.html',
+	styleUrls: ['./inline-message.component.scss.css']
+})
+export class InlineMessageComponent {
+	@Input({ required: true })
+	label: string;
+
+	@Input()
+	state: 'success' | 'warning' | 'error' | 'default' = 'default';
+
+	@Input()
+	size: 'S' | 'M' = 'M';
+}
