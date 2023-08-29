ef7b8c29696ba65eea4ad789ea988ce81e2b5fbf
Jérémie Biron <jbiron@lucca.fr>
2023-08-29 14:45:27 +0200

    checkbox

--- stories/documentation/forms/checkbox/checkbox-basic.stories.ts
+++ stories/documentation/forms/checkbox/checkbox-basic.stories.ts
@@ -81,39 +81,27 @@ function getTemplate(args: CheckboxBasicStory): string {
 	const id = args.id;
 	const label = args.label;
 	const message = args.message;
-	const s = args.s ? `mod-S` : '';
-	const disabled = args.disabled ? `disabled="disabled"` : '';
-	const checked = args.checked ? `checked="checked"` : '';
-	const required = args.required ? `aria-required="true"` : '';
-	const mixed = args.mixed ? `aria-checked="mixed"` : '';
-	const invalid = args.invalid ? `aria-invalid="true"` : '';
+	const s = args.s ? ` mod-S` : '';
+	const disabled = args.disabled ? ` disabled="disabled"` : '';
+	const checked = args.checked ? ` checked="checked"` : '';
+	const required = args.required ? ` aria-required="true"` : '';
+	const mixed = args.mixed ? ` aria-checked="mixed"` : '';
+	const invalid = args.invalid ? ` aria-invalid="true"` : '';
 	const help = args.help;
-	const messageState = 'is-' + args.messageState;
+	const messageState = args.messageState ? ' is-' + args.messageState : '';
 
-	return `
-		<div class="checkboxField ${s}">
-			<input type="checkbox"
-				class="checkboxField-input"
-				id="${id}"
-				aria-labelledby="${id}label"
-				aria-describedby="${id}message"
-				${checked}
-				${mixed}
-				${disabled}
-				${required}
-				${invalid}
-			/>
-			<label class="checkboxField-label" for="${id}">
-				<span class="checkboxField-label-input">
-					<span class="checkboxField-label-input-icon" aria-hidden="true"></span>
-				</span>
-				<span class="formLabel" id="${id}label">
-					Label<sup *ngIf="required" class="formLabel-required" aria-hidden="true">*</sup><span aria-hidden="true" class="lucca-icon icon-helpOutline" *ngIf="help"></span>
-				</span>
-			</label>
-			<div class="inlineMessage ${messageState}" id="${id}message" *ngIf="message"><span aria-hidden="true" class="lucca-icon"></span>${message}</div>
-		</div>
-	`;
+	return `<div class="checkboxField${s}">
+	<input type="checkbox" class="checkboxField-input" id="${id}" aria-labelledby="${id}label" aria-describedby="${id}message" ${checked}${mixed}${disabled}${required}${invalid} />
+	<label class="checkboxField-label" for="${id}">
+		<span class="checkboxField-label-input">
+			<span class="checkboxField-label-input-icon" aria-hidden="true"></span>
+		</span>
+		<span class="formLabel" id="${id}label">
+			Label<sup *ngIf="required" class="formLabel-required" aria-hidden="true">*</sup><span aria-hidden="true" class="lucca-icon icon-helpOutline" *ngIf="help"></span>
+		</span>
+	</label>
+	<div class="inlineMessage${messageState}" id="${id}message" *ngIf="message"><span aria-hidden="true" class="lucca-icon"></span>${message}</div>
+</div>`;
 }
 
 const Template: StoryFn<CheckboxBasicStory> = (args: CheckboxBasicStory) => ({
