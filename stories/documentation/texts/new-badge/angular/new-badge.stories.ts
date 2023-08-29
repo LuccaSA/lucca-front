368cdcb65d428477e7f32ddcf593bcd303b4d596
Jérémie Biron <jbiron@lucca.fr>
2023-08-29 15:41:28 +0200

    new badge

--- stories/documentation/texts/new-badge/angular/new-badge.stories.ts
+++ stories/documentation/texts/new-badge/angular/new-badge.stories.ts
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/angular';
 import { NewBadgeComponent } from "@lucca-front/ng/new-badge";
 
 export default {
-	title: 'Documentation/Texts/NewBadge/Basic',
+	title: 'Documentation/Texts/NewBadge/Angular/Basic',
 	component: NewBadgeComponent
 } as Meta;
 
