f4bf6aa27e607261e40a0ae703088ce61dd0494a
Flavien Normand <contact@flavien-normand.fr>
2023-10-02 11:36:01 +0200

    chore(form-field): final checks and fixes

--- stories/documentation/forms/fields/textfield.stories.ts
+++ stories/documentation/forms/fields/textfield.stories.ts
@@ -1,20 +1,26 @@
 import { TextfieldComponent } from '@lucca-front/ng/forms';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export default {
 	title: 'Documentation/Forms/Fields/TextField',
 	component: TextfieldComponent,
 	decorators: [
 		moduleMetadata({
-			imports: [TextfieldComponent, FormsModule, ReactiveFormsModule],
+			imports: [TextfieldComponent, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
 		}),
 	],
+	argTypes: {
+		tooltip: {
+			type: 'string',
+		},
+	},
 } as Meta;
 
-export const withNgModel: StoryObj<TextfieldComponent> = {
+export const withNgModel: StoryObj<TextfieldComponent & { disabled: boolean }> = {
 	name: 'With NgModel',
-	render: ({ label, required, hiddenLabel, inlineMessage, size, placeholder, prefix, suffix, inlineMessageState, hasClearer }) => {
+	render: ({ label, required, hiddenLabel, inlineMessage, size, placeholder, prefix, suffix, inlineMessageState, hasClearer, disabled, tooltip, hasSearchIcon, searchIcon }) => {
 		return {
 			props: {
 				prefix,
@@ -25,12 +31,16 @@ export const withNgModel: StoryObj<TextfieldComponent> = {
 required="${required}"
 ${hiddenLabel ? 'hiddenLabel' : ''}
 ${hasClearer ? 'hasClearer' : ''}
+${hasSearchIcon ? 'hasSearchIcon' : ''}
+${disabled ? 'disabled' : ''}
 [prefix]="prefix"
 [suffix]="suffix"
 inlineMessage="${inlineMessage}"
 inlineMessageState="${inlineMessageState}"
 size="${size}"
+searchIcon="${searchIcon}"
 placeholder="${placeholder}"
+tooltip="${tooltip}"
 [(ngModel)]="example">
 </lu-textfield>
 
@@ -45,10 +55,14 @@ placeholder="${placeholder}"
 		required: true,
 		hiddenLabel: false,
 		hasClearer: true,
+		hasSearchIcon: false,
+		searchIcon: 'search',
+		disabled: false,
 		inlineMessage: 'Helper Text',
 		inlineMessageState: 'default',
 		size: 'M',
 		placeholder: 'Placeholder',
+		tooltip: '',
 		prefix: {
 			icon: 'dollar',
 			ariaLabel: 'Dollar',
@@ -65,7 +79,7 @@ export const withReactiveForm: StoryObj<TextfieldComponent> = {
 		return {
 			props: {
 				form: new FormGroup({
-					example: new FormControl('', required ? [Validators.required] : []),
+					example: new FormControl({ value: '', disabled: true }, required ? [Validators.required] : []),
 				}),
 				prefix,
 				suffix,
