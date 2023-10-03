dc5aa6407690be27b980487ed2c7ba21732c2f25
Jérémie Biron <jbiron@lucca.fr>
2023-10-03 14:46:24 +0200

    fix story class render

--- stories/documentation/listings/table/table-sortable.stories.ts
+++ stories/documentation/listings/table/table-sortable.stories.ts
@@ -23,36 +23,34 @@ export default {
 
 function getTemplate(args: TableSortableStory): string {
 	const sort = args.ascending ? 'ascending' : 'descending';
-	const alignRight = args.alignRight ? 'mod-alignRight' : null;
-	return `
-	<table class="table">
-		<thead class="table-head">
-			<tr class="table-head-row">
-				<th class="table-head-row-cell ${alignRight}" >
-					Non triable
-				</th>
-				<th class="table-head-row-cell ${alignRight}" aria-sort="none">
-					<button type="button" class="table-head-row-cell-sortableButton">Triable</button>
-				</th>
-				<th class="table-head-row-cell ${alignRight}" aria-sort="${sort}">
-					<button type="button" class="table-head-row-cell-sortableButton">Trié</button>
-				</th>
-			</tr>
-		</thead>
-		<tbody class="table-body">
-			<tr class="table-body-row">
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-			</tr>
-			<tr class="table-body-row">
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-				<td class="table-body-row-cell ${alignRight}">Contenu</td>
-			</tr>
-		</tbody>
-	</table>
-	`;
+	const alignRight = args.alignRight ? ' mod-alignRight' : '';
+	return `<table class="table">
+	<thead class="table-head">
+		<tr class="table-head-row">
+			<th class="table-head-row-cell${alignRight}" >
+				Non triable
+			</th>
+			<th class="table-head-row-cell${alignRight}" aria-sort="none">
+				<button type="button" class="table-head-row-cell-sortableButton">Triable</button>
+			</th>
+			<th class="table-head-row-cell${alignRight}" aria-sort="${sort}">
+				<button type="button" class="table-head-row-cell-sortableButton">Trié</button>
+			</th>
+		</tr>
+	</thead>
+	<tbody class="table-body">
+		<tr class="table-body-row">
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+		</tr>
+		<tr class="table-body-row">
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+			<td class="table-body-row-cell${alignRight}">Contenu</td>
+		</tr>
+	</tbody>
+</table>`;
 }
 
 const Template: StoryFn<TableSortableStory> = (args: TableSortableStory) => ({
