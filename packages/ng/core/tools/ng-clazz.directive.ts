13b41aae4471dfb60d5ad5ba23bb8b1c5a444671
Flavien Normand <contact@flavien-normand.fr>
2023-10-17 10:42:18 +0200

    fix(ngClazz): add constructor proxy for prod DI

--- packages/ng/core/tools/ng-clazz.directive.ts
+++ packages/ng/core/tools/ng-clazz.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, IterableDiffers, KeyValueDiffers, Renderer2 } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 // This directive exists to temporarily resolve a conflict in how directives work, see https://github.com/angular/angular/issues/52072
@@ -8,4 +8,8 @@ import { NgClass } from '@angular/common';
 	standalone: true,
 })
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
-export class NgClazz extends NgClass {}
+export class NgClazz extends NgClass {
+	constructor() {
+		super(inject(IterableDiffers), inject(KeyValueDiffers), inject(ElementRef), inject(Renderer2));
+	}
+}
