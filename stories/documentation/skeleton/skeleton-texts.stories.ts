f81c897a293fcf3cd7c7b3867a717e3c4556fb5b
Jérémie Biron <jbiron@lucca.fr>
2023-10-19 16:31:41 +0200

    skeletons

---
+++ stories/documentation/skeleton/skeleton-texts.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface SkeletonTextsStory {}
+
+export default {
+	title: 'Documentation/Skeleton',
+} as Meta;
+
+function getTemplate(args: SkeletonTextsStory): string {
+	return `<div class="skeleton is-loading">
+	<span class="skeleton-item u-textXXL"></span>
+	<span class="skeleton-item u-textXL"></span>
+	<span class="skeleton-item u-textL"></span>
+	<span class="skeleton-item"></span>
+	<span class="skeleton-item u-textS"></span>
+	<span class="skeleton-item u-textXS"></span>
+</div>`;
+}
+
+const Template: StoryFn<SkeletonTextsStory> = (args: SkeletonTextsStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Texts = Template.bind({});
+Texts.args = {};
