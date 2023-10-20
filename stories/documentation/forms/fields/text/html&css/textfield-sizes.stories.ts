62a913c938a44992fc602266d0814e8f2c304063
Jeremie <jbiron@lucca.fr>
2023-10-20 12:11:18 +0200

    Display S & XS together (#2366)

--- stories/documentation/forms/fields/text/html&css/textfield-sizes.stories.ts
+++ stories/documentation/forms/fields/text/html&css/textfield-sizes.stories.ts
@@ -1,24 +1,25 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
 interface TextfieldSizeStory {
-	size: string;
 }
 
 export default {
 	title: 'Documentation/Forms/Fields/TextField/HTML&CSS',
 	argTypes: {
-		size: {
-			options: ['', 'mod-S', 'mod-XS'],
-			control: {
-				type: 'radio',
-			},
-		},
 	},
 } as Meta;
 
 function getTemplate(args: TextfieldSizeStory): string {
-	const size = ' '+args.size;
-	return `<div class="form-field${size}">
+	return `<div class="form-field mod-S">
+	<label class="formLabel" id="IDlabel" for="ID">Label</label>
+	<div class="textField">
+		<div class="textField-input">
+			<input type="text" id="ID" class="textField-input-value" aria-labelledby="IDlabel" aria-describedby="IDmessage" placeholder="Placeholder" aria-invalid="false" />
+		</div>
+	</div>
+	<div class="inlineMessage" id="IDmessage"><span aria-hidden="true" class="lucca-icon"></span>Helper text</div>
+</div>
+<div class="form-field mod-XS">
 	<label class="formLabel" id="IDlabel" for="ID">Label</label>
 	<div class="textField">
 		<div class="textField-input">
@@ -32,7 +33,12 @@ function getTemplate(args: TextfieldSizeStory): string {
 const Template: StoryFn<TextfieldSizeStory> = (args: TextfieldSizeStory) => ({
 	props: args,
 	template: getTemplate(args),
+	styles: [
+		`.form-field {
+			margin-bottom: var(--spacings-S);
+		}`,
+	],
 });
 
 export const Size = Template.bind({});
-Size.args = { size: 'mod-S' };
+Size.args = { };
