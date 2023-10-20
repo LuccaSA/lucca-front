969c8693d43396224c3358124b1a3672e8061815
Jérémie Biron <jbiron@lucca.fr>
2023-10-20 10:34:51 +0200

    Skeleton in loaders folder

--- stories/documentation/loaders/skeleton/skeleton-circle.stories.ts
+++ stories/documentation/loaders/skeleton/skeleton-circle.stories.ts
@@ -3,7 +3,7 @@ import { Meta, StoryFn } from '@storybook/angular';
 interface SkeletonCircleStory {}
 
 export default {
-	title: 'Documentation/Skeleton',
+	title: 'Documentation/Loaders/Skeleton',
 } as Meta;
 
 function getTemplate(args: SkeletonCircleStory): string {
