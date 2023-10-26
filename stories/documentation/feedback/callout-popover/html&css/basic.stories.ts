980cdba180f804ac0a01a2481ede520a8af86bed
Jérémie Biron <jbiron@lucca.fr>
2023-10-26 10:19:39 +0200

    callout popover

---
+++ stories/documentation/feedback/callout-popover/html&css/basic.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+export default {
+	title: 'Documentation/Feedback/Callout Popover/HTML&CSS',
+	render: () => {
+		return {
+			template: `<button class="calloutPopover"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signInfo"></span>1</button>`
+		}
+	}
+} as Meta;
+
+export const Basic: StoryObj = {};
---
+++ stories/documentation/feedback/callout-popover/html&css/status.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+export default {
+	title: 'Documentation/Feedback/Callout Popover/HTML&CSS',
+	render: () => {
+		return {
+			styles: [`
+				.calloutPopover {
+					margin-right: var(--spacings-XXS);
+				}
+			`],
+			template: `	<button class="calloutPopover palette-success"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signSuccess"></span>1</button>
+<button class="calloutPopover palette-warning"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signWarning"></span>1</button>
+<button class="calloutPopover palette-error"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signError"></span>1</button>`
+		}
+	}
+} as Meta;
+
+export const Status: StoryObj = {};
