c6aa1133cbc6ddf3a9baf7bc3f7f2d523e01df0b
Flavien Normand <contact@flavien-normand.fr>
2023-10-26 12:02:16 +0200

    feat(button): new luButton Angular directive (#2372)

---
+++ stories/documentation/actions/button/button-wrapper.stories.ts
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { ButtonComponent } from '@lucca-front/ng/button';
+
+export default {
+	title: 'Documentation/Actions/Button/Angular',
+	component: ButtonComponent,
+	render: ({ size, block, palette, state, luButton }) => {
+		return {
+			template: `<button luButton${luButton !== 'default' ? `="${luButton}"` : ''} 
+${size !== 'M' ? `size=${size}` : ''}
+${block ? 'block' : ''}
+${palette !== 'none' ? `palette=${palette}` : ''}
+${state !== 'default' ? `state=${state}` : ''}
+>Click me !</button>`,
+		};
+	},
+} as Meta;
+
+export const Template: StoryObj<ButtonComponent> = {
+	args: {
+		size: 'M',
+		block: false,
+		palette: 'none',
+		state: 'default',
+		luButton: 'default',
+	},
+};
