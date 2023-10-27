2065afca0ab1161ac8077c3742f8635ba5fa3c17
Jérémie Biron <jbiron@lucca.fr>
2023-10-27 12:13:23 +0200

    hover style

--- stories/documentation/feedback/callout-popover/html&css/basic.stories.ts
+++ stories/documentation/feedback/callout-popover/html&css/basic.stories.ts
@@ -4,7 +4,7 @@ export default {
 	title: 'Documentation/Feedback/Callout Popover/HTML&CSS',
 	render: () => {
 		return {
-			template: `<button class="calloutPopover"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signInfo"></span>1</button>`
+			template: `<button type="button" class="calloutPopover"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signInfo"></span>1</button>`
 		}
 	}
 } as Meta;
