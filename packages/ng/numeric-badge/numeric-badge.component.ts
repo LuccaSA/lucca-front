bea86c9420876ea863516039b9c2ef1d461e57dc
Flavien Normand <contact@flavien-normand.fr>
2023-10-31 10:45:03 +0100

    refactor(core): components cleanup/upgrade (#2354)

---
+++ packages/ng/numeric-badge/numeric-badge.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component, inject, Input, numberAttribute, OnChanges, SimpleChanges } from '@angular/core';
+import { NgClazz, Palette } from '@lucca-front/ng/core';
+
+@Component({
+	selector: 'lu-numeric-badge',
+	standalone: true,
+	templateUrl: './numeric-badge.component.html',
+	styleUrls: ['./numeric-badge.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	hostDirectives: [NgClazz],
+})
+export class NumericBadgeComponent implements OnChanges {
+	#ngClass = inject(NgClazz);
+
+	@Input({ required: true, transform: numberAttribute })
+	/**
+	 * The value to display, number only.
+	 */
+	value!: number;
+
+	@Input()
+	/**
+	 * The size of the badge
+	 */
+	size: 'XS' | 'S' | 'M' = 'M';
+
+	@Input()
+	/**
+	 * The palette to use for this badge. Defaults to 'none' (inherits parent palette)
+	 */
+	palette: Palette = 'none';
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['palette']?.currentValue || changes['size']?.currentValue) {
+			this.#ngClass.ngClass = ['numericBadge', `palette-${this.palette}`, `mod-${this.size}`];
+		}
+	}
+}
