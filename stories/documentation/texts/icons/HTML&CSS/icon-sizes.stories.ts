692df75e63f4af8d4d2801bb55b6739c732d9653
Jeremie <jbiron@lucca.fr>
2023-10-31 10:55:06 +0100

    test new story format with HTML component (#2375)

--- stories/documentation/texts/icons/HTML&CSS/icon-sizes.stories.ts
+++ stories/documentation/texts/icons/HTML&CSS/icon-sizes.stories.ts
@@ -1,24 +1,17 @@
-import { Meta, StoryFn } from '@storybook/angular';
-
-interface IconSizeStory {}
+import { Meta, StoryObj } from '@storybook/angular';
 
 export default {
 	title: 'Documentation/Texts/Icons/HTML&CSS',
-} as Meta;
-
-function getTemplate(args: IconSizeStory): string {
-	return `<span aria-hidden="true" class="lucca-icon icon-heart mod-XS"></span>
+	render: () => {
+		return {
+			template: `<span aria-hidden="true" class="lucca-icon icon-heart mod-XS"></span>
 <span aria-hidden="true" class="lucca-icon icon-heart mod-S"></span>
 <span aria-hidden="true" class="lucca-icon icon-heart mod-M"></span>
 <span aria-hidden="true" class="lucca-icon icon-heart mod-L"></span>
 <span aria-hidden="true" class="lucca-icon icon-heart mod-XL"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart mod-XXL"></span>`;
-}
+<span aria-hidden="true" class="lucca-icon icon-heart mod-XXL"></span>`
+		}
+	}
+} as Meta;
 
-const Template: StoryFn<IconSizeStory> = (args: IconSizeStory) => ({
-	props: args,
-	template: getTemplate(args),
-});
-
-export const Size = Template.bind({});
-Size.args = {};
+export const Size: StoryObj = {};
