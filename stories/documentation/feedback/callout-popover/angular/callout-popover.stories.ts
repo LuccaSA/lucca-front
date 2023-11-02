85dd87c2619a7eae48f83af219e1d20c3a6269a7
Flavien Normand <contact@flavien-normand.fr>
2023-11-02 11:51:23 +0100

    feat(callout): callout-popover base system

---
+++ stories/documentation/feedback/callout-popover/angular/callout-popover.stories.ts
@@ -0,0 +1,44 @@
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { CalloutFeedbackItemComponent, CalloutFeedbackListComponent, CalloutPopoverComponent } from '@lucca-front/ng/callout';
+import { ButtonComponent } from '@lucca-front/ng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export default {
+	title: 'Documentation/Feedback/Callout Popover/Angular',
+	component: CalloutPopoverComponent,
+	decorators: [
+		moduleMetadata({
+			imports: [CalloutFeedbackItemComponent, CalloutFeedbackListComponent, ButtonComponent, BrowserAnimationsModule],
+		}),
+	],
+	// render: (args: CalloutDisclosureComponent & { iconless: boolean }) => {
+	// 	const { heading, icon, iconless, palette, size } = args;
+	// 	const iconStr = iconless ? '[icon]="null"' : `icon="${icon}"`;
+	// 	// TODO build something to generate these instead of always applying the same logic
+	// 	const paletteStr = palette ? ` palette="${palette}"` : '';
+	// 	const sizeStr = size ? ` size="${size}"` : '';
+	// 	return {
+	// 		template: `<lu-callout-disclosure heading="${heading}"${paletteStr}${sizeStr} ${iconStr}>
+	// 	<ul lu-callout-feedback-list palette="grey">
+	// 		<li lu-callout-feedback-item>
+	// 			<lu-feedback-item-description>
+	// 				 Feedback description.
+	// 			</lu-feedback-item-description>
+	// 			<button lu-feedback-item-action luButton="outlined">Click me !</button>
+	// 			<button lu-feedback-item-action luButton="text">Click me but inverted !</button>
+	// 		</li>
+	// 	</ul>
+	// 	<ul lu-callout-feedback-list palette="grey">
+	// 		<li lu-callout-feedback-item>
+	// 			<lu-feedback-item-description>
+	// 				 Feedback description #2.
+	// 			</lu-feedback-item-description>
+	// 			<button lu-feedback-item-action luButton>Click me !</button>
+	// 		</li>
+	// 	</ul>
+	// </lu-callout-disclosure>`,
+	// 	};
+	// },
+} as Meta;
+
+export const Template: StoryObj<CalloutPopoverComponent> = {};
