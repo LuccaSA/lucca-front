a7d551af64423923e50afaa80adf22f3bb105779
Flavien Normand <contact@flavien-normand.fr>
2023-11-14 17:28:20 +0100

    fix(button): fix compilation error when using luButton without a value (#2414)
    * fix(button): fix compilation error when using luButton without a value
    
    * chore(button): fix stories with new lubButton input type

--- stories/documentation/actions/button/angular/button-counter.stories.ts
+++ stories/documentation/actions/button/angular/button-counter.stories.ts
@@ -12,7 +12,7 @@ export default {
 	],
 	render: ({ size, block, palette, state, luButton }) => {
 		return {
-			template: `<button luButton${luButton !== 'default' ? `="${luButton}"` : ''}
+			template: `<button luButton${luButton !== '' ? `="${luButton}"` : ''}
 ${size !== 'M' ? `size=${size}` : ''}
 ${block ? 'block' : ''}
 ${palette !== 'none' ? `palette=${palette}` : ''}
@@ -28,6 +28,6 @@ export const Basic: StoryObj<ButtonComponent> = {
 		block: false,
 		palette: 'none',
 		state: 'default',
-		luButton: 'default',
+		luButton: '',
 	},
 };
