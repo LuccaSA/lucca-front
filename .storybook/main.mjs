7c73621ef6ecc54c3862ab4b455c914eee8d7d5d
Flavien Normand <contact@flavien-normand.fr>
2023-11-16 14:35:54 +0100

    chore(lint): better quotes and new storybook update

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,18 +1,18 @@
-import { dirname, join } from "path";
-import { URL, fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { fileURLToPath, URL } from 'url';
 
 export default {
 	framework: {
-		name: getAbsolutePath("@storybook/angular"),
-		options: { fastRefresh: true },
+		name: getAbsolutePath('@storybook/angular'),
+		options: { fastRefresh: true }
 	},
 	docs: {
-		autodocs: true, // see below for alternatives
+		autodocs: true // see below for alternatives
 	},
 	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.mdx'],
 	features: { buildStoriesJson: true },
 	staticDirs: ['./public'],
-	addons: [getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-a11y"), getAbsolutePath("@storybook/addon-interactions")],
+	addons: [getAbsolutePath('@storybook/addon-essentials'), getAbsolutePath('@storybook/addon-a11y'), getAbsolutePath('@storybook/addon-interactions')],
 	webpackFinal: (config) => ({
 		...config,
 		resolve: {
@@ -20,12 +20,12 @@ export default {
 			alias: {
 				...config.resolve.alias,
 				'@storybook/blocks': fileURLToPath(new URL('../node_modules/@storybook/blocks', import.meta.url)),
-				'@storybook/docs-tools': fileURLToPath(new URL('../node_modules/@storybook/docs-tools', import.meta.url)),
-			},
-		},
-	}),
+				'@storybook/docs-tools': fileURLToPath(new URL('../node_modules/@storybook/docs-tools', import.meta.url))
+			}
+		}
+	})
 };
 
 function getAbsolutePath(value) {
-    return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, 'package.json')));
 }
