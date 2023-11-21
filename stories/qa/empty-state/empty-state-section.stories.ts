ca6c317c94851f0cf4e4adba12ec911201b278fb
Guillaume Nury <gnury@luccasoftware.com>
2023-11-21 15:09:08 +0100

    feat(safe-content): add save external svg pipe

--- stories/qa/empty-state/empty-state-section.stories.ts
+++ stories/qa/empty-state/empty-state-section.stories.ts
@@ -1,16 +1,24 @@
+import { provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Meta, StoryFn } from '@storybook/angular';
+import { LuSafeExternalSvgPipe } from '@lucca-front/ng/safe-content';
+import { Meta, StoryFn, applicationConfig } from '@storybook/angular';
 
 @Component({
 	standalone: true,
 	selector: 'empty-state-section-stories',
 	templateUrl: './empty-state-section.stories.html',
+	imports: [LuSafeExternalSvgPipe],
 })
 class EmptyStateSectionStory {}
 
 export default {
 	title: 'QA/Empty State/Section',
 	component: EmptyStateSectionStory,
+	decorators: [
+		applicationConfig({
+			providers: [provideHttpClient()],
+		}),
+	],
 } as Meta;
 
 const template: StoryFn<EmptyStateSectionStory> = () => ({});
