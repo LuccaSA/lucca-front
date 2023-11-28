8abc917d5b1b5ffedcb4aea4b6aef7021791fb35
Vincent Valentin <vvalentin@luccasoftware.com>
2023-11-28 09:55:56 +0100

    divider text + action

--- stories/documentation/structure/divider/divider-basic.stories.ts
+++ stories/documentation/structure/divider/divider-basic.stories.ts
@@ -8,11 +8,32 @@ export default {
 
 function getTemplate(args: DividerBasicStory): string {
 	return `
-	Lorem
-	<div class="divider"></div>
-	Ipsum
+	Divider
 	<hr class="divider" />
-	Dolor
+	Decorative divider
+	<div class="divider"></div>
+	Divider with content
+	<div class="divider">Ipsum</div>
+	Divider with small content
+	<div class="divider mod-S">Ipsum</div>
+	Divider with button 
+	<div class="divider"><button class="divider-button" type="button">Ipsum</button></div>
+	Divider with small button 
+	<div class="divider mod-S"><button class="divider-button" type="button">Ipsum</button></div>
+	Divider with action 
+	<div class="divider">
+		<button class="divider-actionIcon" type="button" [luTooltip]="'Modifier'">
+			<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+			<span class="u-mask">Modifier</span>
+		</button>
+	</div>
+	Divider with small action 
+	<div class="divider mod-S">
+		<button class="divider-actionIcon" type="button" [luTooltip]="'Modifier'">
+			<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+			<span class="u-mask">Modifier</span>
+		</button>
+	</div>
 	`;
 }
 
