74b6c6a5957341458470f2c2fd64acc1b1fe9d79
Vincent Valentin <vvalentin@luccasoftware.com>
2024-01-04 09:37:14 +0100

    radiofield

--- stories/documentation/forms/fields/checkbox/html&css/checkboxfield.stories.ts
+++ stories/documentation/forms/fields/checkbox/html&css/checkboxfield.stories.ts
@@ -1,23 +1,23 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
-interface CheckboxBasicStory {
-}
+interface CheckboxBasicStory {}
 
 export default {
 	title: 'Documentation/Forms/Fields/CheckboxField/HTML&CSS',
-	argTypes: {
-	},
+	argTypes: {},
 } as Meta;
 
 function getTemplate(args: CheckboxBasicStory): string {
-	return `<div class="form-field">
+	return `
+<div class="form-field">
 	<label class="formLabel" for="ID">Label</label>
 	<span class="checkboxField">
 		<input type="checkbox" class="checkboxField-input" id="ID" aria-describedby="IDmessage" aria-required="true" />
 		<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
 	</span>
 	<div class="inlineMessage" id="IDmessage">Helper text</div>
-</div>`;
+</div>
+`;
 }
 
 const Template: StoryFn<CheckboxBasicStory> = (args: CheckboxBasicStory) => ({
@@ -26,4 +26,4 @@ const Template: StoryFn<CheckboxBasicStory> = (args: CheckboxBasicStory) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = { };
+Basic.args = {};
---
+++ stories/documentation/forms/fields/radio/html&css/radiofield-disabled.stories.ts
@@ -0,0 +1,37 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface RadioDisabledStory {}
+
+export default {
+	title: 'Documentation/Forms/Fields/RadioField/HTML&CSS',
+	argTypes: {},
+} as Meta;
+
+function getTemplate(args: RadioDisabledStory): string {
+	return `
+<div class="form-field">
+	<label class="formLabel" for="IDradioA">Label A</label>
+	<span class="radioField">
+		<input disabled="disabled" type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" aria-required="true" />
+		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+	</span>
+	<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
+</div>
+<div class="form-field">
+	<label class="formLabel" for="IDradioB">Label B</label>
+	<span class="radioField">
+		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" aria-required="true" />
+		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+	</span>
+	<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
+</div>
+	`;
+}
+
+const Template: StoryFn<RadioDisabledStory> = (args: RadioDisabledStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Disabled = Template.bind({});
+Disabled.args = {};
