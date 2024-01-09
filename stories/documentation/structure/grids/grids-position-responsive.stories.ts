e4e2ff8907e99c41efcde4591dd7e0d38ebc6eeb
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-01-09 10:20:06 +0100

    Demos for responsive grid (#2479)

---
+++ stories/documentation/structure/grids/grids-position-responsive.stories.ts
@@ -0,0 +1,44 @@
+import { Meta, Story } from '@storybook/angular';
+
+interface GridsPositionStory {
+	reverse: boolean;
+}
+
+export default {
+	title: 'Documentation/Structure/Grids/ResponsivePosition',
+	argTypes: {},
+} as Meta;
+
+function getTemplate(args: GridsPositionStory): string {
+	return `<div class="grid" [attr.style]="'--grid-columns: 3; --grid-colspan: 2'">
+	<div class="grid-column" [attr.style]="'--grid-colspan: 1; --grid-rowspan: 2; --grid-columnAtMediaMinS: 3'"><div class="demo">columns 3<br />colspan 1<br />rowspan 2<br />media > S column 3</div></div>
+	<div class="grid-column" [attr.style]="'--grid-rowAtMediaMinS: 2; --grid-columnAtMediaMinS: 1'"><div class="demo">columns 3<br />colspan 2<br />media > S column 1<br />media > S row 2</div></div>
+	<div class="grid-column" [attr.style]="'--grid-rowAtMediaMinS: 1'"><div class="demo">columns 3<br />colspan 2<br />media > S row 1</div></div>
+</div>`;
+}
+
+const Template: Story<GridsPositionStory> = (args: GridsPositionStory) => ({
+	props: args,
+	template: getTemplate(args),
+	styles: [
+		`
+		.demo {
+			background-color: #F3F5FC;
+			padding: var(--spacings-S);
+			border-radius: 1rem;
+			text-align: center;
+			height: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.grid + .grid {
+			margin-top: var(--spacings-L);
+		}
+		`,
+	],
+});
+
+export const Basic = Template.bind({});
+Basic.args = {};
