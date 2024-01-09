3dd96d53747e9cbc568f3cb41a4d29983a7907dd
Jérémie Biron <jbiron@luccasoftware.com>
2024-01-09 11:39:12 +0100

    improve stories + fix disabled color + remove focus radius var

--- stories/documentation/forms/fields/radio/html&css/radiofield-disabled.stories.ts
+++ stories/documentation/forms/fields/radio/html&css/radiofield-disabled.stories.ts
@@ -12,7 +12,7 @@ function getTemplate(args: RadioDisabledStory): string {
 <div class="form-field">
 	<label class="formLabel" for="IDradioA">Label A</label>
 	<span class="radioField">
-		<input disabled="disabled" type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" aria-required="true" />
+		<input disabled="disabled" type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" checked />
 		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
 	</span>
 	<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
@@ -20,7 +20,7 @@ function getTemplate(args: RadioDisabledStory): string {
 <div class="form-field">
 	<label class="formLabel" for="IDradioB">Label B</label>
 	<span class="radioField">
-		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" aria-required="true" />
+		<input disabled="disabled" type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" />
 		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
 	</span>
 	<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
--- stories/documentation/forms/fields/radio/html&css/radiofield-size.stories.ts
+++ stories/documentation/forms/fields/radio/html&css/radiofield-size.stories.ts
@@ -12,7 +12,7 @@ function getTemplate(args: RadioSizeStory): string {
 <div class="form-field mod-S">
 	<label class="formLabel" for="IDradioA">Label A</label>
 	<span class="radioField">
-		<input type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" aria-required="true" />
+		<input type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" checked />
 		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
 	</span>
 	<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
@@ -20,7 +20,7 @@ function getTemplate(args: RadioSizeStory): string {
 <div class="form-field mod-S">
 	<label class="formLabel" for="IDradioB">Label B</label>
 	<span class="radioField">
-		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" aria-required="true" />
+		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" />
 		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
 	</span>
 	<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
