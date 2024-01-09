5fbe2320f7d6a7dca05bb8e1d888a2b172441b3f
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-01-09 12:04:14 +0100

    Radiofield (#2482)
    * radiofield
    
    * improve stories + fix disabled color + remove focus radius var
    
    * arrow story
    
    * remove useless exports
    
    ---------
    
    Co-authored-by: Jérémie Biron <jbiron@luccasoftware.com>

---
+++ stories/documentation/forms/fields/radio/html&css/radiofield-size.stories.ts
@@ -0,0 +1,37 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface RadioSizeStory {}
+
+export default {
+	title: 'Documentation/Forms/Fields/RadioField/HTML&CSS',
+	argTypes: {},
+} as Meta;
+
+function getTemplate(args: RadioSizeStory): string {
+	return `
+<div class="form-field mod-S">
+	<label class="formLabel" for="IDradioA">Label A</label>
+	<span class="radioField">
+		<input type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" checked />
+		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+	</span>
+	<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
+</div>
+<div class="form-field mod-S">
+	<label class="formLabel" for="IDradioB">Label B</label>
+	<span class="radioField">
+		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" />
+		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+	</span>
+	<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
+</div>
+	`;
+}
+
+const Template: StoryFn<RadioSizeStory> = (args: RadioSizeStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Size = Template.bind({});
+Size.args = {};
