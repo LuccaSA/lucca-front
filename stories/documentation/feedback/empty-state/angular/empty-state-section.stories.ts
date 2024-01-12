525f89c3717967f8d3aa6bf371c45c85a08c097a
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-01-12 16:52:14 +0100

    height + stories + background param (#2511)

--- stories/documentation/feedback/empty-state/angular/empty-state-section.stories.ts
+++ stories/documentation/feedback/empty-state/angular/empty-state-section.stories.ts
@@ -1,11 +1,11 @@
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { HttpClientModule } from '@angular/common/http';
 import { ButtonComponent } from '@lucca-front/ng/button';
 import { EmptyStateSectionComponent } from '@lucca-front/ng/empty-state';
-import { HttpClientModule } from '@angular/common/http';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { PaletteArgType } from '../../../../helpers/common-arg-types';
 
 export default {
-	title: 'Documentation/Feedback/Empty State/Angular',
+	title: 'Documentation/Feedback/Empty State Section/Angular',
 	component: EmptyStateSectionComponent,
 	decorators: [
 		moduleMetadata({
@@ -13,9 +13,11 @@ export default {
 		}),
 	],
 	render: (args: EmptyStateSectionComponent) => {
-		const { title, description, center, palette } = args;
+		const { title, description, center, palette, icon } = args;
+		const paramIcon = args.icon === '' ? '' : 'icon="' + args.icon + '"';
 		return {
-			template: `<lu-empty-state-section title="${title}" description="${description}" palette="${palette}"${center ? ' center' : ''}>
+			template: `
+<lu-empty-state-section ${paramIcon} title="${title}" description="${description}" palette="${palette}" ${center ? ' center' : ''}>
 	<button luButton type="button" palette="primary">Button</button>
 	<button luButton="outlined" type="button" palette="primary">Button</button>
 </lu-empty-state-section>`,
@@ -24,14 +26,69 @@ export default {
 	argTypes: {
 		palette: PaletteArgType,
 		icon: {
-			control: 'text',
+			options: [
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/icon-rocket.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknote.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknoteAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknoteActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknoteActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBell.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBulb.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendar.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendarAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendarActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendarActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconChat.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconClock.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconClockAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconClockActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconClockActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCoffee.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCreditCard.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCreditCardAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCreditCardActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCreditCardActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconFolder.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconGift.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconGraduate.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconIDCard.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconIDCardAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconIDCardActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconIDCardActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconLock.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconLockAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconLockActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconLockActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconMail.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconMailAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconMailActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconMailActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconMegaphone.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPaint.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPaper.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPaperAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPaperActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPaperActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconParty.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPicture.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPictureAction.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPictureActionError.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPictureActionSuccess.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconPoc.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconRocket.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconSearch.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconTemperature.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconThumb.svg',
+				'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconWarning.svg',
+			],
+			control: 'select',
 		},
 	},
 } as Meta;
 
 export const Section: StoryObj<EmptyStateSectionComponent> = {
 	args: {
-		icon: 'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknote.svg',
+		icon: 'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/icon-rocket.svg',
 		title: 'Empty state section',
 		description: 'Description can be a string or a ng-template',
 		center: false,
