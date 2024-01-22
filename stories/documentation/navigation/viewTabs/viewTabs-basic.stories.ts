82f61b2325f0bcd60dc1a03d8fd6a96026f935bc
Vincent Valentin <vvalentin@luccasoftware.com>
2024-01-22 17:39:37 +0100

    viewTabs

--- stories/documentation/navigation/viewTabs/viewTabs-basic.stories.ts
+++ stories/documentation/navigation/viewTabs/viewTabs-basic.stories.ts
@@ -1,13 +1,13 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
-interface TabsBasicStory {
+interface viewTabsBasicStory {
 	size: string;
 	withNumericBadge: boolean;
 	modVertical: boolean;
 }
 
 export default {
-	title: 'Documentation/Navigation/Tabs/Basic',
+	title: 'Documentation/Navigation/viewTabs/Basic',
 	argTypes: {
 		size: {
 			options: ['', 'mod-S', 'mod-XS'],
@@ -35,30 +35,30 @@ export default {
 	},
 } as Meta;
 
-function getTemplate(args: TabsBasicStory): string {
+function getTemplate(args: viewTabsBasicStory): string {
 	const vertical = args.modVertical ? 'mod-vertical' : '';
 
 	return `
-<ul class="tablist ${args.size} ${vertical}" role="tablist">
-	<li class="tablist-item" role="presentation">
-		<button class="tablist-item-tab" type="button" role="tab" id="tab1" aria-controls="panel1" [attr.aria-selected]="tabActive === 1" [attr.tabindex]="tabActive === 1 ? null : '-1'">
+<ul class="viewTabs ${args.size} ${vertical}" role="tablist">
+	<li class="viewTabs-item" role="presentation">
+		<button class="viewTabs-item-tab" type="button" role="tab" id="tab1" aria-controls="panel1" [attr.aria-selected]="tabActive === 1" [attr.tabindex]="tabActive === 1 ? null : '-1'">
 			Lorem
 		</button>
 	</li>
-	<li class="tablist-item" role="presentation">
-		<button class="tablist-item-tab" type="button" role="tab" id="tab2" aria-controls="panel2" [attr.aria-selected]="tabActive === 2" [attr.tabindex]="tabActive === 2 ? null : '-1'">
+	<li class="viewTabs-item" role="presentation">
+		<button class="viewTabs-item-tab" type="button" role="tab" id="tab2" aria-controls="panel2" [attr.aria-selected]="tabActive === 2" [attr.tabindex]="tabActive === 2 ? null : '-1'">
 			Ipsum
 			<span class="numericBadge" *ngIf="withNumericBadge">8</span>
 		</button>
 	</li>
-	<li class="tablist-item" role="presentation">
-		<button class="tablist-item-tab" type="button" role="tab" id="tab3" aria-controls="panel3" [attr.aria-selected]="tabActive === 3" [attr.tabindex]="tabActive === 3 ? null : '-1'">
+	<li class="viewTabs-item" role="presentation">
+		<button class="viewTabs-item-tab" type="button" role="tab" id="tab3" aria-controls="panel3" [attr.aria-selected]="tabActive === 3" [attr.tabindex]="tabActive === 3 ? null : '-1'">
 			Dolor sit amet
 			<span class="numericBadge" *ngIf="withNumericBadge">88</span>
 		</button>
 	</li>
-	<li class="tablist-item" role="presentation">
-		<button class="tablist-item-tab" type="button" role="tab" id="tab4" aria-controls="panel4" [attr.aria-selected]="tabActive === 4" [attr.tabindex]="tabActive === 4 ? null : '-1'">
+	<li class="viewTabs-item" role="presentation">
+		<button class="viewTabs-item-tab" type="button" role="tab" id="tab4" aria-controls="panel4" [attr.aria-selected]="tabActive === 4" [attr.tabindex]="tabActive === 4 ? null : '-1'">
 			Consectetur adipisicing elit
 		</button>
 	</li>
@@ -66,25 +66,25 @@ function getTemplate(args: TabsBasicStory): string {
 
 <div class="u-marginTopM"></div>
 
-<div class="tabpanel" [class.active]="tabActive === 1" role="tabpanel" id="panel1" aria-labelledby="tab1" tabindex="0">
+<div class="viewTabsPanel" [class.active]="tabActive === 1" role="tabpanel" id="panel1" aria-labelledby="tab1" tabindex="0">
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, maxime animi perferendis explicabo est eaque ipsa rem, sit quasi sunt corporis iure distinctio. Deleniti deserunt aspernatur est placeat, assumenda provident.
 </div>
 
-<div class="tabpanel" [class.active]="tabActive === 2" role="tabpanel" id="panel2" aria-labelledby="tab2" tabindex="0">
+<div class="viewTabsPanel" [class.active]="tabActive === 2" role="tabpanel" id="panel2" aria-labelledby="tab2" tabindex="0">
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, cumque ad inventore maiores possimus in commodi ea soluta maxime fugiat libero ducimus provident incidunt sit quod! Cum dolorem tempora sit?
 </div>
 
-<div class="tabpanel" [class.active]="tabActive === 3" role="tabpanel" id="panel3" aria-labelledby="tab3" tabindex="0">
+<div class="viewTabsPanel" [class.active]="tabActive === 3" role="tabpanel" id="panel3" aria-labelledby="tab3" tabindex="0">
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita optio voluptates distinctio nam facere consequatur magni suscipit dolore earum molestiae esse placeat commodi, voluptas atque. Maxime, molestiae. Doloremque, reprehenderit numquam.
 </div>
 
-<div class="tabpanel" [class.active]="tabActive === 4" role="tabpanel" id="panel4" aria-labelledby="tab4" tabindex="0">
+<div class="viewTabsPanel" [class.active]="tabActive === 4" role="tabpanel" id="panel4" aria-labelledby="tab4" tabindex="0">
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias amet placeat deserunt nisi. Vitae delectus animi a voluptate, nisi voluptatum perspiciatis? Quisquam quam, eius molestiae vitae nesciunt iste est non.
 </div>
 	`;
 }
 
-const Template: StoryFn<TabsBasicStory> = (args: TabsBasicStory) => ({
+const Template: StoryFn<viewTabsBasicStory> = (args: viewTabsBasicStory) => ({
 	props: args,
 	template: getTemplate(args),
 });
