44d5e227c39b33ab96c9891622cc928dd72806c4
Jeremie <jbiron@luccasoftware.com>
2024-01-26 14:29:54 +0100

    empty states format stories (#2538)

--- stories/documentation/feedback/empty-state/html&css/empty-state-section-center.stories.ts
+++ stories/documentation/feedback/empty-state/html&css/empty-state-section-center.stories.ts
@@ -18,24 +18,24 @@ export default {
 
 function getTemplate(args: EmptyStateSectionCenterStory): string {
 	return `<section class="emptyState mod-center">
-		<div class="emptyState-container">
-			<div class="emptyState-content">
-				<div
-					class="emptyState-content-icon"
-					aria-hidden="true"
-					[innerHtml]="'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendarAction.svg' | luSafeExternalSvg"
-				></div>
-				<div class="emptyState-content-text">
-					<h3 class="emptyState-content-heading">Empty State</h3>
-					<p>Flatus obsequiorum potest inanes pomerium obsequiorum credi homines vero caelibes orbos potest vile diversitate flatus.</p>
-					<div class="emptyState-actions">
-						<button type="button" class="button">Button</button>
-						<button type="button" class="button mod-outlined">Button</button>
-					</div>
+	<div class="emptyState-container">
+		<div class="emptyState-content">
+			<div
+				class="emptyState-content-icon"
+				aria-hidden="true"
+				[innerHtml]="'https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconCalendarAction.svg' | luSafeExternalSvg"
+			></div>
+			<div class="emptyState-content-text">
+				<h3 class="emptyState-content-heading">Empty State</h3>
+				<p>Flatus obsequiorum potest inanes pomerium obsequiorum credi homines vero caelibes orbos potest vile diversitate flatus.</p>
+				<div class="emptyState-actions">
+					<button type="button" class="button">Button</button>
+					<button type="button" class="button mod-outlined">Button</button>
 				</div>
 			</div>
 		</div>
-	</section>`;
+	</div>
+</section>`;
 }
 
 const Template: StoryFn<EmptyStateSectionCenterStory> = (args: EmptyStateSectionCenterStory) => ({
