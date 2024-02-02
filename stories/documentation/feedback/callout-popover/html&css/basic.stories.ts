ae97bd3e54a6bdb419715364039d77be4a0cbfe4
Jérémie Biron <jbiron@luccasoftware.com>
2024-02-02 12:26:01 +0100

    fix callout S icon position & add dropdown html

--- stories/documentation/feedback/callout-popover/html&css/basic.stories.ts
+++ stories/documentation/feedback/callout-popover/html&css/basic.stories.ts
@@ -4,7 +4,42 @@ export default {
 	title: 'Documentation/Feedback/Callout Popover/HTML&CSS',
 	render: () => {
 		return {
-			template: `<button type="button" class="calloutPopover"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signInfo"></span>1</button>`
+			styles: [`
+				:host {
+					display: flex;
+					flex-direction: column;
+					align-items: flex-start;
+					gap: var(--spacings-XXS);
+				}
+			`],
+			template: `<button type="button" class="calloutPopover"><span aria-hidden="true" class="calloutPopover-icon lucca-icon icon-signInfo"></span>1</button>
+<div class="lu-popover-content calloutPopover-overlay">
+  <div class="calloutPopover-overlay-head">
+    <span aria-hidden="true" class="calloutPopover-overlay-head-icon lucca-icon icon-signInfo"></span>
+    <span class="u-mask">Information</span>
+    <strong class="calloutPopover-overlay-head-title">
+      Titre
+    </strong>
+  </div>
+  <div class="calloutPopover-overlay-content">
+    <ul class="calloutFeedbackList">
+      <li class="calloutFeedbackList-item">
+        <span class="calloutFeedbackList-item-description">Feedback description.</span>
+        <div class="calloutFeedbackList-item-actions">
+          <a href class="button mod-outlined">Button</a>
+          <button type="button" class="button mod-text">Button</button>
+        </div>
+      </li>
+      <li class="calloutFeedbackList-item">
+        <span class="calloutFeedbackList-item-description">Feedback description.</span>
+        <div class="calloutFeedbackList-item-actions">
+          <a href class="button mod-outlined">Button</a>
+          <button type="button" class="button mod-text">Button</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>`
 		}
 	}
 } as Meta;
