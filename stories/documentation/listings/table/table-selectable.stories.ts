238127a13cb4bfe9c4d71b375ca448b873a7b351
Jérémie Biron <jbiron@luccasoftware.com>
2024-02-06 13:59:38 +0100

    add label

--- stories/documentation/listings/table/table-selectable.stories.ts
+++ stories/documentation/listings/table/table-selectable.stories.ts
@@ -13,10 +13,11 @@ function getTemplate(args: TableSelectableStory): string {
 	  <thead class="table-head">
 	    <tr class="table-head-row mod-selectable">
 	      <th class="table-head-row-cell">
-				<span class="checkboxField">
-					<input type="checkbox" class="checkboxField-input" id="CB1" aria-describedby="CB1message" aria-required="true" />
-					<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
-				</span>
+					<span class="checkboxField">
+						<input type="checkbox" class="checkboxField-input" id="CBall" />
+						<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					<label class="u-mask" for="CBall">Label</label>
 				</th>
 	      <th class="table-head-row-cell">Label</th>
 	      <th class="table-head-row-cell">Label</th>
@@ -26,10 +27,11 @@ function getTemplate(args: TableSelectableStory): string {
 	  <tbody class="table-body">
 	    <tr class="table-body-row mod-selectable">
 	      <td class="table-body-row-cell">
-				<span class="checkboxField">
-					<input type="checkbox" class="checkboxField-input" id="CB2" aria-describedby="CB2message" aria-required="true" />
-					<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
-				</span>
+					<span class="checkboxField">
+						<input type="checkbox" class="checkboxField-input" id="CB1" />
+						<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					<label class="u-mask" for="CB1">Label</label>
 				</td>
 	      <td class="table-body-row-cell">Contenu</td>
 	      <td class="table-body-row-cell">Contenu</td>
@@ -37,10 +39,11 @@ function getTemplate(args: TableSelectableStory): string {
 	    </tr>
 	    <tr class="table-body-row mod-selectable">
 	      <td class="table-body-row-cell">
-				<span class="checkboxField">
-					<input type="checkbox" class="checkboxField-input" id="CB3" aria-describedby="CB3message" aria-required="true" />
-					<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
-				</span>
+					<span class="checkboxField">
+						<input type="checkbox" class="checkboxField-input" id="CB2" />
+						<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					<label class="u-mask" for="CB2">Label</label>
 				</td>
 	      <td class="table-body-row-cell">Contenu</td>
 	      <td class="table-body-row-cell">Contenu</td>
