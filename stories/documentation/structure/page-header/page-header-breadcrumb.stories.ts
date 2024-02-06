2401c040b58242a2222539b2042c016afcf32128
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-02-06 11:13:16 +0100

    Remove section in header (#2507)
    * remove section
    
    * better formating

--- stories/documentation/structure/page-header/page-header-breadcrumb.stories.ts
+++ stories/documentation/structure/page-header/page-header-breadcrumb.stories.ts
@@ -59,11 +59,13 @@ function getTemplate(args: PageHeaderBreadcrumbStory): string {
 				</button>
 			</div>
 		</div>
-		<section class="pageHeader-description">
-			<p class="u-marginBottom0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo scelerisque, blandit nibh quis, imperdiet justo.
-				Nullam condimentum nulla et neque ultricies bibendum <a target="_blank">Lien<span aria-hidden="true" class="lucca-icon icon-arrowExternal mod-XS u-marginLeftXXS"></span></a>.
+		<div class="pageHeader-description">
+			<p class="u-marginBottom0">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo scelerisque, blandit nibh quis, imperdiet justo.
+				Nullam condimentum nulla et neque ultricies bibendum
+				<a target="_blank">Lien<span aria-hidden="true" class="lucca-icon icon-arrowExternal mod-XS u-marginLeftXXS"></span></a>.
 			</p>
-		</section>
+		</div>
 	</header>
 	`;
 }
