99367b7f1a03fa2057c2db8872b5f99201c86b7a
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-02-13 16:52:01 +0100

    Fieldset for inputs radio (#2527)
    * fieldset-radio
    
    * invalid state + wrapper
    
    * form-fieldset

--- stories/documentation/forms/fields/radio/html&css/radiofield-invalid.stories.ts
+++ stories/documentation/forms/fields/radio/html&css/radiofield-invalid.stories.ts
@@ -9,22 +9,28 @@ export default {
 
 function getTemplate(args: RadioInvalidStory): string {
 	return `
-<div class="form-field">
-	<label class="formLabel" for="IDradioA">Label A</label>
-	<span class="radioField">
-		<input aria-invalid="true" type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA" checked />
-		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
-	</span>
-	<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
-</div>
-<div class="form-field">
-	<label class="formLabel" for="IDradioB">Label B</label>
-	<span class="radioField">
-		<input type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB" />
-		<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
-	</span>
-	<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
-</div>
+<fieldset class="form-fieldset">
+	<legend class="formLabel">
+		Text<sup class="formLabel-required" aria-hidden="true">*</sup>
+	</legend>
+	<div class="form-field">
+		<label class="formLabel" for="IDradioA">Label A</label>
+		<span class="radioField">
+			<input aria-invalid="true" type="radio" class="radioField-input" id="IDradioA" name="radioName1" aria-describedby="IDmessageRadioA IDmessageRadioGlobal" checked />
+			<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+		</span>
+		<div class="inlineMessage" id="IDmessageRadioA">Helper text</div>
+	</div>
+	<div class="form-field">
+		<label class="formLabel" for="IDradioB">Label B</label>
+		<span class="radioField">
+			<input aria-invalid="true" type="radio" class="radioField-input" id="IDradioB" name="radioName1" aria-describedby="IDmessageRadioB IDmessageRadioGlobal" />
+			<span class="radioField-icon" aria-hidden="true"><span class="radioField-icon-check"></span></span>
+		</span>
+		<div class="inlineMessage" id="IDmessageRadioB">Helper text</div>
+	</div>
+	<div class="inlineMessage is-error" id="IDmessageRadioGlobal">Helper text</div>
+</fieldset>
 	`;
 }
 
