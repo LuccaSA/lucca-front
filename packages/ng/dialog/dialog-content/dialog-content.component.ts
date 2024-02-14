f91f22569e5d712fba568faaf8ccffaf3c6468ef
Flavien Normand <contact@flavien-normand.fr>
2024-02-14 13:19:02 +0100

    chore(dialog): use inline template for content too

--- packages/ng/dialog/dialog-content/dialog-content.component.ts
+++ packages/ng/dialog/dialog-content/dialog-content.component.ts
@@ -1,13 +1,10 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { CheckboxInputComponent } from '@lucca-front/ng/forms';
-import { FormFieldComponent } from '@lucca-front/ng/form-field';
 
 @Component({
 	selector: 'lu-dialog-content',
 	standalone: true,
-	imports: [CheckboxInputComponent, FormFieldComponent],
-	templateUrl: './dialog-content.component.html',
-	styleUrl: './dialog-content.component.scss',
+	imports: [],
+	template: '<ng-content></ng-content>',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
 	host: {
