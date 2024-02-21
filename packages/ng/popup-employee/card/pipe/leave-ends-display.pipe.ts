b2755ceb88fa9136485131a3fa67657aa37f0ab3
Arnaud DESFONTAINES <adesfontaines@luccasoftware.com>
2024-02-21 18:15:33 +0100

    add(feat): popover changes

--- packages/ng/popup-employee/card/pipe/leave-ends-display.pipe.ts
+++ packages/ng/popup-employee/card/pipe/leave-ends-display.pipe.ts
@@ -1,6 +1,6 @@
 import { inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { isToday, isTomorrow } from 'date-fns';
-import { getIntl } from '../../../core/translate';
+import { getIntl } from '@lucca-front/ng/core';
 import { LU_POPUP_EMPLOYEE_TRANSLATIONS } from '../../popup-employee.translate';
 import { formatDate } from '@angular/common';
 
@@ -11,12 +11,22 @@ import { formatDate } from '@angular/common';
 export class LeaveEndsDisplayPipe implements PipeTransform {
 	intl = getIntl(LU_POPUP_EMPLOYEE_TRANSLATIONS);
 	locale = inject(LOCALE_ID);
-	public transform(leaveEndsOn: Date): string {
+	public transform(leaveEndsOn: Date, leaveEndIsFirstHalfDay: boolean): string {
 		if (isToday(leaveEndsOn)) {
-			return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_TODAY_LABEL;
+			if (leaveEndIsFirstHalfDay) {
+				return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_TODAY_LABEL_MORNING;
+			} else {
+				return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_TODAY_LABEL;
+			}
 		} else if (isTomorrow(leaveEndsOn)) {
+			if (leaveEndIsFirstHalfDay) {
+				return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_TOMORROW_MORNING_LABEL;
+			}
 			return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_TOMORROW_LABEL;
 		} else {
+			if (leaveEndIsFirstHalfDay) {
+				return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_MORNING_DATE_LABEL.replace('{{date}}', formatDate(leaveEndsOn, 'longDate', this.locale));
+			}
 			return this.intl.EMPLOYEE_CARD_PANEL_UNTIL_DATE_LABEL.replace('{{date}}', formatDate(leaveEndsOn, 'longDate', this.locale));
 		}
 	}
