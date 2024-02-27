6a0ab1a700f3a0392cd3c9043afa786212d3f2f1
Flavien Normand <contact@flavien-normand.fr>
2024-02-27 15:45:12 +0100

    fix(dialog): template-driven directive and style fixes (#2594)

--- stories/documentation/overlays/dialog/dialog-basic.stories.ts
+++ stories/documentation/overlays/dialog/dialog-basic.stories.ts
@@ -7,23 +7,25 @@ interface DialogBasicStory {
 function getTemplate(args: DialogBasicStory): string {
 	return `
 <div class="dialog_backdrop"></div>
-<div role="dialog" aria-modal="true" aria-labelledby="dialogInsideHeaderTitle1" class="dialog ${args.size}">
-	<form class="dialog-inside">
-		<header class="dialog-inside-header">
-			<button type="button" class="dialog-inside-header-button button">
-				<span class="lucca-icon icon-signClose" aria-hidden="true"></span>
-				<span class="u-mask">Fermer</span>
-			</button>
-			<h1 class="dialog-inside-header-title" id="dialogInsideHeaderTitle1">Title</h1>
-		</header>
-		<div class="dialog-inside-content">dialog</div>
-		<footer class="dialog-inside-footer footer">
-			<div class="footer-actions">
-				<button type="submit" class="button">Action</button>
-				<button type="button" class="button mod-text">Action</button>
-			</div>
-		</footer>
-	</form>
+<div class="dialog">
+	<div role="dialog" aria-modal="true" aria-labelledby="dialogInsideHeaderTitle1" class="dialog ${args.size}">
+		<form class="dialog-inside">
+			<header class="dialog-inside-header">
+				<button type="button" class="dialog-inside-header-button button">
+					<span class="lucca-icon icon-signClose" aria-hidden="true"></span>
+					<span class="u-mask">Fermer</span>
+				</button>
+				<h1 class="dialog-inside-header-title" id="dialogInsideHeaderTitle1">Title</h1>
+			</header>
+			<div class="dialog-inside-content">dialog</div>
+			<footer class="dialog-inside-footer footer">
+				<div class="footer-actions">
+					<button type="submit" class="button">Action</button>
+					<button type="button" class="button mod-text">Action</button>
+				</div>
+			</footer>
+		</form>
+	</div>
 </div>
 	`;
 }
