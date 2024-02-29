4799325aa4caa8bce5bb0d1d5a64cc8768b36971
Bpodevin <bpodevin@luccasoftware.com>
2024-02-29 15:21:43 +0100

    feat(front) : WIP responsive card list

---
+++ stories/documentation/listings/index-table/index-table-responsive-card-list.stories.ts
@@ -0,0 +1,129 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface IndexTableResponsiveCardListStory {
+
+}
+
+export default {
+	title: 'Documentation/Listings/Index Table/Responsive Card List',
+	argTypes: {
+		
+	},
+} as Meta;
+
+function getTemplate(args: IndexTableResponsiveCardListStory): string {
+	return `
+	<div style="resize: horizontal; overflow: auto">
+		<h2 class="u-h3">Basic</h2>
+		<table class="indexTable mod-responsiveCardList u-marginBottomL">
+			<thead class="indexTable-head">
+				<tr class="indexTable-head-row">
+					<th class="indexTable-head-row-cell" scope="col">Label 1</th>
+					<th class="indexTable-head-row-cell" scope="col">Label 2</th>
+					<th class="indexTable-head-row-cell" scope="col">Label 3</th>
+				</tr>
+			</thead>
+			<tbody class="indexTable-body">
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<a href="#" class="indexTable-body-row-cell-action">See details</a>
+						Content
+					</td>
+					<td class="indexTable-body-row-cell">Content</td>
+					<td class="indexTable-body-row-cell">Content</td>
+				</tr>
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<a href="#" class="indexTable-body-row-cell-action">See details</a>
+						Content
+					</td>
+					<td class="indexTable-body-row-cell">Content</td>
+					<td class="indexTable-body-row-cell">Content</td>
+				</tr>
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<a href="#" class="indexTable-body-row-cell-action">See details</a>
+						Content
+					</td>
+					<td class="indexTable-body-row-cell">Content</td>
+					<td class="indexTable-body-row-cell">Content</td>
+				</tr>
+			</tbody>
+		</table>
+		<h2 class="u-h3">With data-name</h2>
+		<table class="indexTable mod-responsiveCardList">
+			<thead class="indexTable-head">
+				<tr class="indexTable-head-row">
+					<th class="indexTable-head-row-cell" scope="col">Label 1</th>
+					<th class="indexTable-head-row-cell" scope="col">Label 2</th>
+					<th class="indexTable-head-row-cell" scope="col">Label 3</th>
+				</tr>
+			</thead>
+			<tbody class="indexTable-body">
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 1">
+							<a href="#" class="indexTable-body-row-cell-action">See details</a>
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 2">
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 3">
+							Content
+						</div>
+					</td>
+				</tr>
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 1">
+							<a href="#" class="indexTable-body-row-cell-action">See details</a>
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 2">
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 3">
+							Content
+						</div>
+					</td>
+				</tr>
+				<tr class="indexTable-body-row">
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 1">
+							<a href="#" class="indexTable-body-row-cell-action">See details</a>
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 2">
+							Content
+						</div>
+					</td>
+					<td class="indexTable-body-row-cell">
+						<div class="indexTable-body-row-cell-content" data-name="Label 3">
+							Content
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	`;
+}
+
+const Template: StoryFn<IndexTableResponsiveCardListStory> = (args) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const ResponsiveCardList = Template.bind({});
+ResponsiveCardList.args = { };
