bfa2d891d7f67f049870a4740c39acc8bfee6e24
Jérémie Biron <jbiron@luccasoftware.com>
2024-03-01 15:41:31 +0100

    HTML documentation for links

--- stories/documentation/actions/links/link-hover.stories.ts
+++ stories/documentation/actions/links/link-hover.stories.ts
@@ -1,22 +1,33 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
-interface LinkHoverStory {}
-
-export default {
-	title: 'Documentation/Actions/Link/Hover',
-	argTypes: {},
-} as Meta;
-
-function getTemplate(args: LinkHoverStory): string {
-	return `
-<a href="#" class="link mod-decorationHover">Lorem ipsum dolor</a>
-`;
+interface LinkDecorationHoverStory {
 }
 
-const Template: StoryFn<LinkHoverStory> = (args: LinkHoverStory) => ({
+export default {
+	title: 'Documentation/Actions/Link/DecorationHover',
+	argTypes: {
+	},
+} as Meta;
+
+function getTemplate(args: LinkDecorationHoverStory): string {
+	return `<a href="#" class="link mod-decorationHover">Lien</a>
+<a class="link mod-decorationHover mod-icon" href="#" target="_blank">Lien externe <span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>`;
+}
+
+const Template: StoryFn<LinkDecorationHoverStory> = (args) => ({
 	props: args,
 	template: getTemplate(args),
+	styles: [
+		`
+		:host {
+			display: block;
+		}
+		a {
+			margin-right: 1rem;
+		}
+		`,
+	],
 });
 
-export const HoverLink = Template.bind({});
-HoverLink.args = {};
+export const DecorationHoverLink = Template.bind({});
+DecorationHoverLink.args = {};
