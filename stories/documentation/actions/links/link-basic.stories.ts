95812c9dcafe23c2dd16b150b8219d3905a2a62a
Jeremie <jbiron@luccasoftware.com>
2024-03-01 15:50:15 +0100

    HTML documentation for links (#2620)

--- stories/documentation/actions/links/link-basic.stories.ts
+++ stories/documentation/actions/links/link-basic.stories.ts
@@ -1,36 +1,17 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
 interface LinkBasicStory {
-	disabled: boolean;
-	decorationHover: boolean;
 }
 
 export default {
 	title: 'Documentation/Actions/Link/Basic',
 	argTypes: {
-		disabled: {
-			options: ['', 'is-disabled'],
-			control: {
-				type: 'boolean',
-			},
-		},
-		decorationHover: {
-			control: {
-				type: 'boolean',
-			},
-		},
 	},
 } as Meta;
 
 function getTemplate(args: LinkBasicStory): string {
-	const disabled = args.disabled ? ` is-disabled` : ' ';
-	const decorationHover = args.decorationHover ? ` mod-decorationHover` : ' ';
-
-	return `<a href="#" class="link${decorationHover}${disabled}">Lien</a>
-<a class="link mod-icon${decorationHover}${disabled}" href="#" target="_blank">Lien externe <span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>
-<a class="link mod-icon${decorationHover}${disabled}" href="#" target="_blank">
-	Lien externe sans retour à la ligne possible avant l’icône<span class="link-icon"><span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span>
-</a>`;
+	return `<a href="#" class="link">Lien</a>
+<a class="link mod-icon" href="#" target="_blank">Lien externe <span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>`;
 }
 
 const Template: StoryFn<LinkBasicStory> = (args) => ({
@@ -49,4 +30,4 @@ const Template: StoryFn<LinkBasicStory> = (args) => ({
 });
 
 export const BasicLink = Template.bind({});
-BasicLink.args = { disabled: false, decorationHover: false };
+BasicLink.args = {};
--- stories/documentation/actions/links/link-disabled.stories.ts
+++ stories/documentation/actions/links/link-disabled.stories.ts
@@ -1,20 +1,32 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
-interface LinkDisabledStory {}
+interface LinkDisabledStory {
+}
 
 export default {
 	title: 'Documentation/Actions/Link/Disabled',
+	argTypes: {
+	},
 } as Meta;
 
 function getTemplate(args: LinkDisabledStory): string {
-	return `
-<span class="link is-disabled">Lorem ipsum dolor</span>
-`;
+	return `<a href="#" class="link is-disabled">Lien</a>
+<a class="link is-disabled mod-icon" href="#" target="_blank">Lien externe <span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>`;
 }
 
-const Template: StoryFn<LinkDisabledStory> = (args: LinkDisabledStory) => ({
+const Template: StoryFn<LinkDisabledStory> = (args) => ({
 	props: args,
 	template: getTemplate(args),
+	styles: [
+		`
+		:host {
+			display: block;
+		}
+		a {
+			margin-right: 1rem;
+		}
+		`,
+	],
 });
 
 export const DisabledLink = Template.bind({});
