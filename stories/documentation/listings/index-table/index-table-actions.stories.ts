652895f543260badb5b16c42bb7cab83018274c3
Bpodevin <bpodevin@luccasoftware.com>
2024-03-05 17:52:57 +0100

    feat(front) : add hover + touch behavior for mod-actions

--- stories/documentation/listings/index-table/index-table-actions.stories.ts
+++ stories/documentation/listings/index-table/index-table-actions.stories.ts
@@ -13,7 +13,8 @@ export default {
 
 function getTemplate(args: IndexTableActionsStory): string {
 	return `
-	<table class="indexTable">
+	<h2 class="u-h3">Simple</h2>
+	<table class="indexTable u-marginBottomL">
 		<thead class="indexTable-head">
 			<tr class="indexTable-head-row">
 				<th class="indexTable-head-row-cell" scope="col">Label</th>
@@ -33,16 +34,10 @@ function getTemplate(args: IndexTableActionsStory): string {
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell mod-actions">
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
 					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
-						<span aria-hidden="true" class="lucca-icon icon-copy"></span>
-					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
-						<span aria-hidden="true" class="lucca-icon icon-archive"></span>
-					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
 					</button>
 				</td>
@@ -55,16 +50,10 @@ function getTemplate(args: IndexTableActionsStory): string {
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell mod-actions">
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
 					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
-						<span aria-hidden="true" class="lucca-icon icon-copy"></span>
-					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
-						<span aria-hidden="true" class="lucca-icon icon-archive"></span>
-					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
 					</button>
 				</td>
@@ -77,16 +66,185 @@ function getTemplate(args: IndexTableActionsStory): string {
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell">Content</td>
 				<td class="indexTable-body-row-cell mod-actions">
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
 					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<h2 class="u-h3">4 or more actions + touch detection</h2>
+	<table class="indexTable u-marginBottomL">
+		<thead class="indexTable-head">
+			<tr class="indexTable-head-row">
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">
+					<span class="u-mask">Actions</span>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="indexTable-body">
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-action">See details</a>
+					Content
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-copy"></span>
 					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-archive"></span>
 					</button>
-					<button type="button" class="button mod-text mod-onlyIcon mod-S">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+					<!-- Implement dropdown on this button clic-->
+					<button type="button" class="button indexTable-body-row-cell-subActionDropdownTrigger mod-text mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-ellipsis"></span>
+					</button>
+				</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-action">See details</a>
+					Content
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-copy"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-archive"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+					<!-- Implement dropdown on this button clic-->
+					<button type="button" class="button mod-text mod-onlyIcon mod-S indexTable-body-row-cell-subActionDropdownTrigger">
+						<span aria-hidden="true" class="lucca-icon icon-ellipsis"></span>
+					</button>
+				</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-action">See details</a>
+					Content
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-copy"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-archive"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+					<!-- Implement dropdown on this button clic-->
+					<button type="button" class="button mod-text mod-onlyIcon mod-S indexTable-body-row-cell-subActionDropdownTrigger">
+						<span aria-hidden="true" class="lucca-icon icon-ellipsis"></span>
+					</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<h2 class="u-h3">Test with mod-selectable</h2>
+	<table class="indexTable mod-selectable">
+		<thead class="indexTable-head">
+			<tr class="indexTable-head-row">
+				<th class="indexTable-head-row-transparentCell" scope="col">
+					<label class="checkbox">
+						<input class="checkbox-input" type="checkbox" />
+						<span class="checkbox-label"></span>
+						<span class="u-mask">Select all</span>
+					</label>
+				</th>
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">Label</th>
+				<th class="indexTable-head-row-cell" scope="col">
+					<span class="u-mask">Actions</span>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="indexTable-body">
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-transparentCell">
+					<label class="checkbox">
+						<input class="checkbox-input" type="checkbox" />
+						<span class="checkbox-label"></span>
+						<span class="u-mask">Select this line</span>
+					</label>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+				</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-transparentCell">
+					<label class="checkbox">
+						<input class="checkbox-input" type="checkbox" />
+						<span class="checkbox-label"></span>
+						<span class="u-mask">Select this line</span>
+					</label>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
+					</button>
+				</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-transparentCell">
+					<label class="checkbox">
+						<input class="checkbox-input" type="checkbox" />
+						<span class="checkbox-label"></span>
+						<span class="u-mask">Select this line</span>
+					</label>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell mod-actions">
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
+						<span aria-hidden="true" class="lucca-icon icon-officePen"></span>
+					</button>
+					<button type="button" class="button mod-text indexTable-body-row-cell-subAction mod-onlyIcon mod-S">
 						<span aria-hidden="true" class="lucca-icon icon-trashDelete"></span>
 					</button>
 				</td>
