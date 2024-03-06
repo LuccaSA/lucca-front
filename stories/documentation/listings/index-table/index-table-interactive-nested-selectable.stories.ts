ac050958a711b3f14022b5ef1d1c79e9733f2af3
Bpodevin <bpodevin@luccasoftware.com>
2024-03-06 18:37:05 +0100

    feat(front) : WIP interactive selectable

--- stories/documentation/listings/index-table/index-table-interactive-nested-selectable.stories.ts
+++ stories/documentation/listings/index-table/index-table-interactive-nested-selectable.stories.ts
@@ -9,28 +9,78 @@ import { Meta, StoryFn } from '@storybook/angular';
 class IndexTableInteractiveNestedSelectableStory {
 	toggleRows(btnID, eIDs) {
 		// Feed the list of ids as a selector
-		var theRows = document.querySelectorAll(eIDs);
+		var targetRows = document.querySelectorAll(eIDs);
 		// Get the button that triggered this
-		var theButton = document.getElementById(btnID);
+		var clickedBtn = document.getElementById(btnID);
 		// If the button is not expanded...
-		console.log(theButton);
-		if (theButton.getAttribute("aria-expanded") == "false") {
-		  // Loop through the rows and show them
-		  for (var i = 0; i < theRows.length; i++) {
-			theRows[i].classList.remove("is-closed");
-		  }
-		  // Now set the button to expanded
-		  theButton.setAttribute("aria-expanded", "true");
+		if (clickedBtn.getAttribute("aria-expanded") == "false") {
+			// Loop through the rows and show them
+			for (var i = 0; i < targetRows.length; i++) {
+				targetRows[i].classList.remove("is-closed");
+			}
+			// Now set the button to expanded
+			clickedBtn.setAttribute("aria-expanded", "true");
 		// Otherwise button is not expanded...
 		} else {
-		  // Loop through the rows and hide them
-		  for (var i = 0; i < theRows.length; i++) {
-			theRows[i].classList.add("is-closed");
-		  }
-		  // Now set the button to collapsed
-		  theButton.setAttribute("aria-expanded", "false");
+			// Loop through the rows and hide them
+			for (var i = 0; i < targetRows.length; i++) {
+				targetRows[i].classList.add("is-closed");
+			}
+			// Now set the button to collapsed
+			clickedBtn.setAttribute("aria-expanded", "false");
 		}
-	  }
+	}
+
+	toggleCheckbox(chbxID, eIDs) {
+		// Feed the list of ids as a selector
+		var childChbxs = document.querySelectorAll(eIDs);
+		// Get the checkbox that triggered this
+		var clickedChbx = document.getElementById(chbxID) as HTMLInputElement;
+		var clickedChbxState = clickedChbx.checked;
+
+		// if this checkbox has childs, propagate her states
+		if(childChbxs) {
+			for (var i = 0; i < childChbxs.length; i++) {
+				var childChbx = childChbxs[i] as HTMLInputElement;
+				childChbx.checked = clickedChbxState;
+			}
+		}
+
+		// reverse state propogation on parents + mixed state management
+		// get all clickedChbx ancestors
+		var parentChbxs = document.querySelectorAll(".checkbox-input[aria-controls*='"+clickedChbx.id+"']");
+		if(parentChbxs) {
+			// for each ancestor
+			for (var i = parentChbxs.length -1 ; i >= 0; i--) {
+				var parentChbx = parentChbxs[i] as HTMLInputElement;
+				// get child list and sum their states (unchecked = 0 ; checked = 1)
+				var parentChbxChildIds = parentChbx.getAttribute("aria-controls").split(" ");
+				var calculatedState = 0;
+				for (var j = 0; j < parentChbxChildIds.length; j++) {
+					var parentChbxChildId = "#" + parentChbxChildIds[j];
+					var parentChbxChild = document.querySelectorAll(parentChbxChildId)[0] as HTMLInputElement;
+
+					if(parentChbxChild.checked){
+						calculatedState++;
+					}
+				}
+				// all childs are unchecked
+				if(calculatedState == 0) {
+					parentChbx.checked = false;
+					parentChbx.removeAttribute("aria-checked");
+				}
+				// all childs are checked
+				else if(calculatedState == parentChbxChildIds.length) {
+					parentChbx.checked = true;
+					parentChbx.removeAttribute("aria-checked");
+				}
+				else {
+					parentChbx.checked = true;
+					parentChbx.setAttribute("aria-checked","mixed");
+				}
+			}
+		}
+	}
 }
 
 export default {
