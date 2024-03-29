21f61d5fd8c1b90ba9f0bf52109cae95007b4d5b
Jeremie <jbiron@luccasoftware.com>
2024-03-29 11:41:18 +0100

    parent story (#2687)

--- stories/documentation/listings/table/table-basic.stories.ts
+++ stories/documentation/listings/table/table-basic.stories.ts
@@ -8,8 +8,6 @@ interface TableBasicStory {
 	s: boolean;
 	borderless: boolean;
 	noOffset: boolean;
-	parentChild: boolean;
-	collapsable: boolean;
 }
 
 export default {
@@ -55,18 +53,6 @@ export default {
 				type: 'boolean',
 			},
 		},
-		parentChild: {
-			control: {
-				type: 'boolean',
-			},
-			description: 'Hiérarchise les lignes avec une logique parents/enfants',
-		},
-		collapsable: {
-			control: {
-				type: 'boolean',
-			},
-			description: "Associé à <code>.mod-parent</code> permet de rendre un parent repliable. <code>.is-collapsed</code> permet de changer d'état.",
-		},
 	},
 } as Meta;
 
@@ -78,9 +64,6 @@ function getTemplate(args: TableBasicStory): string {
 	const s = args.s ? `mod-S` : '';
 	const borderless = args.borderless ? `mod-borderless` : '';
 	const noOffset = args.noOffset ? `mod-noOffset` : '';
-	const parent = args.parentChild ? `mod-parent` : '';
-	const child = args.parentChild ? `mod-child` : '';
-	const collapsable = args.collapsable ? `mod-collapsable` : '';
 	return `
 	<table class="table ${clickable} ${zebra} ${alignTop} ${s} ${borderless} ${noOffset}">
 		<thead class="table-head">
@@ -91,24 +74,24 @@ function getTemplate(args: TableBasicStory): string {
 			</tr>
 		</thead>
 		<tbody class="table-body">
-			<tr class="table-body-row ${parent} ${collapsable}">
+			<tr class="table-body-row">
 				<td class="table-body-row-cell">Contenu</td>
 				<td class="table-body-row-cell">Contenu<br />Contenu</td>
 				<td class="table-body-row-cell ${horizontalAlign}">Contenu</td>
 			</tr>
-			<tr class="table-body-row ${child}">
+			<tr class="table-body-row">
 				<td class="table-body-row-cell">Contenu</td>
 				<td class="table-body-row-cell">Contenu<br />Contenu</td>
 				<td class="table-body-row-cell ${horizontalAlign}">Contenu</td>
 			</tr>
-			<tr class="table-body-row ${child}">
+			<tr class="table-body-row">
 				<td class="table-body-row-cell">Contenu</td>
 				<td class="table-body-row-cell">Contenu<br />Contenu</td>
 				<td class="table-body-row-cell ${horizontalAlign}">Contenu</td>
 			</tr>
 		</tbody>
 		<tfoot class="table-foot">
-			<tr class="table-foot-row ${child}">
+			<tr class="table-foot-row">
 				<td class="table-foot-row-cell">Footer</td>
 				<td class="table-foot-row-cell">Footer</td>
 				<td class="table-foot-row-cell ${horizontalAlign}">Footer</td>
@@ -124,4 +107,4 @@ const Template: StoryFn<TableBasicStory> = (args) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = { clickable: false, zebra: false, alignTop: false, horizontalAlign: '', s: false, borderless: false, noOffset: false, parentChild: false, collapsable: false };
+Basic.args = { clickable: false, zebra: false, alignTop: false, horizontalAlign: '', s: false, borderless: false, noOffset: false };
