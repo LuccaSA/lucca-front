8523db1a4b2d3a498d1b73a8b727e9543b49c806
Marc Fairbrother <mfairbrother@luccasoftware.com>
2024-04-25 17:41:45 +0200

    REF rename component folder

--- packages/ng/skeleton/public-api.ts
+++ packages/ng/skeleton/public-api.ts
@@ -1,2 +1,2 @@
 export * from './skeleton-header/skeleton-header.component';
-export * from './skeleton-textfield/skeleton-text-field.component';
+export * from './skeleton-text-field/skeleton-text-field.component';
