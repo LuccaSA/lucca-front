6d9ce5227c55a0d91c83e9f2f032da7e378785b3
Jérémie Biron <jbiron@luccasoftware.com>
2024-04-30 17:26:43 +0200

    add version to textfield url

--- stories/documentation/forms/fields/text/angular/textfield.stories.ts
+++ stories/documentation/forms/fields/text/angular/textfield.stories.ts
@@ -30,7 +30,7 @@ export default {
 		},
 		type: {
 			options: ['text', 'email', 'password', 'url'],
-			description: '[v17.2] Le type password ajoute automatiquement un bouton pour afficher la valeur du champ.',
+			description: '[v17.2] Le type password ajoute automatiquement un bouton pour afficher la valeur du champ.<br>[v17.4.1] Type : url.',
 			control: {
 				type: 'select',
 			},
