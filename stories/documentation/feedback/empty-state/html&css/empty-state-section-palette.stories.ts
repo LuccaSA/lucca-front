d926f50feda00a53fdad19bfb112a5c693aaf448
Jérémie Biron <jbiron@luccasoftware.com>
2024-05-16 10:48:44 +0200

    add description class & apply margin on actions

--- stories/documentation/feedback/empty-state/html&css/empty-state-section-palette.stories.ts
+++ stories/documentation/feedback/empty-state/html&css/empty-state-section-palette.stories.ts
@@ -27,7 +27,7 @@ function getTemplate(args: EmptyStateSectionPaletteStory): string {
 			></div>
 			<div class="emptyState-content-text">
 				<h3 class="emptyState-content-heading">Empty State</h3>
-				<p>Flatus obsequiorum potest inanes pomerium obsequiorum credi homines vero caelibes orbos potest vile diversitate flatus.</p>
+				<p class="emptyState-content-description">Flatus obsequiorum potest inanes pomerium obsequiorum credi homines vero caelibes orbos potest vile diversitate flatus.</p>
 				<div class="emptyState-actions">
 					<button type="button" class="button">Button</button>
 					<button type="button" class="button mod-outlined">Button</button>
