656b1e996c5336fdb0fda73a9a90c0a85fd110c4
Flavien Normand <contact@flavien-normand.fr>
2024-05-23 10:01:55 +0200

    feat(button): apply default type set to `button` when not present on the button (#2775)

--- packages/ng/button/button.component.ts
+++ packages/ng/button/button.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, ChangeDetectionStrategy, Component, ContentChild, ElementRef, inject, Input, OnChanges, ViewEncapsulation } from '@angular/core';
+import { booleanAttribute, ChangeDetectionStrategy, Component, ContentChild, ElementRef, inject, Input, OnChanges, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { LuClass, Palette } from '@lucca-front/ng/core';
 import { IconComponent } from '@lucca-front/ng/icon';
 
@@ -15,9 +15,10 @@ import { IconComponent } from '@lucca-front/ng/icon';
 		class: 'button',
 	},
 })
-export class ButtonComponent implements OnChanges {
+export class ButtonComponent implements OnChanges, OnInit {
 	#luClass = inject(LuClass);
 	#elementRef = inject<ElementRef<HTMLButtonElement>>(ElementRef);
+	#renderer = inject(Renderer2);
 
 	@Input()
 	size: 'M' | 'S' | 'XS';
@@ -66,6 +67,12 @@ export class ButtonComponent implements OnChanges {
 
 	ngOnChanges(): void {
 		this.updateClasses();
+	}
+
+	ngOnInit(): void {
+		if (this.#elementRef.nativeElement.tagName.toLowerCase() === 'button' && this.#elementRef.nativeElement.getAttribute('type') === null) {
+			this.#renderer.setAttribute(this.#elementRef.nativeElement, 'type', 'button');
+		}
 	}
 
 	updateClasses(): void {
