e8108ff2314f87086c0e0fe90b7e31ff14321202
Guillaume Nury <gnury@luccasoftware.com>
2024-05-29 10:39:44 +0200

    chore: fix tests after angular update

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
 import 'jest-preset-angular/setup-jest';
+import { TextDecoder, TextEncoder } from 'util';
 
 expect.extend(toHaveNoViolations);
 
 Element.prototype.scrollIntoView = () => {};
+
+Object.assign(global, { TextDecoder, TextEncoder });
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // x          Global JEST mocks          x
