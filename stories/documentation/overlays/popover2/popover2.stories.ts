8acc43bb45c211f72c69ebf66d66933eadd9f562
Vincent Valentin <vvalentin@luccasoftware.com>
2024-06-06 10:41:51 +0200

    styles + story

--- stories/documentation/overlays/popover2/popover2.stories.ts
+++ stories/documentation/overlays/popover2/popover2.stories.ts
@@ -15,7 +15,15 @@ export default {
 	render: () => {
 		return {
 			template: `<button luButton [luPopover2]="contentRef">Click me!</button>
-<ng-template #contentRef>Content!</ng-template>
+<ng-template #contentRef>
+<ul class="button-group mod-outlined u-flexDirectionColumn" style="width: 15rem">
+    <li class="button-group-item"><button type="button" class="button mod-outlined">A</button></li>
+    <li class="button-group-item"><button type="button" class="button mod-outlined">B</button></li>
+    <li class="button-group-item"><button type="button" class="button mod-outlined">C</button></li>
+	<li class="button-group-item"><button type="button" class="button mod-outlined">D</button></li>
+	<li class="button-group-item"><button type="button" class="button mod-outlined">E</button></li>
+</ul>
+</ng-template>
 `,
 		};
 	},
