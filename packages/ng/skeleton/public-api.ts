1144275a01ab49e03508df8ff2a2ee354af9b1b1
Jérémie Biron <jbiron@luccasoftware.com>
2024-06-11 15:24:27 +0200

    add skeleton button component

--- packages/ng/skeleton/public-api.ts
+++ packages/ng/skeleton/public-api.ts
@@ -1,2 +1,3 @@
+export * from './skeleton-button/skeleton-button.component';
 export * from './skeleton-header/skeleton-header.component';
 export * from './skeleton-text-field/skeleton-text-field.component';
