4361fdb7faf6feafd49111686e2e56fc2744e984
Adèle Guillerot <aguillerot@luccasoftware.com>
2024-06-12 19:28:05 +0200

    test(numberFormat): suffix and prefix

--- packages/ng/number-format/number-format.spec.ts
+++ packages/ng/number-format/number-format.spec.ts
@@ -1,13 +1,13 @@
 import { NumberFormat } from './number-format';
-import { NumberFormatStyle } from './number-format.models';
+import { NumberFormatCurrencyDisplay, NumberFormatStyle, NumberFormatUnit, NumberFormatUnitDisplay } from './number-format.models';
 
 const LOCALE_FR = 'fr-fr';
+const LOCALE_US = 'en-us';
 
 interface ParseTestData {
-	locale: string;
+	style?: NumberFormatStyle;
 	min?: number | undefined;
 	max?: number | undefined;
-	style: NumberFormatStyle;
 	input: string;
 	cleanInput: string;
 	value: number | null;
@@ -15,24 +15,18 @@ interface ParseTestData {
 
 const parseTests: ParseTestData[] = [
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '123456',
 		cleanInput: '123456',
 		value: 123456,
 	},
 	// By default, max fraction digits is 2
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '123.456',
 		cleanInput: '123.45',
 		value: 123.45,
 	},
 	// Empty input is parsed as null
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '',
 		cleanInput: '',
 		value: null,
@@ -40,8 +34,6 @@ const parseTests: ParseTestData[] = [
 	// Minus sign is kept even if no digits come after
 	// To allow user to pursue without losing the sign
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '-',
 		cleanInput: '-',
 		value: null,
@@ -49,37 +41,27 @@ const parseTests: ParseTestData[] = [
 	// Decimal delimiter is kept even if no digits come after
 	// To allow user to pursue without losing the delimiter
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '.',
 		cleanInput: '.',
 		value: null,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '5.',
 		cleanInput: '5.',
 		value: 5,
 	},
 	// Previous rules can be combined
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '-.',
 		cleanInput: '-.',
 		value: null,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '-.-',
 		cleanInput: '-.',
 		value: null,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '.-.',
 		cleanInput: '.',
 		value: null,
@@ -87,8 +69,6 @@ const parseTests: ParseTestData[] = [
 	// Minus sign is only allowed at the beginning of the input
 	// It will be cleaned out
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '5-',
 		cleanInput: '5',
 		value: 5,
@@ -96,45 +76,36 @@ const parseTests: ParseTestData[] = [
 	// Significant zeros are kept to handle following typing
 	// first 5.0, then 5.01
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '5.0',
 		cleanInput: '5.0',
 		value: 5,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '5.000',
 		cleanInput: '5.00',
 		value: 5,
 	},
 	// Only the first decimal delimiter will be used
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '123.456.789',
 		cleanInput: '123.45',
 		value: 123.45,
 	},
+];
+
+const percentParseTests: ParseTestData[] = [
 	// Percent is divided by 100
 	{
-		locale: LOCALE_FR,
-		style: 'percent',
 		input: '123',
 		cleanInput: '123',
 		value: 1.23,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'percent',
 		input: '5',
 		cleanInput: '5',
 		value: 0.05,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'percent',
 		input: '0.05',
 		cleanInput: '0.05',
 		value: 0.0005,
@@ -143,8 +114,6 @@ const parseTests: ParseTestData[] = [
 
 const minMaxTests: ParseTestData[] = [
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '5',
 		min: -10,
 		max: 10,
@@ -152,8 +121,6 @@ const minMaxTests: ParseTestData[] = [
 		value: 5,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '-15',
 		min: -10,
 		max: 10,
@@ -161,8 +128,6 @@ const minMaxTests: ParseTestData[] = [
 		value: -10,
 	},
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '15',
 		min: -10,
 		max: 10,
@@ -171,8 +136,6 @@ const minMaxTests: ParseTestData[] = [
 	},
 	// If values can only be negative, add minus sign by default
 	{
-		locale: LOCALE_FR,
-		style: 'decimal',
 		input: '',
 		min: -10,
 		max: -5,
@@ -181,20 +144,139 @@ const minMaxTests: ParseTestData[] = [
 	},
 ];
 
+interface SuffixPrefixTestData {
+	locale: string;
+	style: NumberFormatStyle;
+	currency?: string;
+	currencyDisplay?: NumberFormatCurrencyDisplay;
+	unit?: NumberFormatUnit;
+	unitDisplay?: NumberFormatUnitDisplay;
+	value: number | null;
+	suffix: string | null;
+	prefix: string | null;
+}
+
+const suffixPrefixCurrencyTests: SuffixPrefixTestData[] = [
+	{
+		locale: LOCALE_FR,
+		style: 'currency',
+		value: 5,
+		currency: 'EUR',
+		currencyDisplay: 'symbol',
+		prefix: null,
+		suffix: '€',
+	},
+	{
+		locale: LOCALE_FR,
+		style: 'currency',
+		value: 5,
+		currency: 'EUR',
+		currencyDisplay: 'name',
+		prefix: null,
+		suffix: 'euros',
+	},
+	{
+		locale: LOCALE_US,
+		style: 'currency',
+		value: 5,
+		currency: 'USD',
+		currencyDisplay: 'symbol',
+		prefix: '$',
+		suffix: null,
+	},
+	{
+		locale: LOCALE_US,
+		style: 'currency',
+		value: 5,
+		currency: 'USD',
+		currencyDisplay: 'name',
+		prefix: null,
+		suffix: 'US dollars',
+	},
+];
+
+const suffixPrefixUnitTests: SuffixPrefixTestData[] = [
+	{
+		locale: LOCALE_US,
+		style: 'unit',
+		value: 5,
+		unit: 'kilometer',
+		unitDisplay: 'short',
+		prefix: null,
+		suffix: 'km',
+	},
+	{
+		locale: LOCALE_US,
+		style: 'unit',
+		value: 5,
+		unit: 'kilometer',
+		unitDisplay: 'long',
+		prefix: null,
+		suffix: 'kilometers',
+	},
+];
+
+const suffixPrefixPercentTests: SuffixPrefixTestData[] = [
+	{
+		locale: LOCALE_US,
+		style: 'percent',
+		value: 5,
+		prefix: null,
+		suffix: '%',
+	},
+];
+
 describe('NumberFormat', () => {
-	it.each<ParseTestData>(parseTests)("should parse '$input' to $value and clean to '$cleanInput'", ({ locale, style, input, cleanInput, value }) => {
-		const numberFormat = new NumberFormat({ locale, style });
+	it.each<ParseTestData>(parseTests)("should parse '$input' to $value and clean to '$cleanInput'", ({ input, cleanInput, value }) => {
+		const numberFormat = new NumberFormat({ locale: LOCALE_FR, style: 'decimal' });
+
+		const parsedInput = numberFormat.parse(input);
+		expect(parsedInput.cleanInput).toBe(cleanInput);
+		expect(parsedInput.value).toBe(value);
+	});
+	it.each<ParseTestData>(percentParseTests)("should parse percent '$input' to $value and clean to '$cleanInput'", ({ input, cleanInput, value }) => {
+		const numberFormat = new NumberFormat({ locale: LOCALE_FR, style: 'percent' });
 
 		const parsedInput = numberFormat.parse(input);
 		expect(parsedInput.cleanInput).toBe(cleanInput);
 		expect(parsedInput.value).toBe(value);
 	});
 
-	it.each<ParseTestData>(minMaxTests)("with range '$min' to '$max' should parse '$input' to $value and clean to '$cleanInput'", ({ locale, min, max, style, input, cleanInput, value }) => {
-		const numberFormat = new NumberFormat({ locale, style, min, max });
+	it.each<ParseTestData>(minMaxTests)("with range '$min' to '$max' should parse '$input' to $value and clean to '$cleanInput'", ({ min, max, input, cleanInput, value }) => {
+		const numberFormat = new NumberFormat({ locale: LOCALE_FR, style: 'decimal', min, max });
 
 		const parsedInput = numberFormat.parse(input);
 		expect(parsedInput.cleanInput).toBe(cleanInput);
 		expect(parsedInput.value).toBe(value);
 	});
+
+	it.each<SuffixPrefixTestData>(suffixPrefixCurrencyTests)(
+		"with currency '$currency' and display '$currencyDisplay', '$value' should get prefix '$prefix' and suffix '$suffix'",
+		({ locale, style, suffix, prefix, value, unit, unitDisplay, currencyDisplay, currency }) => {
+			const numberFormat = new NumberFormat({ locale, style, unit, unitDisplay, currencyDisplay, currency });
+
+			expect(numberFormat.getPrefix(value)).toBe(prefix);
+			expect(numberFormat.getSuffix(value)).toBe(suffix);
+		},
+	);
+
+	it.each<SuffixPrefixTestData>(suffixPrefixUnitTests)(
+		"with unit 'unit' and display '$unitDisplay', '$value' should get prefix '$prefix' and suffix '$suffix'",
+		({ locale, style, suffix, prefix, value, unit, unitDisplay, currencyDisplay, currency }) => {
+			const numberFormat = new NumberFormat({ locale, style, unit, unitDisplay, currencyDisplay, currency });
+
+			expect(numberFormat.getPrefix(value)).toBe(prefix);
+			expect(numberFormat.getSuffix(value)).toBe(suffix);
+		},
+	);
+
+	it.each<SuffixPrefixTestData>(suffixPrefixPercentTests)(
+		"with percent, '$value' should get prefix '$prefix' and suffix '$suffix'",
+		({ locale, style, suffix, prefix, value, unit, unitDisplay, currencyDisplay, currency }) => {
+			const numberFormat = new NumberFormat({ locale, style, unit, unitDisplay, currencyDisplay, currency });
+
+			expect(numberFormat.getPrefix(value)).toBe(prefix);
+			expect(numberFormat.getSuffix(value)).toBe(suffix);
+		},
+	);
 });
