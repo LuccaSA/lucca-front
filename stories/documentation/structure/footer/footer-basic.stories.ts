33a258f991e298b94ac9b84e7eb5306261c311ac
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-06-17 17:20:13 +0200

    Footer: responsive + container + css vars + story (#2843)
    * responsive + container + css vars + story
    
    * remove mixin
    
    * remove class for mod-narrow

--- stories/documentation/structure/footer/footer-basic.stories.ts
+++ stories/documentation/structure/footer/footer-basic.stories.ts
@@ -2,6 +2,9 @@ import { Meta, StoryFn } from '@storybook/angular';
 
 interface FooterBasicStory {
 	sticky: boolean;
+	container: boolean;
+	responsive: string;
+	containerMax: string;
 }
 
 export default {
@@ -12,21 +15,51 @@ export default {
 				type: 'boolean',
 			},
 		},
+		responsive: {
+			options: ['', 'mod-narrow', 'mod-narrowAtMediaMaxM', 'mod-narrowAtMediaMaxS', 'mod-narrowAtMediaMaxXS'],
+			control: {
+				type: 'select',
+			},
+			description: 'Passing no class causes a default switch to XXS.',
+		},
+		container: {
+			type: 'boolean',
+		},
+		containerMax: {
+			options: ['mod-maxXXXL', 'mod-maxXXL', 'mod-maxXL', 'mod-maxL', 'mod-maxM'],
+			control: {
+				type: 'select',
+			},
+		},
 	},
 } as Meta;
 
 function getTemplate(args: FooterBasicStory): string {
-	const sticky = args.sticky ? ` mod-sticky` : '';
-	return `<section class="contentSection">
-	<footer class="footer${sticky}">
+	const sticky = args.sticky ? `mod-sticky` : '';
+	const responsive = args.responsive;
+	const container = args.container;
+	const containerMax = args.containerMax;
+	if (container) {
+		return `
+	<footer class="footer ${sticky} ${responsive}">
+		<div class="footer-containerOptional container ${containerMax}">
+			<div class="footer-content">Content</div>
+			<div class="footer-actions">
+				<button type="button" class="button">Button</button>
+				<button type="button" class="button mod-outlined">Button</button>
+			</div>
+		</div>
+	</footer>`;
+	} else {
+		return `
+	<footer class="footer ${sticky} ${responsive}">
 		<div class="footer-content">Content</div>
 		<div class="footer-actions">
 			<button type="button" class="button">Button</button>
 			<button type="button" class="button mod-outlined">Button</button>
-			<button type="button" class="button mod-text">Button</button>
 		</div>
-	</footer>
-</section>`;
+	</footer>`;
+	}
 }
 
 const Template: StoryFn<FooterBasicStory> = (args) => ({
@@ -35,4 +68,4 @@ const Template: StoryFn<FooterBasicStory> = (args) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = { sticky: false };
+Basic.args = { sticky: false, responsive: '', container: false, containerMax: 'mod-maxL' };
