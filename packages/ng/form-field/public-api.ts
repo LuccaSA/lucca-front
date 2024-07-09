d2dd55653d6b088a833018dda380115ca8d46f02
Guillaume Nury <gnury@luccasoftware.com>
2024-07-09 12:20:33 +0200

    fix(ng): missing translation token exports

--- packages/ng/form-field/public-api.ts
+++ packages/ng/form-field/public-api.ts
@@ -1,4 +1,5 @@
-export * from './form-field.component';
-export * from './input.directive';
 export * from './form-field-size';
+export * from './form-field.component';
 export * from './form-field.token';
+export * from './form-field.translate';
+export * from './input.directive';
