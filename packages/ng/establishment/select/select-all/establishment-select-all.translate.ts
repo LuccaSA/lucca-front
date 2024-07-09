61983e081ae6f6d97c334f2f95a2937192e5cbf1
Guillaume Nury <gnury@luccasoftware.com>
2024-07-09 11:57:35 +0200

    rem(ng): unused translation file

--- packages/ng/establishment/select/select-all/establishment-select-all.translate.ts
+++
@@ -1,5 +0,0 @@
-import { LuTranslation } from '@lucca-front/ng/core';
-import { ILuOptionSelectAllLabel } from '@lucca-front/ng/option';
-import { Translations } from './translations';
-
-export const luEstablishmentSelectAllTranslations: LuTranslation<ILuOptionSelectAllLabel> = Translations;
