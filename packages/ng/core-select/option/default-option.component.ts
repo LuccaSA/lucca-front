468d20c70309732c24dee53158a6cb9416dc822e
Vincent Valentin <vvalentin@luccasoftware.com>
2024-07-10 11:32:42 +0200

    Add ellipsis for multi displayer

--- packages/ng/core-select/option/default-option.component.ts
+++ packages/ng/core-select/option/default-option.component.ts
@@ -7,6 +7,7 @@ import { ILuOptionContext, LU_OPTION_CONTEXT } from './option.token';
 	standalone: true,
 	imports: [AsyncPipe, NgIf],
 	template: `<ng-container *ngIf="context.option$ | async as option">{{ option?.name ?? option }}</ng-container>`,
+	host: { class: 'u-ellipsis' },
 })
 export class LuSimpleSelectDefaultOptionComponent {
 	public context = inject<ILuOptionContext<{ name?: string }>>(LU_OPTION_CONTEXT);
