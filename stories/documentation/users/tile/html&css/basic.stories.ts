ce475d15eaa92941844e3e978513115b1898c830
Jérémie Biron <jbiron@luccasoftware.com>
2024-07-12 16:16:59 +0200

    add documentation story

---
+++ stories/documentation/users/tile/html&css/basic.stories.ts
@@ -0,0 +1,38 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface UserTileBasicStory {
+}
+
+export default {
+	title: 'Documentation/Users/Tile/HTML&CSS/Basic',
+	argTypes: {
+	},
+} as Meta;
+
+function getTemplate(args: UserTileBasicStory): string {
+	return `<div class="userTile">
+	<div class="avatar"></div>
+	<dl class="userTile-content">
+		<dt class="userTile-content-name">Mertens Finn</dt>
+		<dd class="userTile-content-info">Hero</dd>
+	</dl>
+</div>`;
+}
+
+const Template: StoryFn<UserTileBasicStory> = (args) => ({
+	props: args,
+	template: getTemplate(args),
+	styles: [`
+		.avatar {
+			background: lime;
+			height: var(--components-avatar-size, 2.5rem);
+			width: var(--components-avatar-size, 2.5rem);
+			background: var(--palettes-neutral-100) url("https://cdn.lucca.fr/lucca-front/avatars/finn.png") center;
+			background-size: cover;
+			flex-shrink: 0;
+		}
+	`],
+});
+
+export const Basic = Template.bind({});
+Basic.args = { };
