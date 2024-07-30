b369c951bf8a3641ec683c31f7e9a86ed8158daf
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-07-30 17:51:47 +0200

    Add grid display into utilities (#2967)

--- stories/documentation/integration/utilities/display.stories.ts
+++ stories/documentation/integration/utilities/display.stories.ts
@@ -10,9 +10,11 @@ function getTemplate(args: DisplayStory): string {
 	return `<div class="demo-utilityWrapper">
 	<div class="u-displayBlock demo-utility"><code class="code">u-displayBlock</code></div>
 	<div class="u-displayFlex demo-utility"><code class="code">u-displayFlex</code></div>
+	<div class="u-displayGrid demo-utility"><code class="code">u-displayGrid</code></div>
 	<div class="u-displayNone demo-utility"><code class="code">u-displayNone</code></div>
 	<div class="u-displayInlineBlock demo-utility"><code class="code">u-displayInlineBlock</code></div>
 	<div class="u-displayInlineFlex demo-utility"><code class="code">u-displayInlineFlex</code></div>
+	<div class="u-displayInlineGrid demo-utility"><code class="code">u-displayInlineGrid</code></div>
 	<div class="u-displayInline demo-utility"><code class="code">u-displayInline</code></div>
 	<div class="u-displayContents demo-utility"><code class="code">u-displayContents</code></div>
 </div>`;
