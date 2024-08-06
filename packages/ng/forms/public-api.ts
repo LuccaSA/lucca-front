7523cefdfe33df9e77b7571957be76c64c3dea81
Flavien Normand <contact@flavien-normand.fr>
2024-08-06 10:46:54 +0200

    feat(multilanguage-input): new validator and check functions

--- packages/ng/forms/public-api.ts
+++ packages/ng/forms/public-api.ts
@@ -6,6 +6,7 @@ export * from './number-format-input/number-format-input.component';
 export * from './checkbox-input/checkbox-input.component';
 export * from './multilanguage-input/multilanguage-input.component';
 export * from './multilanguage-input/model/multilanguage-translation';
+export * from './multilanguage-input/validators';
 export * from './number-input/number-input.component';
 export * from './radio-group-input/radio-group-input.component';
 export * from './radio-group-input/radio/radio.component';
