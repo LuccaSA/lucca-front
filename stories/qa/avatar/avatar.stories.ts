1cd7b6887efe2012227450057d70a6416224ed0a
Jérémie Biron <jbiron@luccasoftware.com>
2024-08-07 11:57:08 +0200

    avatar basic refactor

---
+++ stories/qa/avatar/avatar.stories.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Meta, StoryFn } from '@storybook/angular';
+
+@Component({
+	standalone: true,
+	selector: 'avatar-stories',
+	templateUrl: './avatar.stories.html',
+	styleUrl: './avatar.stories.scss',
+})
+class AvatarStory {}
+
+export default {
+	title: 'QA/Avatar',
+	component: AvatarStory,
+} as Meta;
+
+const template: StoryFn<AvatarStory> = () => ({});
+
+export const basic = template.bind({});
