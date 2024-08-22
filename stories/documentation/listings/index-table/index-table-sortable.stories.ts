5e1fe38a12ab6e332dc114f30026e3886057340d
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-08-22 16:03:54 +0200

    Fix indexTable, checkbox-radio, tableSortable (#3028)

--- stories/documentation/listings/index-table/index-table-sortable.stories.ts
+++ stories/documentation/listings/index-table/index-table-sortable.stories.ts
@@ -17,69 +17,119 @@ export default {
 } as Meta;
 
 function getTemplate(args: IndexTableSortableStory): string {
-	return `<table class="indexTable">
-	<thead class="indexTable-head">
-		<tr class="indexTable-head-row">
-			<th class="indexTable-head-row-cell" scope="col">Not sortable</th>
-			<th class="indexTable-head-row-cell" scope="col" aria-sort="none">
-				<button type="button" class="tableSortable button">
-					Sortable
-					<span class="tableSortable-arrows">
-						<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
-						<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
-					</span>
-				</button>
-			</th>
-			<th class="indexTable-head-row-cell" scope="col" aria-sort="ascending">
-				<button type="button" class="tableSortable button">
-					Sorted ascending
-					<span class="tableSortable-arrows">
-						<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
-						<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
-					</span>
-				</button>
-			</th>
-			<th class="indexTable-head-row-cell" scope="col" aria-sort="descending">
-				<button type="button" class="tableSortable button">
-					Sorted descending
-					<span class="tableSortable-arrows">
-						<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
-						<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
-					</span>
-				</button>
-			</th>
-			<th class="indexTable-head-row-cell ${args.align}" scope="col" aria-sort="none">
-				<button type="button" class="tableSortable button" onclick="switch (this.parentNode.getAttribute('aria-sort')) { case 'ascending': this.parentNode.setAttribute('aria-sort', 'descending'); break; case 'descending': this.parentNode.setAttribute('aria-sort', 'none'); break; default: this.parentNode.setAttribute('aria-sort', 'ascending'); }">
-					Interactive
-					<span class="tableSortable-arrows">
-						<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
-						<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
-					</span>
-				</button>
-			</th>
-		</tr>
-	</thead>
-	<tbody class="indexTable-body">
-		<tr class="indexTable-body-row">
-			<td class="indexTable-body-row-cell">
-				<a href="#" class="indexTable-body-row-cell-link">Content</a>
-			</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell ${args.align}">Content</td>
-		</tr>
-		<tr class="indexTable-body-row">
-			<td class="indexTable-body-row-cell">
-				<a href="#" class="indexTable-body-row-cell-link">Content</a>
-			</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell">Content</td>
-			<td class="indexTable-body-row-cell ${args.align}">Content</td>
-		</tr>
-	</tbody>
-</table>`;
+	return `
+	<table class="indexTable">
+		<thead class="indexTable-head">
+			<tr class="indexTable-head-row">
+				<th class="indexTable-head-row-cell" scope="col">Not sortable</th>
+				<th class="indexTable-head-row-cell" scope="col">
+					<button type="button" class="tableSortable button">
+						Sortable
+						<span class="tableSortable-arrows">
+							<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
+							<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
+						</span>
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell" scope="col" aria-sort="ascending">
+					<button type="button" class="tableSortable button">
+						Sorted ascending
+						<span class="tableSortable-arrows">
+							<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
+							<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
+						</span>
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell" scope="col" aria-sort="descending">
+					<button type="button" class="tableSortable button">
+						Sorted descending
+						<span class="tableSortable-arrows">
+							<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
+							<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
+						</span>
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell ${args.align}" scope="col" aria-sort="none">
+					<button type="button" class="tableSortable button" onclick="switch (this.parentNode.getAttribute('aria-sort')) { case 'ascending': this.parentNode.setAttribute('aria-sort', 'descending'); break; case 'descending': this.parentNode.setAttribute('aria-sort', 'none'); break; default: this.parentNode.setAttribute('aria-sort', 'ascending'); }">
+						Interactive
+						<span class="tableSortable-arrows">
+							<span class="lucca-icon icon-chevronTop tableSortable-arrows-ascending"></span>
+							<span class="lucca-icon icon-chevronBottom tableSortable-arrows-descending"></span>
+						</span>
+					</button>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="indexTable-body">
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-link">Content</a>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell ${args.align}">Content</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-link">Content</a>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell ${args.align}">Content</td>
+			</tr>
+		</tbody>
+	</table>
+	<br />
+	<table class="indexTable">
+		<thead class="indexTable-head">
+			<tr class="indexTable-head-row">
+				<th class="indexTable-head-row-cell" scope="col">Not sortable</th>
+				<th class="indexTable-head-row-cell" scope="col">
+					<button type="button" class="table-head-row-cell-sortableButton">
+						Sortable
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell" scope="col" aria-sort="ascending">
+					<button type="button" class="table-head-row-cell-sortableButton">
+						Sorted ascending
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell" scope="col" aria-sort="descending">
+					<button type="button" class="table-head-row-cell-sortableButton">
+						Sorted descending
+					</button>
+				</th>
+				<th class="indexTable-head-row-cell ${args.align}" scope="col" aria-sort="none">
+					<button type="button" class="table-head-row-cell-sortableButton" onclick="switch (this.parentNode.getAttribute('aria-sort')) { case 'ascending': this.parentNode.setAttribute('aria-sort', 'descending'); break; case 'descending': this.parentNode.setAttribute('aria-sort', 'none'); break; default: this.parentNode.setAttribute('aria-sort', 'ascending'); }">
+						Interactive
+					</button>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="indexTable-body">
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-link">Content</a>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell ${args.align}">Content</td>
+			</tr>
+			<tr class="indexTable-body-row">
+				<td class="indexTable-body-row-cell">
+					<a href="#" class="indexTable-body-row-cell-link">Content</a>
+				</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell">Content</td>
+				<td class="indexTable-body-row-cell ${args.align}">Content</td>
+			</tr>
+		</tbody>
+	</table>
+`;
 }
 
 const Template: StoryFn<IndexTableSortableStory> = (args) => ({
