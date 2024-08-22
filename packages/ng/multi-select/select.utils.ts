844a5f00f59e5262e2fcbdde29c868213445d820
Guillaume Nury <gnury@luccasoftware.com>
2024-08-22 16:02:01 +0200

    feat(multi-select): add selectionToQueryParams utils

---
+++ packages/ng/multi-select/select.utils.ts
@@ -0,0 +1,8 @@
+import { LuMultiSelectWithSelectAllValue } from './select.model';
+
+export function selectionToQueryParams<T, Key extends string>(key: Key, value: LuMultiSelectWithSelectAllValue<T>, selector: (value: T) => unknown): Partial<Record<Key | `-${Key}`, string>> {
+	if (value.mode === 'all' || value.mode === 'none') {
+		return {};
+	}
+	return { [value.mode === 'include' ? key : `-${key}`]: value.values.map(selector).join(',') } as Record<Key | `-${Key}`, string>;
+}
