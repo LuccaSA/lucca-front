4405d65dd07cf1c13d76d6e58fa3284d23488012
Guillaume Nury <gnury@luccasoftware.com>
2024-08-22 15:59:00 +0200

    ref(multi-select): extract isSelected logic

--- packages/ng/multi-select/select.model.ts
+++ packages/ng/multi-select/select.model.ts
@@ -1,4 +1,18 @@
-import { InjectionToken } from '@angular/core';
+import { Injectable, InjectionToken } from '@angular/core';
+import { LuOptionComparer } from '@lucca-front/ng/core-select';
 import type { LuMultiSelectInputComponent } from './input';
 
 export const MULTI_SELECT_INPUT = new InjectionToken<LuMultiSelectInputComponent<unknown>>('MultiSelectInput');
+@Injectable({
+	providedIn: 'root',
+	useFactory: () => new DefaultIsSelectedStrategy(),
+})
+export abstract class ɵIsSelectedStrategy<TOption> {
+	abstract isSelected(option: TOption, selectedOptions: TOption[], optionComparer: LuOptionComparer<TOption>): boolean;
+}
+
+class DefaultIsSelectedStrategy<TOption> extends ɵIsSelectedStrategy<TOption> {
+	isSelected(option: TOption, selectedOptions: TOption[], optionComparer: LuOptionComparer<TOption>): boolean {
+		return selectedOptions.some((o) => optionComparer(o, option));
+	}
+}
