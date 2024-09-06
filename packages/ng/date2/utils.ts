b65049ab75b756b902d960edfda69563f856860f
Flavien Normand <contact@flavien-normand.fr>
2024-09-06 13:50:55 +0200

    feat(calendar2): base implementation

---
+++ packages/ng/date2/utils.ts
@@ -0,0 +1,14 @@
+import { CalendarWeekDay, CalendarWeekInfo } from './calendar.token';
+import { Day } from 'date-fns';
+
+export function getIntlWeekDay(date: Date): CalendarWeekDay {
+	return (date.getDay() || 7) as CalendarWeekDay;
+}
+
+export function getIntlFirstDayOfWeek(weekInfo: CalendarWeekInfo): CalendarWeekDay {
+	return weekInfo.firstDay;
+}
+
+export function getJSFirstDayOfWeek(weekInfo: CalendarWeekInfo): Day {
+	return (weekInfo.firstDay % 7) as Day;
+}
