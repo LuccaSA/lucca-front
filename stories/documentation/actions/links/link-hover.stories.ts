1dc8248f0b42d6a08ffefb6b919348e5035add8f
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2024-09-09 11:00:31 +0200

    Fix spacing for links (#3052)

--- stories/documentation/actions/links/link-hover.stories.ts
+++ stories/documentation/actions/links/link-hover.stories.ts
@@ -1,17 +1,15 @@
 import { Meta, StoryFn } from '@storybook/angular';
 
-interface LinkDecorationHoverStory {
-}
+interface LinkDecorationHoverStory {}
 
 export default {
 	title: 'Documentation/Actions/Link/DecorationHover',
-	argTypes: {
-	},
+	argTypes: {},
 } as Meta;
 
 function getTemplate(args: LinkDecorationHoverStory): string {
 	return `<a href="#" class="link mod-decorationHover">Lien</a>
-<a class="link mod-decorationHover mod-icon" href="#" target="_blank">Lien externe <span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>`;
+<a class="link mod-decorationHover mod-icon" href="#" target="_blank">Lien externe<!-- no text node here --><span aria-hidden="true" class="lucca-icon icon-arrowExternal"></span><span class="u-mask">Ouvrir dans une nouvelle fenêtre</span></a>`;
 }
 
 const Template: StoryFn<LinkDecorationHoverStory> = (args) => ({
@@ -20,10 +18,8 @@ const Template: StoryFn<LinkDecorationHoverStory> = (args) => ({
 	styles: [
 		`
 		:host {
-			display: block;
-		}
-		a {
-			margin-right: 1rem;
+			display: flex;
+			gap: var(--pr-t-spacings-200);
 		}
 		`,
 	],
