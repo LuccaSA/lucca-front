a490b04160fc84a6c18de7e1311217b41cd9f09b
Guillaume Drouard <gdrouard@lucca.fr>
2024-09-19 21:55:45 +0200

    fix(callout): add getCalloutIcon and getCalloutPalette functions for a better management of callout state

---
+++ packages/ng/callout/callout.utils.ts
@@ -0,0 +1,23 @@
+import { LuccaIcon } from '@lucca-front/icons';
+import { Palette } from '@lucca-front/ng/core';
+import { CalloutState, CalloutStateMap } from './callout-state';
+
+/**
+ * Get icon for callout
+ *
+ * If the icon is defined then it takes priority over the callout state icon
+ */
+export function getCalloutIcon(state: CalloutState | undefined, icon: LuccaIcon | undefined): LuccaIcon | undefined {
+	const stateMap = CalloutStateMap[state] ?? { icon };
+	return icon ?? stateMap.icon;
+}
+
+/**
+ * Get palette for callout
+ *
+ * If the palette is different from "none" then it takes priority over the callout state palette
+ */
+export function getCalloutPalette(state: CalloutState | undefined, palette: Palette): string {
+	const stateMap = CalloutStateMap[state] ?? { palette };
+	return palette !== 'none' ? palette : stateMap.palette;
+}
