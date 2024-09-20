03218a84104762628749ec8f31649928a9a0d38d
Vincent Valentin <vvalentin@luccasoftware.com>
2024-09-20 22:56:34 +0200

    input date style

--- stories/documentation/forms/date2/date-input.stories.ts
+++ stories/documentation/forms/date2/date-input.stories.ts
@@ -1,8 +1,14 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { LOCALE_ID } from '@angular/core';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 import { DateInputComponent } from '../../../../packages/ng/date2/date-input/date-input.component';
 
 export default {
 	title: 'Documentation/Forms/Date2/DateInput',
+	decorators: [
+		applicationConfig({
+			providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+		}),
+	],
 	component: DateInputComponent,
 } as Meta;
 
