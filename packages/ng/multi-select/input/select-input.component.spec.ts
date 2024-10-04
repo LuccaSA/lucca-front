d6a0709e712dfeab6936bb08b8d9b3e46f141c07
Guillaume Nury <gnury@luccasoftware.com>
2024-10-04 11:41:33 +0200

    test(core-select): run test suite on core-select too

--- packages/ng/multi-select/input/select-input.component.spec.ts
+++ packages/ng/multi-select/input/select-input.component.spec.ts
@@ -1,13 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, NgControl } from '@angular/forms';
+import { runALuSelectInputComponentTestSuite } from '../../core-select/input/select-input.component.spec';
 import { LuMultiSelectInputComponent } from './select-input.component';
 
 type Entity = { id: number; name: string };
 
 describe('LuMultiSelectInputComponent', () => {
-	let component: LuMultiSelectInputComponent<Entity>;
 	let fixture: ComponentFixture<LuMultiSelectInputComponent<Entity>>;
-	let nativeElement: HTMLElement;
 	let searchControl: FormControl;
 
 	beforeEach(() => {
@@ -25,78 +24,13 @@ describe('LuMultiSelectInputComponent', () => {
 		});
 
 		fixture = TestBed.createComponent<LuMultiSelectInputComponent<Entity>>(LuMultiSelectInputComponent);
-		component = fixture.componentInstance;
-		nativeElement = fixture.nativeElement as HTMLElement;
 		fixture.detectChanges();
 	});
 
-	it('should openPanel when ArrowDown', () => {
-		// Arrange
-		const event = new KeyboardEvent('keydown', { key: 'ArrowDown' });
-
-		// Act
-		nativeElement.dispatchEvent(event);
-
-		// Assert
-		expect(component.isPanelOpen).toBe(true);
-	});
-
-	it.each(['a', 'A', 'À'])('should openPanel and emit clueChange when pressing %s', (key) => {
-		// Arrange
-		const event = new KeyboardEvent('keydown', { key });
-		const clueChangeSpy = jest.spyOn(component.clueChange, 'emit');
-
-		// Act
-		nativeElement.dispatchEvent(event);
-
-		// Assert
-		expect(component.isPanelOpen).toBe(true);
-		expect(clueChangeSpy).toHaveBeenCalledWith(key);
-	});
-
-	it('should openPanel and but not emit clueChange when pressing Space', () => {
-		// Arrange
-		const event = new KeyboardEvent('keydown', { key: ' ' });
-		const clueChangeSpy = jest.spyOn(component.clueChange, 'emit');
-
-		// Act
-		nativeElement.dispatchEvent(event);
-
-		// Assert
-		expect(component.isPanelOpen).toBe(true);
-		expect(clueChangeSpy).not.toHaveBeenCalled();
-	});
-
-	it('should not open the panel when clicking the clearer', () => {
-		// Arrange
-		component.clearable = true;
-		component.writeValue([{ id: 1, name: 'test' }]);
-		fixture.detectChanges();
-
-		const clearer = nativeElement.querySelector('.multipleSelect-clear');
-
-		if (!(clearer instanceof HTMLElement)) {
-			throw new Error('Clearer not found');
-		}
-
-		// Act
-		clearer.click();
-
-		// Assert
-		expect(component.isPanelOpen).toBe(false);
-		expect(component.value).toEqual([]);
-	});
-
-	it.each(['ArrowLeft', 'ArrowRight', 'Backspace', 'Meta'])('should not open the panel when sending not a letter events (%s)', (key) => {
-		// Arrange
-		const event = new KeyboardEvent('keydown', { key });
-		const clueChangeSpy = jest.spyOn(component.clueChange, 'emit');
-
-		// Act
-		nativeElement.dispatchEvent(event);
-
-		// Assert
-		expect(component.isPanelOpen).toBe(false);
-		expect(clueChangeSpy).not.toHaveBeenCalled();
+	runALuSelectInputComponentTestSuite<Entity[]>({
+		getFixture: () => fixture,
+		exampleValue: [{ id: 1, name: 'test' }],
+		emptyValue: [],
+		clearerSelector: '.multipleSelect-clear',
 	});
 });
