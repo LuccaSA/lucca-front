0e51253cfff1fe3dfca3144ac38d1f77aa9aef58
Flavien Normand <contact@flavien-normand.fr>
2024-10-15 17:28:16 +0200

    feat: new date-input and calendar2 components (#3124)
    * feat(calendar2): base implementation
    
    * styles
    
    * feat(calendar2): keyboard navigation and range support
    
    * month styles
    
    * css vars
    
    * feat(calendar2): add support for overflow days display
    
    * selection in progress
    
    * feat(calendar2): basic structure for date-input
    
    * wip
    
    * input date style
    
    * month year
    
    * feat(calendar2): base implementation
    
    * styles
    
    * feat(calendar2): keyboard navigation and range support
    
    * month styles
    
    * css vars
    
    * feat(calendar2): add support for overflow days display
    
    * selection in progress
    
    * feat(calendar2): basic structure for date-input
    
    * wip
    
    * input date style
    
    * month year
    
    * feat(calendar2): header mode navigation
    
    * chore(calendar2): WIP keyboard navigation
    
    * feat(date-input): CVA implementation
    
    * chore(date-input): code cleanup
    
    * keyboard + range
    
    * chore(date-input): final cleanup and improvements
    
    * chore(date-input): add overflow parameters to date-input
    
    * stories
    
    * clearable
    
    * affix clear
    
    * selected value for clearable
    
    * chore(date-input): better focus management and internal validator
    
    * chore(date-input): expose getDateFormat to public API
    
    * chore(date-input): add placeholder
    
    * chore(date-input): better disabled and tooltip logic
    
    * chore(date-input): stories hotfix
    
    * today
    
    * chore(date-input): hideWeekend input and today button fix
    
    * feat(date-input): docs and mode renaming
    
    * chore: fix build and import paths
    
    * remove default placeholder + fixes
    
    * feat(date-input): focus tabbable date on keyboard open
    
    * feat(date-input): focus input after selecting a date in calendar
    
    * docs(date-input): wording adjustments
    
    ---------
    
    Co-authored-by: Vincent Valentin <vvalentin@luccasoftware.com>

---
+++ packages/ng/date2/calendar2/date-range.ts
@@ -0,0 +1,10 @@
+import { Palette } from '@lucca-front/ng/core';
+import { CalendarMode } from './calendar-mode';
+
+export interface DateRange {
+	class?: `palette-${Palette}` | string;
+	start: Date;
+	end?: Date;
+	label?: string;
+	scope?: CalendarMode;
+}
---
+++ stories/documentation/forms/date2/selectRange.stories.ts
@@ -0,0 +1,53 @@
+import { LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Calendar2Component } from '@lucca-front/ng/date2';
+import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { setDate } from 'date-fns';
+import { DateRange } from 'packages/ng/date2/calendar2/date-range';
+
+export default {
+	title: 'Documentation/Forms/Date2/Calendar',
+	decorators: [
+		moduleMetadata({
+			imports: [Calendar2Component, FormsModule],
+		}),
+		applicationConfig({
+			providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+		}),
+	],
+	argTypes: {},
+	render: (args, { argTypes }) => {
+		return {
+			props: {
+				currentMonth: new Date(),
+				ranges: [
+					{
+						start: setDate(new Date(), 2),
+						end: setDate(new Date(), 8),
+						class: 'palette-lavender',
+						label: 'RTT',
+					},
+					{
+						start: setDate(new Date(), 10),
+						end: setDate(new Date(), 15),
+						class: 'palette-watermelon',
+					},
+					{
+						start: setDate(new Date(), 18),
+						end: setDate(new Date(), 22),
+						class: 'palette-mint',
+					},
+					{
+						start: setDate(new Date(), 26),
+						end: setDate(new Date(), 29),
+					},
+				] as DateRange[],
+			},
+			template: `
+				<lu-calendar2 [hideToday]="false" [showOverflow]="true" [enableOverflow]="true" [ranges]="ranges" [date]="currentMonth" mode="day" (dateClicked)="selected($event)"></lu-calendar2>
+			`,
+		};
+	},
+} as Meta;
+
+export const SelectRange: StoryObj<Calendar2Component> = {};
