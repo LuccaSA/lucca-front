8f2d94d3078cf792f8ff8e5059690d7cbde9ba8d
Flavien Normand <contact@flavien-normand.fr>
2024-10-23 11:16:54 +0200

    chore(prisme): fix test components build

--- packages/prisme/button/button.spec.ts
+++ packages/prisme/button/button.spec.ts
@@ -1,15 +1,15 @@
-import { fakeAsync, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
 	let button: ButtonComponent;
 
-	beforeEach(fakeAsync(() => {
-		TestBed.configureTestingModule({
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
 			imports: [ButtonComponent],
 		}).compileComponents();
 		button = TestBed.createComponent(ButtonComponent).componentInstance;
-	}));
+	});
 
 	it('Should init properly', () => {
 		expect(button).not.toBeUndefined();
