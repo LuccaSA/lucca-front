ec5881a63f107e0860eb84ac12b573dc99dc6d44
Jérémie Biron <jbiron@luccasoftware.com>
2024-10-25 14:51:40 +0200

    format comments

--- stories/documentation/texts/comment/chat.stories.ts
+++ stories/documentation/texts/comment/chat.stories.ts
@@ -8,93 +8,92 @@ export default {
 } as Meta;
 
 function getTemplate(args: CommentChatStory): string {
-	return `
-	<ol class="commentWrapperChat">
-		<li class="commentWrapper">
-			<div class="commentWrapper-item">
-				<div class="comment">
-					<div class="comment-infos">
-						<div class="avatar"></div>
-						<div class="comment-infos-content">
-							<span class="comment-infos-name">Marie Bragoulet</span>&ngsp;
-							<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
-						</div>
+	return `<ol class="commentWrapperChat">
+	<li class="commentWrapper">
+		<div class="commentWrapper-item">
+			<div class="comment">
+				<div class="comment-infos">
+					<div class="avatar"></div>
+					<div class="comment-infos-content">
+						<span class="comment-infos-name">Marie Bragoulet</span>&ngsp;
+						<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
 					</div>
-					<blockquote class="comment-content">
-						<p class="comment-content-text">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus a veniam necessitatibus aut facilis repellendus provident
-							nulla iste neque ex ?
-						</p>
-					</blockquote>
 				</div>
+				<blockquote class="comment-content">
+					<p class="comment-content-text">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus a veniam necessitatibus aut facilis repellendus provident
+						nulla iste neque ex ?
+					</p>
+				</blockquote>
 			</div>
-		</li>
-		<li class="commentWrapper mod-chatAnswer">
-			<div class="commentWrapper-item">
-				<div class="comment">
-					<div class="comment-infos">
-						<div class="avatar"></div>
-						<div class="comment-infos-content">
-							<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
-							<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
-						</div>
+		</div>
+	</li>
+	<li class="commentWrapper mod-chatAnswer">
+		<div class="commentWrapper-item">
+			<div class="comment">
+				<div class="comment-infos">
+					<div class="avatar"></div>
+					<div class="comment-infos-content">
+						<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
+						<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
 					</div>
-					<blockquote class="comment-content">
-						<p class="comment-content-text">Temporibus a veniam necessitatibus aut facilis repellendus provident
-						nulla iste neque ex. Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-					</blockquote>
 				</div>
+				<blockquote class="comment-content">
+					<p class="comment-content-text">Temporibus a veniam necessitatibus aut facilis repellendus provident
+					nulla iste neque ex. Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+				</blockquote>
 			</div>
-		</li>
-		<li class="commentWrapper">
-			<div class="commentWrapper-item">
-				<div class="comment">
-					<div class="comment-infos">
-						<div class="avatar"></div>
-						<div class="comment-infos-content">
-							<span class="comment-infos-name">Marie Bragoulet</span>&ngsp;
-							<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
-						</div>
+		</div>
+	</li>
+	<li class="commentWrapper">
+		<div class="commentWrapper-item">
+			<div class="comment">
+				<div class="comment-infos">
+					<div class="avatar"></div>
+					<div class="comment-infos-content">
+						<span class="comment-infos-name">Marie Bragoulet</span>&ngsp;
+						<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
 					</div>
-					<blockquote class="comment-content">
-						<p class="comment-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-					</blockquote>
 				</div>
+				<blockquote class="comment-content">
+					<p class="comment-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				</blockquote>
 			</div>
-		</li>
-		<li class="commentWrapper mod-chatAnswer">
-			<div class="commentWrapper-item">
-				<div class="comment">
-					<div class="comment-infos">
-						<div class="avatar"></div>
-						<div class="comment-infos-content">
-							<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
-							<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
-						</div>
+		</div>
+	</li>
+	<li class="commentWrapper mod-chatAnswer">
+		<div class="commentWrapper-item">
+			<div class="comment">
+				<div class="comment-infos">
+					<div class="avatar"></div>
+					<div class="comment-infos-content">
+						<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
+						<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
 					</div>
-					<blockquote class="comment-content">
-						<p class="comment-content-text">Lorem ipsum dolor sit amet,</p>
-					</blockquote>
 				</div>
+				<blockquote class="comment-content">
+					<p class="comment-content-text">Lorem ipsum dolor sit amet,</p>
+				</blockquote>
 			</div>
-		</li>
-		<li class="commentWrapper mod-chatAnswer">
-			<div class="commentWrapper-item">
-				<div class="comment">
-					<div class="comment-infos">
-						<div class="avatar"></div>
-						<div class="comment-infos-content">
-							<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
-							<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
-						</div>
+		</div>
+	</li>
+	<li class="commentWrapper mod-chatAnswer">
+		<div class="commentWrapper-item">
+			<div class="comment">
+				<div class="comment-infos">
+					<div class="avatar"></div>
+					<div class="comment-infos-content">
+						<span class="comment-infos-name">Daniel Hernandez</span>&ngsp;
+						<time class="comment-infos-date" datetime="2024-01-04T16:50:00+00:00">Lun. 4 janv. à 16:50</time>
 					</div>
-					<blockquote class="comment-content">
-						<p class="comment-content-text">Lorem</p>
-					</blockquote>
 				</div>
+				<blockquote class="comment-content">
+					<p class="comment-content-text">Lorem</p>
+				</blockquote>
 			</div>
-		</li>
-	</ol>`;
+		</div>
+	</li>
+</ol>`;
 }
 
 const Template: StoryFn<CommentChatStory> = (args) => ({
