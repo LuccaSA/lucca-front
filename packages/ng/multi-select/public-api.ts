b704047e18d7896d9d407902bd44d5fc24dc58c7
Guillaume Nury <gnury@luccasoftware.com>
2024-10-28 18:15:51 +0100

    fix(multi-select): missing export of utils and models

--- packages/ng/multi-select/public-api.ts
+++ packages/ng/multi-select/public-api.ts
@@ -1,3 +1,5 @@
 export * from './displayer/index';
 export * from './input/index';
+export { LuMultiSelection, LuMultiSelectionMode } from './select.model';
 export * from './select.translate';
+export * from './select.utils';
