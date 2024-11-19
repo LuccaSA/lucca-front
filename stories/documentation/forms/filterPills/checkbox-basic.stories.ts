581a33d78a74f815a04547e862403c4c1ea6bd16
Vincent Valentin <vvalentin@luccasoftware.com>
2024-11-19 09:44:22 +0100

    select

--- stories/documentation/forms/filterPills/checkbox-basic.stories.ts
+++ stories/documentation/forms/filterPills/checkbox-basic.stories.ts
@@ -22,9 +22,6 @@ export default {
 		checked: {
 			control: 'boolean',
 		},
-		mixed: {
-			control: 'boolean',
-		},
 		disabled: {
 			control: 'boolean',
 		},
@@ -33,20 +30,19 @@ export default {
 
 function getTemplate(args: CheckboxBasicStory): string {
 	const checkedAttr = args.checked ? `checked="checked"` : ``;
-	const mixedAttr = args.mixed ? `aria-checked="mixed"` : ``;
 	const disabledAttr = args.disabled ? `disabled="disabled"` : ``;
 
 	return `<div class="filterPill">
+	<label for="input1" class="filterPill-label" luTooltip="${args.label}" luTooltipWhenEllipsis="true">
+		${args.label}
+		<span class="filterPill-label-placeholder" aria-hidden="true" data-content-before="${args.label}"></span>
+	</label>
 	<span class="filterPill-checkbox">
-		<input type="checkbox" id="input1" class="filterPill-checkbox-input" ${checkedAttr} ${mixedAttr} ${disabledAttr} />
+		<input type="checkbox" id="input1" class="filterPill-checkbox-input" ${checkedAttr} ${disabledAttr} />
     	<span class="filterPill-checkbox-icon" aria-hidden="true">
       		<span class="filterPill-checkbox-icon-check"></span>
 		</span>
     </span>
-	<label for="input1" class="filterPill-label" luTooltip="${args.label}" luTooltipWhenEllipsis="true" luTooltipOnlyForDisplay="true">
-		${args.label}
-		<span class="filterPill-label-placeholder" aria-hidden="true" data-content-before="${args.label}"></span>
-	</label>
 </div>`;
 }
 
@@ -59,6 +55,5 @@ export const Basic = Template.bind({});
 Basic.args = {
 	label: 'Lorem ipsum dolor',
 	checked: false,
-	mixed: false,
 	disabled: false,
 };
---
+++ stories/documentation/forms/filterPills/select-basic.stories.ts
@@ -0,0 +1,58 @@
+import { IconComponent } from '@lucca-front/ng/icon';
+import { LuTooltipModule } from '@lucca-front/ng/tooltip';
+import { Meta, moduleMetadata, StoryFn } from '@storybook/angular';
+
+interface SelectBasicStory {
+	label: string;
+	value: string;
+	expanded: boolean;
+}
+
+export default {
+	title: 'Documentation/Forms/FiltersPills/Select',
+	decorators: [
+		moduleMetadata({
+			imports: [LuTooltipModule, IconComponent],
+		}),
+	],
+	argTypes: {
+		label: {
+			control: 'text',
+		},
+		value: {
+			control: 'text',
+		},
+		expanded: {
+			control: 'boolean',
+		},
+	},
+} as Meta;
+
+function getTemplate(args: SelectBasicStory): string {
+	const classAttr = args.value !== '' ? `class="filterPill"` : `class="filterPill is-empty"`;
+	const tooltip = args.value !== '' ? `luTooltip="${args.value}"` : ``;
+	return `<div ${classAttr}>
+	<label for="input1" class="filterPill-label" luTooltip="${args.label}" luTooltipWhenEllipsis="true">${args.label}</label>
+	<button class="filterPill-combobox" type="button" id="input1" role="combobox" aria-expanded="${args.expanded}" ${tooltip} luTooltipWhenEllipsis="true">
+		${args.value ? args.value : 'Sélectionner une valeur'}
+	</button>
+	<button type="button" class="filterPill-clear" >
+		<lu-icon icon="signClose" alt="Vider ce champ" size="S" />
+	</button>
+	<button type="button" aria-hidden="true" tabindex="-1" class="filterPill-toggle">
+		<lu-icon icon="arrowChevronBottom" size="S" />
+	</button>
+</div>`;
+}
+
+const Template: StoryFn<SelectBasicStory> = (args) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Basic = Template.bind({});
+Basic.args = {
+	label: 'Lorem ipsum',
+	value: '',
+	expanded: false,
+};
