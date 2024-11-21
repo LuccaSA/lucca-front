e04387beb74f9fb727082c47d33518d70d998390
Guillaume Nury <GuillaumeNury@users.noreply.github.com>
2024-11-21 17:21:00 +0100

    fix(tooltip): too many reflows (#3225)

--- packages/ng/tooltip/trigger/tooltip-trigger.directive.ts
+++ packages/ng/tooltip/trigger/tooltip-trigger.directive.ts
@@ -2,31 +2,31 @@ import { FlexibleConnectedPositionStrategy, HorizontalConnectionPos, OriginConne
 import { ComponentPortal } from '@angular/cdk/portal';
 import {
 	AfterContentInit,
-	booleanAttribute,
-	ChangeDetectorRef,
 	DestroyRef,
 	Directive,
 	ElementRef,
 	HostBinding,
 	HostListener,
-	inject,
-	input,
 	Input,
-	numberAttribute,
 	OnDestroy,
 	Renderer2,
+	booleanAttribute,
+	computed,
+	effect,
+	inject,
+	input,
+	numberAttribute,
 	signal,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { SafeHtml } from '@angular/platform-browser';
 import { LuPopoverPosition } from '@lucca-front/ng/popover';
-import { BehaviorSubject, combineLatest, merge, Observable, startWith, Subject, switchMap, timer } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, merge, startWith, switchMap, timer } from 'rxjs';
 import { debounce, debounceTime, filter, map } from 'rxjs/operators';
 import { LuTooltipPanelComponent } from '../panel';
 import { EllipsisRuler } from './ellipsis.ruler';
 
 let nextId = 0;
-
 @Directive({
 	selector: '[luTooltip]',
 	exportAs: 'luTooltip',
@@ -41,8 +41,6 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 	#ruler = inject(EllipsisRuler);
 
 	#destroyRef = inject(DestroyRef);
-
-	#cdr = inject(ChangeDetectorRef);
 
 	luTooltip = input<string | SafeHtml>();
 
@@ -60,15 +58,7 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 		this.#closeDelay$.next(delay);
 	}
 
-	#disabled = false;
-
-	@Input({ transform: booleanAttribute })
-	set luTooltipDisabled(disabled: boolean) {
-		this.#disabled = disabled;
-		if (disabled) {
-			this.setAccessibilityProperties(null);
-		}
-	}
+	luTooltipDisabled = input(false, { transform: booleanAttribute });
 
 	@Input({ transform: booleanAttribute })
 	luTooltipOnlyForDisplay = false;
@@ -76,8 +66,7 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 	@Input()
 	luTooltipPosition: LuPopoverPosition = 'above';
 
-	@Input({ transform: booleanAttribute })
-	luTooltipWhenEllipsis = false;
+	luTooltipWhenEllipsis = input(false, { transform: booleanAttribute });
 
 	resize$ = new Observable((observer) => {
 		const resizeObserver = new ResizeObserver(() => {
@@ -88,6 +77,22 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 			resizeObserver.disconnect();
 		};
 	});
+
+	#hasEllipsis$ = combineLatest([
+		toObservable(
+			// 1. Group necessary inputs
+			computed(() => ({ whenEllipsis: this.luTooltipWhenEllipsis(), disabled: this.luTooltipDisabled() })),
+		),
+		// Resend resize events to trigger the check
+		this.resize$.pipe(debounceTime(150)),
+	]).pipe(
+		// 2. Keep only necessary inputs
+		filter(([{ whenEllipsis, disabled }]) => !disabled && whenEllipsis),
+		// 3. Check for ellipsis
+		switchMap(() => this.#ruler.hasEllipsis(this.#host.nativeElement)),
+	);
+
+	#hasEllipsis = toSignal(this.#hasEllipsis$, { initialValue: false });
 
 	open$ = new Subject<void>();
 
@@ -122,7 +127,7 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 
 	@HostBinding('attr.aria-describedby')
 	get ariaDescribedBy() {
-		if (this.#disabled || this.luTooltipWhenEllipsis || this.luTooltipOnlyForDisplay) {
+		if (this.luTooltipDisabled() || this.luTooltipWhenEllipsis || this.luTooltipOnlyForDisplay) {
 			return null;
 		}
 		return `${this.#generatedId}-panel`;
@@ -138,12 +143,12 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 					// 	we want the tooltip to be able to close itself no matter what
 					const openEvents$ = this.open$.pipe(
 						filter(() => {
-							if (this.#disabled) {
+							if (this.luTooltipDisabled()) {
 								return false;
 							}
 							// If not disabled, let's check for ellipsis if needed
-							if (this.luTooltipWhenEllipsis) {
-								return this.#ruler.hasEllipsis(this.#host.nativeElement);
+							if (this.luTooltipWhenEllipsis()) {
+								return this.#hasEllipsis();
 							}
 							// If it's not disabled and is not triggered based on ellipsis, just return true
 							return true;
@@ -166,9 +171,12 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 				}
 			});
 
-		this.resize$.pipe(takeUntilDestroyed(this.#destroyRef), debounceTime(150)).subscribe(() => {
-			this.setAccessibilityProperties(0);
-			this.#cdr.markForCheck();
+		effect(() => {
+			if (!this.luTooltipDisabled() && this.#hasEllipsis()) {
+				this.setAccessibilityProperties(0);
+			} else {
+				this.setAccessibilityProperties(null);
+			}
 		});
 	}
 
@@ -218,24 +226,23 @@ export class LuTooltipTriggerDirective implements AfterContentInit, OnDestroy {
 	}
 
 	private setAccessibilityProperties(tabindex: number | null): void {
-		if (this.#disabled || (this.luTooltipWhenEllipsis && !this.#ruler.hasEllipsis(this.#host.nativeElement))) {
+		if (tabindex === null) {
 			this.#renderer.removeAttribute(this.#host.nativeElement, 'tabindex');
 			return;
 		}
 
 		const tag = this.#host.nativeElement.tagName.toLowerCase();
 		const nativelyFocusableTags = ['a', 'button', 'input', 'select', 'textarea'];
-		const isNatevelyFocusableTag = nativelyFocusableTags.includes(tag);
+		const isNativelyFocusableTag = nativelyFocusableTags.includes(tag);
 
 		const hasATabIndex = this.#host.nativeElement.getAttribute('tabindex') !== null;
 
-		if (!isNatevelyFocusableTag && !hasATabIndex) {
+		if (!isNativelyFocusableTag && !hasATabIndex) {
 			this.#renderer.setAttribute(this.#host.nativeElement, 'tabindex', tabindex.toString());
 		}
 	}
 
 	ngAfterContentInit(): void {
-		this.setAccessibilityProperties(0);
 		this._id = this.#host.nativeElement.id || this.#generatedId;
 	}
 
