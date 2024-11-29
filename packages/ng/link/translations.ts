9fa328ca59fed86dc2d9b8b569dc8b98c8093a8f
Vincent Valentin <vvalentin@luccasoftware.com>
2024-11-29 12:32:41 +0100

    translations

--- packages/ng/link/translations.ts
+++ packages/ng/link/translations.ts
@@ -1,8 +1,23 @@
 ﻿export const Translations = {
+	en: {
+		external: '(Open in a new window.)',
+	},
+	de: {
+		external: '(In einem neuen Fenster öffnen.)',
+	},
 	fr: {
 		external: '(Ouvrir dans une nouvelle fenêtre.)',
 	},
-	en: {
-		external: '(Open in a new window.)',
+	it: {
+		external: '(Aprire in una nuova finestra.)',
+	},
+	nl: {
+		external: '(Open in een nieuw venster.)',
+	},
+	es: {
+		external: '(Abrir en una nueva ventana.)',
+	},
+	pt: {
+		external: '(Abrir numa nova janela.)',
 	},
 };
