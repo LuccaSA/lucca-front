5949ed45353f5556f853cb530a812d807b4645cb
Jérémie Biron <jbiron@luccasoftware.com>
2024-12-09 18:25:39 +0100

    Data table skeleton

---
+++ stories/documentation/loaders/skeleton-data-table/angular/skeleton-data-table.stories.ts
@@ -0,0 +1,29 @@
+import { SkeletonDataTableComponent } from '@lucca-front/ng/skeleton';
+import { Meta, StoryObj } from '@storybook/angular';
+
+export default {
+	title: 'Documentation/Loaders/Skeleton/Skeleton DataTable',
+	component: SkeletonDataTableComponent,
+} as Meta;
+
+export const Template: StoryObj<SkeletonDataTableComponent> = {
+	argTypes: {
+		cols: {
+			control: {
+				type: 'number',
+				min: 1,
+			},
+		},
+		rows: {
+			control: {
+				type: 'number',
+				min: 1,
+			},
+		},
+	},
+
+	args: {
+		cols: 5,
+		rows: 10,
+	},
+};
