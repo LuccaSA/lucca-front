2649bc500181a0187a8c99a8f446a64878f6ed70
Jeremie <jbiron@luccasoftware.com>
2024-12-17 15:02:23 +0100

    Update stories/documentation/structure/divider/angular/divider-basic.stories.ts
    Co-authored-by: Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>

--- stories/documentation/structure/divider/angular/divider-basic.stories.ts
+++ stories/documentation/structure/divider/angular/divider-basic.stories.ts
@@ -35,7 +35,7 @@ export default {
 			control: {
 				type: 'boolean',
 			},
-			description: "A préciser que si le divider n'a pas de contenu (le contenu ne sera pas restitué)",
+			description: "À préciser que si le divider n’a pas de contenu (le contenu ne sera pas restitué)",
 		},
 		button: {
 			control: {
