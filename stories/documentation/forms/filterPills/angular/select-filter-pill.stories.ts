973c61d1ae74f457056fd34e304c19523b8e3faf
Flavien Normand <contact@flavien-normand.fr>
2025-01-08 10:03:28 +0100

    chore(filter-pills): add search in select story

--- stories/documentation/forms/filterPills/angular/select-filter-pill.stories.ts
+++ stories/documentation/forms/filterPills/angular/select-filter-pill.stories.ts
@@ -3,14 +3,14 @@ import { FilterPillComponent } from '@lucca-front/ng/filter-pills';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { StoryModelDisplayComponent } from '../../../../helpers/story-model-display.component';
 import { LuSimpleSelectInputComponent } from '@lucca-front/ng/simple-select';
-import { allLegumes } from '@/stories/forms/select/select.utils';
+import { allLegumes, FilterLegumesPipe } from '@/stories/forms/select/select.utils';
 import { JsonPipe } from '@angular/common';
 
 export default {
 	title: 'Documentation/Forms/FiltersPills/Select/Angular',
 	decorators: [
 		moduleMetadata({
-			imports: [FilterPillComponent, LuSimpleSelectInputComponent, FormsModule, StoryModelDisplayComponent, JsonPipe],
+			imports: [FilterPillComponent, LuSimpleSelectInputComponent, FormsModule, StoryModelDisplayComponent, JsonPipe, FilterLegumesPipe],
 		}),
 	],
 	render: (args, context) => {
@@ -19,7 +19,7 @@ export default {
 				example: null,
 				legumes: allLegumes,
 			},
-			template: `<lu-filter-pill label="Légume"><lu-simple-select [(ngModel)]="example" [options]="legumes" /></lu-filter-pill>
+			template: `<lu-filter-pill label="Légume"><lu-simple-select [(ngModel)]="example"	[options]="legumes | filterLegumes:clue"(clueChange)="clue = $event" /></lu-filter-pill>
 
 <pr-story-model-display>{{example | json}}</pr-story-model-display>
 `,
