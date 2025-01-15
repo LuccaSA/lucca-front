662608db52083616100d2a3a9c379c209fe49c7c
pdesiles <pdesiles@luccasoftware.com>
2025-01-15 16:34:55 +0100

    fix: rich-text exports

--- packages/ng/forms/rich-text-input/public-api.ts
+++ packages/ng/forms/rich-text-input/public-api.ts
@@ -1,1 +1,6 @@
 export * from './rich-text-input.component';
+export * from './rich-text-formater';
+// export * from './plugins/headings';
+export * from './plugins/list-format';
+export * from './plugins/tag';
+export * from './plugins/text-style';
