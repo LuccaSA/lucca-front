370c0d40018233de081a6c5f7593dccd497c6e0a
Flavien Normand <contact@flavien-normand.fr>
2025-01-17 17:37:00 +0100

    docs(date-range): add premade shortcuts story (#3341)
    * docs(date-range): add premade shortcuts story
    
    * Remove config from story to add it on documentation
    
    ---------
    
    Co-authored-by: Jérémie Biron <jbiron@luccasoftware.com>

--- stories/documentation/forms/date2/date-range-input.stories.ts
+++ stories/documentation/forms/date2/date-range-input.stories.ts
@@ -1,6 +1,6 @@
 import { LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DateRangeInputComponent } from '@lucca-front/ng/date2';
+import { CalendarShortcut, DateRangeInputComponent, PremadeShortcuts } from '@lucca-front/ng/date2';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
 import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { cleanupTemplate, generateInputs } from '../../../helpers/stories';
@@ -53,6 +53,49 @@ export default {
 } as Meta;
 
 export const Basic: StoryObj<DateRangeInputComponent> = {
+	args: {
+		hideToday: false,
+		hideWeekend: false,
+		clearable: false,
+		mode: 'day',
+	},
+};
+
+const shortcutsStr =
+	"[\n	{\n		label: 'Since start of week',\n		range: PremadeShortcuts['SinceStartOfWeek']('fr'),\n	},\n	{\n		label: 'Last week',\n		range: PremadeShortcuts['LastWeek']('fr'),\n	},\n	{\n		label: 'Last month',\n		range: PremadeShortcuts['LastMonth']('fr'),\n	},\n]";
+
+export const WithShortcuts: StoryObj<DateRangeInputComponent> = {
+	render: (args: any, { argTypes }) => {
+		const { min, max, selected, ...flags } = args;
+		return {
+			props: {
+				min: min ? new Date(min) : null,
+				max: max ? new Date(max) : null,
+				shortcuts: [
+					{
+						label: 'Since start of week',
+						range: PremadeShortcuts['SinceStartOfWeek']('fr'),
+					},
+					{
+						label: 'Last week',
+						range: PremadeShortcuts['LastWeek']('fr'),
+					},
+					{
+						label: 'Last month',
+						range: PremadeShortcuts['LastMonth']('fr'),
+					},
+				] as CalendarShortcut[],
+				shortcutsStr,
+			},
+
+			template: cleanupTemplate(`
+			<lu-form-field label="Date range input example" inlineMessage="Inline message example">
+				<lu-date-range-input [(ngModel)]="selected" [min]="min" [max]="max" [shortcuts]="shortcuts" ${generateInputs(flags, argTypes)}></lu-date-range-input>
+			</lu-form-field>
+
+			<pr-story-model-display>{{selected | json}}</pr-story-model-display>`),
+		};
+	},
 	args: {
 		hideToday: false,
 
