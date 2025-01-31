25814436a780d6124f51baf78ab789eb957a3e7e
fbasmaison-lucca <fbasmaison@luccasoftware.com>
2025-01-31 12:36:26 +0100

    fix(stylelint): Handle cases where version or date are invalid…

--- packages/stylelint-config/stylelintForLF.mjs
+++ packages/stylelint-config/stylelintForLF.mjs
@@ -33,7 +33,6 @@ export function getDisallowedData(disallowedObjects, faultyPattern) {
 
 	objectData = setDates(objectData);
 	objectData.faultyPattern = faultyPattern;
-	objectData.daysLeft = Math.ceil((objectData.dateDeleted.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
 
 	return {
 		message: getMessage(objectData),
@@ -50,9 +49,37 @@ export function getDisallowedData(disallowedObjects, faultyPattern) {
 function setDates(objectData) {
 	return {
 		...objectData,
-		dateDeprecated: new Date(LFVersions[objectData.versionDeprecated]),
-		dateDeleted: new Date(LFVersions[objectData.versionDeleted]),
+		dateDeprecated: getDateForVersion(objectData.versionDeprecated),
+		dateDeleted: getDateForVersion(objectData.versionDeleted),
 	};
+}
+
+/**
+ * Is the parameter a valid date?
+ *
+ * @param {*} date - Value to analise
+ * @return boolean
+ */
+function isValidDate(date) {
+	return date instanceof Date && !isNaN(date);
+}
+
+/**
+ * Get date for the specified LF version.
+ *
+ * @param {string} version - semver formatted LF version
+ * @return {Date|undefined}
+ */
+function getDateForVersion(version) {
+	if (version && version in LFVersions) {
+		const date = new Date(LFVersions[version]);
+
+		if (isValidDate(date)) {
+			return date;
+		}
+	}
+
+	return;
 }
 
 /**
@@ -73,9 +100,27 @@ function comparePatterns(faultyPattern, disallowedPattern) {
  * @return {String}
  */
 function getMessage(objectData) {
-	const dayString = new Intl.RelativeTimeFormat().format(objectData.daysLeft, 'day');
+	let pattern = `${objectData.faultyPattern}`;
+	let status = 'deprecated';
+	let messageDeprecated = '';
+	let messageDeleted = '';
 
-	return `Deprecated since LF ${objectData.versionDeprecated}, until ${objectData.versionDeleted}, ${dayString}, ${objectData.dateDeleted.toLocaleDateString()}: ${objectData.faultyPattern}`;
+	if (objectData.dateDeprecated) {
+		const daysAgo = Math.ceil((objectData.dateDeprecated.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
+		const daysAgoString = new Intl.RelativeTimeFormat().format(daysAgo, 'day');
+
+		messageDeprecated = ` | ${daysAgoString} (${objectData.dateDeprecated.toLocaleDateString()}, LF ${objectData.versionDeprecated})`;
+	}
+
+	if (objectData.dateDeleted) {
+		const daysLeft = Math.ceil((objectData.dateDeleted.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
+		const dayString = new Intl.RelativeTimeFormat().format(daysLeft, 'day');
+
+		status = 'deleted';
+		messageDeleted = ` | ${dayString} (${objectData.dateDeleted.toLocaleDateString()}, LF ${objectData.versionDeleted})`;
+	}
+
+	return `${status}${messageDeprecated}${messageDeleted}: ${pattern}`;
 }
 
 /**
