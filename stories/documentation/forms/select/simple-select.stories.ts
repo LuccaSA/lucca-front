620f34020b8176b37e6f52465ea8819910efe578
Guillaume Nury <GuillaumeNury@users.noreply.github.com>
2025-02-03 14:36:36 +0100

    feat(core-select): add a noClue directive to disable apiV3/V4 search (#3382)
    * feat(core-select): add a noClue directive to disable apiV3/V4 search
    
    * fix(core-select): do not update clue when opening the panel when select is not searchable

--- stories/documentation/forms/select/simple-select.stories.ts
+++ stories/documentation/forms/select/simple-select.stories.ts
@@ -1,7 +1,7 @@
 import { I18nPluralPipe, SlicePipe } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { LuCoreSelectPanelHeaderDirective, LuDisabledOptionDirective, LuDisplayerDirective, LuOptionDirective, LuOptionGroupDirective } from '@lucca-front/ng/core-select';
+import { LuCoreSelectNoClueDirective, LuCoreSelectPanelHeaderDirective, LuDisabledOptionDirective, LuDisplayerDirective, LuOptionDirective, LuOptionGroupDirective } from '@lucca-front/ng/core-select';
 import { LuCoreSelectApiV3Directive, LuCoreSelectApiV4Directive } from '@lucca-front/ng/core-select/api';
 import { LuCoreSelectEstablishmentsDirective } from '@lucca-front/ng/core-select/establishment';
 import { LuCoreSelectJobQualificationsDirective } from '@lucca-front/ng/core-select/job-qualification';
@@ -196,6 +196,22 @@ export const ApiV4 = generateStory({
 ></lu-simple-select>`,
 	neededImports: {
 		'@lucca-front/ng/simple-select': ['LuSimpleSelectInputComponent'],
+		'@lucca-front/ng/core-select/api': ['LuCoreSelectApiV4Directive'],
+	},
+});
+
+export const ApiV4NoCLue = generateStory({
+	name: 'Api V4 (no clue)',
+	description: `Il est possible de désactiver la recherche ajoutée par les directives apiV3 et apiV4 en utilisant la directive \`noClue\`.`,
+	template: `<lu-simple-select
+	placeholder="Placeholder..."
+	apiV4="/organization/structure/api/establishments"
+	noClue
+	[(ngModel)]="selectedEstablishment"
+></lu-simple-select>`,
+	neededImports: {
+		'@lucca-front/ng/simple-select': ['LuSimpleSelectInputComponent'],
+		'@lucca-front/ng/core-select': ['LuCoreSelectNoClueDirective'],
 		'@lucca-front/ng/core-select/api': ['LuCoreSelectApiV4Directive'],
 	},
 });
@@ -452,6 +468,7 @@ const meta: Meta<LuSimpleSelectInputStoryComponent> = {
 				SlicePipe,
 				LuCoreSelectApiV3Directive,
 				LuCoreSelectApiV4Directive,
+				LuCoreSelectNoClueDirective,
 				LuCoreSelectEstablishmentsDirective,
 				LuCoreSelectCustomEstablishmentsDirective,
 				LuCoreSelectCustomUsersDirective,
