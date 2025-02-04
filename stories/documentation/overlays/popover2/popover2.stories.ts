2b471d85a04d8572b31b450740f8f74fa5da1374
Flavien Normand <contact@flavien-normand.fr>
2025-02-04 16:57:48 +0100

    fix: bug squash for 19.1.6 (#3383)
    * fix(date-range): focus on first field after clear
    
    closes #3310
    
    * docs(popover2): add a story for custom connected positions
    
    #3369

--- stories/documentation/overlays/popover2/popover2.stories.ts
+++ stories/documentation/overlays/popover2/popover2.stories.ts
@@ -3,6 +3,7 @@ import { configureLuPopover, PopoverDirective } from '@lucca-front/ng/popover2';
 import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { HiddenArgType } from '../../../helpers/common-arg-types';
 import { cleanupTemplate, generateInputs } from '../../../helpers/stories';
+import { ConnectionPositionPair } from '@angular/cdk/overlay';
 
 export default {
 	title: 'Documentation/Overlays/Popover2/Angular',
@@ -56,6 +57,92 @@ export const Basic: StoryObj<PopoverDirective> = {
 	.demo {
 		display: flex;
 		min-height: 20rem;
+		align-items: center;
+		justify-content: center;
+	}`,
+			],
+		};
+	},
+	args: {
+		luPopoverTrigger: 'click',
+		luPopoverCloseDelay: 300,
+		luPopoverOpenDelay: 300,
+		luPopoverDisabled: false,
+		luPopoverPosition: 'above',
+		luPopoverNoCloseButton: false,
+	},
+};
+export const CustomPosition: StoryObj<PopoverDirective> = {
+	render: (_args, { argTypes }) => {
+		const { luPopoverPosition, ...args } = _args;
+		const action = args.luPopoverTrigger === 'click' ? 'Cliquez-moi' : 'Cliquez ou survolez-moi';
+		let openDelay = '';
+		if (args.luPopoverTrigger !== 'click') {
+			openDelay = ' ' + args.luPopoverOpenDelay + 'ms';
+		}
+		return {
+			props: {
+				examplePosition: [
+					new ConnectionPositionPair(
+						{ originX: 'start', originY: 'bottom' },
+						{
+							overlayX: 'start',
+							overlayY: 'top',
+						},
+						-8,
+						0,
+					),
+					new ConnectionPositionPair(
+						{ originX: 'start', originY: 'top' },
+						{
+							overlayX: 'start',
+							overlayY: 'bottom',
+						},
+						-8,
+						-32,
+					),
+				],
+			},
+			template: cleanupTemplate(`
+
+	examplePosition:
+	<pre>
+	[
+    new ConnectionPositionPair(&#123;originX: 'start', originY: 'bottom' &#125;, &#123; overlayX: 'start', overlayY: 'top' &#125;, -8, 0),
+    new ConnectionPositionPair(
+        &#123; originX: 'start', originY: 'top' &#125;,
+				&#123;
+            overlayX: 'start',
+            overlayY: 'bottom',
+        &#125;,
+        -8,
+        -32,
+    ),
+	]
+	</pre>
+
+	<a href="https://github.com/angular/components/blob/main/src/cdk/overlay/position/connected-position.ts#L28-L53">Angular CDK model for <code>ConnectedPosition</code></a>
+	<br>
+	<br>
+
+	<button luButton [luPopover2]="contentRef" [customPositions]="examplePosition" ${generateInputs(args, argTypes)}>${action}${openDelay} !</button>
+	<ng-template #contentRef>
+		<div class="popover-contentOptional">
+			<div class="verticalNavigation mod-iconless">
+				<ul class="verticalNavigation-list u-listReset">
+					<li class="verticalNavigation-list-item"><a href="#" class="verticalNavigation-list-item-link">Item A</a></li>
+					<li class="verticalNavigation-list-item"><a href="#" class="verticalNavigation-list-item-link">Item B</a></li>
+					<li class="verticalNavigation-list-item"><a href="#" class="verticalNavigation-list-item-link">Item C</a></li>
+				</ul>
+			</div>
+		</div>
+	</ng-template>
+`),
+			styles: [
+				`
+	.demo {
+		display: flex;
+		min-block-size: 20rem;
 		align-items: center;
 		justify-content: center;
 	}`,
