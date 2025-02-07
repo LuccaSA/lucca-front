0eccc47a59f80d523a12f0602d425bd9e4bd8d9b
Flavien Normand <contact@flavien-normand.fr>
2025-02-07 11:50:10 +0100

    fix(filter-pills): fix export order for tokens

--- packages/ng/filter-pills/public-api.ts
+++ packages/ng/filter-pills/public-api.ts
@@ -1,4 +1,4 @@
-export * from './core/index';
+export * from './filter-pill/filter-pill.component';
 export * from './filter-bar/filter-bar.component';
 export * from './filter-bar/filter-pill-addon.directive';
-export * from './filter-pill/filter-pill.component';
+export * from './core/index';
