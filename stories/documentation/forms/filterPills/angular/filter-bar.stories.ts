fafba00ee20de16a38969d51eda391cd6c26f569
Jeremie <jbiron@luccasoftware.com>
2025-02-11 14:49:12 +0100

    [19.2] Format stories (#3410)
    * Format 19.2 stories
    
    * No toto

--- stories/documentation/forms/filterPills/angular/filter-bar.stories.ts
+++ stories/documentation/forms/filterPills/angular/filter-bar.stories.ts
@@ -34,71 +34,51 @@ export default {
 	render: (args, { argTypes }) => {
 		return {
 			props: {
-				example: null,
-				example2: null,
+				example1: null,
 				examplePeriod: null,
 				legumes: allLegumes,
 			},
 
-			template: `
-				<lu-filter-bar>
-
-					<lu-filter-pill label="Légumes">
-						<lu-simple-select disabled [(ngModel)]="example"	[options]="legumes | filterLegumes:clue" (clueChange)="clue = $event" />
-					</lu-filter-pill>
-
-					<lu-filter-pill label="Lorem ipsum">
-						<lu-checkbox-input disabled checked [ngModel]="false"></lu-checkbox-input>
-					</lu-filter-pill>
-
-					<lu-form-field label="Test" hiddenLabel>
-						<lu-text-input [ngModel]="example10" [ngModelOptions]="{standalone: true}" hasSearchIcon hasClearer />
-					</lu-form-field>
-
-				</lu-filter-bar>
-
-				<hr class="divider pr-u-marginBlock400" />
-
-				<lu-filter-bar>
-					<ul *luFilterPillAddonBefore class="segmentedControl filterBar-segmentedControl" role="presentation">
-						<li class="segmentedControl-item">
-							<input type="radio" class="segmentedControl-item-input" name="tab" id="tab1" checked="checked" />
-							<label for="tab1" class="segmentedControl-item-action">
-								Tous
-							</label>
-						</li>
-						<li class="segmentedControl-item">
-							<input type="radio" class="segmentedControl-item-input" name="tab" id="tab2" />
-							<label for="tab2" class="segmentedControl-item-action">
-								En cours d’approbation
-							</label>
-						</li>
-						<li class="segmentedControl-item">
-							<input type="radio" class="segmentedControl-item-input" name="tab" id="tab3" />
-							<label for="tab3" class="segmentedControl-item-action">
-								Approuvés
-							</label>
-						</li>
-						<li class="segmentedControl-item">
-							<input type="radio" class="segmentedControl-item-input" name="tab" id="tab4" />
-							<label for="tab4" class="segmentedControl-item-action">
-								Clos
-							</label>
-						</li>
-					</ul>
-
-					<lu-filter-pill label="Inclure les collaborateurs partis" optional name="includeFormerEmployees">
-						<lu-checkbox-input [ngModel]="false" disabled></lu-checkbox-input>
-					</lu-filter-pill>
-					<lu-filter-pill label="Date de début" optional name="toto">
-						<lu-date-input [(ngModel)]="example2" /></lu-filter-pill>
-					<lu-filter-pill label="Période">
-						<lu-date-range-input [(ngModel)]="examplePeriod"/>
-					</lu-filter-pill>
-
-					<button  *luFilterPillAddonAfter type="submit" size="S" luButton="outlined">Exporter</button>
-				</lu-filter-bar>
-			`,
+			template: `<lu-filter-bar>
+	<ul *luFilterPillAddonBefore class="segmentedControl filterBar-segmentedControl" role="presentation">
+		<li class="segmentedControl-item">
+			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab1" checked="checked" />
+			<label for="tab1" class="segmentedControl-item-action">
+				Tous
+			</label>
+		</li>
+		<li class="segmentedControl-item">
+			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab2" />
+			<label for="tab2" class="segmentedControl-item-action">
+				En cours d’approbation
+			</label>
+		</li>
+		<li class="segmentedControl-item">
+			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab3" />
+			<label for="tab3" class="segmentedControl-item-action">
+				Approuvés
+			</label>
+		</li>
+		<li class="segmentedControl-item">
+			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab4" />
+			<label for="tab4" class="segmentedControl-item-action">
+				Clos
+			</label>
+		</li>
+	</ul>
+	<lu-filter-pill label="Inclure les collaborateurs partis" optional name="includeFormerEmployees">
+		<lu-checkbox-input [ngModel]="false"></lu-checkbox-input>
+	</lu-filter-pill>
+	<lu-filter-pill label="Date de début" optional name="startingDate">
+		<lu-date-input [(ngModel)]="example1" /></lu-filter-pill>
+	<lu-filter-pill label="Période">
+		<lu-date-range-input [(ngModel)]="examplePeriod"/>
+	</lu-filter-pill>
+	<lu-form-field label="Test" hiddenLabel>
+		<lu-text-input [ngModel]="example2" [ngModelOptions]="{standalone: true}" hasSearchIcon hasClearer />
+	</lu-form-field>
+	<button  *luFilterPillAddonAfter type="submit" size="S" luButton="outlined">Exporter</button>
+</lu-filter-bar>`,
 		};
 	},
 } as Meta;
