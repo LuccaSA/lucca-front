9b286cdb9842938e5c39b55981758218c4e3511a
Jérémie Biron <jbiron@luccasoftware.com>
2025-02-11 16:06:07 +0100

    Pills

--- stories/documentation/forms/filterPills/angular/filter-bar.stories.ts
+++ stories/documentation/forms/filterPills/angular/filter-bar.stories.ts
@@ -5,6 +5,7 @@ import { DateInputComponent, DateRangeInputComponent } from '@lucca-front/ng/dat
 import { FilterBarComponent, FilterPillAddonAfterDirective, FilterPillAddonBeforeDirective, FilterPillComponent } from '@lucca-front/ng/filter-pills';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
 import { CheckboxInputComponent, TextInputComponent } from '@lucca-front/ng/forms';
+import { NumericBadgeComponent } from '@lucca-front/ng/numeric-badge';
 import { LuSimpleSelectInputComponent } from '@lucca-front/ng/simple-select';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { StoryModelDisplayComponent } from '../../../../helpers/story-model-display.component';
@@ -28,6 +29,7 @@ export default {
 				FilterPillAddonBeforeDirective,
 				FormFieldComponent,
 				TextInputComponent,
+				NumericBadgeComponent,
 			],
 		}),
 	],
@@ -44,25 +46,25 @@ export default {
 		<li class="segmentedControl-item">
 			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab1" checked="checked" />
 			<label for="tab1" class="segmentedControl-item-action">
-				Tous
+				Tous <lu-numeric-badge [value]="12"></lu-numeric-badge>
 			</label>
 		</li>
 		<li class="segmentedControl-item">
 			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab2" />
 			<label for="tab2" class="segmentedControl-item-action">
-				En cours d’approbation
+				En cours d’approbation <lu-numeric-badge [value]="5"></lu-numeric-badge>
 			</label>
 		</li>
 		<li class="segmentedControl-item">
 			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab3" />
 			<label for="tab3" class="segmentedControl-item-action">
-				Approuvés
+				Approuvés <lu-numeric-badge [value]="3"></lu-numeric-badge>
 			</label>
 		</li>
 		<li class="segmentedControl-item">
 			<input type="radio" class="segmentedControl-item-input" name="tab" id="tab4" />
 			<label for="tab4" class="segmentedControl-item-action">
-				Clos
+				Clos <lu-numeric-badge [value]="4"></lu-numeric-badge>
 			</label>
 		</li>
 	</ul>
---
+++ stories/documentation/forms/filterPills/angular/filter-pills.stories.ts
@@ -0,0 +1,71 @@
+import { allLegumes, FilterLegumesPipe } from '@/stories/forms/select/select.utils';
+import { FormsModule } from '@angular/forms';
+import { DateInputComponent, DateRangeInputComponent } from '@lucca-front/ng/date2';
+import { FilterPillComponent } from '@lucca-front/ng/filter-pills';
+import { FormFieldComponent } from '@lucca-front/ng/form-field';
+import { CheckboxInputComponent, TextInputComponent } from '@lucca-front/ng/forms';
+import { LuSimpleSelectInputComponent } from '@lucca-front/ng/simple-select';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { LuMultiSelectInputComponent } from '../../../../../packages/ng/multi-select/input';
+import { StoryModelDisplayComponent } from '../../../../helpers/story-model-display.component';
+
+export default {
+	title: 'Documentation/Forms/FiltersPills/FilterPills/Angular',
+	decorators: [
+		moduleMetadata({
+			imports: [
+				FilterPillComponent,
+				CheckboxInputComponent,
+				FormsModule,
+				DateRangeInputComponent,
+				DateInputComponent,
+				StoryModelDisplayComponent,
+				LuSimpleSelectInputComponent,
+				LuMultiSelectInputComponent,
+				FilterLegumesPipe,
+				FormFieldComponent,
+				TextInputComponent,
+				FilterLegumesPipe,
+			],
+		}),
+	],
+	render: (args, { argTypes }) => {
+		return {
+			props: {
+				simpleSelect: null,
+				multiSelect: [],
+				date: null,
+				dateRange: null,
+				legumes: allLegumes,
+			},
+
+			template: `<lu-filter-pill label="Inclure les collaborateurs partis">
+	<lu-checkbox-input [ngModel]="false"></lu-checkbox-input>
+</lu-filter-pill>
+<lu-filter-pill label="Légume" name="legume">
+		<lu-simple-select [(ngModel)]="simpleSelect"	[options]="legumes | filterLegumes:clue" (clueChange)="clue = $event" />
+</lu-filter-pill>
+<lu-filter-pill label="Légume" name="legume">
+	<lu-multi-select [(ngModel)]="multiSelect"	[options]="legumes | filterLegumes:clue" (clueChange)="clue = $event" filterPillLabelPlural="légumes" />
+</lu-filter-pill>
+<lu-filter-pill label="Date de début">
+	<lu-date-input [(ngModel)]="date" />
+</lu-filter-pill>
+<lu-filter-pill label="Période">
+	<lu-date-range-input [(ngModel)]="dateRange"/>
+</lu-filter-pill>`,
+			styles: [
+				`
+	:host {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--pr-t-spacings-100);
+	}`,
+			],
+		};
+	},
+} as Meta;
+
+export const Basic: StoryObj<FilterPillComponent> = {
+	args: {},
+};
