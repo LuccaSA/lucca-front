c59e106ad993f997d5f7d7cdb85cb8506137eeb5
Flavien Normand <contact@flavien-normand.fr>
2025-02-13 08:56:59 +0100

    feat(schematics): lu-select migration schematic (#3292)

--- packages/ng/schematics/lu-select/index.ts
+++ packages/ng/schematics/lu-select/index.ts
@@ -1,27 +1,26 @@
-import type { Rule } from '@angular-devkit/schematics';
+import { Rule } from '@angular-devkit/schematics';
 import { migrateFile } from '../lib/schematics';
-import { migrateHTMLFile, migrateScssFile } from './migration';
+import { migrateComponent } from './migration';
+import ts, { ScriptTarget } from 'typescript';
 import { currentSchematicContext, SchematicContextOpts } from '../lib/lf-schematic-context';
 
 // Nx need to see "@angular-devkit/schematics" in order to run this migration correctly (see https://github.com/nrwl/nx/blob/d9fed4b832bf01d1b9a44ae9e486a5e5cd2d2253/packages/nx/src/command-line/migrate/migrate.ts#L1729-L1738)
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 require('@angular-devkit/schematics');
 
 export default (options?: SchematicContextOpts): Rule => {
+
 	return async (tree, context) => {
 		await currentSchematicContext.init(context, options);
-
-		const postCssScss = await import('../lib/local-deps/postcss-scss.js');
 
 		tree.visit((path, entry) => {
 			if (path.includes('node_modules') || !entry) {
 				return;
 			}
-			if (path.endsWith('.scss')) {
-				migrateFile(path, entry, tree, (content) => migrateScssFile(content, postCssScss));
-			}
-			if (path.endsWith('.html')) {
-				migrateFile(path, entry, tree, (content) => migrateHTMLFile(content));
+			if (path.endsWith('.ts')) {
+				migrateFile(path, entry, tree, (content) => {
+					const sourceFile = ts.createSourceFile(path, content, ScriptTarget.ESNext);
+					return migrateComponent(sourceFile, path, tree);
+				});
 			}
 		});
 	};
