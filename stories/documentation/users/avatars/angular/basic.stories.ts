9ca401ca30b00a4df8ccceff322b285b1d75c10d
Flavien Normand <contact@flavien-normand.fr>
2025-02-14 15:09:36 +0100

    docs: add story params and new stories for user popover usage (#3415)

--- stories/documentation/users/avatars/angular/basic.stories.ts
+++ stories/documentation/users/avatars/angular/basic.stories.ts
@@ -1,24 +1,38 @@
-import { Component, Input, Optional } from '@angular/core';
-import { ILuUser, LuDisplayInitials, LuUserPictureModule } from '@lucca-front/ng/user';
-import { Meta, StoryFn } from '@storybook/angular';
+import { LuDisplayInitials, LuUserPictureComponent } from '@lucca-front/ng/user';
+import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { bob, georges, squidwards } from '../../user.mocks';
-
-@Component({
-	selector: 'user-picture-stories',
-	standalone: true,
-	imports: [LuUserPictureModule],
-	template: `<lu-user-picture [user]="user" [displayFormat]="displayFormat" data-testid="lu-user-picture" [class]="sizes" [class.mod-placeholder]="placeholder"></lu-user-picture>`,
-})
-class UserPictureStory {
-	@Input() user: ILuUser;
-	@Input() @Optional() sizes: string;
-	@Input() @Optional() placeholder: boolean;
-	@Input() @Optional() displayFormat: LuDisplayInitials;
-}
+import { LuUserPopoverDirective, provideLuUserPopover } from '@lucca-front/ng/popup-employee';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export default {
 	title: 'Documentation/Users/Avatar/Angular/Basic',
-	component: UserPictureStory,
+	decorators: [
+		moduleMetadata({
+			imports: [LuUserPictureComponent, LuUserPopoverDirective],
+		}),
+		applicationConfig({
+			providers: [provideAnimations(), provideHttpClient(), provideLuUserPopover()],
+		}),
+	],
+	render: ({ user, sizes, placeholder, displayFormat }) => {
+		return {
+			template: `<lu-user-picture
+		[user]="user"
+		[displayFormat]="displayFormat"
+		data-testid="lu-user-picture"
+		[class]="sizes"
+		[class.mod-placeholder]="placeholder"
+		[luUserPopover]="user"
+	></lu-user-picture>`,
+			props: {
+				user,
+				sizes,
+				placeholder,
+				displayFormat,
+			},
+		};
+	},
 	argTypes: {
 		user: {
 			options: ['Avec image', 'Avec image erronée', 'Sans image'],
@@ -44,47 +58,11 @@ export default {
 	},
 } as Meta;
 
-const template: StoryFn<UserPictureStory> = (args) => ({
-	props: args,
-});
-
-export const Basic = template.bind({});
-Basic.args = {
-	user: bob,
-	sizes: '',
-	placeholder: false,
-	displayFormat: LuDisplayInitials.firstlast,
-};
-
-const code = `
-/* 1. Importer LuUserPictureModule */
-import { LuUserPictureModule } from '@lucca-front/ng/user';
-
-@NgModule({
-	imports: [LuUserPictureModule]
-})
-class StoriesModule {}
-
-/* 2. Utiliser lu-user-picture */
-@Component({
-	selector: 'user-picture-stories',
-	template: \`<lu-user-picture [user]="user" [displayFormat]="displayFormat" [class]="sizes" [class.mod-placeholder]="placeholder"></lu-user-picture>\`,
-})
-class UserPictureStory {
-	@Input() user: ILuUser;
-	@Input() @Optional() sizes: string;
-	@Input() @Optional() placeholder: boolean;
-	@Input() @Optional() displayFormat: LuDisplayInitials;
-}`;
-
-Basic.parameters = {
-	// Disable controls as they are not modifiable because of ComponentWrapper
-	controls: { include: ['user', 'displayFormat', 'sizes', 'placeholder'] },
-	docs: {
-		source: {
-			language: 'ts',
-			type: 'code',
-			code,
-		},
+export const Basic: StoryObj<LuUserPictureComponent & { sizes: string; placeholder: boolean }> = {
+	args: {
+		user: bob,
+		sizes: '',
+		placeholder: false,
+		displayFormat: LuDisplayInitials.firstlast,
 	},
 };
