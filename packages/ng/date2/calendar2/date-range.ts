526c4a470751ff50d1d149990a482aa0b9c8a3a2
FXG <fgentilhomme@luccasoftware.com>
2025-02-19 17:52:23 +0100

    feat(date-input): add a "format" attribute to handle date-iso string format

--- packages/ng/date2/calendar2/date-range.ts
+++ packages/ng/date2/calendar2/date-range.ts
@@ -1,10 +1,14 @@
 import { Palette } from '@lucca-front/ng/core';
 import { CalendarMode } from './calendar-mode';
 
-export interface DateRange {
+export interface DateRangeInput {
 	class?: `palette-${Palette}` | string;
-	start: Date;
-	end?: Date;
+	start: Date | string;
+	end?: Date | string;
 	label?: string;
 	scope?: CalendarMode;
+}
+export interface DateRange extends DateRangeInput {
+	start: Date;
+	end?: Date;
 }
--- packages/ng/date2/date-input/with-string-adapter.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-	// The attribute is already prefixed with "lu-date-input"
-	// eslint-disable-next-line @angular-eslint/directive-selector
-	selector: 'lu-date-input[withStringAdapter]',
-	standalone: true,
-})
-export class LuDateInputWithStringAdapterDirective {}
