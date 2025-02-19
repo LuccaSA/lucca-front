32fb0fca44125e0b9611d86195cd44cbfd297bbb
Vincent Valentin <vvalentin@luccasoftware.com>
2025-02-19 17:32:49 +0100

    [button] with arrow

---
+++ stories/documentation/actions/button/html&css/button-with-arrow.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from '@storybook/angular';
+
+interface ButtonWithArrowStory {}
+
+export default {
+	title: 'Documentation/Actions/Button/HTML&CSS/With arrow',
+} as Meta;
+
+function getTemplate(args: ButtonWithArrowStory): string {
+	return `<button aria-expanded="false" type="button" class="button mod-withArrow">Button<span aria-hidden="true" class="lucca-icon icon-arrowChevronBottom"></span></button>`;
+}
+
+const Template: StoryFn<ButtonWithArrowStory> = (args) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const IconWithArrowButton = Template.bind({});
+IconWithArrowButton.args = {};
