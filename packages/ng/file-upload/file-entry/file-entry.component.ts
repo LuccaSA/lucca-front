78dce6d6cfee0a985c495ac56b35fc9b73dc23d7
Flavien Normand <contact@flavien-normand.fr>
2025-03-11 14:20:52 +0100

    feat(file-upload): prepare for final structure

--- packages/ng/file-upload/file-entry/file-entry.component.ts
+++ packages/ng/file-upload/file-entry/file-entry.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, UpperCasePipe } from '@angular/common';
-import { booleanAttribute, Component, computed, inject, input, LOCALE_ID, output, ViewEncapsulation } from '@angular/core';
+import { booleanAttribute, Component, computed, inject, input, LOCALE_ID, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '@lucca-front/ng/button';
 import { DividerComponent } from '@lucca-front/ng/divider';
@@ -9,7 +9,9 @@ import { IconComponent } from '@lucca-front/ng/icon';
 import { InlineMessageComponent } from '@lucca-front/ng/inline-message';
 import { LuTooltipModule } from '@lucca-front/ng/tooltip';
 import { formatSize } from '../formatter';
-import { FileUploadEntry } from '../file-upload-entry';
+import { FileEntry, UploadEntry } from '../file-upload-entry';
+import { Subject } from 'rxjs';
+import { outputFromObservable } from '@angular/core/rxjs-interop';
 
 @Component({
 	selector: 'lu-file-entry',
@@ -29,20 +31,35 @@ export class FileEntryComponent {
 
 	inlineMessageError = input<string | null>(null);
 
-	entry = input.required<FileUploadEntry>();
+	entry = input.required<UploadEntry | FileEntry>();
 
 	format = input<'file' | 'word' | 'excel' | 'powerpoint'>('file');
 
 	size = input<'S' | 'M' | null>(null);
 
 	downloadable = input(false, { transform: booleanAttribute });
-	deletable = input(false, { transform: booleanAttribute });
 	viewable = input(false, { transform: booleanAttribute });
-	withPassword = input(false, { transform: booleanAttribute });
+
+	password = input('');
+	passwordChange$ = new Subject<string>();
+	passwordChange = outputFromObservable(this.passwordChange$);
+
+	get withPassword() {
+		return this.passwordChange$.observed;
+	}
+
+	// TODO story simple/multiple avec fake service cdk qui attend 1s
+	// TODO ajouter une doc md avec Olivier pour parler du combo avec le service lucca files et son intégration
 
 	display = input<'media' | 'single' | null>(null);
 
-	deleteFile = output();
+	deleteFile$ = new Subject<void>();
+
+	deleteFile = outputFromObservable(this.deleteFile$);
+
+	get deletable() {
+		return this.deleteFile$.observed;
+	}
 
 	fileName = computed(() => this.entry().name);
 	fileType = computed(() => this.entry().type);
@@ -51,15 +68,11 @@ export class FileEntryComponent {
 
 	fileSizeDisplay = computed(() => formatSize(this.#locale, this.fileSize()));
 
-	dlClasses = computed(() => {
-		return {
-			[`is-${this.state()}`]: !!this.state(),
-			[`mod-${this.size()}`]: !!this.size(),
-			[`mod-${this.format()}`]: !!this.format(),
-			'mod-media': this.display() === 'media',
-			'mod-single': this.display() === 'single',
-		};
-	});
-
-	password?: string;
+	dlClasses = computed(() => ({
+		[`is-${this.state()}`]: !!this.state(),
+		[`mod-${this.size()}`]: !!this.size(),
+		[`mod-${this.format()}`]: !!this.format(),
+		'mod-media': this.display() === 'media',
+		'mod-single': this.display() === 'single',
+	}));
 }
