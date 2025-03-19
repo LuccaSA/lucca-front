b86a95c0367558cbcd529cf65733ef97dd7ea3e7
Guillaume Nury <gnury@luccasoftware.com>
2025-03-19 18:09:18 +0100

    ref: remove deprecated stuff not used at Lucca

--- packages/ng/sidepanel/sidepanel.service.ts
+++ packages/ng/sidepanel/sidepanel.service.ts
@@ -1,19 +1,19 @@
 import { ComponentType } from '@angular/cdk/overlay';
 import { inject, Injectable } from '@angular/core';
-import { ILuModalContent, LuModal, LuModalContentResult } from '@lucca-front/ng/modal';
-import { ILuSidepanelRef, LuSidepanelConfig, LU_SIDEPANEL_CONFIG } from './sidepanel.model';
+import { ILuModalContent, LU_MODAL_CONFIG, LuModal, LuModalConfig, LuModalContentResult } from '@lucca-front/ng/modal';
+import { ILuSidepanelRef } from './sidepanel.model';
 
 /**
  * @deprecated Use LuModal with `modal.open(component, data, { mode: 'sidepanel' })` instead.
  */
 @Injectable()
 export class LuSidepanel extends LuModal {
-	protected override _config = inject(LU_SIDEPANEL_CONFIG);
+	protected override _config = inject(LU_MODAL_CONFIG);
 
 	/**
 	 * @deprecated Use LuModal with `modal.open(component, data, { mode: 'sidepanel' })` instead.
 	 */
-	override open<T extends ILuModalContent, D>(component: ComponentType<T>, data: D = undefined, config: Partial<LuSidepanelConfig> = {}): ILuSidepanelRef<D, LuModalContentResult<T>> {
+	override open<T extends ILuModalContent, D>(component: ComponentType<T>, data: D = undefined, config: Partial<LuModalConfig> = {}): ILuSidepanelRef<D, LuModalContentResult<T>> {
 		return super.open(component, data, config);
 	}
 }
--- packages/ng/simple-select/api/public-api.ts
+++ packages/ng/simple-select/api/public-api.ts
@@ -1,3 +1,2 @@
-export * from './api-v3.directive';
 export * from './api-v4.directive';
 export { ALuSimpleSelectApiDirective } from './api.directive';
