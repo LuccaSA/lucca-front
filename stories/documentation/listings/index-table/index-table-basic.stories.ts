20ad40b64ef1ac12ece594e5aaf10ec235b38e40
BertrandPodevin <65592565+BertrandPodevin@users.noreply.github.com>
2025-04-04 10:50:13 +0200

    3201 indexTable link and interactives elements (#3534)
    * fix : 3201 - interactives elements do not works when located in the same cell than indexTable-body-row-cell-link
    
    * fix(front) : simpler solution for interactive element in indexTable-body-row-cell-link cell
    
    * fix : 3201 - interactives elements do not works when located in the same cell than indexTable-body-row-cell-link
    
    * remove page-header-basic.stories.ts from the PR
    
    * fix(front) : remove unnecessary condition

--- stories/documentation/listings/index-table/index-table-basic.stories.ts
+++ stories/documentation/listings/index-table/index-table-basic.stories.ts
@@ -1,10 +1,24 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { bob } from '@/stories/users/user.mocks';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { LuUserPopoverDirective, provideLuUserPopover } from '@lucca-front/ng/popup-employee';
+import { LuTooltipModule } from '@lucca-front/ng/tooltip';
+import { LuUserDisplayModule, LuUserPictureComponent } from '@lucca-front/ng/user';
+import { applicationConfig, Meta, moduleMetadata, StoryFn } from '@storybook/angular';
 
 interface IndexTableBasicStory {}
 
 export default {
 	title: 'Documentation/Listings/Index Table/Basic',
 	argTypes: {},
+	decorators: [
+		moduleMetadata({
+			imports: [LuTooltipModule, LuUserPopoverDirective, LuUserDisplayModule, LuUserPictureComponent],
+		}),
+		applicationConfig({
+			providers: [provideAnimations(), provideLuUserPopover(), provideHttpClient()],
+		}),
+	],
 } as Meta;
 
 function getTemplate(args: IndexTableBasicStory): string {
@@ -44,16 +58,25 @@ function getTemplate(args: IndexTableBasicStory): string {
 		</tr>
 		<tr class="indexTable-body-row">
 			<td class="indexTable-body-row-cell">
-				<a href="#" class="indexTable-body-row-cell-link">Content</a>
+				<a href="#" class="indexTable-body-row-cell-link"><span class="u-mask">See details</span></a>
+				<span luTooltip="This is a tooltip">Content with tooltip</span>
 			</td>
 			<td class="indexTable-body-row-cell"><a href="#">Content actionable</a></td>
+			<td class="indexTable-body-row-cell">Content</td>
+		</tr>
+		<tr class="indexTable-body-row">
+			<td class="indexTable-body-row-cell">
+				<button class="indexTable-body-row-cell-link" type="button"><span class="u-mask">See details</span></button>
+				<div class="u-displayFlex pr-u-gap50" [luUserPopover]="bob"><lu-user-picture class="mod-XS" [user]="bob"></lu-user-picture>{{ bob | luUserDisplay:'lf' }} (with userPopover)</div>
+			</td>
+			<td class="indexTable-body-row-cell"><span luTooltip="This is a tooltip">Content with tooltip</span></td>
 			<td class="indexTable-body-row-cell">Content</td>
 		</tr>
 		<!-- indexTable-body-row-cell-action is deprecated -->
 		<tr class="indexTable-body-row">
 			<td class="indexTable-body-row-cell">
 				<a href="#" class="indexTable-body-row-cell-action">Action</a>
-				Content (width deprecated action)
+				Content (with deprecated action)
 			</td>
 			<td class="indexTable-body-row-cell">Content</td>
 			<td class="indexTable-body-row-cell">Content</td>
@@ -68,4 +91,4 @@ const Template: StoryFn<IndexTableBasicStory> = (args) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = {};
+Basic.args = { bob };
