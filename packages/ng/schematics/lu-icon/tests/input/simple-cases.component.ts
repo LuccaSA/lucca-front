bdf3b84944e52068cc6cb390a17be5b6a7fadca9
Flavien Normand <contact@flavien-normand.fr>
2025-04-15 16:19:52 +0200

    feat(schematics): support for size and extra classes

--- packages/ng/schematics/lu-icon/tests/input/simple-cases.component.ts
+++ packages/ng/schematics/lu-icon/tests/input/simple-cases.component.ts
@@ -9,6 +9,8 @@ import { ButtonComponent } from '@lucca-front/ng/button';
 			<span aria-hidden="true" class="lucca-icon icon-signClose"></span>
 			<span class="u-mask">Close</span>
 		</button>
+		<span aria-hidden="true" class="lucca-icon icon-signClose mod-S"></span>
+		<span aria-hidden="true" class="lucca-icon icon-signClose keepme"></span>
 	`,
 	imports: [
 		ButtonComponent
--- packages/ng/schematics/lu-icon/tests/output/simple-cases.component.ts
+++ packages/ng/schematics/lu-icon/tests/output/simple-cases.component.ts
@@ -7,8 +7,10 @@ import { IconComponent } from '@lucca-front/ng/icon';
 	standalone: true,
 	template: `
 		<button luButton>
-			<lu-icon icon="signClose" alt="Close"  />
+			<lu-icon icon="signClose" alt="Close" />
 		</button>
+		<lu-icon icon="signClose" size="S" />
+		<lu-icon icon="signClose" class="keepme"/>
 	`,
 	imports: [
 		ButtonComponent, IconComponent
