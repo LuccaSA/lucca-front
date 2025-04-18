61913b60d77b773ed7a07056dbec556366211407
Vincent Valentin <vvalentin@luccasoftware.com>
2025-04-18 12:28:46 +0200

    wip

---
+++ packages/ng/page-layout/page-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+	selector: 'lu-page-layout',
+	standalone: true,
+	styleUrls: ['./page-layout.component.scss'],
+	templateUrl: './page-layout.component.html',
+	encapsulation: ViewEncapsulation.None,
+})
+export class PageLayoutComponent {}
