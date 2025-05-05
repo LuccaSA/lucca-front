c2630a87bdd438ce160d6a17efb478fd801c6ccf
Jérémie Biron <jbiron@luccasoftware.com>
2025-05-05 17:58:11 +0200

    Deprecated compact mode

--- stories/documentation/navigation/breadcrumbs/angular/breadcrumbs-basic.stories.ts
+++ stories/documentation/navigation/breadcrumbs/angular/breadcrumbs-basic.stories.ts
@@ -19,10 +19,6 @@ export default {
 	<a *luBreadcrumbsLink class="breadcrumbs-list-item-action" routerLink="/" ariaCurrentWhenActive="page">You</a>
 	<a *luBreadcrumbsLink class="breadcrumbs-list-item-action" ariaCurrentWhenActive="page" href="#2">are</a>
 	<a *luBreadcrumbsLink class="breadcrumbs-list-item-action" aria-current="page">here</a>
-</lu-breadcrumbs>
-<lu-breadcrumbs ${generateInputs(otherArgs, argTypes)}>
-	<a *luBreadcrumbsLink class="breadcrumbs-list-item-action" ariaCurrentWhenActive="page" href="#2">Compact</a>
-	<a *luBreadcrumbsLink class="breadcrumbs-list-item-action" aria-current="page">mode</a>
 </lu-breadcrumbs>`,
 		};
 	},
@@ -30,6 +26,5 @@ export default {
 
 export const Basic = {
 	args: {
-		disableCompact: false,
 	},
 };
