3e49b45abf16b48b54ecf6000a66aaf7a107bc7f
Jeremie <jbiron@luccasoftware.com>
2025-06-10 17:06:13 +0200

    [Tokens Typo] (#3648)
    * Add font tokens
    
    * Apply tokens
    
    * Apply token color text
    
    * Apply token color text heading
    
    * Apply token color text reverse
    
    * Apply token color text states
    
    * Replace title imports
    
    * Remove title imports
    
    * Apply shorthands
    
    * apply subtle color + misc
    
    * Apply geometricPrecision everywhere
    
    * Fix neutral 500
    
    * prevent breakings
    
    * Fix skeletons
    
    * Fix lint
    
    * test qa
    
    * test qa
    
    * Fix deprecated overrides
    
    * Misc fixes
    
    * u-text > u-body
    
    * Text color adjustment

--- stories/documentation/integration/utilities/text-size.stories.ts
+++ stories/documentation/integration/utilities/text-size.stories.ts
@@ -8,12 +8,9 @@ export default {
 
 function getTemplate(args: TextSizeStory): string {
 	return `<div class="demo-utilityWrapper">
-	<div class="u-textXS demo-utility"><code class="code">u-textXS</code> Lorem ipsum</div>
-	<div class="u-textS demo-utility"><code class="code">u-textS</code> Lorem ipsum</div>
-	<div class="u-textM demo-utility"><code class="code">u-textM</code> Lorem ipsum</div>
-	<div class="u-textL demo-utility"><code class="code">u-textL</code> Lorem ipsum</div>
-	<div class="u-textXL demo-utility"><code class="code">u-textXL</code> Lorem ipsum</div>
-	<div class="u-textXXL demo-utility"><code class="code">u-textXXL</code> Lorem ipsum</div>
+	<div class="u-bodyM demo-utility"><code class="code">u-bodyM</code> Lorem ipsum</div>
+	<div class="u-bodyS demo-utility"><code class="code">u-bodyS</code> Lorem ipsum</div>
+	<div class="u-bodyXS demo-utility"><code class="code">u-bodyXS</code> Lorem ipsum</div>
 </div>`;
 }
 
