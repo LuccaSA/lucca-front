6ee00109f71cd3b248f9fbcd8253580218fe6cf0
Nina <nsarradin@luccasoftware.com>
2025-06-25 12:14:51 +0200

    [Mobile Push] - adding component (#3706)
    * [Mobile Push] - adding component
    
    * [Mobile Push] - adding angular component+ stories and reordering / renaming classes
    
    * [Mobile Push] - fixing missing alt on image and updating some styles
    
    * translations
    
    ---------
    
    Co-authored-by: Vincent Valentin <vvalentin@luccasoftware.com>

---
+++ packages/ng/mobile-push/mobile-push.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { getIntl } from '@lucca-front/ng/core';
+import { IconComponent } from '@lucca-front/ng/icon';
+import { LU_MOBILE_PUSH_TRANSLATIONS } from './mobile-push.translate';
+
+@Component({
+	selector: 'lu-mobile-push',
+	standalone: true,
+	imports: [IconComponent],
+	templateUrl: './mobile-push.component.html',
+	styleUrls: ['./mobile-push.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+})
+export class MobilePushComponent {
+	intl = getIntl(LU_MOBILE_PUSH_TRANSLATIONS);
+}
