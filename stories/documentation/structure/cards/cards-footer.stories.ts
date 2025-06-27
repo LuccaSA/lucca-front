2c2e94f8defd389702eaa23f9b7e0a1078d54205
Jérémie Biron <jbiron@luccasoftware.com>
2025-06-27 14:40:24 +0200

    cards

--- stories/documentation/structure/cards/cards-footer.stories.ts
+++ stories/documentation/structure/cards/cards-footer.stories.ts
@@ -7,20 +7,18 @@ export default {
 } as Meta;
 
 function getTemplate(args: CardsFooterStory): string {
-	return `
-	<div class="card">
-		<div class="card-content">
-			<h2 class="card-title">Titre de la carte</h2>
-			<p>Contenu de la carte</p>
-		</div>
-		<footer class="card-footer">
-			<div class="card-footer-right">
-				<button type="button" class="button palette-product">Confirmer</button>
-				<button type="button" class="button mod-text">Annuler</button>
-			</div>
-		</footer>
+	return `<div class="card">
+	<div class="card-content">
+		<h2 class="card-title">Titre de la carte</h2>
+		<p>Contenu de la carte</p>
 	</div>
-	`;
+	<footer class="card-footer">
+		<div class="card-footer-right">
+			<button type="button" class="button palette-product">Confirmer</button>
+			<button type="button" class="button mod-text">Annuler</button>
+		</div>
+	</footer>
+</div>`;
 }
 
 const Template: StoryFn<CardsFooterStory> = (args) => ({
