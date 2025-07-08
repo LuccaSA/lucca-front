838b8a20220c5c5d4fa5e9369afd0597fa9f9e23
Vincent Valentin <vvalentin@luccasoftware.com>
2025-07-08 15:22:41 +0200

    wip

---
+++ stories/documentation/forms/option/basic.stories.ts
@@ -0,0 +1,93 @@
+import { Meta } from '@storybook/angular';
+import { cleanupTemplate } from 'stories/helpers/stories';
+
+interface OptionBasicStory {}
+
+export default {
+	title: 'Documentation/Forms/Option/Basic',
+	argTypes: {},
+	render: (args: OptionBasicStory) => {
+		return {
+			template: cleanupTemplate(`<ul class="optionWrapper">
+	<li role="option" class="option">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+	</li>
+	<li role="option" class="option">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+		<ul class="optionWrapper">
+			<li role="option" class="option">
+				<div class="option-content">
+					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+						<span class="checkboxField-input"></span>
+						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					option
+				</div>
+			</li>
+			<li role="option" class="option">
+				<div class="option-content">
+					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+						<span class="checkboxField-input"></span>
+						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					option
+				</div>
+				<ul class="optionWrapper">
+					<li role="option" class="option">
+						<div class="option-content">
+							<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+								<span class="checkboxField-input"></span>
+								<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+							</span>
+							option
+						</div>
+					</li>
+					<li role="option" class="option" aria-selected="true">
+						<div class="option-content">
+							<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+								<span class="checkboxField-input"></span>
+								<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+							</span>
+							option
+						</div>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li role="option"  class="option">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+	</li>
+	<li role="option" class="option" aria-disabled="true">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+	</li>
+</ul>`),
+		};
+	},
+} as Meta;
+
+export const Basic = {};
