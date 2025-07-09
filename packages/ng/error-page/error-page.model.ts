405a5f0d2b9025dad320fd845c35bbc135317785
Guillaume Drouard <gdrouard@lucca.fr>
2025-07-09 14:34:57 +0200

    feat(errorPage): add angular ErrorPageComponent

---
+++ packages/ng/error-page/error-page.model.ts
@@ -0,0 +1,1 @@
+export type ErrorPageImage = '400' | '403' | '404' | '429' | '500' | 'keyboard' | 'lock' | 'map';
--- stories/documentation/feedback/error-page/html&css/error-page-basic.stories.ts
+++ stories/documentation/feedback/error-page/html&css/error-page-basic.stories.ts
@@ -3,7 +3,7 @@ import { Meta, StoryFn } from '@storybook/angular';
 interface ErrorBasicStory {}
 
 export default {
-	title: 'Documentation/Feedback/Error/Basic',
+	title: 'Documentation/Feedback/ErrorPage/HTML & CSS/Basic',
 } as Meta;
 
 function getTemplate(args: ErrorBasicStory): string {
@@ -13,7 +13,7 @@ function getTemplate(args: ErrorBasicStory): string {
 			<div class="errorPage-section-info">
 				<h1 class="errorPage-section-info-title">Titre de l’erreur</h1>
 				<p class="errorPage-section-info-text">Vous n’êtes pas autorisé à consulter cette page ou cette ressource</p>
-				<a href="#" class="errorPage-section-info-link">Revenir à la page précédente</a>
+				<a href="#">Revenir à la page précédente</a>
 			</div>
 			<img src="https://cdn.lucca.fr/errors/svg/404-lucca.svg" alt="Erreur 403" class="errorPage-section-image">
 		</section>
