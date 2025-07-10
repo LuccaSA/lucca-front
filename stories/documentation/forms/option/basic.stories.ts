8530fca67d3335fe707f0f1da9f5683276257462
Vincent Valentin <vvalentin@luccasoftware.com>
2025-07-10 18:46:54 +0200

    wip

--- stories/documentation/forms/option/basic.stories.ts
+++ stories/documentation/forms/option/basic.stories.ts
@@ -8,11 +8,12 @@ export default {
 	argTypes: {},
 	render: (args: OptionBasicStory) => {
 		return {
-			template: cleanupTemplate(`<ul class="optionWrapper">
+			template: cleanupTemplate(`
+<ul role="listbox" class="optionWrapper mod-multiple">
 	<li role="option" class="option">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-				<span class="checkboxField-input"></span>
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
 				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 			</span>
 			option
@@ -21,16 +22,25 @@ export default {
 	<li role="option" class="option">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-				<span class="checkboxField-input"></span>
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
 				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 			</span>
 			option
 		</div>
-		<ul class="optionWrapper">
+	</li>
+	<li role="presentation" class="option mod-groupBy" aria-labelledby="title">
+		<div class="option-content" id="title">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			title
+		</div>
+		<ul role="group" class="optionWrapper">
 			<li role="option" class="option">
 				<div class="option-content">
 					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-						<span class="checkboxField-input"></span>
+						<span class="option-content-checkboxField-input checkboxField-input"></span>
 						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 					</span>
 					option
@@ -39,25 +49,77 @@ export default {
 			<li role="option" class="option">
 				<div class="option-content">
 					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-						<span class="checkboxField-input"></span>
+						<span class="option-content-checkboxField-input checkboxField-input"></span>
+						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					option
+				</div>
+			</li>
+			<li role="presentation" class="option">
+				<div class="option-content">
+					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+						<span class="option-content-checkboxField-input checkboxField-input"></span>
+						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					option
+				</div>
+			</li>
+		</ul>
+	</li>
+</ul>
+<hr class="divider pr-u-marginBlock200" />
+<ul role="tree" class="optionWrapper mod-multiple">
+	<li role="treeitem" class="option">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+	</li>
+	<li role="treeitem" class="option">
+
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option
+		</div>
+
+		<ul class="optionWrapper" role="group">
+			<li role="treeitem" class="option">
+				<div class="option-content">
+					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+						<span class="option-content-checkboxField-input checkboxField-input"></span>
+						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+					</span>
+					option
+				</div>
+			</li>
+			<li role="treeitem" class="option">
+				<div class="option-content">
+					<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+						<span class="option-content-checkboxField-input checkboxField-input"></span>
 						<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 					</span>
 					option
 				</div>
 				<ul class="optionWrapper">
-					<li role="option" class="option">
+					<li role="treeitem" class="option">
 						<div class="option-content">
 							<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-								<span class="checkboxField-input"></span>
+								<span class="option-content-checkboxField-input checkboxField-input"></span>
 								<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 							</span>
 							option
 						</div>
 					</li>
-					<li role="option" class="option" aria-selected="true">
+					<li role="treeitem" class="option" aria-selected="true">
 						<div class="option-content">
 							<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-								<span class="checkboxField-input"></span>
+								<span class="option-content-checkboxField-input checkboxField-input"></span>
 								<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 							</span>
 							option
@@ -67,28 +129,28 @@ export default {
 			</li>
 		</ul>
 	</li>
-	<li role="option"  class="option">
+	<li role="treeitem" class="option">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-				<span class="checkboxField-input"></span>
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
 				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 			</span>
 			option
 		</div>
 	</li>
-	<li role="option" class="option" aria-disabled="true">
+	<li role="treeitem" class="option" aria-disabled="true">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-				<span class="checkboxField-input"></span>
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
 				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 			</span>
 			option
 		</div>
 	</li>
-	<li role="option" class="option" aria-disabled="true" aria-selected="true">
+	<li role="treeitem" class="option" aria-disabled="true" aria-selected="true">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
-				<span class="checkboxField-input"></span>
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
 				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
 			</span>
 			option
