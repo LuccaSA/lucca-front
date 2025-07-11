dd810a88b090f49c2092645dcb10a6f035a67222
Flavien Normand <contact@flavien-normand.fr>
2025-07-11 16:18:16 +0200

    chore: another approach idk it's friday

--- packages/ng/link/link.component.ts
+++ packages/ng/link/link.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Component, effect, ElementRef, HostBinding, inject, input, Input, Renderer2, ViewEncapsulation } from '@angular/core';
+import { booleanAttribute, Component, effect, HostBinding, inject, input, Input, ViewEncapsulation } from '@angular/core';
 import { getIntl } from '@lucca-front/ng/core';
 import { LU_LINK_TRANSLATIONS } from './link.translate';
 import { LuRouterLink } from './lu-router-link';
@@ -20,9 +20,6 @@ import { LuRouterLink } from './lu-router-link';
 })
 export class LinkComponent {
 	intl = getIntl(LU_LINK_TRANSLATIONS);
-
-	#elementRef = inject<ElementRef<HTMLLinkElement>>(ElementRef);
-	#renderer = inject(Renderer2);
 	#routerLink = inject(LuRouterLink);
 
 	routerLinkCommands = input<LuRouterLink['routerLink'] | null>(null, { alias: 'luLink' });
@@ -64,7 +61,7 @@ export class LinkComponent {
 	hrefBackup: string;
 
 	constructor() {
-		const href = this.#routerLink.publicReactiveHref;
+		const href = this.#routerLink.luHref;
 
 		effect(() => {
 			if (href()) {
@@ -73,9 +70,11 @@ export class LinkComponent {
 			if (this.disabled()) {
 				if (this.routerLinkCommands()) {
 					this.#routerLink.routerLink = null;
-					this.#renderer.removeAttribute(this.#elementRef.nativeElement, 'href');
+					this.#routerLink.publicReactiveHref.set(null);
+					// this.#renderer.removeAttribute(this.#elementRef.nativeElement, 'href');
 				} else {
-					this.#renderer.removeAttribute(this.#elementRef.nativeElement, 'href');
+					this.#routerLink.publicReactiveHref.set(null);
+					// this.#renderer.removeAttribute(this.#elementRef.nativeElement, 'href');
 				}
 			} else if (this.routerLinkCommands()) {
 				this.#routerLink.routerLink = this.routerLinkCommands();
@@ -83,7 +82,8 @@ export class LinkComponent {
 				// See https://github.com/angular/angular/blob/main/packages/router/src/directives/router_link.ts#L281
 				this.#routerLink.ngOnChanges({});
 			} else if (!href() && this.hrefBackup) {
-				this.#renderer.setAttribute(this.#elementRef.nativeElement, 'href', this.hrefBackup);
+				this.#routerLink.publicReactiveHref.set(this.hrefBackup);
+				// this.#renderer.setAttribute(this.#elementRef.nativeElement, 'href', this.hrefBackup);
 			}
 		});
 	}
