e0272779885a250a70c48c9e6b7ac5a9c1125d50
Flavien Normand <contact@flavien-normand.fr>
2025-07-15 12:01:28 +0200

    fix: better link fix from Lucas

--- packages/ng/link/lu-router-link.ts
+++ packages/ng/link/lu-router-link.ts
@@ -1,15 +1,10 @@
 import { RouterLink } from '@angular/router';
-import { Directive, input, WritableSignal } from '@angular/core';
+import { Directive, WritableSignal } from '@angular/core';
 
 @Directive({
 	selector: '[luRouterLink]',
-	host: {
-		'[attr.href]': 'reactiveHref()',
-	},
 })
 export class LuRouterLink extends RouterLink {
-	luHref = input('', { alias: 'href' });
-
 	get publicReactiveHref(): WritableSignal<string | null> {
 		return this.reactiveHref;
 	}
