1504fe802dfbf53581b5588f40fd69879d0325ea
Vincent Valentin <vvalentin@luccasoftware.com>
2025-08-07 16:10:14 +0200

    wip

--- stories/documentation/forms/option/tree.stories.ts
+++ stories/documentation/forms/option/tree.stories.ts
@@ -3,6 +3,7 @@ import { cleanupTemplate } from 'stories/helpers/stories';
 
 interface OptionBasicStory {
 	multiple: boolean;
+	deepNesting: boolean;
 }
 
 export default {
@@ -10,9 +11,21 @@ export default {
 	argTypes: {},
 	render: (args: OptionBasicStory) => {
 		const modMultiple = args.multiple ? ` mod-multiple` : ``;
+		const deepNestingItem = args.deepNesting
+			? `<ul class="optionWrapper" role="group" [attr.style]="'--components-optionWrapper-level: 3'">
+	<li role="treeitem" class="option">
+		<div class="option-content">
+			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
+				<span class="option-content-checkboxField-input checkboxField-input"></span>
+				<span class="checkboxField-icon"><span class="checkboxField-icon-check"></span></span>
+			</span>
+			option 2.2.X.1
+		</div>
+	</li>
+</ul>`
+			: ``;
 		return {
-			template: cleanupTemplate(`
-<ul role="tree" class="optionWrapper${modMultiple}">
+			template: cleanupTemplate(`<ul role="tree" class="optionWrapper${modMultiple}">
 	<li role="treeitem" class="option">
 		<div class="option-content">
 			<span class="option-content-checkboxField checkboxField" aria-hidden="true">
@@ -66,6 +79,7 @@ export default {
 							</span>
 							option 2.2.2
 						</div>
+						${deepNestingItem}
 					</li>
 					<li role="treeitem" class="option" aria-disabled="true">
 						<div class="option-content">
@@ -75,6 +89,7 @@ export default {
 							</span>
 							option 2.2.3
 						</div>
+						${deepNestingItem}
 					</li>
 					<li role="treeitem" class="option" aria-selected="true" aria-disabled="true">
 						<div class="option-content">
@@ -84,6 +99,7 @@ export default {
 							</span>
 							option 2.2.4
 						</div>
+						${deepNestingItem}
 					</li>
 				</ul>
 			</li>
@@ -97,5 +113,6 @@ export default {
 export const Basic = {
 	args: {
 		multiple: false,
+		deepNesting: false,
 	},
 };
