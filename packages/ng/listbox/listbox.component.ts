40570087d7eed46225bc4572cfe634227575ef32
Vincent Valentin <vvalentin@luccasoftware.com>
2025-08-13 14:25:43 +0200

    fix build

--- packages/ng/listbox/listbox.component.ts
+++ packages/ng/listbox/listbox.component.ts
@@ -1,5 +1,6 @@
 import { booleanAttribute, Component, forwardRef, input, ViewEncapsulation } from '@angular/core';
-import { LoadingComponent } from '../loading/loading.component';
+
+import { LoadingComponent } from '@lucca-front/ng/loading';
 import { OptionComponent } from './option/option.component';
 import { LISTBOX_INSTANCE } from './tokens';
 
@@ -9,8 +10,8 @@ import { LISTBOX_INSTANCE } from './tokens';
 	templateUrl: './listbox.component.html',
 	encapsulation: ViewEncapsulation.None,
 	host: {
-		'[role]': 'tree() ? "tree" : "listbox"',
 		class: 'optionWrapper',
+		'[role]': 'tree() ? "tree" : "listbox"',
 		'[class.mod-multiple]': 'multiple()',
 		'[attr.aria-busy]': 'state() === "loading"',
 	},
