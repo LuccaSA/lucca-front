51e8db569c7caf53286f1742924eb64943621ac0
Vincent Valentin <vvalentin@luccasoftware.com>
2025-08-19 11:54:03 +0200

    clean

--- packages/ng/core-select/user/translations.ts
+++ packages/ng/core-select/user/translations.ts
@@ -1,34 +1,34 @@
 ﻿export const Translations = {
-	"en": {
-		'me': "Me:",
-		'includeFormerEmployees': "Include former employees",
+	en: {
+		me: 'Me:',
+		includeFormerEmployees: 'Include former employees',
 	},
-	"de": {
-		'me': "Ich:",
-		'includeFormerEmployees': "Ausgeschiedene Mitarbeitende einbeziehen",
+	de: {
+		me: 'Ich:',
+		includeFormerEmployees: 'Ausgeschiedene Mitarbeitende einbeziehen',
 	},
-	"fr": {
-		'me': "Moi :",
-		'includeFormerEmployees': "Inclure les collaborateurs partis",
+	fr: {
+		me: 'Moi :',
+		includeFormerEmployees: 'Inclure les collaborateurs partis',
 	},
-	"it": {
-		'me': "Io:",
-		'includeFormerEmployees': "Includere gli ex collaboratori",
+	it: {
+		me: 'Io:',
+		includeFormerEmployees: 'Includere gli ex collaboratori',
 	},
-	"nl": {
-		'me': "Ik:",
-		'includeFormerEmployees': "Inclusief medewerkers die zijn vertrokken",
+	nl: {
+		me: 'Ik:',
+		includeFormerEmployees: 'Inclusief medewerkers die zijn vertrokken',
 	},
-	"nl-BE": {
-		'me': "Ik:",
-		'includeFormerEmployees': "Inclusief medewerkers die zijn vertrokken",
+	'nl-BE': {
+		me: 'Ik:',
+		includeFormerEmployees: 'Inclusief medewerkers die zijn vertrokken',
 	},
-	"es": {
-		'me': "Yo:",
-		'includeFormerEmployees': "Incluir a los empleados inactivos",
+	es: {
+		me: 'Yo:',
+		includeFormerEmployees: 'Incluir a los empleados inactivos',
 	},
-	"pt": {
-		'me': "Eu:",
-		'includeFormerEmployees': "Incluir os trabalhadores que saíram",
+	pt: {
+		me: 'Eu:',
+		includeFormerEmployees: 'Incluir os trabalhadores que saíram',
 	},
 };
