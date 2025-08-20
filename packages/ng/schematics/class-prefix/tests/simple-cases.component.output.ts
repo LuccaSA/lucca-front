ce93e2467f1f14857e01d469e14a2ce811e09973
Flavien Normand <contact@flavien-normand.fr>
2025-08-20 14:01:24 +0200

    feat(schematics): add class-prefix schematic (#3829)

---
+++ packages/ng/schematics/class-prefix/tests/simple-cases.component.output.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'lu-test',
+	standalone: true,
+	template: `
+		<span class="pr-u-mask"></span>
+	`,
+	imports: [
+	]
+})
+export class SimpleCasesComponent {
+}
