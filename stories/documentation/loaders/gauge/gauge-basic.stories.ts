0d33a1b8c099ab6b03280ab9ad2fe346b189e2a1
Vincent Valentin <vvalentin@luccasoftware.com>
2025-08-25 14:25:20 +0200

    cleanup

--- stories/documentation/loaders/gauge/gauge-basic.stories.ts
+++ stories/documentation/loaders/gauge/gauge-basic.stories.ts
@@ -4,6 +4,7 @@ interface GaugeBasicStory {
 	palette: string;
 	thin: boolean;
 	vertical: boolean;
+	value: number;
 }
 
 export default {
@@ -15,27 +16,22 @@ export default {
 				type: 'select',
 			},
 		},
-		thin: {
-			control: {
-				type: 'boolean',
-			},
-		},
 		vertical: {
-			control: {
-				type: 'boolean',
-			},
 			description: 'Deprecated 💀',
+		},
+		value: {
+			control: { type: 'range', min: 0, max: 100, step: 1 },
 		},
 	},
 } as Meta;
 
 function getTemplate(args: GaugeBasicStory): string {
-	const classes = [args.palette].filter(Boolean).join(' ');
-	const thin = args.thin ? `mod-thin` : '';
-	const vertical = args.vertical ? `mod-vertical` : '';
+	const classes = ' ' + [args.palette].filter(Boolean).join(' ');
+	const thin = args.thin ? ` mod-thin` : '';
+	const vertical = args.vertical ? ` mod-vertical` : '';
 	return `
-	<div class="gauge ${classes} ${thin} ${vertical}">
-		<div class="gauge-bar" style="width:33%; height: 33%"></div>
+	<div class="gauge${classes}${thin}${vertical}" [attr.style]="'--components-gauge-value: ${args.value}%'">
+		<div class="gauge-bar"></div>
 	</div>
 	`;
 }
@@ -53,4 +49,4 @@ const Template: StoryFn<GaugeBasicStory> = (args) => ({
 });
 
 export const Basic = Template.bind({});
-Basic.args = { palette: '', thin: false, vertical: false };
+Basic.args = { palette: '', thin: false, vertical: false, value: 33 };
