314c0089d34135d2ac86f19cc514a868dba2120a
Vincent Valentin <vvalentin@luccasoftware.com>
2025-09-11 17:24:33 +0200

    adjust html

--- stories/documentation/users/popover/html&css/skeleton.stories.ts
+++ stories/documentation/users/popover/html&css/skeleton.stories.ts
@@ -12,10 +12,10 @@ function getTemplate(args: UserPopoverStory): string {
 		<div class="userPopover-details">
 			<div class="userPopover-details-avatar avatar skeleton-item mod-circle" [style.--components-skeleton-shape-width]="'96px'" [style.--components-skeleton-shape-height]="'96px'"></div>
 			<div class="userPopover-details-info">
-				<h1 class="userPopover-details-info-name skeleton-item pr-u-ellipsis pr-u-margin0" [style.--components-skeleton-text-width]="'80%'"></h1>
-				<p class="userPopover-details-info-detail pr-u-ellipsis skeleton-item" [style.--components-skeleton-text-width]="'60%'"></p>
-				<p class="userPopover-details-info-detail pr-u-ellipsis skeleton-item" [style.--components-skeleton-text-width]="'40%'"></p>
-				<p class="userPopover-details-info-detail skeleton-item" [style.--components-skeleton-text-width]="'90%'"></p>
+				<h1 class="userPopover-details-info-name skeleton-item pr-u-margin0" [style.--components-skeleton-text-width]="'80%'"></h1>
+				<p class="userPopover-details-info-detail skeleton-item pr-u-bodyS" [style.--components-skeleton-text-width]="'60%'"></p>
+				<p class="userPopover-details-info-detail skeleton-item pr-u-bodyS" [style.--components-skeleton-text-width]="'40%'"></p>
+				<p class="userPopover-details-info-detail skeleton-item pr-u-bodyS" [style.--components-skeleton-text-width]="'90%'"></p>
 			</div>
 		</div>
 	</section>
