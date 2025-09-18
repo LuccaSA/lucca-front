d6a6ae4f90993139a8665f9ba0661b5ce882b061
Vincent Valentin <vvalentin@luccasoftware.com>
2025-09-18 18:06:52 +0200

    subcomponents

---
+++ packages/ng/dropdown/dropdown-group/public-api.ts
@@ -0,0 +1,1 @@
+export * from './dropdown-group.component';
--- stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
+++ stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
@@ -1,7 +1,7 @@
-import { LuDropdownModule, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
+import { ButtonComponent } from '@lucca-front/ng/button';
+import { DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, DropdownItemComponent, LuDropdownModule, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
 import { IconComponent } from '@lucca-front/ng/icon';
 import { Meta, StoryFn } from '@storybook/angular';
-import { ButtonComponent } from '@lucca-front/ng/button';
 
 export default {
 	title: 'Documentation/Overlays/Dropdown/Angular/Directive',
@@ -12,31 +12,51 @@ export default {
 const Template: StoryFn = (args) => ({
 	props: args,
 	moduleMetadata: {
-		imports: [LuDropdownModule, IconComponent, ButtonComponent],
+		imports: [LuDropdownModule, IconComponent, ButtonComponent, DropdownItemComponent, DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, IconComponent],
 	},
-	template: `
-	<button type="button" luButton [luDropdown]="dropdown">Dropdown with options and a longer text</button>
-<lu-dropdown #dropdown>
-	<li class="dropdown-list-option">
-		<span class="dropdown-list-option-action is-disabled" luDropdownItem>
-			<lu-icon icon="eye" />
-			Prévisualiser
-		</span>
-	</li>
-	<li class="dropdown-list-option">
-		<a routerLink="." fragment="link2" class="dropdown-list-option-action" luDropdownItem>
-			<lu-icon icon="officePen" />
-			Éditer
-		</a>
-	</li>
-	<li class="dropdown-list-option">
-		<button type="button" class="dropdown-list-option-action mod-critical" luDropdownItem>
-			<lu-icon icon="trashDelete" />
-			Supprimer
+	template: `<button type="button" luButton [luDropdown]="dropdownSample">Dropdown</button>
+
+<lu-dropdown #dropdownSample>
+	<lu-dropdown-item>
+		<button lu-dropdown-action type="button">
+			<lu-icon icon="heart" />
+			Lorem
 		</button>
-	</li>
-</lu-dropdown>
-`,
+	</lu-dropdown-item>
+	<lu-dropdown-item>
+		<button lu-dropdown-action disabled type="button">
+			<lu-icon icon="cross" />
+			Lorem
+		</button>
+	</lu-dropdown-item>
+	<lu-dropdown-divider />
+	<lu-dropdown-item>
+		<button lu-dropdown-action type="button">
+			<lu-icon icon="star" />
+			Ipsum
+		</button>
+	</lu-dropdown-item>
+	<lu-dropdown-group label="Group">
+		<lu-dropdown-item>
+			<button lu-dropdown-action type="button">
+				<lu-icon icon="buildingHouse" />
+				Dolor
+			</button>
+		</lu-dropdown-item>
+		<lu-dropdown-item>
+			<a lu-dropdown-action href="#">
+				<lu-icon icon="flag" />
+				Sit amet
+			</a>
+		</lu-dropdown-item>
+		<lu-dropdown-item>
+			<span lu-dropdown-action disabled class="dropdown-list-option-action">
+				<lu-icon icon="cross" />
+				Sit amet
+			</span>
+		</lu-dropdown-item>
+	</lu-dropdown-group>
+</lu-dropdown>`,
 });
 
 export const Directive = Template.bind({});
