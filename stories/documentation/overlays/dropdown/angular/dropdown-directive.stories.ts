cca4f0c5c9892270429550fbc7a28ed269043689
Vincent Valentin <vvalentin@luccasoftware.com>
2025-09-29 15:16:28 +0200

    dropdown menu

--- stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
+++ stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
@@ -1,5 +1,6 @@
 import { ButtonComponent } from '@lucca-front/ng/button';
-import { DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, DropdownItemComponent, LuDropdownModule, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
+import { DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, DropdownItemComponent, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
+import { DropdownMenuComponent } from '@lucca-front/ng/dropdown/dropdown-menu';
 import { IconComponent } from '@lucca-front/ng/icon';
 import { Meta, StoryFn } from '@storybook/angular';
 
@@ -12,51 +13,63 @@ export default {
 const Template: StoryFn = (args) => ({
 	props: args,
 	moduleMetadata: {
-		imports: [LuDropdownModule, IconComponent, ButtonComponent, DropdownItemComponent, DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, IconComponent],
+		imports: [
+			IconComponent,
+			ButtonComponent,
+			DropdownItemComponent,
+			DropdownActionComponent,
+			DropdownDividerComponent,
+			DropdownGroupComponent,
+			IconComponent,
+			LuDropdownTriggerDirective,
+			DropdownMenuComponent,
+		],
 	},
 	template: `<button type="button" luButton [luDropdown]="dropdownSample">Dropdown</button>
 
-<lu-dropdown #dropdownSample>
-	<lu-dropdown-item>
-		<button lu-dropdown-action type="button">
-			<lu-icon icon="heart" />
-			Lorem
-		</button>
-	</lu-dropdown-item>
-	<lu-dropdown-item>
-		<button lu-dropdown-action disabled type="button">
-			<lu-icon icon="cross" />
-			Lorem
-		</button>
-	</lu-dropdown-item>
-	<lu-dropdown-divider />
-	<lu-dropdown-item>
-		<button lu-dropdown-action type="button">
-			<lu-icon icon="star" />
-			Ipsum
-		</button>
-	</lu-dropdown-item>
-	<lu-dropdown-group label="Group">
+<ng-template #dropdownSample>
+	<lu-dropdown-menu>
 		<lu-dropdown-item>
 			<button lu-dropdown-action type="button">
-				<lu-icon icon="buildingHouse" />
-				Dolor
+				<lu-icon icon="heart" />
+				Lorem
 			</button>
 		</lu-dropdown-item>
 		<lu-dropdown-item>
-			<a lu-dropdown-action href="#">
-				<lu-icon icon="flag" />
-				Sit amet
-			</a>
-		</lu-dropdown-item>
-		<lu-dropdown-item>
-			<span lu-dropdown-action disabled class="dropdown-list-option-action">
+			<button lu-dropdown-action disabled type="button">
 				<lu-icon icon="cross" />
-				Sit amet
-			</span>
+				Lorem
+			</button>
 		</lu-dropdown-item>
-	</lu-dropdown-group>
-</lu-dropdown>`,
+		<lu-dropdown-divider />
+		<lu-dropdown-item>
+			<button lu-dropdown-action type="button">
+				<lu-icon icon="star" />
+				Ipsum
+			</button>
+		</lu-dropdown-item>
+		<lu-dropdown-group label="Group">
+			<lu-dropdown-item>
+				<button lu-dropdown-action type="button">
+					<lu-icon icon="buildingHouse" />
+					Dolor
+				</button>
+			</lu-dropdown-item>
+			<lu-dropdown-item>
+				<a lu-dropdown-action href="#">
+					<lu-icon icon="flag" />
+					Sit amet
+				</a>
+			</lu-dropdown-item>
+			<lu-dropdown-item>
+				<span lu-dropdown-action disabled class="dropdown-list-option-action">
+					<lu-icon icon="cross" />
+					Sit amet
+				</span>
+			</lu-dropdown-item>
+		</lu-dropdown-group>
+	</lu-dropdown-menu>
+</ng-template>`,
 });
 
 export const Directive = Template.bind({});
