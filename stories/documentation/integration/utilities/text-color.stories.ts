21f127d0fbcdf00f0ff4886372d281b2d8019edd
Jérémie Biron <jbiron@luccasoftware.com>
2025-10-24 15:23:30 +0200

    Add color text tokens utilities + depracted a bunch of utilities

--- stories/documentation/integration/utilities/text-color.stories.ts
+++ stories/documentation/integration/utilities/text-color.stories.ts
@@ -8,14 +8,20 @@ export default {
 
 function getTemplate(args: TextColorStory): string {
 	return `<div class="demo-utilityWrapper">
-	<div class="pr-u-textNeutral demo-utility"><code class="code">pr-u-textNeutral</code> Lorem ipsum</div>
-	<div class="pr-u-textProduct demo-utility"><code class="code">pr-u-textProduct</code> Lorem ipsum</div>
-	<div class="pr-u-textBrand demo-utility"><code class="code">pr-u-textBrand</code> Lorem ipsum</div>
+	<div class="pr-u-colorText demo-utility"><code class="code">pr-u-colorText</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextHeading demo-utility"><code class="code">pr-u-colorTextHeading</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextHighlight demo-utility"><code class="code">pr-u-colorTextHighlight</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextSubtle demo-utility"><code class="code">pr-u-colorTextSubtle</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextDisabled demo-utility"><code class="code">pr-u-colorTextDisabled</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextReverse demo-utility"><code class="code">pr-u-colorTextReverse</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextSuccess demo-utility"><code class="code">pr-u-colorTextSuccess</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextWarning demo-utility"><code class="code">pr-u-colorTextWarning</code> Lorem ipsum</div>
+	<div class="pr-u-colorTextCritical demo-utility"><code class="code">pr-u-colorTextCritical</code> Lorem ipsum</div>
 </div>
 <div class="demo-utilityWrapper">
-	<div class="pr-u-textSuccess demo-utility"><code class="code">pr-u-textSuccess</code> Lorem ipsum</div>
-	<div class="pr-u-textWarning demo-utility"><code class="code">pr-u-textWarning</code> Lorem ipsum</div>
-	<div class="pr-u-textCritical demo-utility"><code class="code">pr-u-textCritical</code> Lorem ipsum</div>
+	<div class="pr-u-textProduct demo-utility"><code class="code">pr-u-textProduct</code> Lorem ipsum</div>
+	<div class="pr-u-textBrand demo-utility"><code class="code">pr-u-textBrand</code> Lorem ipsum</div>
+	<div class="pr-u-textPlaceholder demo-utility"><code class="code">pr-u-textPlaceholder</code> Lorem ipsum</div>
 </div>
 <div class="demo-utilityWrapper">
 	<div class="pr-u-textKiwi demo-utility"><code class="code">pr-u-textKiwi</code> Lorem ipsum</div>
@@ -31,9 +37,13 @@ function getTemplate(args: TextColorStory): string {
 	<div class="pr-u-textPumpkin demo-utility"><code class="code">pr-u-textPumpkin</code> Lorem ipsum</div>
 	<div class="pr-u-textPineapple demo-utility"><code class="code">pr-u-textPineapple</code> Lorem ipsum</div>
 </div>
+<h2>💀 Deprecated</h2>
 <div class="demo-utilityWrapper">
+	<div class="pr-u-textNeutral demo-utility"><code class="code">pr-u-textNeutral</code> Lorem ipsum</div>
 	<div class="pr-u-textLight demo-utility"><code class="code">pr-u-textLight</code> Lorem ipsum</div>
-	<div class="pr-u-textPlaceholder demo-utility"><code class="code">pr-u-textPlaceholder</code> Lorem ipsum</div>
+	<div class="pr-u-textSuccess demo-utility"><code class="code">pr-u-textSuccess</code> Lorem ipsum</div>
+	<div class="pr-u-textWarning demo-utility"><code class="code">pr-u-textWarning</code> Lorem ipsum</div>
+	<div class="pr-u-textCritical demo-utility"><code class="code">pr-u-textCritical</code> Lorem ipsum</div>
 </div>`;
 }
 
