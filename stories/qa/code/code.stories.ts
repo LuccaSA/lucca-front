72dd4b32d0812d1c603629879cf6fe41ac08fded
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-10-28 09:20:21 +0100

    + Add code stories qa

---
+++ stories/qa/code/code.stories.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { codeComponent } from '@lucca-front/ng/code';
+import { Meta, StoryFn } from '@storybook/angular';
+//import { SampleComponent } from '@lucca-front/ng/sample';
+
+@Component({
+	standalone: true,
+	selector: 'code-stories',
+	templateUrl: './code.stories.html',
+	imports: [codeComponent],
+})
+class CodeStory {}
+
+export default {
+	title: 'QA/Code',
+	component: CodeStory,
+} as Meta;
+
+const template: StoryFn<CodeStory> = () => ({});
+
+export const Basic = template.bind({});
