923f50c9875e8105216f9c2b7c68199e69e7dd1f
Vincent Valentin <vvalentin@luccasoftware.com>
2025-11-05 15:37:13 +0100

    template

--- packages/ng/data-table/base-data-table-cell.ts
+++ packages/ng/data-table/base-data-table-cell.ts
@@ -1,12 +1,13 @@
 import { booleanAttribute, Component, computed, inject, input } from '@angular/core';
+import { LU_DATA_TABLE_BODY_INSTANCE } from './data-table-body/data-table-body.token';
 import { LU_DATA_TABLE_FOOT_INSTANCE } from './data-table-foot/data-table-foot.token';
 import { LU_DATA_TABLE_HEAD_INSTANCE } from './data-table-head/data-table-head.token';
 import { LU_DATA_TABLE_ROW_INSTANCE } from './data-table-row/data-table-row.token';
 import { LU_DATA_TABLE_INSTANCE } from './data-table.token';
-import { LU_DATA_TABLE_BODY_INSTANCE } from './data-table-body/data-table-body.token';
 
 @Component({
 	selector: 'lu-base-data-table-cell',
+	template: '',
 })
 export abstract class BaseDataTableCell {
 	tableRef = inject(LU_DATA_TABLE_INSTANCE);
