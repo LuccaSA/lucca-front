aede2e57864a4426e9762c7a3c9457bd27a8da7f
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-11-07 16:04:12 +0100

    + Add plg traduction

--- packages/ng/plg-push/translations.ts
+++ packages/ng/plg-push/translations.ts
@@ -1,1 +1,26 @@
-export const Translations = {};
+﻿export const Translations = {
+	'nl-BE': {
+		close: 'Sluiten',
+	},
+	fr: {
+		close: 'Fermer',
+	},
+	de: {
+		close: 'Schließen',
+	},
+	en: {
+		close: 'Close',
+	},
+	es: {
+		close: 'Cerrar',
+	},
+	it: {
+		close: 'Chiudere',
+	},
+	nl: {
+		close: 'Sluiten',
+	},
+	pt: {
+		close: 'Fechar',
+	},
+};
