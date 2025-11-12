43c63391e4cab25c395718e1c38b730112a6602d
Jean Beaurepaire <jbeaurepaire@luccasoftware.com>
2025-11-12 16:03:25 +0100

    fix(link): update URL regex for improved matching

--- packages/ng/forms/rich-text-input/plugins/link/link.component.ts
+++ packages/ng/forms/rich-text-input/plugins/link/link.component.ts
@@ -17,7 +17,7 @@ import { FORMAT_LINK, registerLink, registerLinkSelectionChange } from './link.c
 import { PopoverAutoLinkNode } from './popover-autolink-node';
 import { PopoverLinkNode } from './popover-link-node';
 
-const URL_REGEX = /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(?<![-.+():%])/;
+const URL_REGEX = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=;,[\]]*)/;
 const EMAIL_REGEX = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
 
 @Component({
