5e6798cf6732994c6a84d33f8f495b70d655a007
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-11-17 14:54:03 +0100

    + add basic draggable stories for data-table empty && add drag parameter input for dots drag && translation linked

--- stories/documentation/listings/data-table/angular/draggable.stories.ts
+++ stories/documentation/listings/data-table/angular/draggable.stories.ts
@@ -1,4 +1,4 @@
-import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
 import {
 	DataTableBodyComponent,
 	DataTableComponent,
@@ -8,80 +8,34 @@ import {
 	DataTableRowCellHeaderComponent,
 	DataTableRowComponent,
 } from '@lucca-front/ng/data-table';
+import { Meta, StoryFn } from '@storybook/angular';
 
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
-
-interface DataTableDraggableStories {
-	draggable: boolean;
-}
+@Component({
+	selector: 'data-table-draggable-stories',
+	imports: [DataTableComponent, DataTableHeadComponent, DataTableBodyComponent, DataTableFootComponent, DataTableRowComponent, DataTableRowCellComponent, DataTableRowCellHeaderComponent],
+	templateUrl: './draggable.stories.html',
+})
+class DataTableDraggableStory {}
 
 export default {
 	title: 'Documentation/Listings/Data table/Angular/Draggable',
-	argTypes: {
-		draggable: {
-			control: {
-				type: 'boolean',
-			},
-		},
-	},
-	decorators: [
-		moduleMetadata({
-			imports: [
-				DataTableComponent,
-				DataTableHeadComponent,
-				DataTableBodyComponent,
-				DataTableFootComponent,
-				DataTableRowComponent,
-				DataTableRowCellComponent,
-				DataTableRowCellHeaderComponent,
-				CdkDrag,
-				CdkDropList,
-			],
-		}),
-	],
-	render: (args: DataTableDraggableStories) => {
-		const draggable = args.draggable ? ` draggable="true"` : '';
-		const lines = [
-			{ text: 'cell #1', header: 'header #1' },
-			{ text: 'cell #2', header: 'header #2' },
-			{ text: 'cell #3', header: 'header #3' },
-			{ text: 'cell #4', header: 'header #4' },
-		];
-
-		function drop(event: CdkDragDrop<string[]>, tpLines) {
-			moveItemInArray(tpLines, event.previousIndex, event.currentIndex);
-		}
-		return {
-			props: {
-				...args,
-				drop,
-				lines,
-			},
-			template: `<lu-data-table>
-	<thead luDataTableHead>
-		<tr luDataTableRow${draggable}>
-			<th luDataTableCell>Header</th>
-			<th luDataTableCell>Cell</th>
-		</tr>
-	</thead>
-	<tbody luDataTableBody cdkDropList (cdkDropListDropped)="drop($event, lines)">
-		${lines
-			.map(
-				(line) =>
-					`<tr luDataTableRow>
-				<th luDataTableCell>${line.header}</th>
-				<td luDataTableCell>${line.text}</td>
-			</tr>`,
-			)
-			.join('')}
-	</tbody>
-</lu-data-table>`,
-		};
-	},
+	component: DataTableDraggableStory,
 } as Meta;
 
-export const Draggable: StoryObj = {
-	args: {
-		draggable: true,
+const template: StoryFn<DataTableDraggableStory> = (args) => ({
+	props: args,
+});
+
+export const Basic = template.bind({});
+
+const code = ``;
+
+Basic.parameters = {
+	docs: {
+		source: {
+			language: 'ts',
+			type: 'code',
+			code,
+		},
 	},
 };
