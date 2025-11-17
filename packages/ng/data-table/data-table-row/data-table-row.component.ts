fa40bb6f8653fd03f9ebbf90ba0f63a6a3d00ade
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-11-17 14:54:03 +0100

    + add basic draggable stories for data-table empty && add drag parameter input for dots drag && translation linked

--- packages/ng/data-table/data-table-row/data-table-row.component.ts
+++ packages/ng/data-table/data-table-row/data-table-row.component.ts
@@ -1,6 +1,8 @@
+import { CdkDragHandle } from '@angular/cdk/drag-drop';
 import { NgTemplateOutlet } from '@angular/common';
 import { booleanAttribute, Component, contentChildren, ElementRef, forwardRef, inject, input, model, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { getIntl } from '@lucca-front/ng/core';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
 import { CheckboxInputComponent } from '@lucca-front/ng/forms';
 import { LU_DATA_TABLE_BODY_INSTANCE } from '../data-table-body/data-table-body.token';
@@ -8,6 +10,7 @@ import { LU_DATA_TABLE_CELL_INSTANCE } from '../data-table-cell.token';
 import { LU_DATA_TABLE_FOOT_INSTANCE } from '../data-table-foot/data-table-foot.token';
 import { LU_DATA_TABLE_HEAD_INSTANCE } from '../data-table-head/data-table-head.token';
 import { LU_DATA_TABLE_INSTANCE } from '../data-table.token';
+import { LU_DATA_TABLE_TRANSLATIONS } from '../data-table.translate';
 import { LU_DATA_TABLE_ROW_INSTANCE } from './data-table-row.token';
 
 @Component({
@@ -21,7 +24,7 @@ import { LU_DATA_TABLE_ROW_INSTANCE } from './data-table-row.token';
 		'[class.dataTable-foot-row]': 'footRef !== null',
 		'[class.mod-selectable]': 'tableRef.selectable()',
 	},
-	imports: [CheckboxInputComponent, FormFieldComponent, FormsModule, NgTemplateOutlet],
+	imports: [CheckboxInputComponent, FormFieldComponent, FormsModule, NgTemplateOutlet, CdkDragHandle],
 	providers: [
 		{
 			provide: LU_DATA_TABLE_ROW_INSTANCE,
@@ -30,6 +33,7 @@ import { LU_DATA_TABLE_ROW_INSTANCE } from './data-table-row.token';
 	],
 })
 export class DataTableRowComponent {
+	intl = getIntl(LU_DATA_TABLE_TRANSLATIONS);
 	bodyRef = inject(LU_DATA_TABLE_BODY_INSTANCE, { optional: true });
 	headRef = inject(LU_DATA_TABLE_HEAD_INSTANCE, { optional: true });
 	footRef = inject(LU_DATA_TABLE_FOOT_INSTANCE, { optional: true });
