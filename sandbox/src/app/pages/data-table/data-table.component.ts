9028a05ed86b45d4b8d3a55bcc57a0bdc8e0f55a
Guillaume Nury <gnury@luccasoftware.com>
2025-11-18 10:46:59 +0100

    feat(sandbox): initial data table

--- sandbox/src/app/pages/data-table/data-table.component.ts
+++ sandbox/src/app/pages/data-table/data-table.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from '@lucca-front/ng/button';
 import { MainLayoutComponent } from '@lucca-front/ng/main-layout';
+import { LuUserModule } from '@lucca-front/ng/user';
 
 @Component({
 	selector: 'app-data-table-page',
-	imports: [MainLayoutComponent],
+	imports: [MainLayoutComponent, ButtonComponent, LuUserModule],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: './data-table.component.html',
 })
