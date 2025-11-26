8b8e74cf0315cf9e68bd1434b518f0e9a221044e
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-11-26 16:18:54 +0100

    * update dropdown-menu component changeDetection on push

--- packages/ng/dropdown/dropdown-menu/dropdown-menu.component.ts
+++ packages/ng/dropdown/dropdown-menu/dropdown-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'lu-dropdown-menu',
 	templateUrl: './dropdown-menu.component.html',
 	styleUrl: './dropdown-menu.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
 })
 export class DropdownMenuComponent {}
