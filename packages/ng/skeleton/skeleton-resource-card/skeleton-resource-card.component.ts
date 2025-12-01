90edaf09b3c15402e83d4410f116e8f29f174598
Vincent Valentin <vvalentin@luccasoftware.com>
2025-12-01 16:15:51 +0100

    skeleton

---
+++ packages/ng/skeleton/skeleton-resource-card/skeleton-resource-card.component.ts
@@ -0,0 +1,16 @@
+import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input, numberAttribute } from '@angular/core';
+
+@Component({
+	selector: 'lu-skeleton-resource-card',
+	templateUrl: './skeleton-resource-card.component.html',
+	styleUrl: './skeleton-resource-card.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonResourceCardComponent {
+	dark = input(false, { transform: booleanAttribute });
+	descriptionLines = input(0, { transform: numberAttribute });
+
+	readonly lines = computed(() => Array.from({ length: this.descriptionLines() }));
+
+	getRandomPercent = (min: number = 33, max: number = 66): string => `${Math.floor(Math.random() * (max - min) + min).toString()}%`;
+}
