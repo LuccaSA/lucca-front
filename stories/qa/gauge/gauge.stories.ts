0e47f9cbca76651ca70425a9ad649661207b2ec5
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-01 15:13:28 +0100

    + Add qa test for gauge component

--- stories/qa/gauge/gauge.stories.ts
+++ stories/qa/gauge/gauge.stories.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { GaugeComponent } from '@lucca-front/ng/gauge';
 import { Meta, StoryFn } from '@storybook/angular';
 
 @Component({
 	selector: 'gauge-stories',
 	templateUrl: './gauge.stories.html',
+	imports: [GaugeComponent],
 })
 class GaugeStory {}
 
