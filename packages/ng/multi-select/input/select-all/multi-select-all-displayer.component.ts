9e8cb6c2f0b475de7927f1ef2239129641cf1a79
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-02 15:42:12 +0100

    + Add new display for multi select all chip

--- packages/ng/multi-select/input/select-all/multi-select-all-displayer.component.ts
+++ packages/ng/multi-select/input/select-all/multi-select-all-displayer.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, viewChild } from '@angular/core';
+import { Component, computed, ElementRef, inject, viewChild } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ChipComponent } from '@lucca-front/ng/chip';
 import { getIntl } from '@lucca-front/ng/core';
 import { ɵLuOptionOutletDirective } from '@lucca-front/ng/core-select';
-import { NumericBadgeComponent } from '@lucca-front/ng/numeric-badge';
 import { LU_MULTI_SELECT_DISPLAYER_TRANSLATIONS } from '../../displayer/default-displayer.translate';
 import { LuMultiSelectDisplayerInputDirective } from '../../displayer/displayer-input.directive';
 import { LuMultiSelectInputComponent } from '../select-input.component';
@@ -11,8 +10,7 @@ import { MULTI_SELECT_WITH_SELECT_ALL_CONTEXT } from './select-all.models';
 
 @Component({
 	selector: 'lu-multi-select-all-displayer',
-	imports: [NumericBadgeComponent, LuMultiSelectDisplayerInputDirective, ɵLuOptionOutletDirective, ChipComponent],
-	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [LuMultiSelectDisplayerInputDirective, ɵLuOptionOutletDirective, ChipComponent],
 	template: `
 		<div class="multipleSelect-displayer mod-filter" [class.is-filled]="isFilled()">
 			<input type="text" luMultiSelectDisplayerInput />
@@ -24,8 +22,9 @@ import { MULTI_SELECT_WITH_SELECT_ALL_CONTEXT } from './select-all.models';
 							<ng-template *luOptionOutlet="select.displayerTpl(); value: select.value[0]" />
 						</lu-chip>
 					} @else {
-						<lu-numeric-badge disableTooltip class="multipleSelect-displayer-numericBadge" [value]="displayerCount()" />
-						<span class="multipleSelect-displayer-label"> {{ displayerLabel() }} </span>
+						<lu-chip class="multipleSelect-displayer-chip" unkillable>{{ displayerCount() }} {{ displayerLabel() }}</lu-chip>
+						<!-- <lu-numeric-badge disableTooltip class="multipleSelect-displayer-numericBadge" [value]="" />
+						<span class="multipleSelect-displayer-label"> </span> -->
 					}
 				</div>
 			}
