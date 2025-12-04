6486d3ed462897f4c084744dc7a0d579ec7306bb
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2025-12-04 16:40:15 +0100

    tech(QA): add test for pageHeader component (#4214)
    * + add qa test for page header angular component
    
    * + Add more qa test for page header

--- stories/qa/page-header/page-header.stories.ts
+++ stories/qa/page-header/page-header.stories.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BreadcrumbsComponent, BreadcrumbsLinkDirective } from '@lucca-front/ng/breadcrumbs';
+import { ButtonComponent } from '@lucca-front/ng/button';
+import { FormFieldComponent } from '@lucca-front/ng/form-field';
+import { TextInputComponent } from '@lucca-front/ng/forms';
+import { HorizontalNavigationComponent, HorizontalNavigationLinkDirective } from '@lucca-front/ng/horizontal-navigation';
+import { IconComponent } from '@lucca-front/ng/icon';
+import { PageHeaderComponent } from '@lucca-front/ng/page-header';
 import { Meta, StoryFn } from '@storybook/angular';
 
 @Component({
 	selector: 'page-header-stories',
 	templateUrl: './page-header.stories.html',
+	imports: [
+		PageHeaderComponent,
+		BreadcrumbsComponent,
+		BreadcrumbsLinkDirective,
+		HorizontalNavigationComponent,
+		HorizontalNavigationLinkDirective,
+		FormFieldComponent,
+		TextInputComponent,
+		ButtonComponent,
+		IconComponent,
+		FormsModule,
+	],
 })
 class PageHeaderStory {}
 
 export default {
-	title: 'QA/Page-Header',
+	title: 'QA/PageHeader',
 	component: PageHeaderStory,
 } as Meta;
 
