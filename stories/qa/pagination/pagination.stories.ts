c519c7c2d245b8efb3ecb6da41031659dd64078c
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-05 10:50:38 +0100

    + Add qa test for pagination angular component

--- stories/qa/pagination/pagination.stories.ts
+++ stories/qa/pagination/pagination.stories.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PaginationComponent } from '@lucca-front/ng/pagination';
 import { Meta, StoryFn } from '@storybook/angular';
 
 @Component({
 	selector: 'pagination-stories',
 	templateUrl: './pagination.stories.html',
+	imports: [PaginationComponent],
 })
 class PaginationStory {}
 
