fb83bc9b52a9c9775b9423eaf794bdc668386b06
Jérémie Biron <jbiron@luccasoftware.com>
2025-12-05 17:20:18 +0100

    form field

--- stories/documentation/forms/fields/form-field.stories.ts
+++ stories/documentation/forms/fields/form-field.stories.ts
@@ -13,11 +13,63 @@ export default {
 		}),
 	],
 	argTypes: {
+		label: {
+			control: {
+				type: 'text',
+			},
+			description: "Modifie le label de l'input.",
+		},
+		required: {
+			control: {
+				type: 'boolean',
+			},
+			description: 'Marque le champ comme obligatoire.',
+		},
+		hiddenLabel: {
+			description: "Masque le label en le conservant dans le DOM pour les lecteurs d'écrans",
+		},
+		inlineMessage: {
+			control: {
+				type: 'text',
+			},
+			description: 'Ajoute un texte indicatif sous le champ de formulaire.',
+		},
+		inlineMessageState: {
+			options: ['default', 'success', 'warning', 'error'],
+			control: {
+				type: 'select',
+			},
+			description: "Modifie l'état de l'inline message.",
+		},
+		errorInlineMessage: {
+			options: ['default', 'success', 'warning', 'error'],
+			control: {
+				type: 'select',
+			},
+			description: "Ajoute un texte d\'erreur sous le champ de formulaire lorsque celui-ci est en erreur.",
+		},
+		tooltip: {
+			if: { arg: 'hiddenLabel', truthy: false },
+			description: 'Affiche une icône (?) associée à une info-bulle.',
+		},
+		invalid: {
+			control: {
+				type: 'boolean',
+			},
+			description: "Applique l'état invalide au champ.",
+		},
+		counter: {
+			control: {
+				type: 'number',
+			},
+			description: "Nombre de caractère maximum autorisés pour un champ de type texte. A seulement un impact sur l'interface et doit être complé à un réglage au niveau de <code>FormControl</code>.",
+		},
 		width: {
 			options: [null, 20, 30, 40, 50, 60],
 			control: {
 				type: 'select',
 			},
+			description: "Applique une largeur fixe au champ. A n'utiliser que lorsque la grille de formulaire n'est pas adaptée.",
 		},
 	},
 	render: (args, { argTypes }) => {
