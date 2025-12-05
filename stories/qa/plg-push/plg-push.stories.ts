ca1e044a51c9d3a2ecd6a4fd0292ae77840a47b0
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2025-12-05 12:07:35 +0100

    + Add qa test for plg push component (#4220)

--- stories/qa/plg-push/plg-push.stories.ts
+++ stories/qa/plg-push/plg-push.stories.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PLGPushComponent } from '@lucca-front/ng/plg-push';
 import { Meta, StoryFn } from '@storybook/angular';
 
 @Component({
 	selector: 'plg-push-stories',
 	templateUrl: './plg-push.stories.html',
+	imports: [PLGPushComponent],
 })
 class PLGPushStory {}
 
