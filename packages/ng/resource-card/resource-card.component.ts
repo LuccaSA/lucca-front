3f3aba58e367cece7c720e4dc154352b6710b7f5
Vincent Valentin <vvalentin@luccasoftware.com>
2025-12-08 10:48:04 +0100

    wip

--- packages/ng/resource-card/resource-card.component.ts
+++ packages/ng/resource-card/resource-card.component.ts
@@ -1,8 +1,6 @@
 import { CdkDragHandle } from '@angular/cdk/drag-drop';
-import { booleanAttribute, ChangeDetectionStrategy, Component, forwardRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { booleanAttribute, ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
 import { IconComponent } from '@lucca-front/ng/icon';
-import { LU_RESOURCE_CARD_INSTANCE } from './resource-card.token';
-import { LU_RESOURCE_CARD_WRAPPER_INSTANCE } from './wrapper/resource-card-wrapper.token';
 
 @Component({
 	selector: 'lu-resource-card',
@@ -14,26 +12,10 @@ import { LU_RESOURCE_CARD_WRAPPER_INSTANCE } from './wrapper/resource-card-wrapp
 	host: {
 		class: 'resourceCardContainer',
 	},
-	providers: [
-		{
-			provide: LU_RESOURCE_CARD_INSTANCE,
-			useExisting: forwardRef(() => ResourceCardComponent),
-		},
-	],
 })
 export class ResourceCardComponent {
-	wrapperRef = inject(LU_RESOURCE_CARD_WRAPPER_INSTANCE, { optional: true });
-
 	draggable = input(false, { transform: booleanAttribute });
 	disabled = input(false, { transform: booleanAttribute });
 	headingLevel = input<'1' | '2' | '3' | '4' | '5' | '6'>('3');
-	headingStyle = input<'3' | '4'>('3');
-
-	// #tooltip = inject(LuTooltipTriggerDirective);
-
-	constructor() {
-		// this.#tooltip.luTooltipOnlyForDisplay = true;
-		// this.#tooltip.luTooltipWhenEllipsis.set(true);
-		// console.log(this.actionRef()?.contentRef());
-	}
+	size = input<'S' | null>(null);
 }
