83e9452543b95b9828f18716694a3cf1e90e9358
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-12 11:08:59 +0100

    * update user.directive signalize

--- packages/ng/core-select/user/users.directive.ts
+++ packages/ng/core-select/user/users.directive.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Directive, Input, Provider, TemplateRef, Type, booleanAttribute, computed, effect, forwardRef, inject, input, model, signal, untracked } from '@angular/core';
+import { Directive, Provider, TemplateRef, Type, booleanAttribute, computed, effect, forwardRef, inject, input, model, signal, untracked } from '@angular/core';
 import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { ILuApiCollectionResponse } from '@lucca-front/ng/api';
+import { ɵeffectWithDeps } from '@lucca-front/ng/core';
 import { CORE_SELECT_API_TOTAL_COUNT_PROVIDER, CoreSelectApiTotalCountProvider, LuOptionContext, applySearchDelimiter } from '@lucca-front/ng/core-select';
 import { ALuCoreSelectApiDirective } from '@lucca-front/ng/core-select/api';
 import { LuDisplayFormat, LuDisplayFullname } from '@lucca-front/ng/user';
@@ -217,10 +218,15 @@ export class LuCoreSelectUsersDirective<T extends LuCoreSelectUser = LuCoreSelec
 export class LuCoreSelectUserOptionDirective<T extends LuCoreSelectUser = LuCoreSelectUser> {
 	#templateRef = inject(TemplateRef<LuOptionContext<T>>);
 
-	@Input({ alias: 'luUserOptionUsersRef' }) set usersDirective(usersDirective: LuCoreSelectUsersDirective<T>) {
-		usersDirective.customUserOptionTpl.set(this.#templateRef);
-	}
+	readonly usersDirective = input<LuCoreSelectUsersDirective<T>>(null, { alias: 'luUserOptionUsersRef' });
 
+	constructor() {
+		ɵeffectWithDeps([this.usersDirective], (usersDirective) => {
+			if (usersDirective) {
+				usersDirective.customUserOptionTpl.set(this.#templateRef);
+			}
+		});
+	}
 	public static ngTemplateContextGuard<T extends LuCoreSelectUser>(_dir: LuCoreSelectUserOptionDirective<T>, ctx: unknown): ctx is { $implicit: T } {
 		return true;
 	}
