ac8e6dbe57d319654aa2701ef5afed747d248775
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-16 10:16:46 +0100

    * modified input directive

--- packages/ng/form-field/input.directive.ts
+++ packages/ng/form-field/input.directive.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Directive, ElementRef, inject, input, OnInit } from '@angular/core';
+import { booleanAttribute, Directive, ElementRef, inject, Input, OnInit } from '@angular/core';
 import { FORM_FIELD_INSTANCE } from './form-field.token';
 
 @Directive({
@@ -16,7 +16,8 @@ export class InputDirective implements OnInit {
 	/**
 	 * Prevents message and label ids from being propagated, useful if the input holds its own message and label (like for radios)
 	 */
-	public readonly standalone = input(false, { transform: booleanAttribute, alias: 'luInputStandalone' });
+	@Input({ transform: booleanAttribute, alias: 'luInputStandalone' })
+	standalone = false;
 
 	ngOnInit(): void {
 		// If the field is used as standalone, we won't have the ref provided so it'll crash
