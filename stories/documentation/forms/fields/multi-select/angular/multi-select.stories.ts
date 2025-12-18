37d41e9045977a67ee294eb455eb0d74104f8871
Jeremie <jbiron@luccasoftware.com>
2025-12-18 10:46:13 +0100

    [Doc] Add controls descriptions to stories (batch 3) (#4286)
    * Input framed
    
    * Rich text / multi
    
    * Switch
    
    * Textfield
    
    * Textarea
    
    * List box options
    
    * Callout

--- stories/documentation/forms/fields/multi-select/angular/multi-select.stories.ts
+++ stories/documentation/forms/fields/multi-select/angular/multi-select.stories.ts
@@ -20,28 +20,49 @@ export default {
 		tooltip: {
 			type: 'string',
 			if: { arg: 'hiddenLabel', truthy: false },
+			description: 'Affiche une icône (?) associée à une info-bulle. ',
+		},
+		label: {
+			description: 'Modifie le label du champ.',
+		},
+		required: {
+			description: 'Marque le champ comme obligatoire.',
+		},
+		placeholder: {
+			description: 'Modifie le placeholder au champ.',
 		},
 		size: {
 			options: ['M', 'S'],
 			control: {
 				type: 'select',
 			},
+			description: 'Modifie la taille du champ.',
 		},
 		width: {
 			options: [null, 20, 30, 40, 50, 60],
 			control: {
 				type: 'select',
 			},
-			description: '[v19.2]',
+			description: '[v19.2] Applique une largeur fixe au champ.',
+		},
+		inlineMessage: {
+			description: 'Ajoute un texte descriptif (aide, erreur, etc.) sous le champ de formulaire.',
 		},
 		inlineMessageState: {
 			options: ['default', 'success', 'warning', 'error'],
 			control: {
 				type: 'select',
 			},
+			description: "Modifie l'état de l'inline message.",
 		},
 		hiddenLabel: {
 			description: "Masque le label en le conservant dans le DOM pour les lecteurs d'écrans",
+		},
+		clearable: {
+			description: 'Affiche un bouton pour vider le champ lorsque celui-ci est rempli.',
+		},
+		loading: {
+			description: "Applique l'état de chargement.",
 		},
 		clueChange: HiddenArgType,
 		nextPage: HiddenArgType,
