5928f58cf09961a2a2cbc28ee13fed939be472ec
Jeremie <jbiron@luccasoftware.com>
2025-12-19 11:35:29 +0100

    [Utilities] New color texts utilities + deprecate / add homogeneity  (#4215)
    * New tokens utilities + refactor
    
    * + Add migration schematics for color-text tokens
    
    * * used schematics on lf files
    
    * * fix lint
    
    * * duplicated
    
    * Schematics fixes + add deprecated
    
    ---------
    
    Co-authored-by: JeSuisCharlie1 <cjeanneau@luccasoftware.com>

--- stories/documentation/texts/icons/HTML&CSS/icon-colors.stories.ts
+++ stories/documentation/texts/icons/HTML&CSS/icon-colors.stories.ts
@@ -8,12 +8,12 @@ export default {
 
 function getTemplate(args: IconColorStory): string {
 	return `<span aria-hidden="true" class="lucca-icon icon-heart"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textLight"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textPlaceholder"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textProduct"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textError"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textWarning"></span>
-<span aria-hidden="true" class="lucca-icon icon-heart pr-u-textSuccess"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorTextSubtle"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorInputTextPlaceholder"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorTextProduct"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorTextError"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorTextWarning"></span>
+<span aria-hidden="true" class="lucca-icon icon-heart pr-u-colorTextSuccess"></span>
 <!-- 20.3 --><span aria-hidden="true" class="lucca-icon icon-heart mod-AI"></span>`;
 }
 
