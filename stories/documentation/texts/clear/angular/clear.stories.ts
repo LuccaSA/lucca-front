4f2a76728fb83d7b7b651ff95a1f7534190b3cb8
Jérémie Biron <jbiron@luccasoftware.com>
2025-12-23 16:57:57 +0100

    clear

--- stories/documentation/texts/clear/angular/clear.stories.ts
+++ stories/documentation/texts/clear/angular/clear.stories.ts
@@ -23,20 +23,32 @@ export default {
 
 export const Template: StoryObj = {
 	argTypes: {
+		disabled: {
+			description: 'Désactive le bouton.',
+		},
 		palette: {
 			options: ['', 'success', 'warning', 'error', 'product', 'brand', 'neutral', 'none', 'primary', 'grey'],
 			control: {
 				type: 'select',
 			},
+			description: 'Applique une palette de couleurs au bouton.',
 		},
 		inverted: {
 			if: { arg: 'disabled', truthy: false },
+			description: 'Modifie les couleurs du bouton pour un usage sur fond foncé.',
 		},
 		size: {
 			options: ['', 'S'],
 			control: {
 				type: 'select',
 			},
+			description: 'Modifie la taille du bouton.',
+		},
+		alt: {
+			description: "Information restituée par le lecteur d'écran.",
+		},
+		hidden: {
+			description: 'Masque le bouton.',
 		},
 	},
 	args: {
