bbf12b690ee3fae6e1a2fae803fab1a9a734b368
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2025-12-24 15:19:59 +0100

    [schematics] : Add component path schematics updater (#4315)
    * + add schematic path-mapper
    
    * + Add index for component-path schematics test && input & output
    
    * * udpate path component schematics
    
    * * wip update component path map
    
    * * await else
    
    * + Add working component mapper
    
    * + more test && more path to update from schematics
    
    * * udpate naming test migration
    
    * * fix schema.json class-prefix
    
    * - removed camel case folder ng comp

---
+++ packages/ng/schematics/component-path/index.ts
@@ -0,0 +1,27 @@
+import type { Rule } from '@angular-devkit/schematics';
+import { ComponentMapper } from '../lib/component-mapper';
+import { currentSchematicContext, SchematicContextOpts } from '../lib/lf-schematic-context';
+
+// Nx need to see "@angular-devkit/schematics" in order to run this migration correctly (see https://github.com/nrwl/nx/blob/d9fed4b832bf01d1b9a44ae9e486a5e5cd2d2253/packages/nx/src/command-line/migrate/migrate.ts#L1729-L1738)
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('@angular-devkit/schematics');
+
+export default (options: SchematicContextOpts): Rule => {
+	return async (tree, context) => {
+		await currentSchematicContext.init(context, options);
+
+		new ComponentMapper(
+			tree,
+			{
+				paths: {
+					"@lucca-front/ng/scrollBox": "@lucca-front/ng/scroll-box",
+					"@lucca-front/ng/segmentedControl": "@lucca-front/ng/segmented-control",
+					"@lucca-front/ng/segmentedControlTabs": "@lucca-front/ng/segmented-control-tabs",
+					"@lucca-front/ng/statusBadge": "@lucca-front/ng/status-badge"
+				},
+				components: {},
+				selectors: {}
+			}
+		).run()
+	}
+}
