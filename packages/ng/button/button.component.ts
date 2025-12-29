b4cfaabbaf6d2d8b1a4b86f161a40a6a3e0bcc1d
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-29 14:51:39 +0100

    + Add documentation for button component

--- packages/ng/button/button.component.ts
+++ packages/ng/button/button.component.ts
@@ -23,18 +23,39 @@ export class ButtonComponent {
 
 	readonly notifyError = signal(false);
 
+	/**
+	 * Change the size of the Button
+	 */
 	readonly size = input<'M' | 'S' | 'XS'>();
 
+	/**
+	 * Apply block display
+	 */
 	readonly block = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Marks an action with significant consequences
+	 */
 	readonly critical = input(false, { transform: booleanAttribute });
 
+	/**
+	 * @deprecated use `critical` input instead
+	 */
 	readonly delete = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Indicates the presence of a menu
+	 */
 	readonly disclosure = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Applies a color palette to the Button
+	 */
 	readonly palette = input<Palette>('none');
 
+	/**
+	 * Button state
+	 */
 	readonly state = input<'default' | 'loading' | 'error' | 'success'>('default');
 
 	/**
