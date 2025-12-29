7854978f7e2322e120641b44a42dd3adc967687f
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-29 15:33:31 +0100

    + Add documentation for empty-state-section compoennt

--- packages/ng/empty-state/empty-state-section/empty-state-section.component.ts
+++ packages/ng/empty-state/empty-state-section/empty-state-section.component.ts
@@ -17,14 +17,30 @@ export class EmptyStateSectionComponent {
 	 */
 	readonly icon = input<string>('https://cdn.lucca.fr/lucca-front/assets/empty-states/icons/iconBanknote.svg');
 
+	/**
+	 * Which palette should be used for the empty state section.
+	 * Defaults to none (inherits parent palette)
+	 */
 	readonly palette = input<Palette>('none');
 
+	/**
+	 * Display empty state section in center
+	 */
 	readonly center = input(false, { transform: booleanAttribute });
 
+	/**
+	 * The title of the empty state section
+	 */
 	readonly heading = input<string>();
 
+	/**
+	 * The description of the empty state section
+	 */
 	readonly description = input<PortalContent>();
 
+	/**
+	 * Define the aria level of the title
+	 */
 	readonly hx = input(3, { transform: numberAttribute as (value: Hx | `${Hx}`) => Hx });
 
 	readonly emptyStateClasses = computed(() => ({ [`palette-${this.palette()}`]: !!this.palette() }));
