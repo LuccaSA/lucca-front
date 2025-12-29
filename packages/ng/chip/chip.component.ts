e26fad8ff734f1b130704213820a844ba0eaeccc
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2025-12-29 14:59:20 +0100

    + Add documentation for chip component

--- packages/ng/chip/chip.component.ts
+++ packages/ng/chip/chip.component.ts
@@ -24,27 +24,59 @@ import { LU_CHIP_TRANSLATIONS } from './chip.translate';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChipComponent {
-	intl = getIntl(LU_CHIP_TRANSLATIONS);
+	readonly intl = getIntl(LU_CHIP_TRANSLATIONS);
 
+	/**
+	 * Add an ellipsis if the text is too long
+	 */
 	readonly withEllipsis = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Removed kill button
+	 */
 	readonly unkillable = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Which palette should be used for the entire chip.
+	 * Defaults to none (inherits parent palette)
+	 */
 	readonly palette = input<string>();
 
+	/**
+	 * Disables the chip
+	 */
 	readonly disabled = input(false, { transform: booleanAttribute });
 
+	/**
+	 * Which size should the chip be? Defaults or small
+	 */
 	readonly size = input<'S' | null>(null);
 
+	/**
+	 * State is a shorthand to set the icon and the palette to the recommended values for the icon and palette based on
+	 * the provided state.
+	 *
+	 * If one of the icon or palette inputs are filled along with the state input, their values will have the priority over
+	 * state (so setting state to success and palette to warning will make the palette warning)
+	 */
 	readonly state = input<'warning' | 'critical' | null>(null);
+
+	/**
+	 * Which icon should we display in the chip if any?
+	 * Defaults to no icon.
+	 */
+	readonly icon = input<LuccaIcon | null>(null);
+
+	/**
+	 * Emit event when button kill is click
+	 */
+	readonly kill = output<Event>();
 
 	readonly stateAlt = computed(() => (this.isWarning() ? this.intl.warning : this.isCritical() ? this.intl.error : ''));
 
-	readonly icon = input<LuccaIcon | null>(null);
-
-	readonly kill = output<Event>();
-
 	readonly isWarning = computed<boolean>(() => this.state() === 'warning');
+
 	readonly isCritical = computed<boolean>(() => this.state() === 'critical');
+
 	readonly displayedIcon = computed<LuccaIcon | null>(() => (this.isWarning() ? 'signWarning' : this.isCritical() ? 'signError' : this.icon()));
 }
