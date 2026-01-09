4962428040c32f96f4d657172ee854c3fc9db1f2
Jeremie <jbiron@lucca.fr>
2026-01-09 11:53:47 +0100

    Fix header example + sidebar issue (#4339)

--- stories/documentation/structure/main-layout/angular/inAppLayout.stories.ts
+++ stories/documentation/structure/main-layout/angular/inAppLayout.stories.ts
@@ -40,9 +40,10 @@ export default {
 		const headerContainer = args.header
 			? `
 		<ng-container mainLayoutHeader>
-			<lu-container>
-				<div class="fakeContent">header</div>
-			</lu-container>
+			<div class="fakeContent">
+				header
+				<!-- <lu-page-header container /> -->
+			</div>
 		</ng-container>`
 			: ``;
 		const footerContainer = args.footer
@@ -91,95 +92,98 @@ export default {
 		return {
 			styles: [
 				`
-:host ::ng-deep {
-	.appLayout {
-		--components-appLayout-blockSize: 100%;
-		--components-appLayout-inlineSize: 100%;
-		resize: vertical;
-		overflow: hidden;
-		min-block-size: 394px;
-		border-radius: var(--pr-t-border-radius-100);
-		border: 1px solid var(--palettes-neutral-200);
+@layer components {
+	:host ::ng-deep {
 
-		> * {
-			font-family: monospace;
+		.appLayout {
+			--components-appLayout-blockSize: 100%;
+			--components-appLayout-inlineSize: 100%;
+			resize: vertical;
+			overflow: hidden;
+			min-block-size: 394px;
+			border-radius: var(--pr-t-border-radius-100);
+			border: 1px solid var(--palettes-neutral-200);
 
-			&:not(.appLayout-main) {
-				display: grid;
-				place-items: center;
-			}
+			> * {
+				font-family: monospace;
 
-			&.appLayout-banner {
-				padding-block: 0;
-			}
+				&:not(.appLayout-main) {
+					display: grid;
+					place-items: center;
+				}
 
-			&.appLayout-navSide {
-				padding: var(--pr-t-spacings-150) var(--pr-t-spacings-400);
+				&.appLayout-banner {
+					padding-block: 0;
+				}
+
+				&.appLayout-navSide {
+					padding: var(--pr-t-spacings-150) var(--pr-t-spacings-400);
+				}
 			}
 		}
-	}
 
-	.appLayout-banner {
-		background-color: var(--pr-t-elevation-surface-raised);
-		box-shadow: var(--pr-t-elevation-shadow-overflow);
-		position: relative;
-		z-index: 2;
+		.appLayout-banner {
+			background-color: var(--pr-t-elevation-surface-raised);
+			box-shadow: var(--pr-t-elevation-shadow-overflow);
+			position: relative;
+			z-index: 2;
 
-		&::before {
-			content: '';
-			position: absolute;
-			inset-inline-start: var(--pr-t-spacings-100);
-			width: 122px;
-			height: 32px;
+			&::before {
+				content: '';
+				position: absolute;
+				inset-inline-start: var(--pr-t-spacings-100);
+				width: 122px;
+				height: 32px;
+				background-color: var(--palettes-neutral-50);
+				border-radius: var(--pr-t-border-radius-50);
+			}
+
+			&::after {
+				content: '';
+				position: absolute;
+				inset-inline-end: var(--pr-t-spacings-100);
+				width: 32px;
+				height: 32px;
+				background-color: var(--palettes-neutral-200);
+				border-radius: var(--pr-t-border-radius-full);
+			}
+		}
+
+		.appLayout-navSide {
+			background-color: var(--palettes-neutral-500);
+		}
+
+		.mainLayout-sidebar {
 			background-color: var(--palettes-neutral-50);
-			border-radius: var(--pr-t-border-radius-50);
+			padding: var(--pr-t-spacings-150);
+			align-items: center;
+			justify-content: center;
+			display: flex;
+			flex-direction: column;
+			color: var(--palettes-brand-700);
+			font-family: monospace;
 		}
 
-		&::after {
-			content: '';
-			position: absolute;
-			inset-inline-end: var(--pr-t-spacings-100);
-			width: 32px;
-			height: 32px;
-			background-color: var(--palettes-neutral-200);
-			border-radius: var(--pr-t-border-radius-full);
+		.mainLayout-content-inside {
+			gap: var(--pr-t-spacings-100);
 		}
-	}
 
-	.appLayout-navSide {
-		background-color: var(--palettes-neutral-500);
-	}
+		.container {
+			--commons-container-maxWidth: 50rem;
+		}
 
-	.mainLayout-sidebar {
-		background-color: var(--palettes-neutral-50);
-		padding: var(--pr-t-spacings-150);
-		align-items: center;
-		justify-content: center;
-		display: flex;
-		flex-direction: column;
-		color: var(--palettes-brand-700);
-		font-family: monospace;
-	}
-
-	.mainLayout-content-inside {
-		gap: var(--pr-t-spacings-100);
-	}
-
-	.container {
-		--commons-container-maxWidth: 50rem;
-	}
-
-	.fakeContent {
-		background-color: var(--pr-t-elevation-surface-raised);
-		border: 1px solid var(--palettes-neutral-50);
-		padding: var(--pr-t-spacings-150);
-		align-items: center;
-		justify-content: center;
-		display: flex;
-		flex-direction: column;
-		color: var(--palettes-brand-700);
-		font-family: monospace;
-		white-space: nowrap;
+		.fakeContent {
+			background-color: var(--pr-t-elevation-surface-raised);
+			border: 1px solid var(--palettes-neutral-50);
+			padding: var(--pr-t-spacings-150);
+			align-items: center;
+			justify-content: center;
+			display: flex;
+			flex-direction: column;
+			color: var(--palettes-brand-700);
+			font-family: monospace;
+			white-space: nowrap;
+		}
 	}
 }
 				`,
