5ca44a08621a61e8e80450f1bdc3e7b5e3df7128
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-01-13 10:57:24 +0100

    + Add documentation for progress bar component

--- packages/ng/progress-bar/progress-bar.component.ts
+++ packages/ng/progress-bar/progress-bar.component.ts
@@ -8,13 +8,22 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input, 
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProgressBarComponent {
+	/**
+	 * Progress percentage (0 to 100)
+	 */
 	readonly value = input(0, { transform: numberAttribute });
+
+	/**
+	 * Progress bar state
+	 */
 	readonly state = input<'success' | 'error' | 'null'>(null);
+
+	/**
+	 * Displays a loading state without progress information
+	 */
 	readonly indeterminate = input(false, { transform: booleanAttribute });
 
-	readonly stateClass = computed(() => {
-		return {
-			[`is-${this.state()}`]: !!this.state(),
-		};
-	});
+	readonly stateClass = computed(() => ({
+		[`is-${this.state()}`]: !!this.state(),
+	}));
 }
