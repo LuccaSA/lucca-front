b1109c471105fbb8b5758afca3bd6ccc990875af
mstudy <mstudy@luccasoftware.com>
2026-01-16 16:06:06 +0100

    rename formatSize to formatFileSize

--- packages/ng/file-upload/public-api.ts
+++ packages/ng/file-upload/public-api.ts
@@ -1,6 +1,6 @@
 export * from './file-dropzone/file-dropzone.component';
 export * from './file-entry/file-entry.component';
 export * from './file-upload-entry';
-export { formatSize } from './formatter';
+export { formatFileSize } from './formatter';
 export * from './multi/multi-file-upload.component';
 export * from './single/single-file-upload.component';
