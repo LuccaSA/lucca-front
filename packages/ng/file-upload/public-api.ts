b43be6f2682c960373702a93d7f7d8f7a72fa738
mstudy <mstudy@luccasoftware.com>
2026-01-16 15:34:28 +0100

    fix import

--- packages/ng/file-upload/public-api.ts
+++ packages/ng/file-upload/public-api.ts
@@ -1,7 +1,6 @@
 export * from './file-dropzone/file-dropzone.component';
 export * from './file-entry/file-entry.component';
-export { formatSize } from './file-size-formatter';
 export * from './file-upload-entry';
+export { formatSize } from './formatter';
 export * from './multi/multi-file-upload.component';
 export * from './single/single-file-upload.component';
-
