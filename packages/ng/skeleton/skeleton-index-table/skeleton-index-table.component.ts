37e6c0e1e1dfe27422af659cd8f6a6b9d2d48315
Vincent Valentin <vvalentin@luccasoftware.com>
2026-01-20 16:16:17 +0100

    [skeleton*Table] add param for alignement

--- packages/ng/skeleton/skeleton-index-table/skeleton-index-table.component.ts
+++ packages/ng/skeleton/skeleton-index-table/skeleton-index-table.component.ts
@@ -1,6 +1,8 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
+export type ColAlignIndexTable = 'start' | 'center' | 'end';
+
 @Component({
 	selector: 'lu-skeleton-index-table',
 	templateUrl: './skeleton-index-table.component.html',
@@ -18,6 +20,8 @@ export class SkeletonIndexTableComponent {
 	 * Defines the number of cols (5 by default)
 	 */
 	readonly cols = input<number>(5);
+
+	readonly colsAlign = input<Record<number, ColAlignIndexTable>>({});
 
 	/**
 	 * Defines the number of row (8 by default)
--- packages/ng/skeleton/skeleton-table/skeleton-table.component.ts
+++ packages/ng/skeleton/skeleton-table/skeleton-table.component.ts
@@ -1,6 +1,8 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
+export type ColAlignTable = 'start' | 'center' | 'end';
+
 @Component({
 	selector: 'lu-skeleton-table',
 	templateUrl: './skeleton-table.component.html',
@@ -18,6 +20,8 @@ export class SkeletonTableComponent {
 	 * Defines the number of cols (5 by default)
 	 */
 	readonly cols = input<number>(5);
+
+	readonly colsAlign = input<Record<number, ColAlignTable>>({});
 
 	/**
 	 * Defines the number of row (8 by default)
