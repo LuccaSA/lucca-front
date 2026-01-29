d3bc4e77c818e0c7e350b728413a9298cb5b3500
Vincent Valentin <vvalentin@luccasoftware.com>
2026-01-29 14:39:33 +0100

    translations

--- packages/ng/form-label/translations.ts
+++ packages/ng/form-label/translations.ts
@@ -4,55 +4,55 @@
 			one: 'Uw bericht bevat {{counterStatus}} teken. Het maximum aantal is {{counterMax}}.',
 			other: 'Uw bericht bevat {{counterStatus}} tekens. Het maximum aantal is {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Aanvullende informatie',
 	},
 	nl: {
 		counterAlt: {
 			one: 'Uw bericht bevat {{counterStatus}} teken. Het maximum aantal is {{counterMax}}.',
 			other: 'Uw bericht bevat {{counterStatus}} tekens. Het maximum aantal is {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Aanvullende informatie',
 	},
 	it: {
 		counterAlt: {
 			one: 'Il tuo messaggio contiene {{counterStatus}} carattere. Sono consentiti al massimo {{counterMax}}.',
 			other: 'Il tuo messaggio è lungo {{counterStatus}} caratteri. Sono consentiti al massimo {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Informazioni aggiuntive',
 	},
 	fr: {
 		counterAlt: {
 			one: 'Votre message fait {{counterStatus}} caractère de long. {{counterMax}} maximum sont autorisés.',
 			other: 'Votre message fait {{counterStatus}} caractères de long. {{counterMax}} maximum sont autorisés.',
 		},
-		tooltipAlt: 'En savoir plus',
+		tooltipAlt: 'Info supplémentaire',
 	},
 	pt: {
 		counterAlt: {
 			one: 'A sua mensagem tem {{counterStatus}} caractere. São permitidos no máximo {{counterMax}}.',
 			other: 'A sua mensagem tem {{counterStatus}} caracteres. São permitidos no máximo {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Informações adicionais',
 	},
 	es: {
 		counterAlt: {
 			one: 'Tu mensaje tiene {{counterStatus}} carácter. Se permite un máximo de {{counterMax}}.',
 			other: 'Tu mensaje tiene {{counterStatus}} caracteres. Se permite un máximo de {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Información complementaria',
 	},
 	de: {
 		counterAlt: {
 			one: 'Ihre Nachricht enthält {{counterStatus}} Zeichen. Die maximale Anzahl beträgt {{counterMax}}.',
 			other: 'Ihre Nachricht enthält {{counterStatus}} Zeichen. Die maximale Anzahl beträgt {{counterMax}}.',
 		},
-		tooltipAlt: '',
+		tooltipAlt: 'Zusätzliche Info',
 	},
 	en: {
 		counterAlt: {
 			one: 'Your message is {{counterStatus}} character long. A maximum of {{counterMax}} are allowed.',
 			other: 'Your message is {{counterStatus}} characters long. A maximum of {{counterMax}} are allowed.',
 		},
-		tooltipAlt: 'Find out more',
+		tooltipAlt: 'Additional info',
 	},
 };
