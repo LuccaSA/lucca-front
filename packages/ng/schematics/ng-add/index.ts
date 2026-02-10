a6c0a48fb51b386010d7a3331d6022f95a8ffd8c
Guillaume Nury <gnury@luccasoftware.com>
2026-02-10 11:49:36 +0100

    fix(schematics): handle srcRoot in ng-add

--- packages/ng/schematics/ng-add/index.ts
+++ packages/ng/schematics/ng-add/index.ts
@@ -1,19 +1,22 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { createShameScss, updateAngularJson, updateStylesScss } from './tasks';
+import { createShameScss, getAngularJson, getProject, updateAngularJson, updateStylesScss } from './tasks';
 
 export function ngAdd(): Rule {
 	return (tree: Tree, context: SchematicContext) => {
 		context.addTask(new NodePackageInstallTask({ packageName: '@lucca-front/icons' }));
 		context.addTask(new NodePackageInstallTask({ packageName: '@lucca-front/scss' }));
 
+		const angularJson = getAngularJson(tree);
+		const project = getProject(angularJson);
+
 		// Add LF @import and init css vars
-		updateStylesScss(tree);
+		updateStylesScss(tree, project);
 
 		// Add empty shame file
-		createShameScss(tree);
+		createShameScss(tree, project);
 
 		// Add stylePreprocessorOptions
-		updateAngularJson(tree);
+		updateAngularJson(tree, project, angularJson);
 	};
 }
--- packages/ng/schematics/ng-add/tasks.ts
+++ packages/ng/schematics/ng-add/tasks.ts
@@ -2,32 +2,41 @@ import { Tree } from '@angular-devkit/schematics';
 import { shameScss, stylesScss } from './file-content';
 
 // custom to make TS happy
-type AngularProject = { architect: { build: { options: { stylePreprocessorOptions: { includePaths: string[] } } } } };
+type AngularProject = { sourceRoot: string; architect: { build: { options: { stylePreprocessorOptions: { includePaths: string[] } } } } };
 type AngularJsonSchema = { defaultProject?: string; projects: Record<string, AngularProject> };
 
 const files = {
-	globalStyles: 'src/styles.scss',
-	shameStyles: 'src/scss/_shame.scss',
+	globalStyles: 'styles.scss',
+	shameStyles: 'scss/_shame.scss',
 } as const;
 
-export function updateStylesScss(tree: Tree) {
-	const existingLength = tree.read(files.globalStyles)?.toString().length ?? 0;
-	const styleUpdateRecord = tree.beginUpdate(files.globalStyles).insertRight(existingLength, stylesScss);
-	tree.commitUpdate(styleUpdateRecord);
-}
-
-export function updateAngularJson(tree: Tree) {
-	const angularJson = JSON.parse(tree.read('angular.json')?.toString() ?? '{}') as AngularJsonSchema;
+export function getProject(angularJson: AngularJsonSchema): AngularProject {
 	const { defaultProject, projects } = angularJson;
 
 	const project = defaultProject ?? Object.keys(projects)[0];
-	projects[project].architect.build.options.stylePreprocessorOptions = {
+	return projects[project];
+}
+
+export function getAngularJson(tree: Tree): AngularJsonSchema {
+	return JSON.parse(tree.read('angular.json')?.toString() ?? '{}') as AngularJsonSchema;
+}
+
+export function updateStylesScss(tree: Tree, project: AngularProject) {
+	const path = `${project.sourceRoot ?? 'src'}/${files.globalStyles}`;
+	const existingLength = tree.read(path)?.toString().length ?? 0;
+	const styleUpdateRecord = tree.beginUpdate(path).insertRight(existingLength, stylesScss);
+	tree.commitUpdate(styleUpdateRecord);
+}
+
+export function updateAngularJson(tree: Tree, project: AngularProject, angularJson: AngularJsonSchema) {
+	project.architect.build.options.stylePreprocessorOptions = {
 		includePaths: ['src/scss', 'node_modules'],
 	};
 
 	tree.overwrite('angular.json', JSON.stringify(angularJson, undefined, '  ') + '\n');
 }
 
-export function createShameScss(tree: Tree) {
-	tree.create(files.shameStyles, shameScss);
+export function createShameScss(tree: Tree, project: AngularProject) {
+	const path = `${project.sourceRoot ?? 'src'}/${files.shameStyles}`;
+	tree.create(path, shameScss);
 }
