f8ad1e70e79c768c0a8caae4abddc97dc196e2c5
FXG <fgentilhomme@luccasoftware.com>
2026-02-11 16:17:26 +0100

    fix: lint conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ import typescript from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import tsParser from '@typescript-eslint/parser';
 
-const STRICTIFIED_NG_ENTRIES = ['packages/ng/tree-select/**/*.ts', 'packages/ng/user/**/*.ts'];
+const STRICTIFIED_NG_ENTRIES = ['packages/ng/tree-select/**/*.ts', 'packages/ng/user/**/*.ts', 'packages/ng/user-popover/**/*.ts'];
 
 export default defineConfig(
 	{
