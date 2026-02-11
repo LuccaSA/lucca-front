76a0e298e5bc5286617fcf6730fe379e307812c6
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-02-11 16:16:50 +0100

    * fix title service && spec strict

--- packages/ng/title/title.service.spec.ts
+++ packages/ng/title/title.service.spec.ts
@@ -10,7 +10,7 @@ import { TitleSeparator } from './title.model';
 import { LuTitleService } from './title.service';
 
 class TranslateService implements ILuTitleTranslateService {
-	translate(key: string, args: Record<string, unknown> = null): string {
+	translate(key: string, args: Record<string, unknown> | null = null): string {
 		if (args !== null) {
 			Object.entries(args).forEach(([argKey, value]) => {
 				key = key.replace(`{{${argKey}}}`, value as string);
