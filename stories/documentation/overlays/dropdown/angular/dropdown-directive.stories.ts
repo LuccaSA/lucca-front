e380203ccca3ae212bad8a4301783232e1b9c815
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-02-13 11:13:36 +0100

    * rework dropdown directive stories to have control for luPopoverPosition

--- stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
+++ stories/documentation/overlays/dropdown/angular/dropdown-directive.stories.ts
@@ -1,81 +1,108 @@
 import { ButtonComponent } from '@lucca-front/ng/button';
 import { DropdownActionComponent, DropdownDividerComponent, DropdownGroupComponent, DropdownItemComponent, DropdownMenuComponent, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
 import { IconComponent } from '@lucca-front/ng/icon';
-import { Meta, StoryObj } from '@storybook/angular';
+import { PopoverPosition } from '@lucca-front/ng/popover2';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+interface DropdownBasicStory {
+	luPopoverPosition: PopoverPosition;
+}
 
 export default {
 	title: 'Documentation/Overlays/Dropdown/Angular/Directive',
 	component: LuDropdownTriggerDirective,
-	decorators: [],
+	decorators: [
+		moduleMetadata({
+			imports: [
+				IconComponent,
+				ButtonComponent,
+				DropdownItemComponent,
+				DropdownActionComponent,
+				DropdownDividerComponent,
+				DropdownGroupComponent,
+				IconComponent,
+				LuDropdownTriggerDirective,
+				DropdownMenuComponent,
+			],
+		}),
+	],
+	argTypes: {
+		luPopoverPosition: {
+			control: 'select',
+			options: ['above', 'below', 'before', 'after'],
+		},
+	},
 } as Meta;
 
-const Template = (args) => ({
-	props: args,
-	moduleMetadata: {
-		imports: [
-			IconComponent,
-			ButtonComponent,
-			DropdownItemComponent,
-			DropdownActionComponent,
-			DropdownDividerComponent,
-			DropdownGroupComponent,
-			IconComponent,
-			LuDropdownTriggerDirective,
-			DropdownMenuComponent,
-		],
-	},
-	template: `<button type="button" luButton disclosure [luDropdown]="dropdownSample">Dropdown<lu-icon icon="arrowChevronBottom" /></button>
-
-<ng-template #dropdownSample>
-	<lu-dropdown-menu>
-		<lu-dropdown-item>
-			<button lu-dropdown-action type="button">
-				<lu-icon icon="heart" />
-				Lorem
-			</button>
-		</lu-dropdown-item>
-		<lu-dropdown-item>
-			<button lu-dropdown-action disabled type="button">
-				<lu-icon icon="cross" />
-				Lorem
-			</button>
-		</lu-dropdown-item>
-		<lu-dropdown-divider />
-		<lu-dropdown-item>
-			<button lu-dropdown-action type="button">
-				<lu-icon icon="star" />
-				Ipsum
-			</button>
-		</lu-dropdown-item>
-		<lu-dropdown-group label="Group">
+function getTemplate(args: DropdownBasicStory): string {
+	const direction = args.luPopoverPosition !== 'below' ? ` luDropdownPostion="${args.luPopoverPosition}"` : ``;
+	return `<div class="demo">
+	<button type="button" luButton disclosure [luDropdown]="dropdownSample"${direction}>Dropdown<lu-icon icon="arrowChevronBottom" /></button>
+	<ng-template #dropdownSample>
+		<lu-dropdown-menu>
 			<lu-dropdown-item>
 				<button lu-dropdown-action type="button">
-					<lu-icon icon="buildingHouse" />
-					Dolor
+					<lu-icon icon="heart" />
+					Lorem
 				</button>
 			</lu-dropdown-item>
 			<lu-dropdown-item>
-				<a lu-dropdown-action critical href="#">
-					<lu-icon icon="trashDelete" />
-					Sit amet
-				</a>
-			</lu-dropdown-item>
-			<lu-dropdown-item>
-				<span lu-dropdown-action disabled class="dropdown-list-option-action">
+				<button lu-dropdown-action disabled type="button">
 					<lu-icon icon="cross" />
-					Sit amet
-				</span>
+					Lorem
+				</button>
 			</lu-dropdown-item>
-		</lu-dropdown-group>
-	</lu-dropdown-menu>
-</ng-template>`,
+			<lu-dropdown-divider />
+			<lu-dropdown-item>
+				<button lu-dropdown-action type="button">
+					<lu-icon icon="star" />
+					Ipsum
+				</button>
+			</lu-dropdown-item>
+			<lu-dropdown-group label="Group">
+				<lu-dropdown-item>
+					<button lu-dropdown-action type="button">
+						<lu-icon icon="buildingHouse" />
+						Dolor
+					</button>
+				</lu-dropdown-item>
+				<lu-dropdown-item>
+					<a lu-dropdown-action critical href="#">
+						<lu-icon icon="trashDelete" />
+						Sit amet
+					</a>
+				</lu-dropdown-item>
+				<lu-dropdown-item>
+					<span lu-dropdown-action disabled class="dropdown-list-option-action">
+						<lu-icon icon="cross" />
+						Sit amet
+					</span>
+				</lu-dropdown-item>
+			</lu-dropdown-group>
+		</lu-dropdown-menu>
+	</ng-template>
+</div>`;
+}
+
+const Template = (args) => ({
+	props: args,
+	template: getTemplate(args),
+	styles: [
+		`
+		.demo {
+		display: flex;
+		min-block-size: 20rem;
+		align-items: center;
+		justify-content: center;
+	}
+	`,
+	],
 });
 
-export const Directive: StoryObj = {
-	args: {},
+export const Directive: StoryObj<DropdownBasicStory> = {
+	args: {
+		luPopoverPosition: 'below',
+	},
 	render: Template,
-};
-
-Directive.parameters = {
-	controls: { include: [] },
+	argTypes: {},
 };
