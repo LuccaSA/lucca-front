0ec3b49ec4598a227899b31748ea908742a68f08
Alexia Alarza <aalarza@luccasoftware.com>
2026-02-19 17:13:51 +0100

    feat(highlightData): add mealvoucher-card on storie

--- stories/documentation/structure/highlight-data/angular/basic.stories.ts
+++ stories/documentation/structure/highlight-data/angular/basic.stories.ts
@@ -50,7 +50,22 @@ export const Template: StoryObj<HighlightDataComponent & { action: string }> = {
 			},
 		},
 		illustration: {
-			options: ['calculator', 'calendar', 'cleemy-card', 'coffee', 'headphone', 'mail', 'manifying-glass', 'medallon', 'piggy-bank', 'polaroid-female', 'polaroid-male', 'polaroids', null],
+			options: [
+				'calculator',
+				'calendar',
+				'cleemy-card',
+				'coffee',
+				'headphone',
+				'mail',
+				'manifying-glass',
+				'mealvoucher-card',
+				'medallon',
+				'piggy-bank',
+				'polaroid-female',
+				'polaroid-male',
+				'polaroids',
+				null,
+			],
 			control: {
 				type: 'select',
 			},
