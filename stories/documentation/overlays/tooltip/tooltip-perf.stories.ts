5eda47b975e68315a0ff500af19c78878326569f
Flavien Normand <contact@flavien-normand.fr>
2026-03-03 11:45:37 +0100

    feat(story): add toggle button to tooltips perf story

--- stories/documentation/overlays/tooltip/tooltip-perf.stories.ts
+++ stories/documentation/overlays/tooltip/tooltip-perf.stories.ts
@@ -22,6 +22,7 @@ export default {
 			props: {
 				width: new Array(25),
 				height: new Array(20),
+				showTooltips: true,
 			},
 			styles: [
 				`.button {
@@ -30,16 +31,19 @@ export default {
 			],
 			template: `
 <h1>{{height.length * width.length}} tooltips</h1>
-@for(x of height; track $index)  {
-	<div>
-		@for(y of width; track $index)  {
-				<button
-					luButton
-					class="button"
-					[luTooltip]="'Hello there'"
-				>A</button>
-		}
-	</div>
+<button luButton (click)="showTooltips = !showTooltips">Toggle tooltips</button>
+@if(showTooltips) {
+	@for(x of height; track $index)  {
+		<div>
+			@for(y of width; track $index)  {
+					<button
+						luButton
+						class="button"
+						[luTooltip]="'Hello there'"
+					>A</button>
+			}
+		</div>
+	}
 }
 `,
 		};
