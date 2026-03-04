f06c694e0e4931ce821f71f73466e4da51e8ff36
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-04 10:13:41 +0100

    * update date format todo trad

--- packages/ng/activity-feed/activity-feed-step/activity-feed-step.component.ts
+++ packages/ng/activity-feed/activity-feed-step/activity-feed-step.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, LOCALE_ID, ViewEncapsulation } from '@angular/core';
+import { PortalDirective } from '@lucca-front/ng/core';
 import { ILuUser, LuUserPictureComponent } from '@lucca-front/ng/user';
 
 @Component({
@@ -7,14 +8,38 @@ import { ILuUser, LuUserPictureComponent } from '@lucca-front/ng/user';
 	templateUrl: './activity-feed-step.component.html',
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [LuUserPictureComponent, DatePipe],
+	imports: [LuUserPictureComponent, DatePipe, PortalDirective],
 	host: {
 		role: 'listitem',
 		class: 'activityFeed-step',
 	},
 })
 export class ActivityFeedStepComponent {
-	readonly date = input.required<Date>();
-	readonly user = input.required<ILuUser>();
+	#locale = inject(LOCALE_ID);
+	#intlDateTimeFormat = new Intl.DateTimeFormat(this.#locale, {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	});
+
 	readonly label = input.required<string>();
+
+	readonly user = input.required<ILuUser>();
+
+	readonly date = input<Date>();
+
+	/**
+	 * format given to the date pipe for display.
+	 *
+	 * See https://angular.dev/api/common/DatePipe#custom-format-options
+	 */
+	readonly datePipeFormat = input<string>();
+
+	protected readonly dateDisplay = computed<string>(() => {
+		const formatted = this.#intlDateTimeFormat.format(this.date());
+		return `${formatted[0].toUpperCase()}${formatted.slice(1)}`;
+	});
 }
