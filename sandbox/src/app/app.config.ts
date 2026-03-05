a01b842d9ad96a1c34e1d7c46628c86619903ccc
Julie cottu <juliecottu@gmail.com>
2026-03-05 11:31:03 +0100

    ADD creer une tâche

--- sandbox/src/app/app.config.ts
+++ sandbox/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import fr from '@angular/common/locales/fr';
 import { routes } from './app.routes';
 
+registerLocaleData(fr);
+
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()],
+  providers: [provideBrowserGlobalErrorListeners(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), { provide: LOCALE_ID, useValue: 'fr' }],
 };
