5b65a0d8e63e285cd05e3321ea4f46b29547ee3b
François-Xavier Gentilhomme <fgentilhomme@luccasoftware.com>
2026-03-09 11:09:30 +0100

    fix(simple-select): align panel and input typing with strict ts-error

--- packages/ng/simple-select/input/select-input.component.spec.ts
+++ packages/ng/simple-select/input/select-input.component.spec.ts
@@ -19,7 +19,7 @@ describe('LuSimpleSelectInputComponent', () => {
 	runALuSelectInputComponentTestSuite<Entity>({
 		getFixture: () => fixture,
 		exampleValue: { id: 1, name: 'test' },
-		emptyValue: null,
+		emptyValue: null as unknown as Entity,
 		clearerSelector: '.simpleSelect-field-clear',
 	});
 });
