1e29b9f567e54d3770e3d95019d673c2a5b1d299
François-Xavier Gentilhomme <fgentilhomme@luccasoftware.com>
2026-03-09 11:15:39 +0100

    chore(eslint): enable strict ts-error on multi-select

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,6 @@ const STRICTIFIED_NG_EXCLUDE_ENTRIES = [
 	'packages/ng/index-table/**/*.ts',
 	'packages/ng/input/**/*.ts',
 	'packages/ng/modal/**/*.ts',
-	'packages/ng/multi-select/**/*.ts',
 	'packages/ng/option/**/*.ts',
 	'packages/ng/popup/**/*.ts',
 	'packages/ng/schematics/**/*.ts',
