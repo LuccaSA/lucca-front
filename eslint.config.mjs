f914246257a6fdb498938f7faaebed30121c84f5
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-10 16:19:47 +0100

    * fix strict error in dialog

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,6 @@ const STRICTIFIED_NG_EXCLUDE_ENTRIES = [
 	'packages/ng/core/**/*.ts',
 	'packages/ng/core-select/**/*.ts',
 	'packages/ng/date2/**/*.ts',
-	'packages/ng/dialog/**/*.ts',
 	'packages/ng/establishment/**/*.ts',
 	'packages/ng/formly/**/*.ts',
 	'packages/ng/forms/**/*.ts',
