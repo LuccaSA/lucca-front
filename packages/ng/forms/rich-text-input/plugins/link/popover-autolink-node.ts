5180991ab2fd52277ce02ed6f6981c7f51913649
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-10 16:02:17 +0100

    * fix popover autoLink && popoverLink

--- packages/ng/forms/rich-text-input/plugins/link/popover-autolink-node.ts
+++ packages/ng/forms/rich-text-input/plugins/link/popover-autolink-node.ts
@@ -10,7 +10,11 @@ export class PopoverAutoLinkNode extends AutoLinkNode {
 		PopoverAutoLinkNode.#viewContainerRef = vcr;
 	}
 	static setTemplateRef(vcr: TemplateRef<unknown>): void {
-		PopoverAutoLinkNode.#templateRef = vcr;
+		PopoverAutoLinkNode.#templateRef = vcr as TemplateRef<{
+			href?: string;
+			title?: string;
+			target?: string;
+		}>;
 	}
 
 	static override getType(): string {
@@ -19,12 +23,14 @@ export class PopoverAutoLinkNode extends AutoLinkNode {
 
 	override createDOM() {
 		if (PopoverAutoLinkNode.#viewContainerRef && PopoverAutoLinkNode.#templateRef) {
-			// Create the view
-			const view = PopoverAutoLinkNode.#viewContainerRef.createEmbeddedView(PopoverAutoLinkNode.#templateRef, {
+			// Create context
+			const context: { href?: string; title?: string; target?: string } = {
 				href: this.sanitizeUrl(this.__url),
-				title: this.__title,
-				target: this.__target,
-			});
+				title: this.__title ?? undefined,
+				target: this.__target ?? undefined,
+			};
+			// Create the view
+			const view = PopoverAutoLinkNode.#viewContainerRef.createEmbeddedView(PopoverAutoLinkNode.#templateRef, context);
 
 			// Return the template DOM element
 			return view.rootNodes[0] as HTMLElement;
