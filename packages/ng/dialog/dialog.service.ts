eac9f9420971b37f906b8f7eefe8e6718d5220f6
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-17 15:48:55 +0100

    * fix test

--- packages/ng/dialog/dialog.service.ts
+++ packages/ng/dialog/dialog.service.ts
@@ -64,7 +64,7 @@ export class LuDialogService {
 				.pipe(
 					filter(() => config.canCloseWithBackdrop ?? true),
 					switchMap(() => {
-						const canClose = cdkRef.componentInstance ? (config.canClose?.(cdkRef.componentInstance) ?? true) : false;
+						const canClose = cdkRef.componentInstance ? (config.canClose?.(cdkRef.componentInstance) ?? true) : true;
 						const canClose$ = isObservable(canClose) ? canClose : of(canClose);
 						return canClose$.pipe(take(1));
 					}),
