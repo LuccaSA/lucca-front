fa5413f27caac0ea9e02a9209ee54b37fc04f3a8
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-17 14:15:20 +0100

    * fix strict error in select input

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,6 @@ const STRICTIFIED_NG_EXCLUDE_ENTRIES = [
 	'packages/ng/modal/**/*.ts',
 	'packages/ng/popup/**/*.ts',
 	'packages/ng/schematics/**/*.ts',
-	'packages/ng/select/**/*.ts',
 	'stories/**/*.ts',
 ];
 
