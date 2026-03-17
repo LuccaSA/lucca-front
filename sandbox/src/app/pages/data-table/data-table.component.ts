411d458b61dfe4ee1a54b44c7ad064a40db2273b
Guillaume Nury <gnury@luccasoftware.com>
2026-03-17 11:43:38 +0100

    feat(sandbox): add removeTask

--- sandbox/src/app/pages/data-table/data-table.component.ts
+++ sandbox/src/app/pages/data-table/data-table.component.ts
@@ -14,193 +14,208 @@ import { TaskFormDialog } from './components/task-form-dialog.component';
 import { Task, TaskGroup, TaskStatus } from './models';
 
 @Pipe({
-  name: 'ariaControls',
+	name: 'ariaControls',
 })
 export class AriaControlsPipe implements PipeTransform {
-  transform(group: TaskGroup, groupId: string): string {
-    return group.tasks.map((_, index) => `${groupId}-row-${index}`).join(' ');
-  }
+	transform(group: TaskGroup, groupId: string): string {
+		return group.tasks.map((_, index) => `${groupId}-row-${index}`).join(' ');
+	}
 }
 
 const groupsMock: TaskGroup[] = [
-  {
-    groupName: 'Novembre 2025',
-    isExpanded: true,
-    tasks: [
-      {
-        id: 1,
-        name: 'Créer une micro interaction',
-        status: 'InProgress',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-11-15'),
-      },
-      {
-        id: 2,
-        name: 'S’assigner un objectif PLG',
-        status: 'Completed',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-11-03'),
-      },
-    ],
-  },
-  {
-    groupName: 'Octobre 2025',
-    isExpanded: true,
-    tasks: [
-      {
-        id: 3,
-        name: 'Créer une route',
-        status: 'Completed',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-10-03'),
-      },
-      {
-        id: 4,
-        name: 'Initialiser une SPA',
-        status: 'InProgress',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-10-03'),
-      },
-      {
-        id: 5,
-        name: 'Penser à créer une route',
-        status: 'Completed',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-10-03'),
-      },
-    ],
-  },
+	{
+		groupName: 'Novembre 2025',
+		isExpanded: true,
+		tasks: [
+			{
+				id: 1,
+				name: 'Créer une micro interaction',
+				status: 'InProgress',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-11-15'),
+			},
+			{
+				id: 2,
+				name: 'S’assigner un objectif PLG',
+				status: 'Completed',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-11-03'),
+			},
+		],
+	},
+	{
+		groupName: 'Octobre 2025',
+		isExpanded: true,
+		tasks: [
+			{
+				id: 3,
+				name: 'Créer une route',
+				status: 'Completed',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-10-03'),
+			},
+			{
+				id: 4,
+				name: 'Initialiser une SPA',
+				status: 'InProgress',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-10-03'),
+			},
+			{
+				id: 5,
+				name: 'Penser à créer une route',
+				status: 'Completed',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-10-03'),
+			},
+		],
+	},
 
-  {
-    groupName: 'Septembre 2025',
-    isExpanded: false,
-    tasks: [
-      {
-        id: 6,
-        name: 'Penser à initialiser une SPA',
-        status: 'Late',
-        user: { firstName: 'Marie', lastName: 'Bragoulet' },
-        dueDate: new Date('2025-09-03'),
-      },
-    ],
-  },
+	{
+		groupName: 'Septembre 2025',
+		isExpanded: false,
+		tasks: [
+			{
+				id: 6,
+				name: 'Penser à initialiser une SPA',
+				status: 'Late',
+				user: { firstName: 'Marie', lastName: 'Bragoulet' },
+				dueDate: new Date('2025-09-03'),
+			},
+		],
+	},
 ];
 
 @Component({
-  selector: 'app-data-table-page',
-  imports: [
-    MainLayoutComponent,
-    ButtonComponent,
-    LuUserModule,
-    DatePipe,
-    LuUserTileComponent,
-    StatusBadgeComponent,
-    AriaControlsPipe,
-    SkeletonIndexTableComponent,
-    EmptyStateSectionComponent,
-    NumericBadgeComponent,
-    IconComponent,
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './data-table.component.html',
-  providers: [provideLuDialog()], // 1. On provide
+	selector: 'app-data-table-page',
+	imports: [
+		MainLayoutComponent,
+		ButtonComponent,
+		LuUserModule,
+		DatePipe,
+		LuUserTileComponent,
+		StatusBadgeComponent,
+		AriaControlsPipe,
+		SkeletonIndexTableComponent,
+		EmptyStateSectionComponent,
+		NumericBadgeComponent,
+		IconComponent,
+	],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	templateUrl: './data-table.component.html',
+	providers: [provideLuDialog()], // 1. On provide
 })
 export class DataTableComponent {
-  private dialog = inject(LuDialogService); // 2. On injecte
-  private locale = inject(LOCALE_ID);
-  groups = signal<TaskGroup[]>([]);
+	private dialog = inject(LuDialogService); // 2. On injecte
+	private locale = inject(LOCALE_ID);
+	groups = signal<TaskGroup[]>([]);
 
-  isLoading = signal(true);
-  isEmpty = computed(() => this.groups().length === 0);
+	isLoading = signal(true);
+	isEmpty = computed(() => this.groups().length === 0);
 
-  // Complètement équivalent à isEmpty
-  // isEmpty2 = computed(() => {
-  // 	return this.groups().length === 0;
-  // });
+	// Complètement équivalent à isEmpty
+	// isEmpty2 = computed(() => {
+	// 	return this.groups().length === 0;
+	// });
 
-  tasksCount = computed(() => this.groups().reduce((count, group) => count + group.tasks.length, 0));
-  expandedGroupsCount = computed(() => this.groups().filter((group) => group.isExpanded).length);
+	tasksCount = computed(() => this.groups().reduce((count, group) => count + group.tasks.length, 0));
+	expandedGroupsCount = computed(() => this.groups().filter((group) => group.isExpanded).length);
 
-  // Complètement équivalent à tasksCount
-  // tasksCount2 = computed(() => {
-  // 	let count = 0;
+	// Complètement équivalent à tasksCount
+	// tasksCount2 = computed(() => {
+	// 	let count = 0;
 
-  // 	for (const group of this.groups()) {
-  // 		// count = count + group.tasks.length;
-  // 		count += group.tasks.length;
-  // 	}
+	// 	for (const group of this.groups()) {
+	// 		// count = count + group.tasks.length;
+	// 		count += group.tasks.length;
+	// 	}
 
-  // 	return count;
-  // });
+	// 	return count;
+	// });
 
-  paletteByStatus: Record<TaskStatus, Palette> = {
-    Late: 'error',
-    InProgress: 'product',
-    Completed: 'success',
-  };
+	paletteByStatus: Record<TaskStatus, Palette> = {
+		Late: 'error',
+		InProgress: 'product',
+		Completed: 'success',
+	};
 
-  labelByStatus: Record<TaskStatus, string> = {
-    Late: 'En retard',
-    InProgress: 'En cours',
-    Completed: 'Terminé',
-  };
+	labelByStatus: Record<TaskStatus, string> = {
+		Late: 'En retard',
+		InProgress: 'En cours',
+		Completed: 'Terminé',
+	};
 
-  constructor() {
-    setTimeout(() => {
-      this.isLoading.set(false);
-    }, 1000);
-    this.useGroupMocks();
-  }
+	constructor() {
+		setTimeout(() => {
+			this.isLoading.set(false);
+		}, 1000);
+		this.useGroupMocks();
+	}
 
-  toggleGroup(groupToUpdate: TaskGroup): void {
-    const updatedGroups = this.groups().map((group) => {
-      if (group === groupToUpdate) {
-        return { ...group, isExpanded: !group.isExpanded };
-      } else {
-        return group;
-      }
-    });
+	toggleGroup(groupToUpdate: TaskGroup): void {
+		const updatedGroups = this.groups().map((group) => {
+			if (group === groupToUpdate) {
+				return { ...group, isExpanded: !group.isExpanded };
+			} else {
+				return group;
+			}
+		});
 
-    // const updatedGroups2 = this.groups().map((group) => (group === groupToUpdate ? { ...group, isExpanded: !group.isExpanded } : group));
+		// const updatedGroups2 = this.groups().map((group) => (group === groupToUpdate ? { ...group, isExpanded: !group.isExpanded } : group));
 
-    this.groups.set(updatedGroups);
-  }
+		this.groups.set(updatedGroups);
+	}
 
-  useGroupMocks(): void {
-    this.groups.set(groupsMock);
-  }
+	useGroupMocks(): void {
+		this.groups.set(groupsMock);
+	}
 
-  useEmptyGroup(): void {
-    this.groups.set([]);
-  }
+	useEmptyGroup(): void {
+		this.groups.set([]);
+	}
 
-  toggleLoadingState(): void {
-    this.isLoading.set(!this.isLoading());
-  }
+	toggleLoadingState(): void {
+		this.isLoading.set(!this.isLoading());
+	}
 
-  createNewTask(): void {
-    const ref = this.dialog.open({
-      content: TaskFormDialog,
-      size: 'XL',
-    });
+	createNewTask(): void {
+		const ref = this.dialog.open({
+			content: TaskFormDialog,
+			size: 'XL',
+		});
 
-    ref.result$.subscribe((task) => {
-      console.log({ task });
-      this.addTask(task);
-    });
-  }
+		ref.result$.subscribe((task) => {
+			console.log({ task });
+			this.addTask(task);
+		});
+	}
 
-  addTask(task: Task): void {
-    let groupName = formatDate(task.dueDate, 'MMMM y', this.locale);
-    groupName = groupName[0].toUpperCase() + groupName.slice(1);
+	addTask(task: Task): void {
+		let groupName = formatDate(task.dueDate, 'MMMM y', this.locale);
+		groupName = groupName[0].toUpperCase() + groupName.slice(1);
 
-    const existingGroups = this.groups();
-    const existingGroup = existingGroups.find((grp) => grp.groupName === groupName);
+		const existingGroups = this.groups();
+		const existingGroup = existingGroups.find((grp) => grp.groupName === groupName);
 
-    if (existingGroup) {
-    } else {
-      const newGroup: TaskGroup = { groupName: groupName, isExpanded: true, tasks: [task] };
-      this.groups.set([...existingGroups, newGroup]);
-    }
-  }
+		if (existingGroup) {
+			const newGroup: TaskGroup = { ...existingGroup, tasks: [...existingGroup.tasks, task], isExpanded: true };
+			this.groups.set(existingGroups.map((grp) => (grp === existingGroup ? newGroup : grp)));
+		} else {
+			const newGroup: TaskGroup = { groupName: groupName, isExpanded: true, tasks: [task] };
+			this.groups.set([...existingGroups, newGroup]);
+		}
+	}
+
+	removeTask(taskGroup: TaskGroup, task: Task): void {
+		const updatedGroup: TaskGroup = {
+			...taskGroup,
+			tasks: taskGroup.tasks.filter((t) => t !== task),
+		};
+
+		if (updatedGroup.tasks.length === 0) {
+			this.groups.set(this.groups().filter((grp) => grp !== taskGroup));
+		} else {
+			this.groups.set(this.groups().map((grp) => (grp === taskGroup ? updatedGroup : grp)));
+		}
+	}
 }
