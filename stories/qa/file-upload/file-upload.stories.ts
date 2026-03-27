d56d4468991a2141d0100524457b277456633041
Vincent Valentin <vvalentin@luccasoftware.com>
2026-03-27 16:50:44 +0100

    [fileUpload] [fileEntry] add QA stories

---
+++ stories/qa/file-upload/file-upload.stories.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MultiFileUploadComponent, SingleFileUploadComponent } from '@lucca-front/ng/file-upload';
+import { LuSafeExternalSvgPipe } from '@lucca-front/ng/safe-content';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'file-upload-stories',
+	templateUrl: './file-upload.stories.html',
+	imports: [SingleFileUploadComponent, MultiFileUploadComponent, LuSafeExternalSvgPipe],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	styles: [
+		`
+			:host ::ng-deep .fileUpload,
+			lu-single-file-upload,
+			lu-multi-file-upload {
+				inline-size: 100%;
+			}
+			.demo-QAtable {
+				width: 100%;
+				table-layout: fixed;
+			}
+		`,
+	],
+})
+class FileUploadStory {}
+
+export default {
+	title: 'QA/FileUpload',
+	component: FileUploadStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [FileUploadStory],
+		}),
+	],
+} as Meta;
+
+const template = () => ({});
+
+export const Basic: StoryObj<FileUploadStory> = {
+	args: {},
+	render: template,
+};
