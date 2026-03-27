0b1f4961f4547a7a0ee1683f74b19a5632006e0f
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-03-27 17:19:25 +0100

    [fileUpload] [fileEntry] add QA stories (#4694)
    ## Description
    
    
    
    -----
    
    
    
    -----
    
    <img width="1376" height="867" alt="Capture d’écran 2026-03-27 à 17 11
    33"
    src="https://github.com/user-attachments/assets/e6bffb9d-f152-41bd-98b5-4b7272ca4686"
    />
    
    <img width="1371" height="844" alt="Capture d’écran 2026-03-27 à 17 11
    24"
    src="https://github.com/user-attachments/assets/a538cc69-82fd-4413-b74a-92e89c687452"
    />

---
+++ stories/qa/file-entry/file-entry.stories.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FileEntryComponent } from '@lucca-front/ng/file-upload';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+
+@Component({
+	selector: 'file-entry-stories',
+	templateUrl: './file-entry.stories.html',
+	imports: [FileEntryComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	styles: [
+		`
+			:host ::ng-deep .fileEntryDisplayWrapper {
+				inline-size: 100%;
+			}
+			.demo-QAtable {
+				width: 100%;
+				table-layout: fixed;
+			}
+		`,
+	],
+})
+class FileEntryStory {}
+
+export default {
+	title: 'QA/FileEntry',
+	component: FileEntryStory,
+	decorators: [
+		moduleMetadata({
+			entryComponents: [FileEntryStory],
+		}),
+	],
+} as Meta;
+
+const template = () => ({});
+
+export const Basic: StoryObj<FileEntryStory> = {
+	args: {},
+	render: template,
+};
