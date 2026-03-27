0b1e30e046bf2a15aed07fe5faef9f1e219947a1
Vincent Valentin <vvalentin@luccasoftware.com>
2026-03-27 14:22:15 +0100

    wip

--- packages/ng/file-upload/base-file-upload/base-file-upload.component.ts
+++ packages/ng/file-upload/base-file-upload/base-file-upload.component.ts
@@ -57,7 +57,7 @@ export abstract class BaseFileUploadComponent {
 
 	maxSizeDisplay = computed(() => formatFileSize(this.locale, this.fileMaxSize()));
 
-	size = input<'S' | null>(null);
+	size = input<'L' | null>(null);
 
 	password = input(false, { transform: booleanAttribute });
 
