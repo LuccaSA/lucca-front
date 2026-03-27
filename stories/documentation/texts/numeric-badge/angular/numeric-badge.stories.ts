6c4d45ec4a0ebf9d8022cc2195e3d49366957599
jeremie-lucca <jbiron@luccasoftware.com>
2026-03-27 17:48:09 +0100

    Cover decorative palettes

--- stories/documentation/texts/numeric-badge/angular/numeric-badge.stories.ts
+++ stories/documentation/texts/numeric-badge/angular/numeric-badge.stories.ts
@@ -12,7 +12,26 @@ export default {
 	],
 	argTypes: {
 		palette: {
-			options: ['none', 'product', 'neutral', 'success', 'warning', 'error'],
+			options: [
+				'none',
+				'product',
+				'neutral',
+				'success',
+				'warning',
+				'error',
+				'kiwi',
+				'lime',
+				'cucumber',
+				'mint',
+				'glacier',
+				'lagoon',
+				'blueberry',
+				'lavender',
+				'grape',
+				'watermelon',
+				'pumpkin',
+				'pineapple',
+			],
 			control: {
 				type: 'select',
 			},
