5be24297896ea2cd02b99464958538848c8971fe
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-30 15:05:15 +0200

    * numeric badge types

---
+++ packages/ng/numeric-badge/numeric-badge-type.ts
@@ -0,0 +1,6 @@
+/**
+ * Available NumericBadgeComponent Sizes
+ */
+
+export const NUMERIC_BADGE_SIZE = ['XS', 'S', 'M'] as const;
+export type NumericBadgeSize = (typeof NUMERIC_BADGE_SIZE)[number];
