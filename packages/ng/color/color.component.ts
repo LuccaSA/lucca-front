6007abaef656ad63112282e631fe095d69ef393f
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-30 14:55:51 +0200

    * color types

--- packages/ng/color/color.component.ts
+++ packages/ng/color/color.component.ts
@@ -1,4 +1,5 @@
 import { booleanAttribute, ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
+import { ColorSize } from './color-type';
 
 @Component({
 	selector: 'lu-color',
@@ -15,6 +16,6 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, input, ViewEncaps
 export class ColorComponent {
 	readonly value = input<string | null>(null);
 	readonly borderColor = input<string | null>(null);
-	readonly size = input<'L' | 'XL' | null>(null);
+	readonly size = input<ColorSize | null>(null);
 	readonly hiddenName = input(false, { transform: booleanAttribute });
 }
