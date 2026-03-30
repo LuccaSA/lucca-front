414270f573dd5f870c956dc4e6ef4a4d078d25ac
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-03-30 15:01:28 +0200

    * radio group types

--- packages/ng/forms/radio-group-input/radio-group-input.component.ts
+++ packages/ng/forms/radio-group-input/radio-group-input.component.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FORM_FIELD_INSTANCE, FormFieldComponent } from '@lucca-front/ng/form-field';
 import { injectNgControl } from '../inject-ng-control';
 import { NoopValueAccessorDirective } from '../noop-value-accessor.directive';
+import { RadioGroupInputArrow, RadioGroupInputFramedSize, RadioGroupInputSize } from './radio-group-input-type';
 import { RADIO_GROUP_INSTANCE } from './radio-group-token';
 
 let nextId = 0;
@@ -30,15 +31,15 @@ export class RadioGroupInputComponent {
 
 	ngControl = injectNgControl();
 
-	readonly size = input<'S' | 'M'>();
+	readonly size = input<RadioGroupInputSize>();
 
 	readonly framed = input(false, { transform: booleanAttribute });
 
 	readonly framedCenter = input(false, { transform: booleanAttribute });
 
-	readonly framedSize = input<'L' | null>(null);
+	readonly framedSize = input<RadioGroupInputFramedSize | null>(null);
 
-	readonly arrow = input<'neutral' | 'default'>();
+	readonly arrow = input<RadioGroupInputArrow>();
 
 	name = `radio-group-${nextId++}`;
 
