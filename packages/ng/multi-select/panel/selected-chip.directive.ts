340de60b1515e2c2efc052164e2520aa3fabbb4d
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-04-07 17:01:15 +0200

    * migrate selected-chip signal

--- packages/ng/multi-select/panel/selected-chip.directive.ts
+++ packages/ng/multi-select/panel/selected-chip.directive.ts
@@ -1,17 +1,18 @@
 import { Highlightable } from '@angular/cdk/a11y';
-import { Directive, ElementRef, Input, inject } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
 	selector: '[luMultiSelectSelectedChip]',
 })
 export class ɵLuMultiSelectSelectedChipDirective<T> implements Highlightable {
-	@Input('luMultiSelectSelectedChip') option?: T;
+	readonly option = input<T>(undefined, { alias: 'luMultiSelectSelectedChip' });
 
 	elementRef = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
 
 	setActiveStyles(): void {
 		this.elementRef.querySelector('button')?.focus();
 	}
+
 	setInactiveStyles(): void {
 		// Nothing to do
 	}
