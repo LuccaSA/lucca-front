17b87f97a5090484278e5d002b499d49eb512a51
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-04-07 17:50:46 +0200

    * avoid fnc

--- packages/ng/core-select/option/option.directive.ts
+++ packages/ng/core-select/option/option.directive.ts
@@ -10,9 +10,7 @@ export class LuOptionDirective<TOption, TValue> {
 	readonly select = input<ALuSelectInputComponent<TOption, TValue>>(undefined, { alias: 'luOptionSelect' });
 
 	public constructor(private templateRef: TemplateRef<LuOptionContext<TOption>>) {
-		syncInputSignal(this.select, (select) => {
-			select.optionTpl.set(this.templateRef);
-		});
+		syncInputSignal(this.select, (select) => select.optionTpl.set(this.templateRef));
 	}
 
 	public static ngTemplateContextGuard<TOption, TValue>(_dir: LuOptionDirective<TOption, TValue>, ctx: unknown): ctx is LuOptionContext<TOption> {
