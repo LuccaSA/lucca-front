8964a8e9ac7b23bf817b7c6d3cfd87faea7014cb
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-04-07 16:00:45 +0200

    * migrate popover-panel signal

--- packages/ng/popover/panel/popover-panel.component.ts
+++ packages/ng/popover/panel/popover-panel.component.ts
@@ -1,7 +1,8 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { NgTemplateOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, Output, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, input, OnDestroy, Output, TemplateRef, viewChild } from '@angular/core';
+import { syncSignal, ɵeffectWithDeps } from '@lucca-front/ng/core';
 import { luTransformPopover } from '../animation/index';
 import { ALuPopoverPanel, ILuPopoverPanel, LuPopoverScrollStrategy } from './popover-panel.model';
 
@@ -20,49 +21,28 @@ export class LuPopoverPanelComponent extends ALuPopoverPanel implements ILuPopov
 	protected _templateContext: unknown;
 
 	/** Template to Use for the popover */
-	get template(): TemplateRef<unknown> {
-		return this._template;
-	}
-	@Input()
-	set template(value: TemplateRef<unknown>) {
-		this._template = value;
-	}
+	template = input<TemplateRef<unknown>>();
 
 	/** Template context to use for the popover when created using a template */
-	get templateContext(): unknown {
-		return this._templateContext;
-	}
-	@Input('template-context')
-	set templateContext(value: unknown) {
-		this._templateContext = value;
-	}
+	templateContext = input<unknown>(undefined, { alias: 'template-context' });
 
 	/**
 	 * Popover container close on click
 	 * default: false
 	 */
-	@Input('close-on-click')
-	set inputCloseOnClick(v: boolean) {
-		this.closeOnClick = v;
-	}
+	inputCloseOnClick = input<boolean>(false, { alias: 'close-on-click' });
 
 	/**
 	 * Popover focus trap using cdkTrapFocus
 	 * default: false
 	 */
-	@Input('trap-focus')
-	set inputTrapFocus(v: boolean) {
-		this.trapFocus = v;
-	}
+	inputTrapFocus = input<boolean>(false, { alias: 'trap-focus' });
 
 	/**
 	 * Popover scrollStrategy
 	 * default: reposition
 	 */
-	@Input('scroll-strategy')
-	set inputScrollStrategy(v: LuPopoverScrollStrategy) {
-		this.scrollStrategy = v;
-	}
+	inputScrollStrategy = input<LuPopoverScrollStrategy>('reposition', { alias: 'scroll-strategy' });
 
 	/**
 	 * This method takes classes set on the host lu-popover element and applies them on the
@@ -70,20 +50,15 @@ export class LuPopoverPanelComponent extends ALuPopoverPanel implements ILuPopov
 	 * to style the containing popover from outside the component.
 	 * @param classes list of class names
 	 */
-	@Input('panel-classes')
-	set inputPanelClasses(classes: string) {
-		this.panelClasses = classes;
-	}
+	inputPanelClasses = input<string>('', { alias: 'panel-classes' });
+
 	/**
 	 * This method takes classes set on the host lu-popover element and applies them on the
 	 * popover template that displays in the overlay container. Otherwise, it's difficult
 	 * to style the containing popover from outside the component.
 	 * @param classes list of class names
 	 */
-	@Input('content-classes')
-	set inputContentClasses(classes: string) {
-		this.contentClasses = classes;
-	}
+	inputContentClasses = input<string>('', { alias: 'content-classes' });
 
 	/** Event emitted when the popover is closed. */
 	// eslint-disable-next-line @angular-eslint/no-output-native
@@ -91,13 +66,24 @@ export class LuPopoverPanelComponent extends ALuPopoverPanel implements ILuPopov
 	@Output() override open = new EventEmitter<void>();
 	@Output() override hovered = new EventEmitter<boolean>();
 
-	@ViewChild(TemplateRef, { static: true })
-	set vcTemplateRef(tr: TemplateRef<unknown>) {
-		this.templateRef = tr;
-	}
+	vcTemplateRef = viewChild(TemplateRef);
 
 	constructor() {
 		super();
+
+		syncSignal(this.template, (template) => (this._template = template));
+		syncSignal(this.templateContext, (templateContext) => (this._templateContext = templateContext));
+		syncSignal(this.inputCloseOnClick, (closeOnClick) => (this.closeOnClick = closeOnClick));
+		syncSignal(this.inputTrapFocus, (trapFocus) => (this.trapFocus = trapFocus));
+		syncSignal(this.inputScrollStrategy, (scrollStrategy) => (this.scrollStrategy = scrollStrategy));
+		syncSignal(this.inputPanelClasses, (panelClasses) => (this.panelClasses = panelClasses));
+		syncSignal(this.inputContentClasses, (contentClasses) => (this.contentClasses = contentClasses));
+
+		ɵeffectWithDeps([this.vcTemplateRef], (vcTemplateRef) => {
+			if (vcTemplateRef) {
+				this.templateRef = vcTemplateRef;
+			}
+		});
 	}
 
 	ngOnDestroy() {
