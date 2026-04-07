2f98e31bb1fee9efef5a0e1993a76f0190031f6e
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-04-07 15:48:42 +0200

    * migrate popover-traget directive signal

--- packages/ng/popover/target/popover-target.directive.ts
+++ packages/ng/popover/target/popover-target.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, input } from '@angular/core';
+import { syncSignal } from '@lucca-front/ng/core';
 import { ALuPopoverTarget, LuPopoverAlignment, LuPopoverPosition } from './popover-target.model';
 
 @Directive({
@@ -6,27 +7,27 @@ import { ALuPopoverTarget, LuPopoverAlignment, LuPopoverPosition } from './popov
 	exportAs: 'LuPopoverTarget',
 })
 export class LuPopoverTargetDirective extends ALuPopoverTarget {
+	/** how you want to position the panel relative to the target, allowed values: above, below, before, after */
+	inputPosition = input<LuPopoverPosition>(undefined, { alias: 'luPopoverPosition' });
+
+	/** how the panel will be align with the target, allowed values: top, bottom, left, right */
+	inputAlignment = input<LuPopoverAlignment>(undefined, { alias: 'luPopoverAlignment' });
+
+	/** set to true if you want the panel to appear on top of the target */
+	inputOverlap = input<boolean>(undefined, { alias: 'luPopoverOverlap' });
+
+	inputOffsetX = input<number>(undefined, { alias: 'luPopoverOffsetX' });
+
+	inputOffsetY = input<number>(undefined, { alias: 'luPopoverOffsetY' });
+
 	constructor(ref: ElementRef) {
 		super();
 		this.elementRef = ref;
-	}
-	/** how you want to position the panel relative to the target, allowed values: above, below, before, after */
-	@Input('luPopoverPosition') set inputPosition(pos: LuPopoverPosition) {
-		this.position = pos;
-	}
-	/** how the panel will be align with the target, allowed values: top, bottom, left, right */
-	@Input('luPopoverAlignment') set inputAlignment(al: LuPopoverAlignment) {
-		this.alignment = al;
-	}
-	/** set to true if you want the panel to appear on top of the target */
-	@Input('luPopoverOverlap') set inputOverlap(ov: boolean) {
-		this.overlap = ov;
-	}
 
-	@Input('luPopoverOffsetX') set inputOffsetX(ox: number) {
-		this.offsetX = ox;
-	}
-	@Input('luPopoverOffsetY') set inputOffsetY(oy: number) {
-		this.offsetY = oy;
+		syncSignal(this.inputPosition, (inputPosition) => (this.position = inputPosition));
+		syncSignal(this.inputAlignment, (inputAlignment) => (this.alignment = inputAlignment));
+		syncSignal(this.inputOverlap, (inputOverlap) => (this.overlap = inputOverlap));
+		syncSignal(this.inputOffsetX, (inputOffsetX) => (this.offsetX = inputOffsetX));
+		syncSignal(this.inputOffsetY, (inputOffsetY) => (this.offsetY = inputOffsetY));
 	}
 }
