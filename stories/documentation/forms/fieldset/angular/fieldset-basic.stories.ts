27161f3df2378ec3ae4fc3199b46f79573ef8aa3
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-04-14 14:47:59 +0200

    [fieldset] max inline size inherit (#4727)
    ## Description
    
    - Adjust icon position
    - Change default max inline size to 50rem
    
    -----
    
    
    
    -----
    <img width="826" height="142" alt="Capture d’écran 2026-03-27 à 17 54
    05"
    src="https://github.com/user-attachments/assets/5a3c9bc2-ea7a-400a-8023-26246399bb1f"
    />

--- stories/documentation/forms/fieldset/angular/fieldset-basic.stories.ts
+++ stories/documentation/forms/fieldset/angular/fieldset-basic.stories.ts
@@ -1,5 +1,6 @@
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '@lucca-front/ng/button';
+import { FormComponent } from '@lucca-front/ng/form';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
 import { FieldsetComponent, TextInputComponent } from '@lucca-front/ng/forms';
 import { GridColumnComponent, GridComponent } from '@lucca-front/ng/grid';
@@ -24,7 +25,7 @@ export default {
 			description: 'Permet au fieldset de se replier.',
 		},
 		withAction: {
-			if: { arg: 'expandable', eq: false },
+			if: { arg: 'expandable', truthy: false },
 			description: "Ajoute un bouton d'action à droite du titre.",
 		},
 		expanded: {
@@ -59,43 +60,45 @@ export default {
 	},
 	decorators: [
 		moduleMetadata({
-			imports: [FieldsetComponent, GridComponent, GridColumnComponent, FormFieldComponent, TextInputComponent, FormsModule, ButtonComponent],
+			imports: [FieldsetComponent, GridComponent, GridColumnComponent, FormFieldComponent, TextInputComponent, FormsModule, ButtonComponent, FormComponent],
 		}),
 	],
-	render: ({ expanded, size, helper, action, withAction, presentation, ...args }, { argTypes }) => {
+	render: ({ expanded, size, helper, action, withAction, presentation, horizontal, maxWidth, ...args }, { argTypes }) => {
 		const expandedParam = expanded ? ` [expanded]="true"` : ``;
 		const helperParam = helper ? ` helper="${helper}"` : ``;
 		const sizeParam = size ? ` size="S"` : ``;
 		const actionParam = withAction ? ` [action]="portalSample"` : ``;
+		const maxWidthParam = maxWidth ? ` maxWidth` : ``;
+		const horizontalParam = horizontal ? ` horizontal` : ``;
 		const portalSample = withAction
 			? `
-<ng-template #portalSample>
-	<button luButton>button</button>
-</ng-template>
-`
+
+	<ng-template #portalSample>
+		<button luButton>button</button>
+	</ng-template>`
 			: ``;
 		return {
-			template: `@let column = { colspanAtMediaMinXXS: 2 };
-${portalSample}
-<lu-fieldset${helperParam}${expandedParam}${sizeParam}${actionParam}${generateInputs(args, argTypes)}>
-	<lu-grid mode="form">
-		<lu-grid-column colspan="4" [responsive]="column">
-			<lu-form-field label="Label"${generateInputs({ presentation }, argTypes)}>
-				<lu-text-input type="text" [(ngModel)]="example1" />
-			</lu-form-field>
-		</lu-grid-column>
-		<lu-grid-column colspan="4" [responsive]="column">
-			<lu-form-field label="Label"${generateInputs({ presentation }, argTypes)}>
-				<lu-text-input type="text" [(ngModel)]="example2" />
-			</lu-form-field>
-		</lu-grid-column>
-	</lu-grid>
-</lu-fieldset>`,
+			template: `<form luForm${maxWidthParam}>
+	<lu-fieldset${horizontalParam}${helperParam}${expandedParam}${sizeParam}${actionParam}${generateInputs(args, argTypes)}>
+		<lu-grid mode="form">
+			<lu-grid-column colspan="2">
+				<lu-form-field label="Label"${generateInputs({ presentation }, argTypes)}>
+					<lu-text-input type="text" ngModel [ngModelOptions]="{ standalone: true }" />
+				</lu-form-field>
+			</lu-grid-column>
+			<lu-grid-column colspan="2">
+				<lu-form-field label="Label"${generateInputs({ presentation }, argTypes)}>
+					<lu-text-input type="text" ngModel [ngModelOptions]="{ standalone: true }" />
+				</lu-form-field>
+			</lu-grid-column>
+		</lu-grid>
+	</lu-fieldset>${portalSample}
+</form>`,
 		};
 	},
 } as Meta;
 
-export const Basic: StoryObj<FieldsetComponent & { content: string; withAction: boolean; presentation: boolean }> = {
+export const Basic: StoryObj<FieldsetComponent & { content: string; withAction: boolean; presentation: boolean; maxWidth: boolean }> = {
 	args: {
 		heading: 'Title',
 		helper: '',
@@ -105,5 +108,6 @@ export const Basic: StoryObj<FieldsetComponent & { content: string; withAction: 
 		horizontal: false,
 		withAction: false,
 		presentation: false,
+		maxWidth: false,
 	},
 };
