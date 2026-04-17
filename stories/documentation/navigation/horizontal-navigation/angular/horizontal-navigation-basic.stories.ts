461debd0eec99904c2205b7b3292b851a409790d
jeremie-lucca <jbiron@luccasoftware.com>
2026-04-17 12:02:57 +0200

    Add routeur link active

--- stories/documentation/navigation/horizontal-navigation/angular/horizontal-navigation-basic.stories.ts
+++ stories/documentation/navigation/horizontal-navigation/angular/horizontal-navigation-basic.stories.ts
@@ -1,4 +1,4 @@
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterLink, RouterLinkActive } from '@angular/router';
 import { HorizontalNavigationComponent, HorizontalNavigationLinkDirective } from '@lucca-front/ng/horizontal-navigation';
 import { NumericBadgeComponent } from '@lucca-front/ng/numeric-badge';
 import { applicationConfig, Meta, moduleMetadata } from '@storybook/angular';
@@ -26,7 +26,7 @@ export default {
 	},
 	decorators: [
 		moduleMetadata({
-			imports: [HorizontalNavigationComponent, HorizontalNavigationLinkDirective, NumericBadgeComponent],
+			imports: [HorizontalNavigationComponent, HorizontalNavigationLinkDirective, NumericBadgeComponent, RouterLink, RouterLinkActive],
 		}),
 		applicationConfig({
 			providers: [provideRouter([])],
@@ -38,7 +38,7 @@ export default {
 
 		return {
 			template: `<lu-horizontal-navigation${generateInputs(otherArgs, argTypes)}>
-	<a *luHorizontalNavigationLink class="horizontalNavigation-list-item-action" routerLink="/" ariaCurrentWhenActive="page">Page 1${numericBadgeElement}</a>
+	<a *luHorizontalNavigationLink class="horizontalNavigation-list-item-action" routerLink="/" routerLinkActive="active" ariaCurrentWhenActive="page">Page 1${numericBadgeElement}</a>
 	<a *luHorizontalNavigationLink class="horizontalNavigation-list-item-action" href="#2" aria-current="page">Page 2${numericBadgeElement}</a>
 	<a *luHorizontalNavigationLink class="horizontalNavigation-list-item-action is-disabled">Page 3${numericBadgeElement}</a>
 </lu-horizontal-navigation>`,
