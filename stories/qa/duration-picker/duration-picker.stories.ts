2caa30b6892a1a3c78f34022bf1ad981a248833e
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-04-24 11:17:27 +0200

    * replace time qa test into split component

--- stories/qa/duration-picker/duration-picker.stories.ts
+++ stories/qa/duration-picker/duration-picker.stories.ts
@@ -1,30 +1,26 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
+import { DurationPickerComponent } from '@lucca-front/ng/time';
 import { Meta } from '@storybook/angular';
-import { DurationPickerComponent, TimePickerComponent } from '@lucca-front/ng/time';
 
 @Component({
-	selector: 'time-stories',
-	templateUrl: './time.stories.html',
-	imports: [DurationPickerComponent, FormFieldComponent, FormsModule, ReactiveFormsModule, TimePickerComponent],
+	selector: 'duration-picker-stories',
+	templateUrl: './duration-picker.stories.html',
+	imports: [DurationPickerComponent, FormFieldComponent, FormsModule, ReactiveFormsModule],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-class TimeStory {
+class DurationPickerStory {
 	emptyDuration = null;
 	filledDuration = 'PT1H';
 	emptyDurationControl = new FormControl(null);
 	filledDurationControl = new FormControl('PT1H');
 	filledDuration1000hControl = new FormControl('PT1000H');
-	emptyTime = null;
-	filledTime = '12:30:00';
-	emptyTimeControl = new FormControl(null);
-	filledTimeControl = new FormControl('12:30:00');
 }
 
 export default {
-	title: 'QA/Time',
-	component: TimeStory,
+	title: 'QA/DurationPicker',
+	component: DurationPickerStory,
 } as Meta;
 
 export const Basic = {};
