1c58b78e036ec1d20dc5a8a3c41244cce010d383
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-05-04 11:38:07 +0200

    [animations] fix css version + story + QA (#4809)
    ## Description
    
    
    
    -----
    
    
    
    -----
    
    <img width="1041" height="455" alt="2026-04-29 10 16 54"
    src="https://github.com/user-attachments/assets/11d23724-a655-4e3f-aa23-3de87480667f"
    />

---
+++ stories/qa/animations/animations.stories.ts
@@ -0,0 +1,52 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Meta, StoryObj } from '@storybook/angular';
+
+@Component({
+	selector: 'animations-stories',
+	templateUrl: './animations.stories.html',
+	styles: [
+		`
+			.demo {
+				inline-size: 2.5rem;
+				block-size: 2.5rem;
+				background-color: var(--palettes-neutral-500);
+				border-radius: var(--pr-t-border-radius-structure);
+				animation-iteration-count: infinite;
+			}
+
+			.demo-QAtable-list {
+				overflow: hidden;
+				margin: -0.5rem;
+				padding: 0.5rem;
+				justify-content: center;
+
+				&.mod-vertical {
+					min-block-size: 10rem;
+				}
+
+				&.mod-horizontal {
+					min-inline-size: 10rem;
+				}
+			}
+
+			tr:not(:hover) .demo {
+				animation-play-state: paused;
+			}
+		`,
+	],
+
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class AnimationsStory {}
+
+export default {
+	title: 'QA/Animations',
+	component: AnimationsStory,
+} as Meta;
+
+const template = () => ({});
+
+export const Basic: StoryObj<AnimationsStory> = {
+	args: {},
+	render: template,
+};
