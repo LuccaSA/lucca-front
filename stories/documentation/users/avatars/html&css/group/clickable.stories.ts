4646806f76fd6b0607ecc9cb4afcca4d5ded0adc
Vincent Valentin <vvalentin@luccasoftware.com>
2026-05-05 16:32:19 +0200

    [avatars] fix stories with groups

--- stories/documentation/users/avatars/html&css/group/clickable.stories.ts
+++ stories/documentation/users/avatars/html&css/group/clickable.stories.ts
@@ -33,6 +33,62 @@ function getTemplate(args: UserAvatarGroupClickableStory): string {
 			<span class="pr-u-mask">Finn Mertens</span>
 		</a>
 	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
+	<li class="avatarWrapper-item" translate="no">
+		<a href="#" class="avatarWrapper-item-action">
+			<span class="avatar">
+				<img alt="" class="avatar-picture" loading="lazy" src="https://cdn.lucca.fr/lucca-front/avatars/finn.png" />
+			</span>
+			<span class="pr-u-mask">Finn Mertens</span>
+		</a>
+	</li>
 </ul>`;
 }
 
