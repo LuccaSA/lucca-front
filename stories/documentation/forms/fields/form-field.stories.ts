2be89d11d1a239257df2efad38b961ca4d392891
jeremie-lucca <jbiron@luccasoftware.com>
2026-05-07 11:32:51 +0200

    Add portalContent tag to controls

--- stories/documentation/forms/fields/form-field.stories.ts
+++ stories/documentation/forms/fields/form-field.stories.ts
@@ -17,7 +17,7 @@ export default {
 			control: {
 				type: 'text',
 			},
-			description: 'Modifie le label de l’input.',
+			description: 'Modifie le label de l’input. [PortalContent]',
 		},
 		required: {
 			control: {
@@ -32,7 +32,7 @@ export default {
 			control: {
 				type: 'text',
 			},
-			description: 'Ajoute un texte indicatif sous le champ de formulaire.',
+			description: 'Ajoute un texte indicatif sous le champ de formulaire. [PortalContent]',
 		},
 		inlineMessageState: {
 			options: ['default', 'success', 'warning', 'error'],
@@ -42,7 +42,7 @@ export default {
 			description: 'Modifie l’état de l’inline message.',
 		},
 		errorInlineMessage: {
-			description: 'Ajoute un texte d’erreur sous le champ de formulaire lorsque celui-ci est en erreur.',
+			description: 'Ajoute un texte d’erreur sous le champ de formulaire lorsque celui-ci est en erreur. [PortalContent]',
 		},
 		tooltip: {
 			if: { arg: 'hiddenLabel', truthy: false },
