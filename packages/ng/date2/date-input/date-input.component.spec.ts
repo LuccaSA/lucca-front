dd168b85d371b9add567868af75ded627bb3bc54
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-05-18 15:42:39 +0200

    - remove describe

--- packages/ng/date2/date-input/date-input.component.spec.ts
+++ packages/ng/date2/date-input/date-input.component.spec.ts
@@ -13,78 +13,76 @@ describe('DateInputComponent', () => {
 		providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
 	});
 
-	describe('CVA value update on clear', () => {
-		it('should emit null when user clear the input', () => {
-			const valueChanges = jest.fn();
+	it('should emit null when user clear the input', () => {
+		const valueChanges = jest.fn();
 
-			const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
-			formControl.valueChanges.subscribe((value) => {
-				valueChanges(value);
-			});
-
-			spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
-				hostProps: { formControl },
-			});
-
-			const input = spectator.query('[data-testid="lu-date-input"]') as HTMLInputElement;
-			expect(input).toBeTruthy();
-
-			spectator.typeInElement('', input);
-
-			expect(formControl.value).toBeNull();
+		const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
+		formControl.valueChanges.subscribe((value) => {
+			valueChanges(value);
 		});
 
-		it('should emit error when user enter a invalid date', () => {
-			const valueChanges = jest.fn();
-
-			const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
-			formControl.valueChanges.subscribe((value) => {
-				valueChanges(value);
-			});
-
-			spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
-				hostProps: { formControl },
-			});
-
-			const input = spectator.query('[data-testid="lu-date-input"]') as HTMLInputElement;
-			expect(input).toBeTruthy();
-
-			spectator.typeInElement('12', input);
-
-			expect(valueChanges).toHaveBeenCalledTimes(1);
-			expect(formControl.errors).toEqual({ date: true });
+		spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
+			hostProps: { formControl },
 		});
 
-		it('should not emit value at init if null value', fakeAsync(() => {
-			const valueChanges = jest.fn();
+		const input = spectator.query('[data-testid="lu-date-input"]') as HTMLInputElement;
+		expect(input).toBeTruthy();
 
-			const formControl = new FormControl(null);
-			formControl.valueChanges.subscribe((value) => {
-				valueChanges(value);
-			});
+		spectator.typeInElement('', input);
 
-			spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
-				hostProps: { formControl },
-			});
-
-			spectator.tick();
-			expect(valueChanges).toHaveBeenCalledTimes(0);
-		}));
-
-		it('should not emit value at init if there is a value', fakeAsync(() => {
-			const valueChanges = jest.fn();
-
-			const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
-			formControl.valueChanges.subscribe((value) => {
-				valueChanges(value);
-			});
-
-			spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
-				hostProps: { formControl },
-			});
-
-			spectator.tick();
-			expect(valueChanges).toHaveBeenCalledTimes(0);
-		}));
+		expect(formControl.value).toBeNull();
 	});
+
+	it('should emit error when user enter a invalid date', () => {
+		const valueChanges = jest.fn();
+
+		const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
+		formControl.valueChanges.subscribe((value) => {
+			valueChanges(value);
+		});
+
+		spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
+			hostProps: { formControl },
+		});
+
+		const input = spectator.query('[data-testid="lu-date-input"]') as HTMLInputElement;
+		expect(input).toBeTruthy();
+
+		spectator.typeInElement('12', input);
+
+		expect(valueChanges).toHaveBeenCalledTimes(1);
+		expect(formControl.errors).toEqual({ date: true });
+	});
+
+	it('should not emit value at init if null value', fakeAsync(() => {
+		const valueChanges = jest.fn();
+
+		const formControl = new FormControl(null);
+		formControl.valueChanges.subscribe((value) => {
+			valueChanges(value);
+		});
+
+		spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
+			hostProps: { formControl },
+		});
+
+		spectator.tick();
+		expect(valueChanges).toHaveBeenCalledTimes(0);
+	}));
+
+	it('should not emit value at init if there is a value', fakeAsync(() => {
+		const valueChanges = jest.fn();
+
+		const formControl = new FormControl(new Date('2024-01-01T00:00:00.000Z'));
+		formControl.valueChanges.subscribe((value) => {
+			valueChanges(value);
+		});
+
+		spectator = createHost(`<lu-date-input [formControl]="formControl"></lu-date-input>`, {
+			hostProps: { formControl },
+		});
+
+		spectator.tick();
+		expect(valueChanges).toHaveBeenCalledTimes(0);
+	}));
 });
