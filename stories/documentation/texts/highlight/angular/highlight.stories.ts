218dac0445f26a2fc38ec83bbb5d642f5f9edaa6
Vincent Valentin <vvalentin@luccasoftware.com>
2026-05-18 16:37:07 +0200

    story palette

--- stories/documentation/texts/highlight/angular/highlight.stories.ts
+++ stories/documentation/texts/highlight/angular/highlight.stories.ts
@@ -1,24 +1,31 @@
 import { HighlightComponent } from '@lucca-front/ng/highlight';
 import { Meta, moduleMetadata } from '@storybook/angular';
+import { PaletteAllArgType } from 'stories/helpers/common-arg-types';
+
 import { cleanupTemplate } from 'stories/helpers/stories';
 
 interface HighlightBasicStory {}
 
 export default {
 	title: 'Documentation/Texts/Highlight/Angular/Basic',
-	argTypes: {},
+	argTypes: {
+		palette: PaletteAllArgType,
+	},
 	decorators: [
 		moduleMetadata({
 			imports: [HighlightComponent],
 		}),
 	],
 	render: (args: HighlightBasicStory) => {
+		const paletteArg = args['palette'] !== 'product' ? ` palette="${args['palette']}"` : ``;
 		return {
-			template: cleanupTemplate(`<h1>Lorem <lu-highlight palette="pineapple">ipsum</lu-highlight> dolor</h1>`),
+			template: cleanupTemplate(`<h1>Lorem <lu-highlight${paletteArg}>ipsum</lu-highlight> dolor</h1>`),
 		};
 	},
 } as Meta;
 
 export const Basic = {
-	args: {},
+	args: {
+		palette: 'product',
+	},
 };
