08fa7ec6dad207a9a6d4d7fbea49bf84eb906f77
JeSuisCharlie1 <cjeanneau@luccasoftware.com>
2026-05-19 15:52:36 +0200

    * migrate option-outlet directive input signal

--- packages/ng/core-select/option/option-outlet.directive.ts
+++ packages/ng/core-select/option/option-outlet.directive.ts
@@ -1,5 +1,5 @@
-import { ComponentRef, Directive, EmbeddedViewRef, inject, Injector, Input, OnChanges, OnDestroy, SimpleChanges, TemplateRef, Type, ViewContainerRef } from '@angular/core';
-import { isNotNil } from '@lucca-front/ng/core';
+import { ComponentRef, Directive, EmbeddedViewRef, inject, Injector, input, OnDestroy, TemplateRef, Type, ViewContainerRef } from '@angular/core';
+import { isNotNil, ɵeffectWithDeps } from '@lucca-front/ng/core';
 import { LuOptionContext } from '../select.model';
 import { LU_OPTION_CONTEXT, provideOptionContext } from './option.token';
 
@@ -11,10 +11,12 @@ function hasRenderableValue<T>(value: T | undefined): value is T {
 	selector: '[luOptionOutlet]',
 	providers: [provideOptionContext()],
 })
-export class LuOptionOutletDirective<T> implements OnChanges, OnDestroy {
-	@Input() luOptionOutlet?: Type<unknown> | TemplateRef<LuOptionContext<T>>;
-	@Input() luOptionOutletValue: T | undefined;
-	@Input() luOptionShowNull = false;
+export class LuOptionOutletDirective<T> implements OnDestroy {
+	readonly luOptionOutlet = input<Type<unknown> | TemplateRef<LuOptionContext<T>>>();
+
+	readonly luOptionOutletValue = input<T | undefined>();
+
+	readonly luOptionShowNull = input<boolean>(false);
 
 	private viewContainerRef = inject(ViewContainerRef);
 	private injector = inject(Injector);
@@ -22,25 +24,34 @@ export class LuOptionOutletDirective<T> implements OnChanges, OnDestroy {
 	private componentRef?: ComponentRef<unknown>;
 	private optionContext = inject(LU_OPTION_CONTEXT);
 
-	ngOnChanges(changes: SimpleChanges): void {
-		if (changes['luOptionOutlet'] || !hasRenderableValue(this.luOptionOutletValue)) {
-			this.clearContainer();
-		}
+	constructor() {
+		let previousOutlet: Type<unknown> | TemplateRef<LuOptionContext<T>> | undefined;
 
-		const hasRef = this.embeddedViewRef || this.componentRef;
+		ɵeffectWithDeps([this.luOptionOutlet, this.luOptionOutletValue], (outlet, value) => {
+			const outletChanged = outlet !== previousOutlet;
+			previousOutlet = outlet;
 
-		if (changes['luOptionOutlet']) {
-			// Outlet template changed: recreate if current value is renderable
-			if (hasRenderableValue(this.luOptionOutletValue)) {
-				this.createComponent();
+			if (outletChanged || !hasRenderableValue(value)) {
+				this.clearContainer();
 			}
-		} else if (changes['luOptionOutletValue']) {
-			if (!hasRef && hasRenderableValue(this.luOptionOutletValue)) {
-				this.createComponent();
-			} else if (hasRef) {
-				this.updateRefValue();
+
+			if (outlet && hasRenderableValue(value)) {
+				if (!(this.embeddedViewRef || this.componentRef)) {
+					if (outlet instanceof TemplateRef) {
+						this.embeddedViewRef = this.viewContainerRef.createEmbeddedView(outlet, { $implicit: value }, { injector: this.injector });
+					} else {
+						this.optionContext.option$.next(value);
+						this.componentRef = this.viewContainerRef.createComponent(outlet, { injector: this.injector });
+					}
+				} else {
+					if (this.embeddedViewRef) {
+						this.embeddedViewRef.context.$implicit = value;
+					} else if (this.componentRef) {
+						this.optionContext.option$.next(value);
+					}
+				}
 			}
-		}
+		});
 	}
 
 	ngOnDestroy(): void {
@@ -53,32 +64,6 @@ export class LuOptionOutletDirective<T> implements OnChanges, OnDestroy {
 		this.componentRef?.destroy();
 		this.embeddedViewRef = undefined;
 		this.componentRef = undefined;
-	}
-
-	private createComponent(): void {
-		if (!this.luOptionOutlet || !hasRenderableValue(this.luOptionOutletValue)) {
-			return;
-		}
-
-		if (this.luOptionOutlet instanceof TemplateRef) {
-			this.embeddedViewRef = this.viewContainerRef.createEmbeddedView(this.luOptionOutlet, { $implicit: this.luOptionOutletValue }, { injector: this.injector });
-		} else {
-			this.optionContext.option$.next(this.luOptionOutletValue);
-			this.componentRef = this.viewContainerRef.createComponent(this.luOptionOutlet, { injector: this.injector });
-		}
-	}
-
-	private updateRefValue(): void {
-		if (!hasRenderableValue(this.luOptionOutletValue)) {
-			this.clearContainer();
-			return;
-		}
-
-		if (this.embeddedViewRef) {
-			this.embeddedViewRef.context.$implicit = this.luOptionOutletValue;
-		} else if (this.componentRef) {
-			this.optionContext.option$.next(this.luOptionOutletValue);
-		}
 	}
 
 	public static ngTemplateContextGuard<T>(_dir: LuOptionOutletDirective<T>, ctx: unknown): ctx is void {
