dda9e462c53629e474c70a2b3df00e55ed987997
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-05-20 10:35:35 +0200

    [softwareIcon] update (#4871)
    ## Description
    
    To be merged after https://github.com/LuccaSA/cdn.lucca.fr/pull/644
    
    -----
    
    
    
    -----

--- stories/documentation/structure/software-icon/html&css/basic.stories.ts
+++ stories/documentation/structure/software-icon/html&css/basic.stories.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { LuSafeExternalSvgPipe } from '@lucca-front/ng/safe-content';
+import { SoftwareIconList } from '@lucca-front/ng/software-icon';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
 interface SoftwareIconBasicStory {
@@ -17,40 +18,7 @@ export default {
 	],
 	argTypes: {
 		icon: {
-			options: [
-				// Time and Activities
-				'absences',
-				'timesheet',
-				'office',
-				'projects',
-				'shifts',
-				// Talent Management
-				'performance',
-				'engagement',
-				'training',
-				'recruitment',
-				'mood',
-				// Employee Administration
-				'analytics',
-				'employee-administration',
-				'shared-documents',
-				'faces',
-				// Spend Management
-				'business-expenses',
-				'invoices',
-				'payment-methods',
-				'accounting-assistant',
-				'certified-einvoicing-platform',
-				// Compensation and Benefits
-				'compensation',
-				'payslip',
-				'benefits',
-				'payroll-assistant',
-				// Lucca
-				'administration',
-				'cloud-control',
-				'lucca',
-			],
+			options: SoftwareIconList,
 			control: {
 				type: 'select',
 			},
