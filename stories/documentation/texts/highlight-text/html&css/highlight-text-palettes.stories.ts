b9e68c166f552a74df1e97a3dc5172596ccdd9b6
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-06-01 16:55:58 +0200

    Merge pull request #4934 from LuccaSA/feat/highlight-stories
    [highlightText] add HTML stories

---
+++ stories/documentation/texts/highlight-text/html&css/highlight-text-palettes.stories.ts
@@ -0,0 +1,38 @@
+import { DECORATIVE_PALETTE, PALETTE } from '@lucca/prisme/core';
+import { Meta, StoryObj } from '@storybook/angular';
+import { setStoryOptions } from 'stories/helpers/stories';
+
+interface TextHighlightStory {
+	palette: string;
+}
+
+export default {
+	title: 'Documentation/Texts/Highlight Text/HTML&CSS/Palettes',
+	argTypes: {
+		palette: {
+			options: setStoryOptions([...PALETTE, ...DECORATIVE_PALETTE]),
+			control: {
+				type: 'select',
+			},
+			description: 'Applique une palette de couleurs au highlight text.',
+		},
+	},
+} as Meta;
+
+function getTemplate(args: TextHighlightStory): string {
+	const { palette, ...inputs } = args;
+	const paletteArg = palette !== 'none' && palette !== undefined && palette !== '' ? ` palette-${palette}` : ``;
+	return `<h1>Lorem <strong class="highlightText${paletteArg}">ipsum</strong> dolor</h1>`;
+}
+
+const Template = (args: TextHighlightStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Basic: StoryObj<TextHighlightStory> = {
+	args: {
+		palette: 'none',
+	},
+	render: Template,
+};
