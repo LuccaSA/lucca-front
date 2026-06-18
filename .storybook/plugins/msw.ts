41bd06a16b8c4c2c91e7c6a44fb7ebc2c57880b4
Flavien Normand <contact@flavien-normand.fr>
2026-06-18 14:39:02 +0200

    fix: make storybook work with analogjs
    For some reason, global style.scss won't import properly so it's working without it for now.

--- .storybook/plugins/msw.ts
+++ .storybook/plugins/msw.ts
@@ -1,3 +1,5 @@
+import { worker } from '../msw/browser';
+
 export default {
 	init() {
 		// ---------------------- MSW ----------------------
@@ -6,7 +8,6 @@ export default {
 		// and a story's runtime (browser). However, we cannot call `setupWorker`
 		// in Node environment, so need to check if we're in a browser.
 		if (typeof global.process === 'undefined') {
-			const { worker } = require('../msw/browser');
 			// Start the mocking when each story is loaded.
 			// Repetitive calls to the `.start()` method do not register a new worker,
 			// but check whether there's an existing once, reusing it, if so.
@@ -16,7 +17,9 @@ export default {
 				serviceWorker: {
 					// Points to the custom location of the Service Worker file.
 					url: `${pathname}/mockServiceWorker.js`,
-					scope: '/',
+					options: {
+						scope: '/',
+					},
 				},
 				onUnhandledRequest: 'bypass',
 			});
