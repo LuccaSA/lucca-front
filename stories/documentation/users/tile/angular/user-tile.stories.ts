46521eadd0541cd16f8f1b1149c1f14d22b2aa43
Vincent Valentin <vvalentin@luccasoftware.com>
2026-06-18 10:50:27 +0200

    [userTile] cleanup user tile angular story

--- stories/documentation/users/tile/angular/user-tile.stories.ts
+++ stories/documentation/users/tile/angular/user-tile.stories.ts
@@ -1,34 +1,160 @@
-import { bob } from '@/stories/users/user.mocks';
+import { finn, jake, marceline } from '@/stories/users/user.mocks';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { LuUserTileComponent } from '@lucca-front/ng/user';
+import { ILuUser, LuUserTileComponent } from '@lucca-front/ng/user';
 import { LuUserPopoverDirective } from '@lucca-front/ng/user-popover';
+import { LuUserPopoverStore } from '@lucca-front/ng/user-popover/service/user-popover.store';
 import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { of } from 'rxjs';
 
-export default {
+type NamePartFormat = 'full' | 'abbreviated' | 'hidden';
+type UserChoice = 'finn' | 'marceline' | 'jake';
+
+const users: Record<UserChoice, ILuUser> = {
+	finn,
+	marceline,
+	jake,
+};
+
+type UserTileStoryArgs = Omit<LuUserTileComponent, 'displayFormat'> & {
+	selectedUser: UserChoice;
+	withPopover: boolean;
+	firstNameFormat: NamePartFormat;
+	lastNameFormat: NamePartFormat;
+	lastNameFirst: boolean;
+};
+
+function getDisplayFormat(args: Pick<UserTileStoryArgs, 'firstNameFormat' | 'lastNameFormat' | 'lastNameFirst'>) {
+	if (args.firstNameFormat === 'hidden' && args.lastNameFormat === 'hidden') {
+		return null;
+	}
+
+	if (args.firstNameFormat === 'hidden') {
+		return args.lastNameFormat === 'abbreviated' ? 'L' : 'l';
+	}
+
+	if (args.lastNameFormat === 'hidden') {
+		return args.firstNameFormat === 'abbreviated' ? 'F' : 'f';
+	}
+
+	const firstNameToken = args.firstNameFormat === 'abbreviated' ? 'F' : 'f';
+	const lastNameToken = args.lastNameFormat === 'abbreviated' ? 'L' : 'l';
+
+	return args.lastNameFirst ? `${lastNameToken}${firstNameToken}` : `${firstNameToken}${lastNameToken}`;
+}
+
+const meta: Meta<UserTileStoryArgs> = {
 	title: 'Documentation/Users/Tile/Angular/Basic',
+	component: LuUserTileComponent,
 	decorators: [
 		moduleMetadata({
 			imports: [LuUserTileComponent, LuUserPopoverDirective],
 		}),
 		applicationConfig({
-			providers: [provideAnimations(), provideHttpClient()],
+			providers: [
+				provideAnimations(),
+				provideHttpClient(),
+				{
+					provide: LuUserPopoverStore,
+					useValue: {
+						get: (id: number) =>
+							of({
+								...(Object.values(users).find((user) => user.id === id) ?? finn),
+								leaveEndIsFirstHalfDay: false,
+								pictureHref: (Object.values(users).find((user) => user.id === id) ?? finn).pictureHref ?? (Object.values(users).find((user) => user.id === id) ?? finn).picture?.href,
+							}),
+					},
+				},
+			],
 		}),
 	],
-	render: () => {
-		return {
-			props: {
-				bob,
-			},
-			template: `<div class="pr-u-displayFlex pr-u-gap300">
-	<button [luUserPopover]="bob" type="button" class="userPopover_trigger">
-		<lu-user-tile [user]="bob" />
-	</button>
-	<lu-user-tile [user]="bob" displayFormat="LF" role="Administrateur" />
-	<lu-user-tile [user]="bob" size="L" />
-</div>`,
-		};
-	},
-} as Meta;
+	render: (args) => {
+		const selectedUser = users[args.selectedUser];
+		const resolvedDisplayFormat = getDisplayFormat(args);
+		const size = args.size === 'M' ? '' : ` size="${args.size}"`;
+		const role = args.role ? ` role="${args.role}"` : '';
+		const displayFormat = !resolvedDisplayFormat || resolvedDisplayFormat === 'lf' ? '' : ` displayFormat="${resolvedDisplayFormat}"`;
+		const user = resolvedDisplayFormat ? selectedUser : { ...selectedUser, firstName: '', lastName: '' };
 
-export const Basic: StoryObj<LuUserTileComponent> = {};
+		if (args.withPopover) {
+			return {
+				props: {
+					...args,
+					selectedUser,
+					user,
+				},
+				template: `
+		<button [luUserPopover]="selectedUser" type="button" class="userPopover_trigger">
+			<lu-user-tile [user]="user"${size}${role}${displayFormat} />
+		</button>
+
+`,
+			};
+		} else {
+			return {
+				props: {
+					...args,
+					selectedUser,
+					user,
+				},
+				template: `<lu-user-tile [user]="user"${size}${role}${displayFormat} />
+`,
+			};
+		}
+	},
+};
+
+export default meta;
+
+export const Basic: StoryObj<UserTileStoryArgs> = {
+	args: {
+		selectedUser: 'finn',
+		size: 'M',
+		role: '',
+		firstNameFormat: 'full',
+		lastNameFormat: 'full',
+		lastNameFirst: true,
+		withPopover: false,
+	},
+	argTypes: {
+		selectedUser: {
+			options: ['finn', 'marceline', 'jake'],
+			control: {
+				type: 'select',
+			},
+		},
+		size: {
+			options: ['XS', 'S', 'M', 'L'],
+			control: {
+				type: 'select',
+			},
+		},
+		role: {
+			control: {
+				type: 'text',
+			},
+		},
+		firstNameFormat: {
+			options: ['full', 'abbreviated', 'hidden'],
+			control: {
+				type: 'select',
+			},
+		},
+		lastNameFormat: {
+			options: ['full', 'abbreviated', 'hidden'],
+			control: {
+				type: 'select',
+			},
+		},
+		lastNameFirst: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		withPopover: {
+			control: {
+				type: 'boolean',
+			},
+		},
+	},
+};
