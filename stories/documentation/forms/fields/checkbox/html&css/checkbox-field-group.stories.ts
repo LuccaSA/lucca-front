b00d930f76ec2078377611c74bc78742bf7110ff
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-06-19 15:33:47 +0200

    [checkboxField] add story with group (fieldset+legend) (#5040)
    ## Description
    
    
    
    -----
    
    Close #3618 
    
    -----
    
    <img width="100" height="140" alt="Capture d’écran 2026-06-18 à 11 37
    11"
    src="https://github.com/user-attachments/assets/18ea0562-b3ee-4c6e-af40-942b95c07e2c"
    />

---
+++ stories/documentation/forms/fields/checkbox/html&css/checkbox-field-group.stories.ts
@@ -0,0 +1,59 @@
+import { LuTooltipModule } from '@lucca-front/ng/tooltip';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+interface CheckboxGroupStory {}
+
+export default {
+	title: 'Documentation/Forms/Fields/CheckboxField/HTML&CSS',
+	decorators: [
+		moduleMetadata({
+			imports: [LuTooltipModule],
+		}),
+	],
+	argTypes: {},
+} as Meta;
+
+function getTemplate(args: CheckboxGroupStory): string {
+	return `
+<fieldset class="form-fieldset">
+	<legend class="formLabel">
+		Label<sup aria-hidden="true" class="formLabel-required">*</sup>
+		<span class="formLabel-info" luTooltip="Additional info" luTooltipOnlyForDisplay>
+			<span aria-hidden="true" class="lucca-icon icon-signHelp"></span>
+			<span class="pr-u-mask">Additional info</span>
+		</span>
+	</legend>
+	<div class="form-field">
+		<label class="formLabel" for="ID">Label</label>
+		<span class="checkboxField">
+			<input type="checkbox" class="checkboxField-input" id="ID" aria-required="true" />
+			<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+		</span>
+	</div>
+	<div class="form-field">
+		<label class="formLabel" for="ID2">Label</label>
+		<span class="checkboxField">
+			<input type="checkbox" class="checkboxField-input" id="ID2" aria-required="true" />
+			<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+		</span>
+	</div>
+	<div class="form-field">
+		<label class="formLabel" for="ID3">Label</label>
+		<span class="checkboxField">
+			<input type="checkbox" class="checkboxField-input" id="ID3" aria-required="true" />
+			<span class="checkboxField-icon" aria-hidden="true"><span class="checkboxField-icon-check"></span></span>
+		</span>
+	</div>
+</fieldset>
+`;
+}
+
+const Template = (args: CheckboxGroupStory) => ({
+	props: args,
+	template: getTemplate(args),
+});
+
+export const Group: StoryObj<CheckboxGroupStory> = {
+	args: {},
+	render: Template,
+};
