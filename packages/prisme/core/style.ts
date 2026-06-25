2bf02f04a789d8c6b04766a34f7b2a63c22d67fb
jeremie-lucca <jbiron@luccasoftware.com>
2026-06-25 11:27:09 +0200

    Remove deprecated palettes

--- packages/prisme/core/style.ts
+++ packages/prisme/core/style.ts
@@ -3,7 +3,7 @@
  */
 // primary is deprecated
 // grey is deprecated
-export const PALETTE = ['success', 'warning', 'error', 'product', 'neutral', 'none', 'primary', 'grey', 'brand'] as const;
+export const PALETTE = ['success', 'warning', 'error', 'product', 'neutral', 'none', 'brand'] as const;
 export type Palette = (typeof PALETTE)[number];
 
 export const DECORATIVE_PALETTE = ['kiwi', 'lime', 'cucumber', 'mint', 'glacier', 'lagoon', 'blueberry', 'lavender', 'grape', 'watermelon', 'pumpkin', 'pineapple'] as const;
