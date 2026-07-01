654e9c5b892b464e709704671993cbd47879b9e8
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-07-01 10:16:24 +0200

    * fix translation legal-units

--- packages/ng/core-select/legal-units/translations.ts
+++ packages/ng/core-select/legal-units/translations.ts
@@ -1,29 +1,29 @@
 ﻿export const Translations = {
-	"pl": {
-		'includeArchivedLegalUnits': "Include archived legal units",
+	pl: {
+		includeArchivedLegalUnits: 'Include archived legal units',
 	},
-	"nl-BE": {
-		'includeArchivedLegalUnits': "Gearchiveerde wettelijke eenheden opnemen",
+	'nl-BE': {
+		includeArchivedLegalUnits: 'Gearchiveerde wettelijke eenheden opnemen',
 	},
-	"fr": {
-		'includeArchivedLegalUnits': "Inclure les unités légales archivées",
+	fr: {
+		includeArchivedLegalUnits: 'Inclure les unités légales archivées',
 	},
-	"de": {
-		'includeArchivedLegalUnits': "Archivierte gesetzliche Einheiten einbeziehen",
+	de: {
+		includeArchivedLegalUnits: 'Archivierte gesetzliche Einheiten einbeziehen',
 	},
-	"en": {
-		'includeArchivedLegalUnits': "Include archived legal units",
+	en: {
+		includeArchivedLegalUnits: 'Include archived legal units',
 	},
-	"es": {
-		'includeArchivedLegalUnits': "Incluir las unidades legales archivadas",
+	es: {
+		includeArchivedLegalUnits: 'Incluir las unidades legales archivadas',
 	},
-	"it": {
-		'includeArchivedLegalUnits': "Includere le unità giuridiche archiviate",
+	it: {
+		includeArchivedLegalUnits: 'Includere le unità giuridiche archiviate',
 	},
-	"nl": {
-		'includeArchivedLegalUnits': "Gearchiveerde wettelijke eenheden opnemen",
+	nl: {
+		includeArchivedLegalUnits: 'Gearchiveerde wettelijke eenheden opnemen',
 	},
-	"pt": {
-		'includeArchivedLegalUnits': "Incluir as unidades legais arquivadas",
+	pt: {
+		includeArchivedLegalUnits: 'Incluir as unidades legais arquivadas',
 	},
 };
