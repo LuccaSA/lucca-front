a78b03a98b457c735e0c89ccc40eca1c6e8952a2
jeremie-lucca <jbiron@luccasoftware.com>
2026-07-01 14:59:16 +0200

    Open in a new tab

--- packages/ng/file-upload/file-entry/file-entry.component.ts
+++ packages/ng/file-upload/file-entry/file-entry.component.ts
@@ -45,6 +45,8 @@ export class FileEntryComponent {
 
 	readonly downloadURL = input('');
 
+	readonly openInNewTab = input(false, { transform: booleanAttribute });
+
 	readonly password = input('');
 	passwordChange$ = new Subject<string>();
 	passwordChange = outputFromObservable(this.passwordChange$);
