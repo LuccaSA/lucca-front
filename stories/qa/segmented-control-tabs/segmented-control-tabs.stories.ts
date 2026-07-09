05d1818723699dd692ee66735b6e20d6090cf48f
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-07-09 11:07:20 +0200

    + Add segmented control tabs qa page

---
+++ stories/qa/segmented-control-tabs/segmented-control-tabs.stories.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NumericBadgeComponent } from '@lucca-front/ng/numeric-badge';
+import { SegmentedControlTabsComponent, SegmentedControlTabsPanelComponent } from '@lucca-front/ng/segmented-control-tabs';
+import { Meta, StoryObj } from '@storybook/angular';
+
+@Component({
+	selector: 'segmented-control-tabs-stories',
+	templateUrl: './segmented-control-tabs.stories.html',
+	imports: [SegmentedControlTabsComponent, SegmentedControlTabsPanelComponent, NumericBadgeComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class SegmentedControlTabsStory {}
+
+export default {
+	title: 'QA/SegmentedControlTabs',
+	component: SegmentedControlTabsStory,
+} as Meta;
+
+const template = () => ({});
+
+export const Basic: StoryObj<SegmentedControlTabsStory> = {
+	args: {},
+	render: template,
+};
