d3e3af883f6721b487ae54c1f58ab02851382943
Vincent Valentin <vvalentin@luccasoftware.com>
2026-07-16 17:28:03 +0200

    first story

--- stories/documentation/forms/filter-pills/angular/filter-bar-save.stories.ts
+++ stories/documentation/forms/filter-pills/angular/filter-bar-save.stories.ts
@@ -5,6 +5,7 @@ import { ButtonComponent } from '@lucca-front/ng/button';
 import { LuCoreSelectApiV4Directive } from '@lucca-front/ng/core-select/api';
 import { DateInputComponent, DateRangeInputComponent } from '@lucca-front/ng/date2';
 import { DividerComponent } from '@lucca-front/ng/divider';
+import { DropdownActionComponent, DropdownItemComponent, DropdownMenuComponent, LuDropdownTriggerDirective } from '@lucca-front/ng/dropdown';
 import { FilterBarComponent, FilterPillAddonAfterDirective, FilterPillAddonBeforeDirective, FilterPillComponent } from '@lucca-front/ng/filter-pills';
 import { FormFieldComponent } from '@lucca-front/ng/form-field';
 import { CheckboxInputComponent, TextInputComponent } from '@lucca-front/ng/forms';
@@ -12,7 +13,9 @@ import { LuMultiSelectInputComponent } from '@lucca-front/ng/multi-select';
 import { NumericBadgeComponent } from '@lucca-front/ng/numeric-badge';
 import { SegmentedControlComponent, SegmentedControlFilterComponent } from '@lucca-front/ng/segmented-control';
 import { LuSimpleSelectInputComponent } from '@lucca-front/ng/simple-select';
-import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular-vite';
+import { LuTooltipTriggerDirective } from '@lucca-front/ng/tooltip';
+import { IconComponent } from '@lucca/prisme/icon';
+import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
 export default {
 	title: 'Documentation/Forms/FiltersPills/FilterBar/Angular',
@@ -37,6 +40,12 @@ export default {
 				DividerComponent,
 				SegmentedControlComponent,
 				SegmentedControlFilterComponent,
+				IconComponent,
+				DropdownMenuComponent,
+				DropdownItemComponent,
+				DropdownActionComponent,
+				LuDropdownTriggerDirective,
+				LuTooltipTriggerDirective,
 			],
 		}),
 		applicationConfig({ providers: [provideHttpClient(), { provide: LOCALE_ID, useValue: 'fr-FR' }] }),
@@ -53,10 +62,33 @@ export default {
 		<ng-template #label1>En cours d’approbation <lu-numeric-badge [value]="5" /></ng-template>
 		<ng-template #label2>Approuvés <lu-numeric-badge [value]="3" /></ng-template>
 		<ng-template #label3>Clos <lu-numeric-badge [value]="4" /></ng-template>
+		<ng-template #label4>
+			Produit
+			<button type="button" size="XS" luButton="ghost" aria-expanded="false" disclosure [luDropdown]="optionsDropdown">
+				<lu-icon alt="Options" icon="menuDots" />
+			</button>
+			<ng-template #optionsDropdown>
+				<lu-dropdown-menu>
+					<lu-dropdown-item>
+						<button lu-dropdown-action type="button">
+							<lu-icon icon="edit" />
+							Modifier le nom
+						</button>
+					</lu-dropdown-item>
+					<lu-dropdown-item>
+						<button lu-dropdown-action type="button" critical>
+							<lu-icon icon="trash" />
+							Supprimer
+						</button>
+					</lu-dropdown-item>
+				</lu-dropdown-menu>
+			</ng-template>
+		</ng-template>
 		<lu-segmented-control-filter [label]="label0" value="0" />
 		<lu-segmented-control-filter [label]="label1" value="1" />
 		<lu-segmented-control-filter [label]="label2" value="2" />
 		<lu-segmented-control-filter [label]="label3" value="3" />
+		<lu-segmented-control-filter [label]="label4" value="4" />
 	</lu-segmented-control>
 	<lu-filter-pill label="Inclure les collaborateurs partis" optional name="includeFormerEmployees">
 		<lu-checkbox-input [ngModel]="false" />
@@ -73,12 +105,35 @@ export default {
 	<lu-form-field label="Test" hiddenLabel>
 		<lu-text-input [ngModel]="example2" [ngModelOptions]="{ standalone: true }" hasSearchIcon hasClearer />
 	</lu-form-field>
-	<button *luFilterPillAddonAfter type="submit" size="S" luButton="outlined">Exporter</button>
-</lu-filter-bar>`,
+	<ng-container *luFilterPillAddonAfter>
+		<button type="button" size="S" luButton="outlined" palette="product" disclosure aria-expanded="false" [luDropdown]="saveDropdown">
+			<!--Enregistrer en tant que nouvelle vue-->
+			Enregistrer la vue
+			<lu-icon icon="arrowChevronBottom" />
+		</button>
+		<button type="submit" size="S" luButton="outlined">Exporter</button>
+	</ng-container>
+</lu-filter-bar>
+<ng-template #saveDropdown>
+	<lu-dropdown-menu>
+		<lu-dropdown-item>
+			<button lu-dropdown-action type="button">
+				<lu-icon icon="save" />
+				Enregistrer les modifications
+			</button>
+		</lu-dropdown-item>
+		<lu-dropdown-item>
+			<button lu-dropdown-action type="button" aria-disabled="true" class="is-disabled" luTooltip="Supprimer des vues pour en créer des nouvelles">
+				<lu-icon icon="mathsPlus" />
+				Enregistrer en tant que nouvelle vue
+			</button>
+		</lu-dropdown-item>
+	</lu-dropdown-menu>
+</ng-template>`,
 		};
 	},
 } as Meta;
 
-export const Basic: StoryObj<FilterBarComponent> = {
+export const Save: StoryObj<FilterBarComponent> = {
 	args: {},
 };
