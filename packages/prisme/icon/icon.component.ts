af35a94678ab079d517f96b467c9584f3fa9ed7c
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-07-21 10:26:35 +0200

    * fix button && icon type input

--- packages/prisme/icon/icon.component.ts
+++ packages/prisme/icon/icon.component.ts
@@ -35,7 +35,7 @@ export class IconComponent {
 	/**
 	 * Display icon in AI mode
 	 */
-	readonly AI = input<boolean, boolean | `${boolean}`>(false, { transform: booleanAttribute });
+	readonly AI = input<boolean, boolean | `${boolean}` | ''>(false, { transform: booleanAttribute });
 
 	readonly iconClasses = computed(() => {
 		const size = this.size();
