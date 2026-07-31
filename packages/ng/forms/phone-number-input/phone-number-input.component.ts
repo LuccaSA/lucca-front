f85500559a44c550618b220351398c88372e67bb
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-07-31 12:01:05 +0200

    * fix build & lint

--- packages/ng/forms/phone-number-input/phone-number-input.component.ts
+++ packages/ng/forms/phone-number-input/phone-number-input.component.ts
@@ -34,7 +34,7 @@ function tryParsePhoneNumber(phoneNumber: string, countryCode?: CountryCode): Pa
 		};
 	} catch {
 		return {
-			number: phoneNumber,
+			number: phoneNumber as E164Number,
 			nationalNumber: phoneNumber,
 			isValid: false,
 		};
@@ -195,7 +195,7 @@ export class PhoneNumberInputComponent implements ControlValueAccessor, Validato
 				this.#onChange?.(number);
 				return;
 			} catch {
-				this.#onChange?.(displayedNumber);
+				this.#onChange?.(displayedNumber as E164Number);
 			}
 		}
 	}
