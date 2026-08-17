58531b80687e02467fee697e5019a8c1c662d802
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-08-17 13:01:44 +0200

    * fix lint: drop no-op E164Number assertions, revert dialog-routing reformat

--- packages/ng/forms/phone-number-input/phone-number-input.component.ts
+++ packages/ng/forms/phone-number-input/phone-number-input.component.ts
@@ -34,7 +34,7 @@ function tryParsePhoneNumber(phoneNumber: string, countryCode?: CountryCode): Pa
 		};
 	} catch {
 		return {
-			number: phoneNumber as E164Number,
+			number: phoneNumber,
 			nationalNumber: phoneNumber,
 			isValid: false,
 		};
@@ -195,7 +195,7 @@ export class PhoneNumberInputComponent implements ControlValueAccessor, Validato
 				this.#onChange?.(number);
 				return;
 			} catch {
-				this.#onChange?.(displayedNumber as E164Number);
+				this.#onChange?.(displayedNumber);
 			}
 		}
 	}
