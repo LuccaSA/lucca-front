baeb4ef667a4561320dcb34b7f7fd80a133b345b
Vincent Valentin <vvalentin@luccasoftware.com>
2026-09-03 15:52:10 +0200

    first implementation

---
+++ packages/prisme/icon/icon-sprite.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { LuSafeExternalSvgPipe } from '@lucca/prisme/safe-content';
+
+// TODO: point back to the CDN once the sprite is generated and published there.
+const ICON_SPRITE_URL = 'https://tmp.vincent-valentin.name/lucca/symbol-defs.svg';
+
+@Component({
+	// eslint-disable-next-line @angular-eslint/component-selector
+	selector: 'pr-icon-sprite',
+	templateUrl: './icon-sprite.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+	imports: [LuSafeExternalSvgPipe],
+	host: {
+		hidden: 'hidden',
+	},
+})
+export class IconSpriteComponent {
+	protected readonly spriteUrl = ICON_SPRITE_URL;
+}
