4a7cfdc8b87f550a382f968b8630d0027b7b4635
Vincent Valentin <64789527+vvalentin-lucca@users.noreply.github.com>
2026-09-18 17:11:48 +0200

    [scrollBox] fix shadows with chrome (#5366)
    ## Description
    
    ScrollBox determined when to hide its shadows by comparing `scrollWidth`
    (the total width of the content, calculated by the browser) to
    `clientWidth` (the visible width).
    
    Chrome calculates `scrollWidth` differently whenever there are `sticky`
    pseudo-elements combined with negative margins, which is the case here.
    It counts a few extra pixels that don’t actually exist visually (even
    though this remains a gray area in the specification).
    
    To work around this, we stop relying on `scrollWidth` and instead use
    `getBoundingClientRect()` to retrieve the actual positions of the first
    and last child elements in the content, to determine whether any content
    remains hidden at either edge.
    
    -----
    
    Fixes #5155 
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1488950
    
    -----
    
    <img width="746" height="53" alt="Capture d’écran 2026-09-18 à 14 45
    45"
    src="https://github.com/user-attachments/assets/ef58fb58-c88e-4668-bd1f-272d796e6252"
    />

--- packages/ng/scroll-box/scroll-box.component.ts
+++ packages/ng/scroll-box/scroll-box.component.ts
@@ -27,12 +27,17 @@ export class ScrollBoxComponent implements OnInit {
 	readonly isLastVisible = signal(false);
 
 	scroll() {
+		const container = this.#elementRef.nativeElement.getBoundingClientRect();
+		const first = this.#elementRef.nativeElement.firstElementChild?.getBoundingClientRect();
+		const last = this.#elementRef.nativeElement.lastElementChild?.getBoundingClientRect();
+		const tolerance = 1;
+
 		if (this.vertical()) {
-			this.isFirstVisible.set(this.#elementRef.nativeElement.scrollTop === 0);
-			this.isLastVisible.set(this.#elementRef.nativeElement.scrollTop >= this.#elementRef.nativeElement.scrollHeight - this.#elementRef.nativeElement.clientHeight);
+			this.isFirstVisible.set(!first || first.top >= container.top - tolerance);
+			this.isLastVisible.set(!last || last.bottom <= container.bottom + tolerance);
 		} else {
-			this.isFirstVisible.set(this.#elementRef.nativeElement.scrollLeft === 0);
-			this.isLastVisible.set(this.#elementRef.nativeElement.scrollLeft >= this.#elementRef.nativeElement.scrollWidth - this.#elementRef.nativeElement.clientWidth);
+			this.isFirstVisible.set(!first || first.left >= container.left - tolerance);
+			this.isLastVisible.set(!last || last.right <= container.right + tolerance);
 		}
 	}
 
