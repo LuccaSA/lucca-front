3629b9fb85867713a51500003a5e8e186761685a
Vincent Valentin <vvalentin@luccasoftware.com>
2026-09-18 14:21:44 +0200

    [scrollBox] fix shadows with chrome

--- packages/ng/scroll-box/scroll-box.component.ts
+++ packages/ng/scroll-box/scroll-box.component.ts
@@ -27,12 +27,16 @@ export class ScrollBoxComponent implements OnInit {
 	readonly isLastVisible = signal(false);
 
 	scroll() {
+		const container = this.#elementRef.nativeElement.getBoundingClientRect();
+		const first = this.#elementRef.nativeElement.firstElementChild?.getBoundingClientRect();
+		const last = this.#elementRef.nativeElement.lastElementChild?.getBoundingClientRect();
+
 		if (this.vertical()) {
-			this.isFirstVisible.set(this.#elementRef.nativeElement.scrollTop === 0);
-			this.isLastVisible.set(this.#elementRef.nativeElement.scrollTop >= this.#elementRef.nativeElement.scrollHeight - this.#elementRef.nativeElement.clientHeight);
+			this.isFirstVisible.set(!first || first.top >= container.top);
+			this.isLastVisible.set(!last || last.bottom <= container.bottom);
 		} else {
-			this.isFirstVisible.set(this.#elementRef.nativeElement.scrollLeft === 0);
-			this.isLastVisible.set(this.#elementRef.nativeElement.scrollLeft >= this.#elementRef.nativeElement.scrollWidth - this.#elementRef.nativeElement.clientWidth);
+			this.isFirstVisible.set(!first || first.left >= container.left);
+			this.isLastVisible.set(!last || last.right <= container.right);
 		}
 	}
 
