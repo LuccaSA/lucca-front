90985aa0b9113a42f71a0487baa9744c11b0c96a
Charlie JEANNEAU <35218368+JeSuisCharlie1@users.noreply.github.com>
2026-09-21 10:24:25 +0200

    * fix compare default isNil

--- packages/ng/select/input/select-input.component.spec.ts
+++ packages/ng/select/input/select-input.component.spec.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { ClearComponent } from '@lucca-front/ng/clear';
 import { LuInputDisplayerDirective } from '@lucca-front/ng/input';
-import { LuOptionItemComponent, LuOptionPickerComponent } from '@lucca-front/ng/option';
+import { LuOptionComparer, LuOptionItemComponent, LuOptionPickerComponent } from '@lucca-front/ng/option';
 import { LuSelectInputComponent } from './select-input.component';
 
 type Entity = { id: number; name: string };
@@ -38,13 +38,37 @@ class SelectHostComponent {
 	options: Entity[] = options;
 }
 
+@Component({
+	selector: 'lu-select-custom-comparer-host',
+	imports: [ReactiveFormsModule, LuSelectInputComponent, LuOptionPickerComponent, LuOptionItemComponent, LuInputDisplayerDirective, ClearComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	template: `
+		<lu-select [formControl]="formControl">
+			<ng-container *luDisplayer="let value">{{ value.name }}</ng-container>
+			<lu-option-picker [option-comparer]="comparer">
+				@for (option of options; track option.id) {
+					<lu-option [value]="option">{{ option.name }}</lu-option>
+				}
+			</lu-option-picker>
+			<lu-clear />
+		</lu-select>
+	`,
+})
+class SelectCustomComparerHostComponent {
+	formControl = new FormControl<Entity | null>(null);
+
+	options: Entity[] = options;
+
+	comparer: LuOptionComparer<Entity> = (a, b) => a.id === b.id;
+}
+
 describe(LuSelectInputComponent.name, () => {
 	let fixture: ComponentFixture<SelectHostComponent>;
 	let host: SelectHostComponent;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [SelectHostComponent],
+			imports: [SelectHostComponent, SelectCustomComparerHostComponent],
 			providers: [provideNoopAnimations()],
 		});
 
@@ -83,6 +107,24 @@ describe(LuSelectInputComponent.name, () => {
 		// Assert
 		expect(host.formControl.value).toEqual([]);
 	});
+
+	it('should clear the value of a select using a custom option comparer', fakeAsync(() => {
+		// Arrange
+		const customFixture = TestBed.createComponent(SelectCustomComparerHostComponent);
+		const customHost = customFixture.componentInstance;
+		customFixture.detectChanges();
+		// the picker registers its options asynchronously, and only calls the comparer on registered ones
+		tick();
+		customHost.formControl.setValue(options[0]);
+		customFixture.detectChanges();
+
+		// Act
+		(customFixture.nativeElement.querySelector('lu-clear') as HTMLElement).click();
+		customFixture.detectChanges();
+
+		// Assert
+		expect(customHost.formControl.value).toBeUndefined();
+	}));
 
 	it('should not clear the value when disabled', () => {
 		// Arrange
